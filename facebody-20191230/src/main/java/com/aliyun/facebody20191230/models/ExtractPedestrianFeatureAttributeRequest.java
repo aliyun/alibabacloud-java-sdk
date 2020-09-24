d4070dc9cba795dfac349778e381fa876120854e
sdk-team <sdk-team@alibabacloud.com>
2020-09-24 13:32:46 +0000

    Update ExtractPedestrianFeatureAttr.

--- facebody-20191230/src/main/java/com/aliyun/facebody20191230/models/ExtractPedestrianFeatureAttributeRequest.java
+++ facebody-20191230/src/main/java/com/aliyun/facebody20191230/models/ExtractPedestrianFeatureAttributeRequest.java
@@ -4,6 +4,9 @@ package com.aliyun.facebody20191230.models;
 import com.aliyun.tea.*;
 
 public class ExtractPedestrianFeatureAttributeRequest extends TeaModel {
+    @NameInMap("Mode")
+    public String mode;
+
     @NameInMap("ImageURL")
     @Validation(required = true)
     public String imageURL;
@@ -11,6 +14,14 @@ public class ExtractPedestrianFeatureAttributeRequest extends TeaModel {
     public static ExtractPedestrianFeatureAttributeRequest build(java.util.Map<String, ?> map) throws Exception {
         ExtractPedestrianFeatureAttributeRequest self = new ExtractPedestrianFeatureAttributeRequest();
         return TeaModel.build(map, self);
+    }
+
+    public ExtractPedestrianFeatureAttributeRequest setMode(String mode) {
+        this.mode = mode;
+        return this;
+    }
+    public String getMode() {
+        return this.mode;
     }
 
     public ExtractPedestrianFeatureAttributeRequest setImageURL(String imageURL) {
