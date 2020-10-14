2e7a65baa517476d5b9b3c41c68bb6bad21a36ea
sdk-team <sdk-team@alibabacloud.com>
2020-10-14 16:05:42 +0000

    Generated java 2018-07-13 for Ft.

--- ft-20180713/src/main/java/com/aliyun/ft20180713/Client.java
+++ ft-20180713/src/main/java/com/aliyun/ft20180713/Client.java
@@ -5,6 +5,7 @@ import com.aliyun.tea.*;
 import com.aliyun.ft20180713.models.*;
 
 public class Client extends com.aliyun.tearpc.Client {
+
     public Client(com.aliyun.tearpc.models.Config config) throws Exception {
         super(config);
         this._endpointRule = "regional";
