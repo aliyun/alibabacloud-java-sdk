29d00fd62c7d1be9f53268c832db32860dcdf792
sdk-team <sdk-team@alibabacloud.com>
2020-11-13 09:44:01 +0000

    Update Imageaudit.

--- imageaudit-20191230/src/main/java/com/aliyun/imageaudit20191230/models/ScanTextResponse.java
+++ imageaudit-20191230/src/main/java/com/aliyun/imageaudit20191230/models/ScanTextResponse.java
@@ -1,5 +1,5 @@
 // This file is auto-generated, don't edit it. Thanks.
-package com.aliyun.imageaudit.models;
+package com.aliyun.imageaudit20191230.models;
 
 import com.aliyun.tea.*;
 
@@ -17,6 +17,22 @@ public class ScanTextResponse extends TeaModel {
         return TeaModel.build(map, self);
     }
 
+    public ScanTextResponse setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public ScanTextResponse setData(ScanTextResponseData data) {
+        this.data = data;
+        return this;
+    }
+    public ScanTextResponseData getData() {
+        return this.data;
+    }
+
     public static class ScanTextResponseDataElementsResultsDetailsContexts extends TeaModel {
         @NameInMap("Context")
         @Validation(required = true)
@@ -25,6 +41,14 @@ public class ScanTextResponse extends TeaModel {
         public static ScanTextResponseDataElementsResultsDetailsContexts build(java.util.Map<String, ?> map) throws Exception {
             ScanTextResponseDataElementsResultsDetailsContexts self = new ScanTextResponseDataElementsResultsDetailsContexts();
             return TeaModel.build(map, self);
+        }
+
+        public ScanTextResponseDataElementsResultsDetailsContexts setContext(String context) {
+            this.context = context;
+            return this;
+        }
+        public String getContext() {
+            return this.context;
         }
 
     }
@@ -43,6 +67,22 @@ public class ScanTextResponse extends TeaModel {
             return TeaModel.build(map, self);
         }
 
+        public ScanTextResponseDataElementsResultsDetails setLabel(String label) {
+            this.label = label;
+            return this;
+        }
+        public String getLabel() {
+            return this.label;
+        }
+
+        public ScanTextResponseDataElementsResultsDetails setContexts(java.util.List<ScanTextResponseDataElementsResultsDetailsContexts> contexts) {
+            this.contexts = contexts;
+            return this;
+        }
+        public java.util.List<ScanTextResponseDataElementsResultsDetailsContexts> getContexts() {
+            return this.contexts;
+        }
+
     }
 
     public static class ScanTextResponseDataElementsResults extends TeaModel {
@@ -56,7 +96,7 @@ public class ScanTextResponse extends TeaModel {
 
         @NameInMap("Rate")
         @Validation(required = true)
-        public Double rate;
+        public Float rate;
 
         @NameInMap("Details")
         @Validation(required = true)
@@ -65,6 +105,38 @@ public class ScanTextResponse extends TeaModel {
         public static ScanTextResponseDataElementsResults build(java.util.Map<String, ?> map) throws Exception {
             ScanTextResponseDataElementsResults self = new ScanTextResponseDataElementsResults();
             return TeaModel.build(map, self);
+        }
+
+        public ScanTextResponseDataElementsResults setLabel(String label) {
+            this.label = label;
+            return this;
+        }
+        public String getLabel() {
+            return this.label;
+        }
+
+        public ScanTextResponseDataElementsResults setSuggestion(String suggestion) {
+            this.suggestion = suggestion;
+            return this;
+        }
+        public String getSuggestion() {
+            return this.suggestion;
+        }
+
+        public ScanTextResponseDataElementsResults setRate(Float rate) {
+            this.rate = rate;
+            return this;
+        }
+        public Float getRate() {
+            return this.rate;
+        }
+
+        public ScanTextResponseDataElementsResults setDetails(java.util.List<ScanTextResponseDataElementsResultsDetails> details) {
+            this.details = details;
+            return this;
+        }
+        public java.util.List<ScanTextResponseDataElementsResultsDetails> getDetails() {
+            return this.details;
         }
 
     }
@@ -83,6 +155,22 @@ public class ScanTextResponse extends TeaModel {
             return TeaModel.build(map, self);
         }
 
+        public ScanTextResponseDataElements setTaskId(String taskId) {
+            this.taskId = taskId;
+            return this;
+        }
+        public String getTaskId() {
+            return this.taskId;
+        }
+
+        public ScanTextResponseDataElements setResults(java.util.List<ScanTextResponseDataElementsResults> results) {
+            this.results = results;
+            return this;
+        }
+        public java.util.List<ScanTextResponseDataElementsResults> getResults() {
+            return this.results;
+        }
+
     }
 
     public static class ScanTextResponseData extends TeaModel {
@@ -93,6 +181,14 @@ public class ScanTextResponse extends TeaModel {
         public static ScanTextResponseData build(java.util.Map<String, ?> map) throws Exception {
             ScanTextResponseData self = new ScanTextResponseData();
             return TeaModel.build(map, self);
+        }
+
+        public ScanTextResponseData setElements(java.util.List<ScanTextResponseDataElements> elements) {
+            this.elements = elements;
+            return this;
+        }
+        public java.util.List<ScanTextResponseDataElements> getElements() {
+            return this.elements;
         }
 
     }
