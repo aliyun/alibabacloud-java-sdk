d99b50dfdcaa81779ec320d555686de59b7a7f0c
sdk-team <sdk-team@alibabacloud.com>
2020-11-16 04:01:48 +0000

    Generated java 2020-10-01 for ecd.

---
+++ ecd-20201001/src/main/java/com/aliyun/ecd20201001/models/DescribeDesktopsRequest.java
@@ -0,0 +1,112 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ecd20201001.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeDesktopsRequest extends TeaModel {
+    @NameInMap("RegionId")
+    @Validation(required = true)
+    public String regionId;
+
+    @NameInMap("DirectoryId")
+    public String directoryId;
+
+    @NameInMap("GroupId")
+    public String groupId;
+
+    @NameInMap("DesktopStatus")
+    public String desktopStatus;
+
+    @NameInMap("MaxResults")
+    public Integer maxResults;
+
+    @NameInMap("NextToken")
+    public String nextToken;
+
+    @NameInMap("UserName")
+    public String userName;
+
+    @NameInMap("DesktopName")
+    public String desktopName;
+
+    @NameInMap("DesktopId")
+    public java.util.List<String> desktopId;
+
+    public static DescribeDesktopsRequest build(java.util.Map<String, ?> map) throws Exception {
+        DescribeDesktopsRequest self = new DescribeDesktopsRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeDesktopsRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public DescribeDesktopsRequest setDirectoryId(String directoryId) {
+        this.directoryId = directoryId;
+        return this;
+    }
+    public String getDirectoryId() {
+        return this.directoryId;
+    }
+
+    public DescribeDesktopsRequest setGroupId(String groupId) {
+        this.groupId = groupId;
+        return this;
+    }
+    public String getGroupId() {
+        return this.groupId;
+    }
+
+    public DescribeDesktopsRequest setDesktopStatus(String desktopStatus) {
+        this.desktopStatus = desktopStatus;
+        return this;
+    }
+    public String getDesktopStatus() {
+        return this.desktopStatus;
+    }
+
+    public DescribeDesktopsRequest setMaxResults(Integer maxResults) {
+        this.maxResults = maxResults;
+        return this;
+    }
+    public Integer getMaxResults() {
+        return this.maxResults;
+    }
+
+    public DescribeDesktopsRequest setNextToken(String nextToken) {
+        this.nextToken = nextToken;
+        return this;
+    }
+    public String getNextToken() {
+        return this.nextToken;
+    }
+
+    public DescribeDesktopsRequest setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public DescribeDesktopsRequest setDesktopName(String desktopName) {
+        this.desktopName = desktopName;
+        return this;
+    }
+    public String getDesktopName() {
+        return this.desktopName;
+    }
+
+    public DescribeDesktopsRequest setDesktopId(java.util.List<String> desktopId) {
+        this.desktopId = desktopId;
+        return this;
+    }
+    public java.util.List<String> getDesktopId() {
+        return this.desktopId;
+    }
+
+}
