a83438b95d13d61876dd4652f713b84c43e4ce9f
sdk-team <sdk-team@alibabacloud.com>
2020-12-25 13:26:04 +0000

    Support reconnet param .

---
+++ cloudgameapi-20200728/src/main/java/com/aliyun/cloudgameapi20200728/models/GetSessionRequest.java
@@ -0,0 +1,24 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cloudgameapi20200728.models;
+
+import com.aliyun.tea.*;
+
+public class GetSessionRequest extends TeaModel {
+    @NameInMap("Token")
+    @Validation(required = true)
+    public String token;
+
+    public static GetSessionRequest build(java.util.Map<String, ?> map) throws Exception {
+        GetSessionRequest self = new GetSessionRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public GetSessionRequest setToken(String token) {
+        this.token = token;
+        return this;
+    }
+    public String getToken() {
+        return this.token;
+    }
+
+}
