ce2c28a2c0c5ba4e60c0096a084a09c8541d1815
sdk-team <sdk-team@alibabacloud.com>
2020-12-25 10:11:57 +0000

    Release GenRealPersonVerificationToken GetRealPersonVerificationResult.

---
+++ facebody-20191230/src/main/java/com/aliyun/facebody20191230/models/ListBodyPersonResponseBody.java
@@ -0,0 +1,156 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.facebody20191230.models;
+
+import com.aliyun.tea.*;
+
+public class ListBodyPersonResponseBody extends TeaModel {
+    // RequestId
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("Data")
+    public ListBodyPersonResponseBodyData data;
+
+    @NameInMap("Code")
+    public String code;
+
+    @NameInMap("Message")
+    public String message;
+
+    public static ListBodyPersonResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        ListBodyPersonResponseBody self = new ListBodyPersonResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public ListBodyPersonResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public ListBodyPersonResponseBody setData(ListBodyPersonResponseBodyData data) {
+        this.data = data;
+        return this;
+    }
+    public ListBodyPersonResponseBodyData getData() {
+        return this.data;
+    }
+
+    public ListBodyPersonResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public ListBodyPersonResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public static class ListBodyPersonResponseBodyDataPersonList extends TeaModel {
+        // 搜索引擎实例Id
+        @NameInMap("InstanceId")
+        public Long instanceId;
+
+        // 数据库ID
+        @NameInMap("DbId")
+        public Long dbId;
+
+        // 人员名称
+        @NameInMap("Name")
+        public String name;
+
+        // Trace数量
+        @NameInMap("TraceCount")
+        public Long traceCount;
+
+        // 人员ID
+        @NameInMap("Id")
+        public Long id;
+
+        public static ListBodyPersonResponseBodyDataPersonList build(java.util.Map<String, ?> map) throws Exception {
+            ListBodyPersonResponseBodyDataPersonList self = new ListBodyPersonResponseBodyDataPersonList();
+            return TeaModel.build(map, self);
+        }
+
+        public ListBodyPersonResponseBodyDataPersonList setInstanceId(Long instanceId) {
+            this.instanceId = instanceId;
+            return this;
+        }
+        public Long getInstanceId() {
+            return this.instanceId;
+        }
+
+        public ListBodyPersonResponseBodyDataPersonList setDbId(Long dbId) {
+            this.dbId = dbId;
+            return this;
+        }
+        public Long getDbId() {
+            return this.dbId;
+        }
+
+        public ListBodyPersonResponseBodyDataPersonList setName(String name) {
+            this.name = name;
+            return this;
+        }
+        public String getName() {
+            return this.name;
+        }
+
+        public ListBodyPersonResponseBodyDataPersonList setTraceCount(Long traceCount) {
+            this.traceCount = traceCount;
+            return this;
+        }
+        public Long getTraceCount() {
+            return this.traceCount;
+        }
+
+        public ListBodyPersonResponseBodyDataPersonList setId(Long id) {
+            this.id = id;
+            return this;
+        }
+        public Long getId() {
+            return this.id;
+        }
+
+    }
+
+    public static class ListBodyPersonResponseBodyData extends TeaModel {
+        // 数据总量
+        @NameInMap("Total")
+        public Long total;
+
+        @NameInMap("PersonList")
+        public java.util.List<ListBodyPersonResponseBodyDataPersonList> personList;
+
+        public static ListBodyPersonResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
+            ListBodyPersonResponseBodyData self = new ListBodyPersonResponseBodyData();
+            return TeaModel.build(map, self);
+        }
+
+        public ListBodyPersonResponseBodyData setTotal(Long total) {
+            this.total = total;
+            return this;
+        }
+        public Long getTotal() {
+            return this.total;
+        }
+
+        public ListBodyPersonResponseBodyData setPersonList(java.util.List<ListBodyPersonResponseBodyDataPersonList> personList) {
+            this.personList = personList;
+            return this;
+        }
+        public java.util.List<ListBodyPersonResponseBodyDataPersonList> getPersonList() {
+            return this.personList;
+        }
+
+    }
+
+}
