71ecd968c192650f1ecfd2008f70b21b03689065
sdk-team <sdk-team@alibabacloud.com>
2020-12-30 03:34:17 +0000

    AMP Version Change.

---
+++ saf-20170331/src/main/java/com/aliyun/saf20170331/Client.java
@@ -0,0 +1,49 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.saf20170331;
+
+import com.aliyun.tea.*;
+import com.aliyun.saf20170331.models.*;
+import com.aliyun.teautil.*;
+import com.aliyun.teautil.models.*;
+import com.aliyun.teaopenapi.*;
+import com.aliyun.teaopenapi.models.*;
+import com.aliyun.endpointutil.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(Config config) throws Exception {
+        super(config);
+        this._endpointRule = "regional";
+        this._endpointMap = TeaConverter.buildMap(
+            new TeaPair("cn-hangzhou", "saf.cn-shanghai.aliyuncs.com")
+        );
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("saf", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public ExecuteRequestResponse executeRequestWithOptions(ExecuteRequestRequest request, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("body", com.aliyun.teautil.Common.toMap(request))
+        ));
+        return TeaModel.toModel(this.doRPCRequest("ExecuteRequest", "2017-03-31", "HTTPS", "POST", "AK", "json", req, runtime), new ExecuteRequestResponse());
+    }
+
+    public ExecuteRequestResponse executeRequest(ExecuteRequestRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        return this.executeRequestWithOptions(request, runtime);
+    }
+}
---
+++ saf-20170331/src/main/java/com/aliyun/saf20170331/models/ExecuteRequestRequest.java
@@ -0,0 +1,34 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.saf20170331.models;
+
+import com.aliyun.tea.*;
+
+public class ExecuteRequestRequest extends TeaModel {
+    @NameInMap("Service")
+    public String service;
+
+    @NameInMap("ServiceParameters")
+    public String serviceParameters;
+
+    public static ExecuteRequestRequest build(java.util.Map<String, ?> map) throws Exception {
+        ExecuteRequestRequest self = new ExecuteRequestRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public ExecuteRequestRequest setService(String service) {
+        this.service = service;
+        return this;
+    }
+    public String getService() {
+        return this.service;
+    }
+
+    public ExecuteRequestRequest setServiceParameters(String serviceParameters) {
+        this.serviceParameters = serviceParameters;
+        return this;
+    }
+    public String getServiceParameters() {
+        return this.serviceParameters;
+    }
+
+}
