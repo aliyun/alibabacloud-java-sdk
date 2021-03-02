7e95606ba6832fd8333e112b7f9fc5b753ccc54c
sdk-team <sdk-team@alibabacloud.com>
2021-03-02 05:36:09 +0000

    Fix http method.

---
+++ yuqing-20210126/src/main/java/com/aliyun/yuqing20210126/models/CreateProjectRequest.java
@@ -0,0 +1,72 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.yuqing20210126.models;
+
+import com.aliyun.tea.*;
+
+public class CreateProjectRequest extends TeaModel {
+    // 创建者uid
+    @NameInMap("createUserId")
+    public String createUserId;
+
+    // 创建者名称
+    @NameInMap("createUserName")
+    public String createUserName;
+
+    // 舆情项目对象
+    @NameInMap("project")
+    public Project project;
+
+    // 舆情团队HashId
+    @NameInMap("teamHashId")
+    public String teamHashId;
+
+    // 请求id
+    @NameInMap("requestId")
+    public String requestId;
+
+    public static CreateProjectRequest build(java.util.Map<String, ?> map) throws Exception {
+        CreateProjectRequest self = new CreateProjectRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateProjectRequest setCreateUserId(String createUserId) {
+        this.createUserId = createUserId;
+        return this;
+    }
+    public String getCreateUserId() {
+        return this.createUserId;
+    }
+
+    public CreateProjectRequest setCreateUserName(String createUserName) {
+        this.createUserName = createUserName;
+        return this;
+    }
+    public String getCreateUserName() {
+        return this.createUserName;
+    }
+
+    public CreateProjectRequest setProject(Project project) {
+        this.project = project;
+        return this;
+    }
+    public Project getProject() {
+        return this.project;
+    }
+
+    public CreateProjectRequest setTeamHashId(String teamHashId) {
+        this.teamHashId = teamHashId;
+        return this;
+    }
+    public String getTeamHashId() {
+        return this.teamHashId;
+    }
+
+    public CreateProjectRequest setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+}
---
+++ yuqing-20210126/src/main/java/com/aliyun/yuqing20210126/models/DeleteProjectRequest.java
@@ -0,0 +1,72 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.yuqing20210126.models;
+
+import com.aliyun.tea.*;
+
+public class DeleteProjectRequest extends TeaModel {
+    // 舆情项目id
+    @NameInMap("id")
+    public Long id;
+
+    // 修改人uid
+    @NameInMap("modifiedUserId")
+    public String modifiedUserId;
+
+    // 修改人名称
+    @NameInMap("modifiedUserName")
+    public String modifiedUserName;
+
+    // 舆情团队HashId
+    @NameInMap("teamHashId")
+    public String teamHashId;
+
+    // 请求id
+    @NameInMap("requestId")
+    public String requestId;
+
+    public static DeleteProjectRequest build(java.util.Map<String, ?> map) throws Exception {
+        DeleteProjectRequest self = new DeleteProjectRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public DeleteProjectRequest setId(Long id) {
+        this.id = id;
+        return this;
+    }
+    public Long getId() {
+        return this.id;
+    }
+
+    public DeleteProjectRequest setModifiedUserId(String modifiedUserId) {
+        this.modifiedUserId = modifiedUserId;
+        return this;
+    }
+    public String getModifiedUserId() {
+        return this.modifiedUserId;
+    }
+
+    public DeleteProjectRequest setModifiedUserName(String modifiedUserName) {
+        this.modifiedUserName = modifiedUserName;
+        return this;
+    }
+    public String getModifiedUserName() {
+        return this.modifiedUserName;
+    }
+
+    public DeleteProjectRequest setTeamHashId(String teamHashId) {
+        this.teamHashId = teamHashId;
+        return this;
+    }
+    public String getTeamHashId() {
+        return this.teamHashId;
+    }
+
+    public DeleteProjectRequest setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+}
