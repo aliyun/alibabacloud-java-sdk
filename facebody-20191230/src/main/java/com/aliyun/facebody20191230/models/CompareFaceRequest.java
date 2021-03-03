5063ff156e0d957f74feeb29458dbe12e4a54ae4
sdk-team <sdk-team@alibabacloud.com>
2021-03-03 10:04:12 +0000

    Update Compareface.

--- facebody-20191230/src/main/java/com/aliyun/facebody20191230/models/CompareFaceRequest.java
+++ facebody-20191230/src/main/java/com/aliyun/facebody20191230/models/CompareFaceRequest.java
@@ -4,8 +4,9 @@ package com.aliyun.facebody20191230.models;
 import com.aliyun.tea.*;
 
 public class CompareFaceRequest extends TeaModel {
-    @NameInMap("ImageType")
-    public Integer imageType;
+    // 质量分阈值，取值范围 [0.0, 100.0],   0.0或空  表示不做质量分判断逻辑。
+    @NameInMap("QualityScoreThreshold")
+    public Float qualityScoreThreshold;
 
     @NameInMap("ImageURLA")
     public String imageURLA;
@@ -13,17 +14,20 @@ public class CompareFaceRequest extends TeaModel {
     @NameInMap("ImageURLB")
     public String imageURLB;
 
+    @NameInMap("ImageType")
+    public Integer imageType;
+
     public static CompareFaceRequest build(java.util.Map<String, ?> map) throws Exception {
         CompareFaceRequest self = new CompareFaceRequest();
         return TeaModel.build(map, self);
     }
 
-    public CompareFaceRequest setImageType(Integer imageType) {
-        this.imageType = imageType;
+    public CompareFaceRequest setQualityScoreThreshold(Float qualityScoreThreshold) {
+        this.qualityScoreThreshold = qualityScoreThreshold;
         return this;
     }
-    public Integer getImageType() {
-        return this.imageType;
+    public Float getQualityScoreThreshold() {
+        return this.qualityScoreThreshold;
     }
 
     public CompareFaceRequest setImageURLA(String imageURLA) {
@@ -40,6 +44,14 @@ public class CompareFaceRequest extends TeaModel {
     }
     public String getImageURLB() {
         return this.imageURLB;
+    }
+
+    public CompareFaceRequest setImageType(Integer imageType) {
+        this.imageType = imageType;
+        return this;
+    }
+    public Integer getImageType() {
+        return this.imageType;
     }
 
 }
