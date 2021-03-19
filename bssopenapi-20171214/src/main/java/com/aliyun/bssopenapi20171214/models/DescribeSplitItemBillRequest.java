a29dbb85c45ede671e743e048d6bf087ebf97029
sdk-team <sdk-team@alibabacloud.com>
2021-03-19 04:20:49 +0000

    AMP Version Change.

---
+++ bssopenapi-20171214/src/main/java/com/aliyun/bssopenapi20171214/models/DescribeSplitItemBillRequest.java
@@ -0,0 +1,163 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.bssopenapi20171214.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeSplitItemBillRequest extends TeaModel {
+    @NameInMap("BillingCycle")
+    public String billingCycle;
+
+    @NameInMap("ProductCode")
+    public String productCode;
+
+    @NameInMap("ProductType")
+    public String productType;
+
+    @NameInMap("SubscriptionType")
+    public String subscriptionType;
+
+    @NameInMap("OwnerId")
+    public Long ownerId;
+
+    @NameInMap("NextToken")
+    public String nextToken;
+
+    @NameInMap("MaxResults")
+    public Integer maxResults;
+
+    @NameInMap("BillOwnerId")
+    public Long billOwnerId;
+
+    @NameInMap("InstanceID")
+    public String instanceID;
+
+    @NameInMap("SplitItemID")
+    public String splitItemID;
+
+    @NameInMap("TagFilter")
+    public java.util.List<DescribeSplitItemBillRequestTagFilter> tagFilter;
+
+    public static DescribeSplitItemBillRequest build(java.util.Map<String, ?> map) throws Exception {
+        DescribeSplitItemBillRequest self = new DescribeSplitItemBillRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeSplitItemBillRequest setBillingCycle(String billingCycle) {
+        this.billingCycle = billingCycle;
+        return this;
+    }
+    public String getBillingCycle() {
+        return this.billingCycle;
+    }
+
+    public DescribeSplitItemBillRequest setProductCode(String productCode) {
+        this.productCode = productCode;
+        return this;
+    }
+    public String getProductCode() {
+        return this.productCode;
+    }
+
+    public DescribeSplitItemBillRequest setProductType(String productType) {
+        this.productType = productType;
+        return this;
+    }
+    public String getProductType() {
+        return this.productType;
+    }
+
+    public DescribeSplitItemBillRequest setSubscriptionType(String subscriptionType) {
+        this.subscriptionType = subscriptionType;
+        return this;
+    }
+    public String getSubscriptionType() {
+        return this.subscriptionType;
+    }
+
+    public DescribeSplitItemBillRequest setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+        return this;
+    }
+    public Long getOwnerId() {
+        return this.ownerId;
+    }
+
+    public DescribeSplitItemBillRequest setNextToken(String nextToken) {
+        this.nextToken = nextToken;
+        return this;
+    }
+    public String getNextToken() {
+        return this.nextToken;
+    }
+
+    public DescribeSplitItemBillRequest setMaxResults(Integer maxResults) {
+        this.maxResults = maxResults;
+        return this;
+    }
+    public Integer getMaxResults() {
+        return this.maxResults;
+    }
+
+    public DescribeSplitItemBillRequest setBillOwnerId(Long billOwnerId) {
+        this.billOwnerId = billOwnerId;
+        return this;
+    }
+    public Long getBillOwnerId() {
+        return this.billOwnerId;
+    }
+
+    public DescribeSplitItemBillRequest setInstanceID(String instanceID) {
+        this.instanceID = instanceID;
+        return this;
+    }
+    public String getInstanceID() {
+        return this.instanceID;
+    }
+
+    public DescribeSplitItemBillRequest setSplitItemID(String splitItemID) {
+        this.splitItemID = splitItemID;
+        return this;
+    }
+    public String getSplitItemID() {
+        return this.splitItemID;
+    }
+
+    public DescribeSplitItemBillRequest setTagFilter(java.util.List<DescribeSplitItemBillRequestTagFilter> tagFilter) {
+        this.tagFilter = tagFilter;
+        return this;
+    }
+    public java.util.List<DescribeSplitItemBillRequestTagFilter> getTagFilter() {
+        return this.tagFilter;
+    }
+
+    public static class DescribeSplitItemBillRequestTagFilter extends TeaModel {
+        @NameInMap("TagValues")
+        public java.util.List<String> tagValues;
+
+        @NameInMap("TagKey")
+        public String tagKey;
+
+        public static DescribeSplitItemBillRequestTagFilter build(java.util.Map<String, ?> map) throws Exception {
+            DescribeSplitItemBillRequestTagFilter self = new DescribeSplitItemBillRequestTagFilter();
+            return TeaModel.build(map, self);
+        }
+
+        public DescribeSplitItemBillRequestTagFilter setTagValues(java.util.List<String> tagValues) {
+            this.tagValues = tagValues;
+            return this;
+        }
+        public java.util.List<String> getTagValues() {
+            return this.tagValues;
+        }
+
+        public DescribeSplitItemBillRequestTagFilter setTagKey(String tagKey) {
+            this.tagKey = tagKey;
+            return this;
+        }
+        public String getTagKey() {
+            return this.tagKey;
+        }
+
+    }
+
+}
---
+++ bssopenapi-20171214/src/main/java/com/aliyun/bssopenapi20171214/models/UntagResourcesResponseBody.java
@@ -0,0 +1,67 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.bssopenapi20171214.models;
+
+import com.aliyun.tea.*;
+
+public class UntagResourcesResponseBody extends TeaModel {
+    @NameInMap("Message")
+    public String message;
+
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("Data")
+    public Boolean data;
+
+    @NameInMap("Code")
+    public String code;
+
+    @NameInMap("Success")
+    public Boolean success;
+
+    public static UntagResourcesResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        UntagResourcesResponseBody self = new UntagResourcesResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public UntagResourcesResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public UntagResourcesResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public UntagResourcesResponseBody setData(Boolean data) {
+        this.data = data;
+        return this;
+    }
+    public Boolean getData() {
+        return this.data;
+    }
+
+    public UntagResourcesResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public UntagResourcesResponseBody setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+}
