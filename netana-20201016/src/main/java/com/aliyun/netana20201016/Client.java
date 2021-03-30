cb2b9ce0b068171d7daa0038630fd090f8a03a7f
sdk-team <sdk-team@alibabacloud.com>
2021-03-30 04:47:51 +0000

    Generated java 2020-10-16 for Netana.

--- netana-20201016/src/main/java/com/aliyun/netana20201016/Client.java
+++ netana-20201016/src/main/java/com/aliyun/netana20201016/Client.java
@@ -3,26 +3,22 @@ package com.aliyun.netana20201016;
 
 import com.aliyun.tea.*;
 import com.aliyun.netana20201016.models.*;
+import com.aliyun.teautil.*;
+import com.aliyun.teautil.models.*;
+import com.aliyun.teaopenapi.*;
+import com.aliyun.teaopenapi.models.*;
+import com.aliyun.openapiutil.*;
+import com.aliyun.endpointutil.*;
 
-public class Client extends com.aliyun.tearpc.Client {
+public class Client extends com.aliyun.teaopenapi.Client {
 
-    public Client(com.aliyun.tearpc.models.Config config) throws Exception {
+    public Client(Config config) throws Exception {
         super(config);
         this._endpointRule = "regional";
         this.checkConfig(config);
         this._endpoint = this.getEndpoint("netana", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
     }
 
-
-    public ListIpGeolocationsResponse listIpGeolocationsWithOptions(ListIpGeolocationsRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        return TeaModel.toModel(this.doRequest("ListIpGeolocations", "HTTPS", "POST", "2020-10-16", "AK", null, TeaModel.buildMap(request), runtime), new ListIpGeolocationsResponse());
-    }
-
-    public ListIpGeolocationsResponse listIpGeolocations(ListIpGeolocationsRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        return this.listIpGeolocationsWithOptions(request, runtime);
-    }
 
     public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
         if (!com.aliyun.teautil.Common.empty(endpoint)) {
@@ -34,5 +30,41 @@ public class Client extends com.aliyun.tearpc.Client {
         }
 
         return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public ListIpGeolocationsResponse listIpGeolocationsWithOptions(ListIpGeolocationsRequest request, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        query.put("CountryCode", request.countryCode);
+        query.put("CityCode", request.cityCode);
+        query.put("Ipv4Prefix", request.ipv4Prefix);
+        query.put("ResourcePoolName", request.resourcePoolName);
+        query.put("NextToken", request.nextToken);
+        query.put("MaxResults", request.maxResults);
+        query.put("OwnerId", request.ownerId);
+        query.put("ResourceOwnerAccount", request.resourceOwnerAccount);
+        query.put("ResourceOwnerId", request.resourceOwnerId);
+        query.put("OwnerAccount", request.ownerAccount);
+        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query)),
+            new TeaPair("body", com.aliyun.teautil.Common.toMap(request))
+        ));
+        Params params = Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "ListIpGeolocations"),
+            new TeaPair("version", "2020-10-16"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new ListIpGeolocationsResponse());
+    }
+
+    public ListIpGeolocationsResponse listIpGeolocations(ListIpGeolocationsRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        return this.listIpGeolocationsWithOptions(request, runtime);
     }
 }
