5b77467e557a35bda17c08b742816385ece7890e
sdk-team <sdk-team@alibabacloud.com>
2021-06-04 09:24:02 +0000

    Test.

---
+++ nbftestpop-20210602_214152752/src/main/java/com/aliyun/nbftestpop20210602_214152752/Client.java
@@ -0,0 +1,46 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nbftestpop20210602_214152752;
+
+import com.aliyun.tea.*;
+import com.aliyun.nbftestpop20210602_214152752.models.*;
+import com.aliyun.teautil.*;
+import com.aliyun.teautil.models.*;
+import com.aliyun.teaopenapi.*;
+import com.aliyun.teaopenapi.models.*;
+import com.aliyun.endpointutil.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("nbftestpop", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public YxTestApiResponse yxTestApiWithOptions(YxTestApiRequest request, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("body", com.aliyun.teautil.Common.toMap(request))
+        ));
+        return TeaModel.toModel(this.doRPCRequest("YxTestApi", "2021-06-02_21-41-52-752", "HTTPS", "POST", "AK", "json", req, runtime), new YxTestApiResponse());
+    }
+
+    public YxTestApiResponse yxTestApi(YxTestApiRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        return this.yxTestApiWithOptions(request, runtime);
+    }
+}
