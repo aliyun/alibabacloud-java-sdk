7ca6e21481c8ecb3819b910258d35a116bf693d5
sdk-team <sdk-team@alibabacloud.com>
2021-06-09 11:27:22 +0000

    AMP Version.

--- cs-20151215/src/main/java/com/aliyun/cs20151215/models/DescribeClusterUserKubeconfigRequest.java
+++ cs-20151215/src/main/java/com/aliyun/cs20151215/models/DescribeClusterUserKubeconfigRequest.java
@@ -8,6 +8,10 @@ public class DescribeClusterUserKubeconfigRequest extends TeaModel {
     @NameInMap("PrivateIpAddress")
     public Boolean privateIpAddress;
 
+    // 临时kubeconfig有效期，单位：分钟。  最小值：15（15分钟）  最大值：4320（3天）。
+    @NameInMap("TemporaryDurationMinutes")
+    public Long temporaryDurationMinutes;
+
     public static DescribeClusterUserKubeconfigRequest build(java.util.Map<String, ?> map) throws Exception {
         DescribeClusterUserKubeconfigRequest self = new DescribeClusterUserKubeconfigRequest();
         return TeaModel.build(map, self);
@@ -19,6 +23,14 @@ public class DescribeClusterUserKubeconfigRequest extends TeaModel {
     }
     public Boolean getPrivateIpAddress() {
         return this.privateIpAddress;
+    }
+
+    public DescribeClusterUserKubeconfigRequest setTemporaryDurationMinutes(Long temporaryDurationMinutes) {
+        this.temporaryDurationMinutes = temporaryDurationMinutes;
+        return this;
+    }
+    public Long getTemporaryDurationMinutes() {
+        return this.temporaryDurationMinutes;
     }
 
 }
