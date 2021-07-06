9d331dfb38f9ba0b11e7826eccdc77ae05eb41f7
sdk-team <sdk-team@alibabacloud.com>
2021-07-06 02:27:48 +0000

    Update.

---
+++ aiccs-20191015/src/main/java/com/aliyun/aiccs20191015/models/AiccsSmartCallOperateResponseBody.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.aiccs20191015.models;
+
+import com.aliyun.tea.*;
+
+public class AiccsSmartCallOperateResponseBody extends TeaModel {
+    @NameInMap("Message")
+    public String message;
+
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("Data")
+    public String data;
+
+    @NameInMap("Code")
+    public String code;
+
+    public static AiccsSmartCallOperateResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        AiccsSmartCallOperateResponseBody self = new AiccsSmartCallOperateResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public AiccsSmartCallOperateResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public AiccsSmartCallOperateResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public AiccsSmartCallOperateResponseBody setData(String data) {
+        this.data = data;
+        return this;
+    }
+    public String getData() {
+        return this.data;
+    }
+
+    public AiccsSmartCallOperateResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+}
