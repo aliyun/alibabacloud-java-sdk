237be1c36b5eab69c05492d9138e81511fa663ab
sdk-team <sdk-team@alibabacloud.com>
2021-07-12 04:47:11 +0000

    Without.

---
+++ nbf-20210709_163152377/src/main/java/com/aliyun/nbf20210709_163152377/models/ChenjingtestRequest.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nbf20210709_163152377.models;
+
+import com.aliyun.tea.*;
+
+public class ChenjingtestRequest extends TeaModel {
+    @NameInMap("a")
+    public Integer a;
+
+    public static ChenjingtestRequest build(java.util.Map<String, ?> map) throws Exception {
+        ChenjingtestRequest self = new ChenjingtestRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public ChenjingtestRequest setA(Integer a) {
+        this.a = a;
+        return this;
+    }
+    public Integer getA() {
+        return this.a;
+    }
+
+}
---
+++ nbf-20210709_163152377/src/main/java/com/aliyun/nbf20210709_163152377/models/ChenjingtestResponseBody.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nbf20210709_163152377.models;
+
+import com.aliyun.tea.*;
+
+public class ChenjingtestResponseBody extends TeaModel {
+    @NameInMap("c")
+    public Integer c;
+
+    public static ChenjingtestResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        ChenjingtestResponseBody self = new ChenjingtestResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public ChenjingtestResponseBody setC(Integer c) {
+        this.c = c;
+        return this;
+    }
+    public Integer getC() {
+        return this.c;
+    }
+
+}
