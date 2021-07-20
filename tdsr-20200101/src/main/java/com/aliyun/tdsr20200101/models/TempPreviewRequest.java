8599e85e3863d6bde339625af363cf98e2ca95b1
sdk-team <sdk-team@alibabacloud.com>
2021-07-20 03:13:24 +0000

    Temppreview interface parameter name change.

--- tdsr-20200101/src/main/java/com/aliyun/tdsr20200101/models/TempPreviewRequest.java
+++ tdsr-20200101/src/main/java/com/aliyun/tdsr20200101/models/TempPreviewRequest.java
@@ -5,20 +5,20 @@ import com.aliyun.tea.*;
 
 public class TempPreviewRequest extends TeaModel {
     // 场景ID
-    @NameInMap("Id")
-    public String id;
+    @NameInMap("SceneId")
+    public String sceneId;
 
     public static TempPreviewRequest build(java.util.Map<String, ?> map) throws Exception {
         TempPreviewRequest self = new TempPreviewRequest();
         return TeaModel.build(map, self);
     }
 
-    public TempPreviewRequest setId(String id) {
-        this.id = id;
+    public TempPreviewRequest setSceneId(String sceneId) {
+        this.sceneId = sceneId;
         return this;
     }
-    public String getId() {
-        return this.id;
+    public String getSceneId() {
+        return this.sceneId;
     }
 
 }
