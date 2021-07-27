1bdc9e6742366b922459cc0846e7aa81e4b474d5
sdk-team <sdk-team@alibabacloud.com>
2021-07-27 09:00:45 +0000

    Generated java 2019-12-19 for OpenPlatform.

--- openplatform-20191219/src/main/java/com/aliyun/openplatform20191219/Client.java
+++ openplatform-20191219/src/main/java/com/aliyun/openplatform20191219/Client.java
@@ -5,12 +5,11 @@ import com.aliyun.tea.*;
 import com.aliyun.openplatform20191219.models.*;
 import com.aliyun.teautil.*;
 import com.aliyun.teautil.models.*;
-import com.aliyun.teaopenapi.*;
-import com.aliyun.teaopenapi.models.*;
-import com.aliyun.openapiutil.*;
+import com.aliyun.tearpc.*;
+import com.aliyun.tearpc.models.*;
 import com.aliyun.endpointutil.*;
 
-public class Client extends com.aliyun.teaopenapi.Client {
+public class Client extends com.aliyun.tearpc.Client {
 
     public Client(Config config) throws Exception {
         super(config);
@@ -19,6 +18,16 @@ public class Client extends com.aliyun.teaopenapi.Client {
         this._endpoint = this.getEndpoint("openplatform", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
     }
 
+
+    public AuthorizeFileUploadResponse authorizeFileUploadWithOptions(AuthorizeFileUploadRequest request, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        return TeaModel.toModel(this.doRequest("AuthorizeFileUpload", "HTTPS", "GET", "2019-12-19", "AK", TeaModel.buildMap(request), null, runtime), new AuthorizeFileUploadResponse());
+    }
+
+    public AuthorizeFileUploadResponse authorizeFileUpload(AuthorizeFileUploadRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        return this.authorizeFileUploadWithOptions(request, runtime);
+    }
 
     public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
         if (!com.aliyun.teautil.Common.empty(endpoint)) {
@@ -30,19 +39,5 @@ public class Client extends com.aliyun.teaopenapi.Client {
         }
 
         return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
-    }
-
-    public AuthorizeFileUploadResponse authorizeFileUploadWithOptions(AuthorizeFileUploadRequest request, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, String> query = com.aliyun.openapiutil.Client.query(com.aliyun.teautil.Common.toMap(request));
-        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("query", query)
-        ));
-        return TeaModel.toModel(this.doRPCRequest("AuthorizeFileUpload", "2019-12-19", "HTTPS", "GET", "AK", "json", req, runtime), new AuthorizeFileUploadResponse());
-    }
-
-    public AuthorizeFileUploadResponse authorizeFileUpload(AuthorizeFileUploadRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        return this.authorizeFileUploadWithOptions(request, runtime);
     }
 }
--- openplatform-20191219/src/main/java/com/aliyun/openplatform20191219/models/AuthorizeFileUploadResponse.java
+++ openplatform-20191219/src/main/java/com/aliyun/openplatform20191219/models/AuthorizeFileUploadResponse.java
@@ -4,33 +4,105 @@ package com.aliyun.openplatform20191219.models;
 import com.aliyun.tea.*;
 
 public class AuthorizeFileUploadResponse extends TeaModel {
-    @NameInMap("headers")
+    @NameInMap("AccessKeyId")
     @Validation(required = true)
-    public java.util.Map<String, String> headers;
+    public String accessKeyId;
 
-    @NameInMap("body")
+    @NameInMap("Bucket")
     @Validation(required = true)
-    public AuthorizeFileUploadResponseBody body;
+    public String bucket;
+
+    @NameInMap("EncodedPolicy")
+    @Validation(required = true)
+    public String encodedPolicy;
+
+    @NameInMap("Endpoint")
+    @Validation(required = true)
+    public String endpoint;
+
+    @NameInMap("ObjectKey")
+    @Validation(required = true)
+    public String objectKey;
+
+    @NameInMap("RequestId")
+    @Validation(required = true)
+    public String requestId;
+
+    @NameInMap("Signature")
+    @Validation(required = true)
+    public String signature;
+
+    @NameInMap("UseAccelerate")
+    @Validation(required = true)
+    public Boolean useAccelerate;
 
     public static AuthorizeFileUploadResponse build(java.util.Map<String, ?> map) throws Exception {
         AuthorizeFileUploadResponse self = new AuthorizeFileUploadResponse();
         return TeaModel.build(map, self);
     }
 
-    public AuthorizeFileUploadResponse setHeaders(java.util.Map<String, String> headers) {
-        this.headers = headers;
+    public AuthorizeFileUploadResponse setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
         return this;
     }
-    public java.util.Map<String, String> getHeaders() {
-        return this.headers;
+    public String getAccessKeyId() {
+        return this.accessKeyId;
     }
 
-    public AuthorizeFileUploadResponse setBody(AuthorizeFileUploadResponseBody body) {
-        this.body = body;
+    public AuthorizeFileUploadResponse setBucket(String bucket) {
+        this.bucket = bucket;
         return this;
     }
-    public AuthorizeFileUploadResponseBody getBody() {
-        return this.body;
+    public String getBucket() {
+        return this.bucket;
+    }
+
+    public AuthorizeFileUploadResponse setEncodedPolicy(String encodedPolicy) {
+        this.encodedPolicy = encodedPolicy;
+        return this;
+    }
+    public String getEncodedPolicy() {
+        return this.encodedPolicy;
+    }
+
+    public AuthorizeFileUploadResponse setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+        return this;
+    }
+    public String getEndpoint() {
+        return this.endpoint;
+    }
+
+    public AuthorizeFileUploadResponse setObjectKey(String objectKey) {
+        this.objectKey = objectKey;
+        return this;
+    }
+    public String getObjectKey() {
+        return this.objectKey;
+    }
+
+    public AuthorizeFileUploadResponse setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public AuthorizeFileUploadResponse setSignature(String signature) {
+        this.signature = signature;
+        return this;
+    }
+    public String getSignature() {
+        return this.signature;
+    }
+
+    public AuthorizeFileUploadResponse setUseAccelerate(Boolean useAccelerate) {
+        this.useAccelerate = useAccelerate;
+        return this;
+    }
+    public Boolean getUseAccelerate() {
+        return this.useAccelerate;
     }
 
 }
