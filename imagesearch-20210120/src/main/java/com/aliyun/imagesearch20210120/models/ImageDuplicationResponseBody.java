4a1ef28da1c1b9559ee5f8d84149767447929402
sdk-team <sdk-team@alibabacloud.com>
2021-07-29 03:09:36 +0000

    Add General Recognition Function.

---
+++ imagesearch-20210120/src/main/java/com/aliyun/imagesearch20210120/models/ImageDuplicationResponseBody.java
@@ -0,0 +1,67 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.imagesearch20210120.models;
+
+import com.aliyun.tea.*;
+
+public class ImageDuplicationResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public Integer code;
+
+    @NameInMap("Message")
+    public String message;
+
+    @NameInMap("Data")
+    public java.util.List<String> data;
+
+    @NameInMap("Success")
+    public Boolean success;
+
+    @NameInMap("RequestId")
+    public String requestId;
+
+    public static ImageDuplicationResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        ImageDuplicationResponseBody self = new ImageDuplicationResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public ImageDuplicationResponseBody setCode(Integer code) {
+        this.code = code;
+        return this;
+    }
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public ImageDuplicationResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public ImageDuplicationResponseBody setData(java.util.List<String> data) {
+        this.data = data;
+        return this;
+    }
+    public java.util.List<String> getData() {
+        return this.data;
+    }
+
+    public ImageDuplicationResponseBody setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+    public ImageDuplicationResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+}
