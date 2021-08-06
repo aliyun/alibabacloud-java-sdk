13282d9a70bbda726a262ac573be1d4d5808dcca
sdk-team <sdk-team@alibabacloud.com>
2021-08-06 05:33:18 +0000

    Update TaggingAdImage.

--- imagerecog-20190930/src/main/java/com/aliyun/imagerecog20190930/models/TaggingAdImageRequest.java
+++ imagerecog-20190930/src/main/java/com/aliyun/imagerecog20190930/models/TaggingAdImageRequest.java
@@ -4,23 +4,12 @@ package com.aliyun.imagerecog20190930.models;
 import com.aliyun.tea.*;
 
 public class TaggingAdImageRequest extends TeaModel {
-    @NameInMap("ImageType")
-    public Integer imageType;
-
     @NameInMap("ImageURL")
     public String imageURL;
 
     public static TaggingAdImageRequest build(java.util.Map<String, ?> map) throws Exception {
         TaggingAdImageRequest self = new TaggingAdImageRequest();
         return TeaModel.build(map, self);
-    }
-
-    public TaggingAdImageRequest setImageType(Integer imageType) {
-        this.imageType = imageType;
-        return this;
-    }
-    public Integer getImageType() {
-        return this.imageType;
     }
 
     public TaggingAdImageRequest setImageURL(String imageURL) {
