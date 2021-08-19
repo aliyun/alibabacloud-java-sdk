c70e3e9bbd82f337016c85ceb6c99a267751c181
sdk-team <sdk-team@alibabacloud.com>
2021-08-19 08:11:20 +0000

    OnlineRelease.

---
+++ nbftestpop-20210802_16102392/src/main/java/com/aliyun/nbftestpop20210802_16102392/Client.java
@@ -0,0 +1,58 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nbftestpop20210802_16102392;
+
+import com.aliyun.tea.*;
+import com.aliyun.nbftestpop20210802_16102392.models.*;
+import com.aliyun.teautil.*;
+import com.aliyun.teautil.models.*;
+import com.aliyun.teaopenapi.*;
+import com.aliyun.teaopenapi.models.*;
+import com.aliyun.openapiutil.*;
+import com.aliyun.endpointutil.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("nbftestpop", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public AddResponse add(AddRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.addWithOptions(request, headers, runtime);
+    }
+
+    public AddResponse addWithOptions(AddRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.x)) {
+            query.put("x", request.x);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.y)) {
+            query.put("y", request.y);
+        }
+
+        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        return TeaModel.toModel(this.doROARequest("Add", "2021-08-02_16-10-23-92", "HTTP", "GET", "AK", "/kxThree/headerTest", "json", req, runtime), new AddResponse());
+    }
+}
