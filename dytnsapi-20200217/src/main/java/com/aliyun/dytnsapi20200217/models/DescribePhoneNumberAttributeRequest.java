f7654b9be66917d3ff04cd1d89048b0b1e534bd4
sdk-team <sdk-team@alibabacloud.com>
2021-08-19 07:37:04 +0000

    AMP version.

---
+++ dytnsapi-20200217/src/main/java/com/aliyun/dytnsapi20200217/models/DescribePhoneNumberAttributeRequest.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.dytnsapi20200217.models;
+
+import com.aliyun.tea.*;
+
+public class DescribePhoneNumberAttributeRequest extends TeaModel {
+    @NameInMap("OwnerId")
+    public Long ownerId;
+
+    @NameInMap("ResourceOwnerAccount")
+    public String resourceOwnerAccount;
+
+    @NameInMap("ResourceOwnerId")
+    public Long resourceOwnerId;
+
+    @NameInMap("PhoneNumber")
+    public String phoneNumber;
+
+    public static DescribePhoneNumberAttributeRequest build(java.util.Map<String, ?> map) throws Exception {
+        DescribePhoneNumberAttributeRequest self = new DescribePhoneNumberAttributeRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribePhoneNumberAttributeRequest setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+        return this;
+    }
+    public Long getOwnerId() {
+        return this.ownerId;
+    }
+
+    public DescribePhoneNumberAttributeRequest setResourceOwnerAccount(String resourceOwnerAccount) {
+        this.resourceOwnerAccount = resourceOwnerAccount;
+        return this;
+    }
+    public String getResourceOwnerAccount() {
+        return this.resourceOwnerAccount;
+    }
+
+    public DescribePhoneNumberAttributeRequest setResourceOwnerId(Long resourceOwnerId) {
+        this.resourceOwnerId = resourceOwnerId;
+        return this;
+    }
+    public Long getResourceOwnerId() {
+        return this.resourceOwnerId;
+    }
+
+    public DescribePhoneNumberAttributeRequest setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+}
