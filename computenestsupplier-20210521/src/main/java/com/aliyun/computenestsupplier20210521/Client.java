220dc9916ba822548fcebc5d55645cc4aa662cf3
sdk-team <sdk-team@alibabacloud.com>
2021-08-31 03:02:38 +0000

    Support ComputeNest Operation.

--- computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/Client.java
+++ computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/Client.java
@@ -13,7 +13,7 @@ public class Client extends com.aliyun.teaopenapi.Client {
 
     public Client(Config config) throws Exception {
         super(config);
-        this._endpointRule = "";
+        this._endpointRule = "regional";
         this.checkConfig(config);
         this._endpoint = this.getEndpoint("computenestsupplier", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
     }
