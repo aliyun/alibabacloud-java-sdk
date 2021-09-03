dcbd97d3cb76e93a95de9d8774bbd472a160b523
sdk-team <sdk-team@alibabacloud.com>
2021-09-03 07:41:58 +0000

    Test.

--- nbftestpop-20210831_223006136/src/main/java/com/aliyun/nbftestpop20210831_223006136/Client.java
+++ nbftestpop-20210831_223006136/src/main/java/com/aliyun/nbftestpop20210831_223006136/Client.java
@@ -32,6 +32,30 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
     }
 
+    public AddResponse add(AddRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.addWithOptions(request, headers, runtime);
+    }
+
+    public AddResponse addWithOptions(AddRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.arg0)) {
+            query.put("arg0", request.arg0);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.arg1)) {
+            query.put("arg1", request.arg1);
+        }
+
+        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        return TeaModel.toModel(this.doROARequest("Add", "2021-08-31_22-30-06-136", "HTTPS", "POST", "AK", "/kxNineThreeNineSeven/kxAddTest", "json", req, runtime), new AddResponse());
+    }
+
     public AddAssetResponse addAsset(AddAssetRequest request) throws Exception {
         RuntimeOptions runtime = new RuntimeOptions();
         java.util.Map<String, String> headers = new java.util.HashMap<>();
