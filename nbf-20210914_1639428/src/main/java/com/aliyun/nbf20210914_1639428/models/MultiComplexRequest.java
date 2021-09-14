5c4fd1d3a955c71707df1921ed43265b0ce66ef6
sdk-team <sdk-team@alibabacloud.com>
2021-09-14 08:57:52 +0000

    Test.

---
+++ nbf-20210914_1639428/src/main/java/com/aliyun/nbf20210914_1639428/models/MultiComplexRequest.java
@@ -0,0 +1,190 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nbf20210914_1639428.models;
+
+import com.aliyun.tea.*;
+
+public class MultiComplexRequest extends TeaModel {
+    @NameInMap("arg0")
+    public java.io.InputStream arg0;
+
+    @NameInMap("arg1")
+    public Integer arg1;
+
+    @NameInMap("arg2")
+    public MultiComplexRequestArg2 arg2;
+
+    public static MultiComplexRequest build(java.util.Map<String, ?> map) throws Exception {
+        MultiComplexRequest self = new MultiComplexRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public MultiComplexRequest setArg0(java.io.InputStream arg0) {
+        this.arg0 = arg0;
+        return this;
+    }
+    public java.io.InputStream getArg0() {
+        return this.arg0;
+    }
+
+    public MultiComplexRequest setArg1(Integer arg1) {
+        this.arg1 = arg1;
+        return this;
+    }
+    public Integer getArg1() {
+        return this.arg1;
+    }
+
+    public MultiComplexRequest setArg2(MultiComplexRequestArg2 arg2) {
+        this.arg2 = arg2;
+        return this;
+    }
+    public MultiComplexRequestArg2 getArg2() {
+        return this.arg2;
+    }
+
+    public static class MultiComplexRequestArg2SecondInputThirdList extends TeaModel {
+        @NameInMap("thirdInt")
+        public Integer thirdInt;
+
+        @NameInMap("thirdString")
+        public java.io.InputStream thirdString;
+
+        public static MultiComplexRequestArg2SecondInputThirdList build(java.util.Map<String, ?> map) throws Exception {
+            MultiComplexRequestArg2SecondInputThirdList self = new MultiComplexRequestArg2SecondInputThirdList();
+            return TeaModel.build(map, self);
+        }
+
+        public MultiComplexRequestArg2SecondInputThirdList setThirdInt(Integer thirdInt) {
+            this.thirdInt = thirdInt;
+            return this;
+        }
+        public Integer getThirdInt() {
+            return this.thirdInt;
+        }
+
+        public MultiComplexRequestArg2SecondInputThirdList setThirdString(java.io.InputStream thirdString) {
+            this.thirdString = thirdString;
+            return this;
+        }
+        public java.io.InputStream getThirdString() {
+            return this.thirdString;
+        }
+
+    }
+
+    public static class MultiComplexRequestArg2SecondInput extends TeaModel {
+        @NameInMap("secondString")
+        public java.io.InputStream secondString;
+
+        @NameInMap("thirdList")
+        public java.util.List<MultiComplexRequestArg2SecondInputThirdList> thirdList;
+
+        @NameInMap("secondInt")
+        public Integer secondInt;
+
+        public static MultiComplexRequestArg2SecondInput build(java.util.Map<String, ?> map) throws Exception {
+            MultiComplexRequestArg2SecondInput self = new MultiComplexRequestArg2SecondInput();
+            return TeaModel.build(map, self);
+        }
+
+        public MultiComplexRequestArg2SecondInput setSecondString(java.io.InputStream secondString) {
+            this.secondString = secondString;
+            return this;
+        }
+        public java.io.InputStream getSecondString() {
+            return this.secondString;
+        }
+
+        public MultiComplexRequestArg2SecondInput setThirdList(java.util.List<MultiComplexRequestArg2SecondInputThirdList> thirdList) {
+            this.thirdList = thirdList;
+            return this;
+        }
+        public java.util.List<MultiComplexRequestArg2SecondInputThirdList> getThirdList() {
+            return this.thirdList;
+        }
+
+        public MultiComplexRequestArg2SecondInput setSecondInt(Integer secondInt) {
+            this.secondInt = secondInt;
+            return this;
+        }
+        public Integer getSecondInt() {
+            return this.secondInt;
+        }
+
+    }
+
+    public static class MultiComplexRequestArg2 extends TeaModel {
+        @NameInMap("stringValue")
+        public java.io.InputStream stringValue;
+
+        @NameInMap("intValue")
+        public Integer intValue;
+
+        @NameInMap("booleanValue")
+        public Boolean booleanValue;
+
+        @NameInMap("integerValue")
+        public Integer integerValue;
+
+        @NameInMap("longValue")
+        public Long longValue;
+
+        @NameInMap("secondInput")
+        public MultiComplexRequestArg2SecondInput secondInput;
+
+        public static MultiComplexRequestArg2 build(java.util.Map<String, ?> map) throws Exception {
+            MultiComplexRequestArg2 self = new MultiComplexRequestArg2();
+            return TeaModel.build(map, self);
+        }
+
+        public MultiComplexRequestArg2 setStringValue(java.io.InputStream stringValue) {
+            this.stringValue = stringValue;
+            return this;
+        }
+        public java.io.InputStream getStringValue() {
+            return this.stringValue;
+        }
+
+        public MultiComplexRequestArg2 setIntValue(Integer intValue) {
+            this.intValue = intValue;
+            return this;
+        }
+        public Integer getIntValue() {
+            return this.intValue;
+        }
+
+        public MultiComplexRequestArg2 setBooleanValue(Boolean booleanValue) {
+            this.booleanValue = booleanValue;
+            return this;
+        }
+        public Boolean getBooleanValue() {
+            return this.booleanValue;
+        }
+
+        public MultiComplexRequestArg2 setIntegerValue(Integer integerValue) {
+            this.integerValue = integerValue;
+            return this;
+        }
+        public Integer getIntegerValue() {
+            return this.integerValue;
+        }
+
+        public MultiComplexRequestArg2 setLongValue(Long longValue) {
+            this.longValue = longValue;
+            return this;
+        }
+        public Long getLongValue() {
+            return this.longValue;
+        }
+
+        public MultiComplexRequestArg2 setSecondInput(MultiComplexRequestArg2SecondInput secondInput) {
+            this.secondInput = secondInput;
+            return this;
+        }
+        public MultiComplexRequestArg2SecondInput getSecondInput() {
+            return this.secondInput;
+        }
+
+    }
+
+}
---
+++ nbf-20210914_1639428/src/main/java/com/aliyun/nbf20210914_1639428/models/MultiComplexResponseBody.java
@@ -0,0 +1,149 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nbf20210914_1639428.models;
+
+import com.aliyun.tea.*;
+
+public class MultiComplexResponseBody extends TeaModel {
+    @NameInMap("stringValue")
+    public java.io.InputStream stringValue;
+
+    @NameInMap("secondOutput")
+    public MultiComplexResponseBodySecondOutput secondOutput;
+
+    @NameInMap("intValue")
+    public Integer intValue;
+
+    @NameInMap("booleanValue")
+    public Boolean booleanValue;
+
+    @NameInMap("integerValue")
+    public Integer integerValue;
+
+    @NameInMap("longValue")
+    public Long longValue;
+
+    public static MultiComplexResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        MultiComplexResponseBody self = new MultiComplexResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public MultiComplexResponseBody setStringValue(java.io.InputStream stringValue) {
+        this.stringValue = stringValue;
+        return this;
+    }
+    public java.io.InputStream getStringValue() {
+        return this.stringValue;
+    }
+
+    public MultiComplexResponseBody setSecondOutput(MultiComplexResponseBodySecondOutput secondOutput) {
+        this.secondOutput = secondOutput;
+        return this;
+    }
+    public MultiComplexResponseBodySecondOutput getSecondOutput() {
+        return this.secondOutput;
+    }
+
+    public MultiComplexResponseBody setIntValue(Integer intValue) {
+        this.intValue = intValue;
+        return this;
+    }
+    public Integer getIntValue() {
+        return this.intValue;
+    }
+
+    public MultiComplexResponseBody setBooleanValue(Boolean booleanValue) {
+        this.booleanValue = booleanValue;
+        return this;
+    }
+    public Boolean getBooleanValue() {
+        return this.booleanValue;
+    }
+
+    public MultiComplexResponseBody setIntegerValue(Integer integerValue) {
+        this.integerValue = integerValue;
+        return this;
+    }
+    public Integer getIntegerValue() {
+        return this.integerValue;
+    }
+
+    public MultiComplexResponseBody setLongValue(Long longValue) {
+        this.longValue = longValue;
+        return this;
+    }
+    public Long getLongValue() {
+        return this.longValue;
+    }
+
+    public static class MultiComplexResponseBodySecondOutputThirdList extends TeaModel {
+        @NameInMap("thirdInt")
+        public Integer thirdInt;
+
+        @NameInMap("thirdString")
+        public java.io.InputStream thirdString;
+
+        public static MultiComplexResponseBodySecondOutputThirdList build(java.util.Map<String, ?> map) throws Exception {
+            MultiComplexResponseBodySecondOutputThirdList self = new MultiComplexResponseBodySecondOutputThirdList();
+            return TeaModel.build(map, self);
+        }
+
+        public MultiComplexResponseBodySecondOutputThirdList setThirdInt(Integer thirdInt) {
+            this.thirdInt = thirdInt;
+            return this;
+        }
+        public Integer getThirdInt() {
+            return this.thirdInt;
+        }
+
+        public MultiComplexResponseBodySecondOutputThirdList setThirdString(java.io.InputStream thirdString) {
+            this.thirdString = thirdString;
+            return this;
+        }
+        public java.io.InputStream getThirdString() {
+            return this.thirdString;
+        }
+
+    }
+
+    public static class MultiComplexResponseBodySecondOutput extends TeaModel {
+        @NameInMap("secondString")
+        public java.io.InputStream secondString;
+
+        @NameInMap("thirdList")
+        public java.util.List<MultiComplexResponseBodySecondOutputThirdList> thirdList;
+
+        @NameInMap("secondInt")
+        public Integer secondInt;
+
+        public static MultiComplexResponseBodySecondOutput build(java.util.Map<String, ?> map) throws Exception {
+            MultiComplexResponseBodySecondOutput self = new MultiComplexResponseBodySecondOutput();
+            return TeaModel.build(map, self);
+        }
+
+        public MultiComplexResponseBodySecondOutput setSecondString(java.io.InputStream secondString) {
+            this.secondString = secondString;
+            return this;
+        }
+        public java.io.InputStream getSecondString() {
+            return this.secondString;
+        }
+
+        public MultiComplexResponseBodySecondOutput setThirdList(java.util.List<MultiComplexResponseBodySecondOutputThirdList> thirdList) {
+            this.thirdList = thirdList;
+            return this;
+        }
+        public java.util.List<MultiComplexResponseBodySecondOutputThirdList> getThirdList() {
+            return this.thirdList;
+        }
+
+        public MultiComplexResponseBodySecondOutput setSecondInt(Integer secondInt) {
+            this.secondInt = secondInt;
+            return this;
+        }
+        public Integer getSecondInt() {
+            return this.secondInt;
+        }
+
+    }
+
+}
