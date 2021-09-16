bc73d8a6b326a69e2051215c2a80d369e84046ee
sdk-team <sdk-team@alibabacloud.com>
2021-09-16 06:11:43 +0000

    Add externalUrl for SubmitMediaCensorJob.

--- mts-20140618/src/main/java/com/aliyun/mts20140618/models/SubmitOssFileCopyJobRequest.java
+++ mts-20140618/src/main/java/com/aliyun/mts20140618/models/SubmitOssFileCopyJobRequest.java
@@ -80,6 +80,21 @@ public class SubmitOssFileCopyJobRequest extends TeaModel {
         @NameInMap("ObjectRoleName")
         public String objectRoleName;
 
+        @NameInMap("S3Provider")
+        public String s3Provider;
+
+        @NameInMap("S3Endpoint")
+        public String s3Endpoint;
+
+        @NameInMap("S3AccessKey")
+        public String s3AccessKey;
+
+        @NameInMap("S3SecretKey")
+        public String s3SecretKey;
+
+        @NameInMap("S3SessionToken")
+        public String s3SessionToken;
+
         public static SubmitOssFileCopyJobRequestSourceStorage build(java.util.Map<String, ?> map) throws Exception {
             SubmitOssFileCopyJobRequestSourceStorage self = new SubmitOssFileCopyJobRequestSourceStorage();
             return TeaModel.build(map, self);
@@ -123,6 +138,46 @@ public class SubmitOssFileCopyJobRequest extends TeaModel {
         }
         public String getObjectRoleName() {
             return this.objectRoleName;
+        }
+
+        public SubmitOssFileCopyJobRequestSourceStorage setS3Provider(String s3Provider) {
+            this.s3Provider = s3Provider;
+            return this;
+        }
+        public String getS3Provider() {
+            return this.s3Provider;
+        }
+
+        public SubmitOssFileCopyJobRequestSourceStorage setS3Endpoint(String s3Endpoint) {
+            this.s3Endpoint = s3Endpoint;
+            return this;
+        }
+        public String getS3Endpoint() {
+            return this.s3Endpoint;
+        }
+
+        public SubmitOssFileCopyJobRequestSourceStorage setS3AccessKey(String s3AccessKey) {
+            this.s3AccessKey = s3AccessKey;
+            return this;
+        }
+        public String getS3AccessKey() {
+            return this.s3AccessKey;
+        }
+
+        public SubmitOssFileCopyJobRequestSourceStorage setS3SecretKey(String s3SecretKey) {
+            this.s3SecretKey = s3SecretKey;
+            return this;
+        }
+        public String getS3SecretKey() {
+            return this.s3SecretKey;
+        }
+
+        public SubmitOssFileCopyJobRequestSourceStorage setS3SessionToken(String s3SessionToken) {
+            this.s3SessionToken = s3SessionToken;
+            return this;
+        }
+        public String getS3SessionToken() {
+            return this.s3SessionToken;
         }
 
     }
