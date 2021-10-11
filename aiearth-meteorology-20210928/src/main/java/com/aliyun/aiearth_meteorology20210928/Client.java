926fd88e34a8495ef356758ef49a3d6229acbecc
sdk-team <sdk-team@alibabacloud.com>
2021-10-11 13:11:13 +0000

    Support mete grid data query.

---
+++ aiearth-meteorology-20210928/src/main/java/com/aliyun/aiearth_meteorology20210928/Client.java
@@ -0,0 +1,79 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.aiearth_meteorology20210928;
+
+import com.aliyun.tea.*;
+import com.aliyun.aiearth_meteorology20210928.models.*;
+import com.aliyun.teautil.*;
+import com.aliyun.teautil.models.*;
+import com.aliyun.teaopenapi.*;
+import com.aliyun.teaopenapi.models.*;
+import com.aliyun.openapiutil.*;
+import com.aliyun.endpointutil.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("aiearth-meteorology", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public GridQueryResponse gridQuery(String dataType, GridQueryRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.gridQueryWithOptions(dataType, request, headers, runtime);
+    }
+
+    public GridQueryResponse gridQueryWithOptions(String dataType, GridQueryRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        dataType = com.aliyun.openapiutil.Client.getEncodeParam(dataType);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.product)) {
+            query.put("product", request.product);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.pageNo)) {
+            query.put("pageNo", request.pageNo);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.pageSize)) {
+            query.put("pageSize", request.pageSize);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.element)) {
+            query.put("element", request.element);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.forecastTimestamp)) {
+            query.put("forecastTimestamp", request.forecastTimestamp);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.longitude)) {
+            query.put("longitude", request.longitude);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.latitude)) {
+            query.put("latitude", request.latitude);
+        }
+
+        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        return TeaModel.toModel(this.doROARequest("GridQuery", "2021-09-28", "HTTPS", "GET", "AK", "/grid/" + dataType + "/v1", "json", req, runtime), new GridQueryResponse());
+    }
+}
---
+++ aiearth-meteorology-20210928/src/main/java/com/aliyun/aiearth_meteorology20210928/models/GridQueryRequest.java
@@ -0,0 +1,96 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.aiearth_meteorology20210928.models;
+
+import com.aliyun.tea.*;
+
+public class GridQueryRequest extends TeaModel {
+    // 产品
+    @NameInMap("product")
+    public String product;
+
+    // 分页编号
+    @NameInMap("pageNo")
+    public Integer pageNo;
+
+    // 分页大小
+    @NameInMap("pageSize")
+    public Integer pageSize;
+
+    // 要素
+    @NameInMap("element")
+    public String element;
+
+    // 预报时间
+    @NameInMap("forecastTimestamp")
+    public String forecastTimestamp;
+
+    // 经度
+    @NameInMap("longitude")
+    public Double longitude;
+
+    // 纬度
+    @NameInMap("latitude")
+    public Double latitude;
+
+    public static GridQueryRequest build(java.util.Map<String, ?> map) throws Exception {
+        GridQueryRequest self = new GridQueryRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public GridQueryRequest setProduct(String product) {
+        this.product = product;
+        return this;
+    }
+    public String getProduct() {
+        return this.product;
+    }
+
+    public GridQueryRequest setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+        return this;
+    }
+    public Integer getPageNo() {
+        return this.pageNo;
+    }
+
+    public GridQueryRequest setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+        return this;
+    }
+    public Integer getPageSize() {
+        return this.pageSize;
+    }
+
+    public GridQueryRequest setElement(String element) {
+        this.element = element;
+        return this;
+    }
+    public String getElement() {
+        return this.element;
+    }
+
+    public GridQueryRequest setForecastTimestamp(String forecastTimestamp) {
+        this.forecastTimestamp = forecastTimestamp;
+        return this;
+    }
+    public String getForecastTimestamp() {
+        return this.forecastTimestamp;
+    }
+
+    public GridQueryRequest setLongitude(Double longitude) {
+        this.longitude = longitude;
+        return this;
+    }
+    public Double getLongitude() {
+        return this.longitude;
+    }
+
+    public GridQueryRequest setLatitude(Double latitude) {
+        this.latitude = latitude;
+        return this;
+    }
+    public Double getLatitude() {
+        return this.latitude;
+    }
+
+}
