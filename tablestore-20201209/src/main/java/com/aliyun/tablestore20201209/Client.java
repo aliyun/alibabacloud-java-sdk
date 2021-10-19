9de07665a1fab22d0f610223130855685198a8d1
sdk-team <sdk-team@alibabacloud.com>
2021-10-19 13:03:51 +0000

    Tablestore init.

--- tablestore-20201209/src/main/java/com/aliyun/tablestore20201209/Client.java
+++ tablestore-20201209/src/main/java/com/aliyun/tablestore20201209/Client.java
@@ -52,17 +52,24 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return TeaModel.toModel(this.doROARequest("GetInstance", "2020-12-09", "HTTPS", "GET", "AK", "/v2/openapi/getinstance", "json", req, runtime), new GetInstanceResponse());
     }
 
-    public ListTagResourcesResponse listTagResources() throws Exception {
+    public DescribeRegionsResponse describeRegions(DescribeRegionsRequest request) throws Exception {
         RuntimeOptions runtime = new RuntimeOptions();
         java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.listTagResourcesWithOptions(headers, runtime);
+        return this.describeRegionsWithOptions(request, headers, runtime);
     }
 
-    public ListTagResourcesResponse listTagResourcesWithOptions(java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+    public DescribeRegionsResponse describeRegionsWithOptions(DescribeRegionsRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.clientToken)) {
+            query.put("ClientToken", request.clientToken);
+        }
+
         OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("headers", headers)
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
         ));
-        return TeaModel.toModel(this.doROARequest("ListTagResources", "2020-12-09", "HTTPS", "GET", "AK", "/v2/openapi/listtagresources", "json", req, runtime), new ListTagResourcesResponse());
+        return TeaModel.toModel(this.doROARequest("DescribeRegions", "2020-12-09", "HTTPS", "GET", "AK", "/region/DescribeRegions", "json", req, runtime), new DescribeRegionsResponse());
     }
 
     public ListInstancesResponse listInstances(ListInstancesRequest request) throws Exception {
@@ -123,105 +130,5 @@ public class Client extends com.aliyun.teaopenapi.Client {
             new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
         ));
         return TeaModel.toModel(this.doROARequest("UpdateInstance", "2020-12-09", "HTTPS", "POST", "AK", "/v2/openapi/updateinstance", "json", req, runtime), new UpdateInstanceResponse());
-    }
-
-    public DescribeRegionsResponse describeRegions(DescribeRegionsRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.describeRegionsWithOptions(request, headers, runtime);
-    }
-
-    public DescribeRegionsResponse describeRegionsWithOptions(DescribeRegionsRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> query = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.clientToken)) {
-            query.put("ClientToken", request.clientToken);
-        }
-
-        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("headers", headers),
-            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
-        ));
-        return TeaModel.toModel(this.doROARequest("DescribeRegions", "2020-12-09", "HTTPS", "GET", "AK", "/region/DescribeRegions", "json", req, runtime), new DescribeRegionsResponse());
-    }
-
-    public CreateInstanceResponse createInstance(CreateInstanceRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.createInstanceWithOptions(request, headers, runtime);
-    }
-
-    public CreateInstanceResponse createInstanceWithOptions(CreateInstanceRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> body = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.instanceName)) {
-            body.put("InstanceName", request.instanceName);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.clusterType)) {
-            body.put("ClusterType", request.clusterType);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.instanceDescription)) {
-            body.put("InstanceDescription", request.instanceDescription);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.resourceGroupId)) {
-            body.put("ResourceGroupId", request.resourceGroupId);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.tags)) {
-            body.put("Tags", request.tags);
-        }
-
-        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("headers", headers),
-            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
-        ));
-        return TeaModel.toModel(this.doROARequest("CreateInstance", "2020-12-09", "HTTPS", "POST", "AK", "/v2/openapi/createinstance", "json", req, runtime), new CreateInstanceResponse());
-    }
-
-    public ChangeResourceGroupResponse changeResourceGroup(ChangeResourceGroupRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.changeResourceGroupWithOptions(request, headers, runtime);
-    }
-
-    public ChangeResourceGroupResponse changeResourceGroupWithOptions(ChangeResourceGroupRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> body = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.resourceId)) {
-            body.put("ResourceId", request.resourceId);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.newResourceGroupId)) {
-            body.put("NewResourceGroupId", request.newResourceGroupId);
-        }
-
-        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("headers", headers),
-            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
-        ));
-        return TeaModel.toModel(this.doROARequest("ChangeResourceGroup", "2020-12-09", "HTTPS", "POST", "AK", "/v2/openapi/changeresourcegroup", "json", req, runtime), new ChangeResourceGroupResponse());
-    }
-
-    public DeleteInstanceResponse deleteInstance(DeleteInstanceRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.deleteInstanceWithOptions(request, headers, runtime);
-    }
-
-    public DeleteInstanceResponse deleteInstanceWithOptions(DeleteInstanceRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> body = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.instanceName)) {
-            body.put("InstanceName", request.instanceName);
-        }
-
-        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("headers", headers),
-            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
-        ));
-        return TeaModel.toModel(this.doROARequest("DeleteInstance", "2020-12-09", "HTTPS", "POST", "AK", "/v2/openapi/deleteinstance", "json", req, runtime), new DeleteInstanceResponse());
     }
 }
