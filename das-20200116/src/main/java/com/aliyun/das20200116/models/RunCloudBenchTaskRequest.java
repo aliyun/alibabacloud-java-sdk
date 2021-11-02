882a3dbcb82045b0ff7033662e914066b7f4cbca
sdk-team <sdk-team@alibabacloud.com>
2021-11-02 13:30:31 +0000

    Fixed useless variables.

--- das-20200116/src/main/java/com/aliyun/das20200116/models/RunCloudBenchTaskRequest.java
+++ das-20200116/src/main/java/com/aliyun/das20200116/models/RunCloudBenchTaskRequest.java
@@ -4,23 +4,12 @@ package com.aliyun.das20200116.models;
 import com.aliyun.tea.*;
 
 public class RunCloudBenchTaskRequest extends TeaModel {
-    @NameInMap("ConsoleContext")
-    public String consoleContext;
-
     @NameInMap("TaskId")
     public String taskId;
 
     public static RunCloudBenchTaskRequest build(java.util.Map<String, ?> map) throws Exception {
         RunCloudBenchTaskRequest self = new RunCloudBenchTaskRequest();
         return TeaModel.build(map, self);
-    }
-
-    public RunCloudBenchTaskRequest setConsoleContext(String consoleContext) {
-        this.consoleContext = consoleContext;
-        return this;
-    }
-    public String getConsoleContext() {
-        return this.consoleContext;
     }
 
     public RunCloudBenchTaskRequest setTaskId(String taskId) {
