2cb6572b9a2353b723c9fa90c2ea12512fb8219e
sdk-team <sdk-team@alibabacloud.com>
2021-11-03 07:31:19 +0000

    Fix http method.

---
+++ yuqing-20210126/src/main/java/com/aliyun/yuqing20210126/models/UpdateProjectRequest.java
@@ -0,0 +1,84 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.yuqing20210126.models;
+
+import com.aliyun.tea.*;
+
+public class UpdateProjectRequest extends TeaModel {
+    // 舆情项目对象
+    @NameInMap("project")
+    public Project project;
+
+    // 项目id
+    @NameInMap("projectId")
+    public Long projectId;
+
+    // 请求id
+    @NameInMap("requestId")
+    public String requestId;
+
+    // 舆情团队HashId
+    @NameInMap("teamHashId")
+    public String teamHashId;
+
+    // 修改者uid
+    @NameInMap("updateUserId")
+    public String updateUserId;
+
+    // 修改者名称
+    @NameInMap("updateUserName")
+    public String updateUserName;
+
+    public static UpdateProjectRequest build(java.util.Map<String, ?> map) throws Exception {
+        UpdateProjectRequest self = new UpdateProjectRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public UpdateProjectRequest setProject(Project project) {
+        this.project = project;
+        return this;
+    }
+    public Project getProject() {
+        return this.project;
+    }
+
+    public UpdateProjectRequest setProjectId(Long projectId) {
+        this.projectId = projectId;
+        return this;
+    }
+    public Long getProjectId() {
+        return this.projectId;
+    }
+
+    public UpdateProjectRequest setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public UpdateProjectRequest setTeamHashId(String teamHashId) {
+        this.teamHashId = teamHashId;
+        return this;
+    }
+    public String getTeamHashId() {
+        return this.teamHashId;
+    }
+
+    public UpdateProjectRequest setUpdateUserId(String updateUserId) {
+        this.updateUserId = updateUserId;
+        return this;
+    }
+    public String getUpdateUserId() {
+        return this.updateUserId;
+    }
+
+    public UpdateProjectRequest setUpdateUserName(String updateUserName) {
+        this.updateUserName = updateUserName;
+        return this;
+    }
+    public String getUpdateUserName() {
+        return this.updateUserName;
+    }
+
+}
