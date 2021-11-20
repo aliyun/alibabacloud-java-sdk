64415252efd4852c0a45ff8d8be74a48eb29c184
sdk-team <sdk-team@alibabacloud.com>
2021-11-20 06:53:13 +0000

    Test.

---
+++ nbf-vpc-cloud-20211115_131123360/src/main/java/com/aliyun/nbf_vpc_cloud20211115_131123360/models/YxTestApiResponseBody.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nbf_vpc_cloud20211115_131123360.models;
+
+import com.aliyun.tea.*;
+
+public class YxTestApiResponseBody extends TeaModel {
+    @NameInMap("errorCode")
+    public String errorCode;
+
+    @NameInMap("errorMsg")
+    public String errorMsg;
+
+    @NameInMap("result")
+    public String result;
+
+    @NameInMap("success")
+    public Boolean success;
+
+    public static YxTestApiResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        YxTestApiResponseBody self = new YxTestApiResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public YxTestApiResponseBody setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+        return this;
+    }
+    public String getErrorCode() {
+        return this.errorCode;
+    }
+
+    public YxTestApiResponseBody setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+        return this;
+    }
+    public String getErrorMsg() {
+        return this.errorMsg;
+    }
+
+    public YxTestApiResponseBody setResult(String result) {
+        this.result = result;
+        return this;
+    }
+    public String getResult() {
+        return this.result;
+    }
+
+    public YxTestApiResponseBody setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+}
