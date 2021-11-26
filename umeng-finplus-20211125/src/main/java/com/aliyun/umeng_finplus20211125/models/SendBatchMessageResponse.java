4b73d2637410c70e9fa283bbce9ee56dd0951c58
sdk-team <sdk-team@alibabacloud.com>
2021-11-26 06:00:45 +0000

    Test.

---
+++ umeng-finplus-20211125/src/main/java/com/aliyun/umeng_finplus20211125/models/SendBatchMessageResponse.java
@@ -0,0 +1,36 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.umeng_finplus20211125.models;
+
+import com.aliyun.tea.*;
+
+public class SendBatchMessageResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public SendBatchMessageResponseBody body;
+
+    public static SendBatchMessageResponse build(java.util.Map<String, ?> map) throws Exception {
+        SendBatchMessageResponse self = new SendBatchMessageResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public SendBatchMessageResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public SendBatchMessageResponse setBody(SendBatchMessageResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public SendBatchMessageResponseBody getBody() {
+        return this.body;
+    }
+
+}
