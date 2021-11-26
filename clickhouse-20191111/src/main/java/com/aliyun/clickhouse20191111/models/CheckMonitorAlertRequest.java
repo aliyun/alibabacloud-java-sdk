c94923f8cf1708c9e3a491ed1b2151ac52919e11
sdk-team <sdk-team@alibabacloud.com>
2021-11-26 10:20:15 +0000

    For test.

---
+++ clickhouse-20191111/src/main/java/com/aliyun/clickhouse20191111/models/CheckMonitorAlertRequest.java
@@ -0,0 +1,78 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.clickhouse20191111.models;
+
+import com.aliyun.tea.*;
+
+public class CheckMonitorAlertRequest extends TeaModel {
+    @NameInMap("DBClusterId")
+    public String DBClusterId;
+
+    @NameInMap("OwnerAccount")
+    public String ownerAccount;
+
+    @NameInMap("OwnerId")
+    public Long ownerId;
+
+    @NameInMap("RegionId")
+    public String regionId;
+
+    @NameInMap("ResourceOwnerAccount")
+    public String resourceOwnerAccount;
+
+    @NameInMap("ResourceOwnerId")
+    public Long resourceOwnerId;
+
+    public static CheckMonitorAlertRequest build(java.util.Map<String, ?> map) throws Exception {
+        CheckMonitorAlertRequest self = new CheckMonitorAlertRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public CheckMonitorAlertRequest setDBClusterId(String DBClusterId) {
+        this.DBClusterId = DBClusterId;
+        return this;
+    }
+    public String getDBClusterId() {
+        return this.DBClusterId;
+    }
+
+    public CheckMonitorAlertRequest setOwnerAccount(String ownerAccount) {
+        this.ownerAccount = ownerAccount;
+        return this;
+    }
+    public String getOwnerAccount() {
+        return this.ownerAccount;
+    }
+
+    public CheckMonitorAlertRequest setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+        return this;
+    }
+    public Long getOwnerId() {
+        return this.ownerId;
+    }
+
+    public CheckMonitorAlertRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public CheckMonitorAlertRequest setResourceOwnerAccount(String resourceOwnerAccount) {
+        this.resourceOwnerAccount = resourceOwnerAccount;
+        return this;
+    }
+    public String getResourceOwnerAccount() {
+        return this.resourceOwnerAccount;
+    }
+
+    public CheckMonitorAlertRequest setResourceOwnerId(Long resourceOwnerId) {
+        this.resourceOwnerId = resourceOwnerId;
+        return this;
+    }
+    public Long getResourceOwnerId() {
+        return this.resourceOwnerId;
+    }
+
+}
---
+++ clickhouse-20191111/src/main/java/com/aliyun/clickhouse20191111/models/CreateMonitorDataReportResponseBody.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.clickhouse20191111.models;
+
+import com.aliyun.tea.*;
+
+public class CreateMonitorDataReportResponseBody extends TeaModel {
+    @NameInMap("RequestId")
+    public String requestId;
+
+    public static CreateMonitorDataReportResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        CreateMonitorDataReportResponseBody self = new CreateMonitorDataReportResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateMonitorDataReportResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+}
