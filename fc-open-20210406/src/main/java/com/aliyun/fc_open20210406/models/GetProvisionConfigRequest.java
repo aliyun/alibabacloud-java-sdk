6624bf2a6f035b06d83e2b433b05ec29c61a6319
sdk-team <sdk-team@alibabacloud.com>
2021-12-03 10:15:32 +0000

    Update fc pop sdk.

--- fc-open-20210406/src/main/java/com/aliyun/fc_open20210406/models/GetProvisionConfigRequest.java
+++ fc-open-20210406/src/main/java/com/aliyun/fc_open20210406/models/GetProvisionConfigRequest.java
@@ -4,10 +4,6 @@ package com.aliyun.fc_open20210406.models;
 import com.aliyun.tea.*;
 
 public class GetProvisionConfigRequest extends TeaModel {
-    // 预留的目标资源个数
-    @NameInMap("target")
-    public Long target;
-
     // 别名名称
     @NameInMap("qualifier")
     public String qualifier;
@@ -15,14 +11,6 @@ public class GetProvisionConfigRequest extends TeaModel {
     public static GetProvisionConfigRequest build(java.util.Map<String, ?> map) throws Exception {
         GetProvisionConfigRequest self = new GetProvisionConfigRequest();
         return TeaModel.build(map, self);
-    }
-
-    public GetProvisionConfigRequest setTarget(Long target) {
-        this.target = target;
-        return this;
-    }
-    public Long getTarget() {
-        return this.target;
     }
 
     public GetProvisionConfigRequest setQualifier(String qualifier) {
