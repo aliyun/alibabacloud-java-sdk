cc182e92ed99a60a3bf21297d3ea8b9fbeee7589
sdk-team <sdk-team@alibabacloud.com>
2021-12-08 02:10:17 +0000

    Init dbfs ops.

--- dbfs-ops-20210721/src/main/java/com/aliyun/dbfs_ops20210721/models/UploadFileResponseBody.java
+++ dbfs-ops-20210721/src/main/java/com/aliyun/dbfs_ops20210721/models/UploadFileResponseBody.java
@@ -8,6 +8,9 @@ public class UploadFileResponseBody extends TeaModel {
     @NameInMap("RequestId")
     public String requestId;
 
+    @NameInMap("result")
+    public java.io.InputStream result;
+
     public static UploadFileResponseBody build(java.util.Map<String, ?> map) throws Exception {
         UploadFileResponseBody self = new UploadFileResponseBody();
         return TeaModel.build(map, self);
@@ -19,6 +22,14 @@ public class UploadFileResponseBody extends TeaModel {
     }
     public String getRequestId() {
         return this.requestId;
+    }
+
+    public UploadFileResponseBody setResult(java.io.InputStream result) {
+        this.result = result;
+        return this;
+    }
+    public java.io.InputStream getResult() {
+        return this.result;
     }
 
 }
