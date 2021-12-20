1a8f4e6c6c260a2c5e35917e1baf575e223e23f1
sdk-team <sdk-team@alibabacloud.com>
2021-12-20 06:05:05 +0000

    Add runningData reponse field, startTimeTS, hordFor.

--- pts-20201020/src/main/java/com/aliyun/pts20201020/models/SaveEnvRequest.java
+++ pts-20201020/src/main/java/com/aliyun/pts20201020/models/SaveEnvRequest.java
@@ -110,10 +110,6 @@ public class SaveEnvRequest extends TeaModel {
         @NameInMap("Files")
         public java.util.List<SaveEnvRequestEnvFiles> files;
 
-        // jmeter插件的环境标签
-        @NameInMap("JmeterPluginLabel")
-        public String jmeterPluginLabel;
-
         // jmeter属性
         @NameInMap("Properties")
         public java.util.List<SaveEnvRequestEnvProperties> properties;
@@ -145,14 +141,6 @@ public class SaveEnvRequest extends TeaModel {
         }
         public java.util.List<SaveEnvRequestEnvFiles> getFiles() {
             return this.files;
-        }
-
-        public SaveEnvRequestEnv setJmeterPluginLabel(String jmeterPluginLabel) {
-            this.jmeterPluginLabel = jmeterPluginLabel;
-            return this;
-        }
-        public String getJmeterPluginLabel() {
-            return this.jmeterPluginLabel;
         }
 
         public SaveEnvRequestEnv setProperties(java.util.List<SaveEnvRequestEnvProperties> properties) {
