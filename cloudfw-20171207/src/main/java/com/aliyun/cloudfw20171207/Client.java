dc1e4b1b7589c1a8931878a5fe37d771811504cd
sdk-team <sdk-team@alibabacloud.com>
2022-01-20 05:12:57 +0000

    Update NAT Firewall APIs.

--- cloudfw-20171207/src/main/java/com/aliyun/cloudfw20171207/Client.java
+++ cloudfw-20171207/src/main/java/com/aliyun/cloudfw20171207/Client.java
@@ -148,10 +148,6 @@ public class Client extends com.aliyun.teaopenapi.Client {
             query.put("Lang", request.lang);
         }
 
-        if (!com.aliyun.teautil.Common.isUnset(request.messageType)) {
-            query.put("MessageType", request.messageType);
-        }
-
         if (!com.aliyun.teautil.Common.isUnset(request.newOrder)) {
             query.put("NewOrder", request.newOrder);
         }
