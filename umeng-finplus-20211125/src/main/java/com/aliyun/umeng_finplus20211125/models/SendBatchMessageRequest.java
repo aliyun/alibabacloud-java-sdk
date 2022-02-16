1bc002dd4761fb541eac8a403eaad79eca4a2c18
sdk-team <sdk-team@alibabacloud.com>
2022-02-16 03:17:11 +0000

    Test.

--- umeng-finplus-20211125/src/main/java/com/aliyun/umeng_finplus20211125/models/SendBatchMessageRequest.java
+++ umeng-finplus-20211125/src/main/java/com/aliyun/umeng_finplus20211125/models/SendBatchMessageRequest.java
@@ -4,11 +4,23 @@ package com.aliyun.umeng_finplus20211125.models;
 import com.aliyun.tea.*;
 
 public class SendBatchMessageRequest extends TeaModel {
+    @NameInMap("BatchFlag")
+    public String batchFlag;
+
+    @NameInMap("ExtendInfo")
+    public String extendInfo;
+
+    @NameInMap("IdType")
+    public String idType;
+
     @NameInMap("PhoneNumberJson")
     public String phoneNumberJson;
 
     @NameInMap("SignNameJson")
     public String signNameJson;
+
+    @NameInMap("SpecificChannel")
+    public String specificChannel;
 
     @NameInMap("TemplateCode")
     public String templateCode;
@@ -19,6 +31,30 @@ public class SendBatchMessageRequest extends TeaModel {
     public static SendBatchMessageRequest build(java.util.Map<String, ?> map) throws Exception {
         SendBatchMessageRequest self = new SendBatchMessageRequest();
         return TeaModel.build(map, self);
+    }
+
+    public SendBatchMessageRequest setBatchFlag(String batchFlag) {
+        this.batchFlag = batchFlag;
+        return this;
+    }
+    public String getBatchFlag() {
+        return this.batchFlag;
+    }
+
+    public SendBatchMessageRequest setExtendInfo(String extendInfo) {
+        this.extendInfo = extendInfo;
+        return this;
+    }
+    public String getExtendInfo() {
+        return this.extendInfo;
+    }
+
+    public SendBatchMessageRequest setIdType(String idType) {
+        this.idType = idType;
+        return this;
+    }
+    public String getIdType() {
+        return this.idType;
     }
 
     public SendBatchMessageRequest setPhoneNumberJson(String phoneNumberJson) {
@@ -35,6 +71,14 @@ public class SendBatchMessageRequest extends TeaModel {
     }
     public String getSignNameJson() {
         return this.signNameJson;
+    }
+
+    public SendBatchMessageRequest setSpecificChannel(String specificChannel) {
+        this.specificChannel = specificChannel;
+        return this;
+    }
+    public String getSpecificChannel() {
+        return this.specificChannel;
     }
 
     public SendBatchMessageRequest setTemplateCode(String templateCode) {
