c6e3acf8387d96ff9ff1c69108ac8cc90ef6d2d8
sdk-team <sdk-team@alibabacloud.com>
2022-03-31 13:32:28 +0000

    Update user config api.

---
+++ cdn-20180510/src/main/java/com/aliyun/cdn20180510/models/DescribeIpStatusResponse.java
@@ -0,0 +1,36 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cdn20180510.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeIpStatusResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public DescribeIpStatusResponseBody body;
+
+    public static DescribeIpStatusResponse build(java.util.Map<String, ?> map) throws Exception {
+        DescribeIpStatusResponse self = new DescribeIpStatusResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeIpStatusResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public DescribeIpStatusResponse setBody(DescribeIpStatusResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public DescribeIpStatusResponseBody getBody() {
+        return this.body;
+    }
+
+}
