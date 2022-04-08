7051f1d18ed789cd1b3ec5f6ecfbacd152f38ada
sdk-team <sdk-team@alibabacloud.com>
2022-04-08 13:42:59 +0000

    Release FeedbackSession.

--- imageprocess-20200320/src/main/java/com/aliyun/imageprocess20200320/models/FeedbackSessionResponseBody.java
+++ imageprocess-20200320/src/main/java/com/aliyun/imageprocess20200320/models/FeedbackSessionResponseBody.java
@@ -4,9 +4,6 @@ package com.aliyun.imageprocess20200320.models;
 import com.aliyun.tea.*;
 
 public class FeedbackSessionResponseBody extends TeaModel {
-    @NameInMap("Data")
-    public FeedbackSessionResponseBodyData data;
-
     // Id of the request
     @NameInMap("RequestId")
     public String requestId;
@@ -16,39 +13,12 @@ public class FeedbackSessionResponseBody extends TeaModel {
         return TeaModel.build(map, self);
     }
 
-    public FeedbackSessionResponseBody setData(FeedbackSessionResponseBodyData data) {
-        this.data = data;
-        return this;
-    }
-    public FeedbackSessionResponseBodyData getData() {
-        return this.data;
-    }
-
     public FeedbackSessionResponseBody setRequestId(String requestId) {
         this.requestId = requestId;
         return this;
     }
     public String getRequestId() {
         return this.requestId;
-    }
-
-    public static class FeedbackSessionResponseBodyData extends TeaModel {
-        @NameInMap("Data")
-        public String data;
-
-        public static FeedbackSessionResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
-            FeedbackSessionResponseBodyData self = new FeedbackSessionResponseBodyData();
-            return TeaModel.build(map, self);
-        }
-
-        public FeedbackSessionResponseBodyData setData(String data) {
-            this.data = data;
-            return this;
-        }
-        public String getData() {
-            return this.data;
-        }
-
     }
 
 }
