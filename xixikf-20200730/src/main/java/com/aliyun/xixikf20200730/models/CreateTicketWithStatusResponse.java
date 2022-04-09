26f933f2408b18aafbaa8d4f963773b9d4a8a24f
sdk-team <sdk-team@alibabacloud.com>
2022-04-09 13:43:50 +0000

    Init.

---
+++ xixikf-20200730/src/main/java/com/aliyun/xixikf20200730/models/CreateTicketWithStatusResponse.java
@@ -0,0 +1,36 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.xixikf20200730.models;
+
+import com.aliyun.tea.*;
+
+public class CreateTicketWithStatusResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public CreateTicketWithStatusResponseBody body;
+
+    public static CreateTicketWithStatusResponse build(java.util.Map<String, ?> map) throws Exception {
+        CreateTicketWithStatusResponse self = new CreateTicketWithStatusResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateTicketWithStatusResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public CreateTicketWithStatusResponse setBody(CreateTicketWithStatusResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public CreateTicketWithStatusResponseBody getBody() {
+        return this.body;
+    }
+
+}
--- xixikf-20200730/src/main/java/com/aliyun/xixikf20200730/models/GetPostPolicyRequest.java
+++ xixikf-20200730/src/main/java/com/aliyun/xixikf20200730/models/GetPostPolicyRequest.java
@@ -4,8 +4,11 @@ package com.aliyun.xixikf20200730.models;
 import com.aliyun.tea.*;
 
 public class GetPostPolicyRequest extends TeaModel {
-    @NameInMap("bizCode")
+    @NameInMap("BizCode")
     public String bizCode;
+
+    @NameInMap("InstanceId")
+    public String instanceId;
 
     public static GetPostPolicyRequest build(java.util.Map<String, ?> map) throws Exception {
         GetPostPolicyRequest self = new GetPostPolicyRequest();
@@ -18,6 +21,14 @@ public class GetPostPolicyRequest extends TeaModel {
     }
     public String getBizCode() {
         return this.bizCode;
+    }
+
+    public GetPostPolicyRequest setInstanceId(String instanceId) {
+        this.instanceId = instanceId;
+        return this;
+    }
+    public String getInstanceId() {
+        return this.instanceId;
     }
 
 }
