9c3fe0fc39dbc465cf8f9561ba81049900b1f544
sdk-team <sdk-team@alibabacloud.com>
2022-04-21 04:10:55 +0000

    Some APIs are changed to invisiable.

--- dm-20151123/src/main/java/com/aliyun/dm20151123/Client.java
+++ dm-20151123/src/main/java/com/aliyun/dm20151123/Client.java
@@ -699,67 +699,6 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.descDomainWithOptions(request, runtime);
     }
 
-    public GetRegionListResponse getRegionListWithOptions(GetRegionListRequest request, RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> query = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.offset)) {
-            query.put("Offset", request.offset);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.offsetCreateTime)) {
-            query.put("OffsetCreateTime", request.offsetCreateTime);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.offsetCreateTimeDesc)) {
-            query.put("OffsetCreateTimeDesc", request.offsetCreateTimeDesc);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.ownerId)) {
-            query.put("OwnerId", request.ownerId);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.pageNumber)) {
-            query.put("PageNumber", request.pageNumber);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.pageSize)) {
-            query.put("PageSize", request.pageSize);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.resourceOwnerAccount)) {
-            query.put("ResourceOwnerAccount", request.resourceOwnerAccount);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.resourceOwnerId)) {
-            query.put("ResourceOwnerId", request.resourceOwnerId);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.total)) {
-            query.put("Total", request.total);
-        }
-
-        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
-        ));
-        Params params = Params.build(TeaConverter.buildMap(
-            new TeaPair("action", "GetRegionList"),
-            new TeaPair("version", "2015-11-23"),
-            new TeaPair("protocol", "HTTPS"),
-            new TeaPair("pathname", "/"),
-            new TeaPair("method", "POST"),
-            new TeaPair("authType", "AK"),
-            new TeaPair("style", "RPC"),
-            new TeaPair("reqBodyType", "formData"),
-            new TeaPair("bodyType", "json")
-        ));
-        return TeaModel.toModel(this.callApi(params, req, runtime), new GetRegionListResponse());
-    }
-
-    public GetRegionListResponse getRegionList(GetRegionListRequest request) throws Exception {
-        RuntimeOptions runtime = new RuntimeOptions();
-        return this.getRegionListWithOptions(request, runtime);
-    }
-
     public GetTrackListResponse getTrackListWithOptions(GetTrackListRequest request, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
