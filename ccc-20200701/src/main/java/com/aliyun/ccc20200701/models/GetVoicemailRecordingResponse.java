f0670027a198d690af89858a2b3fd26def0a886b
sdk-team <sdk-team@alibabacloud.com>
2022-04-26 09:11:47 +0000

    Supported GetVoicemailRecording.

---
+++ ccc-20200701/src/main/java/com/aliyun/ccc20200701/models/GetVoicemailRecordingResponse.java
@@ -0,0 +1,36 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ccc20200701.models;
+
+import com.aliyun.tea.*;
+
+public class GetVoicemailRecordingResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public GetVoicemailRecordingResponseBody body;
+
+    public static GetVoicemailRecordingResponse build(java.util.Map<String, ?> map) throws Exception {
+        GetVoicemailRecordingResponse self = new GetVoicemailRecordingResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public GetVoicemailRecordingResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public GetVoicemailRecordingResponse setBody(GetVoicemailRecordingResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public GetVoicemailRecordingResponseBody getBody() {
+        return this.body;
+    }
+
+}
