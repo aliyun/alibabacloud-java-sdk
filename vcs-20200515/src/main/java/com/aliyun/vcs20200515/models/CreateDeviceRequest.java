d99858622167a166134f4a3730b0388a336896d4
sdk-team <sdk-team@alibabacloud.com>
2022-04-28 15:10:51 +0000

    The data service provides super password interface.

--- vcs-20200515/src/main/java/com/aliyun/vcs20200515/models/CreateDeviceRequest.java
+++ vcs-20200515/src/main/java/com/aliyun/vcs20200515/models/CreateDeviceRequest.java
@@ -88,9 +88,6 @@ public class CreateDeviceRequest extends TeaModel {
     @NameInMap("SubDeviceIdList")
     public String subDeviceIdList;
 
-    @NameInMap("UpStreamMode")
-    public String upStreamMode;
-
     @NameInMap("Vendor")
     public String vendor;
 
@@ -321,14 +318,6 @@ public class CreateDeviceRequest extends TeaModel {
     }
     public String getSubDeviceIdList() {
         return this.subDeviceIdList;
-    }
-
-    public CreateDeviceRequest setUpStreamMode(String upStreamMode) {
-        this.upStreamMode = upStreamMode;
-        return this;
-    }
-    public String getUpStreamMode() {
-        return this.upStreamMode;
     }
 
     public CreateDeviceRequest setVendor(String vendor) {
