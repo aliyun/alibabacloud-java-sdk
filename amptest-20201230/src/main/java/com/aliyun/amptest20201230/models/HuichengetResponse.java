041f11f3eff3d98e104c88fc367b4bddaf721070
sdk-team <sdk-team@alibabacloud.com>
2022-05-11 09:31:02 +0000

    Fix.

--- amptest-20201230/src/main/java/com/aliyun/amptest20201230/models/HuichengetResponse.java
+++ amptest-20201230/src/main/java/com/aliyun/amptest20201230/models/HuichengetResponse.java
@@ -8,6 +8,10 @@ public class HuichengetResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public HuichengetResponseBody body;
@@ -23,6 +27,14 @@ public class HuichengetResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public HuichengetResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public HuichengetResponse setBody(HuichengetResponseBody body) {
