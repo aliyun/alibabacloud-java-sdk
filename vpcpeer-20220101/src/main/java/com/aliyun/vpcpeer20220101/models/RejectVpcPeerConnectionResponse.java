91356179d91c1bb37e9ecd2e429abcb25f59029f
sdk-team <sdk-team@alibabacloud.com>
2022-05-12 13:36:09 +0000

    VpcPeer publish.

--- vpcpeer-20220101/src/main/java/com/aliyun/vpcpeer20220101/models/RejectVpcPeerConnectionResponse.java
+++ vpcpeer-20220101/src/main/java/com/aliyun/vpcpeer20220101/models/RejectVpcPeerConnectionResponse.java
@@ -8,6 +8,10 @@ public class RejectVpcPeerConnectionResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public RejectVpcPeerConnectionResponseBody body;
@@ -23,6 +27,14 @@ public class RejectVpcPeerConnectionResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public RejectVpcPeerConnectionResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public RejectVpcPeerConnectionResponse setBody(RejectVpcPeerConnectionResponseBody body) {
