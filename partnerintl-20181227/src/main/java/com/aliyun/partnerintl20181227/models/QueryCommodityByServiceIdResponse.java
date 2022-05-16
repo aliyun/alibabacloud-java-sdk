2366bbe32001381587c2066ff13dcd780f85698c
sdk-team <sdk-team@alibabacloud.com>
2022-05-16 03:18:30 +0000

    Support query for compute nest.

--- partnerintl-20181227/src/main/java/com/aliyun/partnerintl20181227/models/QueryCommodityByServiceIdResponse.java
+++ partnerintl-20181227/src/main/java/com/aliyun/partnerintl20181227/models/QueryCommodityByServiceIdResponse.java
@@ -8,6 +8,10 @@ public class QueryCommodityByServiceIdResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public QueryCommodityByServiceIdResponseBody body;
@@ -23,6 +27,14 @@ public class QueryCommodityByServiceIdResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public QueryCommodityByServiceIdResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public QueryCommodityByServiceIdResponse setBody(QueryCommodityByServiceIdResponseBody body) {
--- partnerintl-20181227/src/main/java/com/aliyun/partnerintl20181227/models/QueryCommodityBySupplierUidResponse.java
+++ partnerintl-20181227/src/main/java/com/aliyun/partnerintl20181227/models/QueryCommodityBySupplierUidResponse.java
@@ -8,6 +8,10 @@ public class QueryCommodityBySupplierUidResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public QueryCommodityBySupplierUidResponseBody body;
@@ -23,6 +27,14 @@ public class QueryCommodityBySupplierUidResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public QueryCommodityBySupplierUidResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public QueryCommodityBySupplierUidResponse setBody(QueryCommodityBySupplierUidResponseBody body) {
