ba6386cd2b6dfd90fb6141e2ded59ef8a5233afa
sdk-team <sdk-team@alibabacloud.com>
2022-05-17 09:20:16 +0000

    EduCloud API DescribeLabToken 修改.

--- educloud-20220202/src/main/java/com/aliyun/educloud20220202/models/ListCoursesResponse.java
+++ educloud-20220202/src/main/java/com/aliyun/educloud20220202/models/ListCoursesResponse.java
@@ -8,6 +8,10 @@ public class ListCoursesResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public ListCoursesResponseBody body;
@@ -23,6 +27,14 @@ public class ListCoursesResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public ListCoursesResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public ListCoursesResponse setBody(ListCoursesResponseBody body) {
