1bc0ed47674ce1393efc9c0abc35f04dd26d2f5d
sdk-team <sdk-team@alibabacloud.com>
2022-05-24 07:56:05 +0000

    Supports for 5GConnection .

--- xgippop-20220520/src/main/java/com/aliyun/xgippop20220520/models/GetApplicationResponseBody.java
+++ xgippop-20220520/src/main/java/com/aliyun/xgippop20220520/models/GetApplicationResponseBody.java
@@ -8,9 +8,8 @@ public class GetApplicationResponseBody extends TeaModel {
     @NameInMap("Code")
     public String code;
 
-    // 结果
     @NameInMap("Data")
-    public java.util.List<GetApplicationResponseBodyData> data;
+    public java.util.List<java.util.Map<String, ?>> data;
 
     // 结果描述
     @NameInMap("Message")
@@ -41,11 +40,11 @@ public class GetApplicationResponseBody extends TeaModel {
         return this.code;
     }
 
-    public GetApplicationResponseBody setData(java.util.List<GetApplicationResponseBodyData> data) {
+    public GetApplicationResponseBody setData(java.util.List<java.util.Map<String, ?>> data) {
         this.data = data;
         return this;
     }
-    public java.util.List<GetApplicationResponseBodyData> getData() {
+    public java.util.List<java.util.Map<String, ?>> getData() {
         return this.data;
     }
 
@@ -79,197 +78,6 @@ public class GetApplicationResponseBody extends TeaModel {
     }
     public Boolean getSuccess() {
         return this.success;
-    }
-
-    public static class GetApplicationResponseBodyDataAppingList extends TeaModel {
-        @NameInMap("ExtId")
-        public Long extId;
-
-        // cdn ip
-        @NameInMap("FlowIp")
-        public java.util.List<String> flowIp;
-
-        // cdn 域名信息
-        @NameInMap("FlowUrl")
-        public java.util.List<String> flowUrl;
-
-        // 业务方ip集合
-        @NameInMap("OriginalIpList")
-        public java.util.List<String> originalIpList;
-
-        // 业务方域名集合
-        @NameInMap("OriginalUrlList")
-        public java.util.List<String> originalUrlList;
-
-        public static GetApplicationResponseBodyDataAppingList build(java.util.Map<String, ?> map) throws Exception {
-            GetApplicationResponseBodyDataAppingList self = new GetApplicationResponseBodyDataAppingList();
-            return TeaModel.build(map, self);
-        }
-
-        public GetApplicationResponseBodyDataAppingList setExtId(Long extId) {
-            this.extId = extId;
-            return this;
-        }
-        public Long getExtId() {
-            return this.extId;
-        }
-
-        public GetApplicationResponseBodyDataAppingList setFlowIp(java.util.List<String> flowIp) {
-            this.flowIp = flowIp;
-            return this;
-        }
-        public java.util.List<String> getFlowIp() {
-            return this.flowIp;
-        }
-
-        public GetApplicationResponseBodyDataAppingList setFlowUrl(java.util.List<String> flowUrl) {
-            this.flowUrl = flowUrl;
-            return this;
-        }
-        public java.util.List<String> getFlowUrl() {
-            return this.flowUrl;
-        }
-
-        public GetApplicationResponseBodyDataAppingList setOriginalIpList(java.util.List<String> originalIpList) {
-            this.originalIpList = originalIpList;
-            return this;
-        }
-        public java.util.List<String> getOriginalIpList() {
-            return this.originalIpList;
-        }
-
-        public GetApplicationResponseBodyDataAppingList setOriginalUrlList(java.util.List<String> originalUrlList) {
-            this.originalUrlList = originalUrlList;
-            return this;
-        }
-        public java.util.List<String> getOriginalUrlList() {
-            return this.originalUrlList;
-        }
-
-    }
-
-    public static class GetApplicationResponseBodyData extends TeaModel {
-        // 用户编号
-        @NameInMap("AliUid")
-        public Long aliUid;
-
-        // 应用编号
-        @NameInMap("AppId")
-        public String appId;
-
-        // 应用名称
-        @NameInMap("AppName")
-        public String appName;
-
-        @NameInMap("AppStatus")
-        public String appStatus;
-
-        // //dynamic（动态业务）/static（静态业务）  // 前端传递，参数3
-        @NameInMap("AppTypeList")
-        public String appTypeList;
-
-        @NameInMap("AppingList")
-        public java.util.List<GetApplicationResponseBodyDataAppingList> appingList;
-
-        @NameInMap("EndTime")
-        public String endTime;
-
-        // Database Column Remarks:
-        @NameInMap("ItemCode")
-        public String itemCode;
-
-        // 开通时间
-        @NameInMap("OpenTime")
-        public String openTime;
-
-        @NameInMap("StartTime")
-        public String startTime;
-
-        public static GetApplicationResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
-            GetApplicationResponseBodyData self = new GetApplicationResponseBodyData();
-            return TeaModel.build(map, self);
-        }
-
-        public GetApplicationResponseBodyData setAliUid(Long aliUid) {
-            this.aliUid = aliUid;
-            return this;
-        }
-        public Long getAliUid() {
-            return this.aliUid;
-        }
-
-        public GetApplicationResponseBodyData setAppId(String appId) {
-            this.appId = appId;
-            return this;
-        }
-        public String getAppId() {
-            return this.appId;
-        }
-
-        public GetApplicationResponseBodyData setAppName(String appName) {
-            this.appName = appName;
-            return this;
-        }
-        public String getAppName() {
-            return this.appName;
-        }
-
-        public GetApplicationResponseBodyData setAppStatus(String appStatus) {
-            this.appStatus = appStatus;
-            return this;
-        }
-        public String getAppStatus() {
-            return this.appStatus;
-        }
-
-        public GetApplicationResponseBodyData setAppTypeList(String appTypeList) {
-            this.appTypeList = appTypeList;
-            return this;
-        }
-        public String getAppTypeList() {
-            return this.appTypeList;
-        }
-
-        public GetApplicationResponseBodyData setAppingList(java.util.List<GetApplicationResponseBodyDataAppingList> appingList) {
-            this.appingList = appingList;
-            return this;
-        }
-        public java.util.List<GetApplicationResponseBodyDataAppingList> getAppingList() {
-            return this.appingList;
-        }
-
-        public GetApplicationResponseBodyData setEndTime(String endTime) {
-            this.endTime = endTime;
-            return this;
-        }
-        public String getEndTime() {
-            return this.endTime;
-        }
-
-        public GetApplicationResponseBodyData setItemCode(String itemCode) {
-            this.itemCode = itemCode;
-            return this;
-        }
-        public String getItemCode() {
-            return this.itemCode;
-        }
-
-        public GetApplicationResponseBodyData setOpenTime(String openTime) {
-            this.openTime = openTime;
-            return this;
-        }
-        public String getOpenTime() {
-            return this.openTime;
-        }
-
-        public GetApplicationResponseBodyData setStartTime(String startTime) {
-            this.startTime = startTime;
-            return this;
-        }
-        public String getStartTime() {
-            return this.startTime;
-        }
-
     }
 
 }
