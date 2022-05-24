6f4d43a3b3145bf63a663111da9ae16e58871a86
sdk-team <sdk-team@alibabacloud.com>
2022-05-24 13:42:37 +0000

    Add Cipher for Trace.

--- mts-20210728/src/main/java/com/aliyun/mts20210728/models/SubmitTracemuResponse.java
+++ mts-20210728/src/main/java/com/aliyun/mts20210728/models/SubmitTracemuResponse.java
@@ -8,6 +8,10 @@ public class SubmitTracemuResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public SubmitTracemuResponseBody body;
@@ -23,6 +27,14 @@ public class SubmitTracemuResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public SubmitTracemuResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public SubmitTracemuResponse setBody(SubmitTracemuResponseBody body) {
