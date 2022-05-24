78dfad5a9341a988838bbf7bde47fd87beb8d13c
sdk-team <sdk-team@alibabacloud.com>
2022-05-24 09:39:49 +0000

    Supports for 5GConnection .

--- xgippop-20220520/src/main/java/com/aliyun/xgippop20220520/models/GetFreeFlowProductListResponseBody.java
+++ xgippop-20220520/src/main/java/com/aliyun/xgippop20220520/models/GetFreeFlowProductListResponseBody.java
@@ -9,7 +9,7 @@ public class GetFreeFlowProductListResponseBody extends TeaModel {
     public String code;
 
     @NameInMap("Data")
-    public String data;
+    public java.util.List<GetFreeFlowProductListResponseBodyData> data;
 
     // 结果描述
     @NameInMap("Message")
@@ -40,11 +40,11 @@ public class GetFreeFlowProductListResponseBody extends TeaModel {
         return this.code;
     }
 
-    public GetFreeFlowProductListResponseBody setData(String data) {
+    public GetFreeFlowProductListResponseBody setData(java.util.List<GetFreeFlowProductListResponseBodyData> data) {
         this.data = data;
         return this;
     }
-    public String getData() {
+    public java.util.List<GetFreeFlowProductListResponseBodyData> getData() {
         return this.data;
     }
 
@@ -78,6 +78,132 @@ public class GetFreeFlowProductListResponseBody extends TeaModel {
     }
     public Boolean getSuccess() {
         return this.success;
+    }
+
+    public static class GetFreeFlowProductListResponseBodyData extends TeaModel {
+        // 产品当前状态，true为已配置，false为未配置
+        @NameInMap("Configured")
+        public Boolean configured;
+
+        // 产品包含的流量大小
+        @NameInMap("FlowProductAmount")
+        public String flowProductAmount;
+
+        // 免流产品ID
+        @NameInMap("FlowProductId")
+        public String flowProductId;
+
+        // 流量包名称
+        @NameInMap("FlowProductName")
+        public String flowProductName;
+
+        // 产品周期
+        @NameInMap("FlowProductPeriod")
+        public String flowProductPeriod;
+
+        // 取值包括free（定向流量）/normal（通用流量）
+        @NameInMap("FlowType")
+        public String flowType;
+
+        // 取值包括cm（中国移动）/ct（中国电信）/cu（中国联通）
+        @NameInMap("Operator")
+        public String operator;
+
+        // 注意这里返回的全量套餐里，只能包含SpecType与该InstanceId的SpecType相同的规格
+        @NameInMap("SpecType")
+        public String specType;
+
+        @NameInMap("Spid")
+        public String spid;
+
+        @NameInMap("UnitPrice")
+        public Integer unitPrice;
+
+        public static GetFreeFlowProductListResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
+            GetFreeFlowProductListResponseBodyData self = new GetFreeFlowProductListResponseBodyData();
+            return TeaModel.build(map, self);
+        }
+
+        public GetFreeFlowProductListResponseBodyData setConfigured(Boolean configured) {
+            this.configured = configured;
+            return this;
+        }
+        public Boolean getConfigured() {
+            return this.configured;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setFlowProductAmount(String flowProductAmount) {
+            this.flowProductAmount = flowProductAmount;
+            return this;
+        }
+        public String getFlowProductAmount() {
+            return this.flowProductAmount;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setFlowProductId(String flowProductId) {
+            this.flowProductId = flowProductId;
+            return this;
+        }
+        public String getFlowProductId() {
+            return this.flowProductId;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setFlowProductName(String flowProductName) {
+            this.flowProductName = flowProductName;
+            return this;
+        }
+        public String getFlowProductName() {
+            return this.flowProductName;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setFlowProductPeriod(String flowProductPeriod) {
+            this.flowProductPeriod = flowProductPeriod;
+            return this;
+        }
+        public String getFlowProductPeriod() {
+            return this.flowProductPeriod;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setFlowType(String flowType) {
+            this.flowType = flowType;
+            return this;
+        }
+        public String getFlowType() {
+            return this.flowType;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setOperator(String operator) {
+            this.operator = operator;
+            return this;
+        }
+        public String getOperator() {
+            return this.operator;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setSpecType(String specType) {
+            this.specType = specType;
+            return this;
+        }
+        public String getSpecType() {
+            return this.specType;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setSpid(String spid) {
+            this.spid = spid;
+            return this;
+        }
+        public String getSpid() {
+            return this.spid;
+        }
+
+        public GetFreeFlowProductListResponseBodyData setUnitPrice(Integer unitPrice) {
+            this.unitPrice = unitPrice;
+            return this;
+        }
+        public Integer getUnitPrice() {
+            return this.unitPrice;
+        }
+
     }
 
 }
