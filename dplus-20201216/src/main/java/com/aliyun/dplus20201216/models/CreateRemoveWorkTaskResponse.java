506fb88e40a4c5347986e50d12ea3c927a570a2c
sdk-team <sdk-team@alibabacloud.com>
2022-05-26 09:01:32 +0000

    Prod.

--- dplus-20201216/src/main/java/com/aliyun/dplus20201216/models/CreateRemoveWorkTaskResponse.java
+++ dplus-20201216/src/main/java/com/aliyun/dplus20201216/models/CreateRemoveWorkTaskResponse.java
@@ -8,6 +8,10 @@ public class CreateRemoveWorkTaskResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public CreateRemoveWorkTaskResponseBody body;
@@ -23,6 +27,14 @@ public class CreateRemoveWorkTaskResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public CreateRemoveWorkTaskResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public CreateRemoveWorkTaskResponse setBody(CreateRemoveWorkTaskResponseBody body) {
