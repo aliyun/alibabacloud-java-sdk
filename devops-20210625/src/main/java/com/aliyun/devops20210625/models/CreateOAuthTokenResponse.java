ded8ed6608dcf3886d081adaf399f5784fd49940
sdk-team <sdk-team@alibabacloud.com>
2022-06-01 07:00:18 +0000

    ADD CreateOAuthToken API.

---
+++ devops-20210625/src/main/java/com/aliyun/devops20210625/models/CreateOAuthTokenResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.devops20210625.models;
+
+import com.aliyun.tea.*;
+
+public class CreateOAuthTokenResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public CreateOAuthTokenResponseBody body;
+
+    public static CreateOAuthTokenResponse build(java.util.Map<String, ?> map) throws Exception {
+        CreateOAuthTokenResponse self = new CreateOAuthTokenResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateOAuthTokenResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public CreateOAuthTokenResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public CreateOAuthTokenResponse setBody(CreateOAuthTokenResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public CreateOAuthTokenResponseBody getBody() {
+        return this.body;
+    }
+
+}
