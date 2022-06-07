094c3f31450219f970e8030f525d08ca7fd9e8ff
sdk-team <sdk-team@alibabacloud.com>
2022-06-07 02:09:38 +0000

    Generated java 2022-03-02 for Green.

---
+++ green-20220302/src/main/java/com/aliyun/green20220302/models/TextModerationResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.green20220302.models;
+
+import com.aliyun.tea.*;
+
+public class TextModerationResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public TextModerationResponseBody body;
+
+    public static TextModerationResponse build(java.util.Map<String, ?> map) throws Exception {
+        TextModerationResponse self = new TextModerationResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public TextModerationResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public TextModerationResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public TextModerationResponse setBody(TextModerationResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public TextModerationResponseBody getBody() {
+        return this.body;
+    }
+
+}
