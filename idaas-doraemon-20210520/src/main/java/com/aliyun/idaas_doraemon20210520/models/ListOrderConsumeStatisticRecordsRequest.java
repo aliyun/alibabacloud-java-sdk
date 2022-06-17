83f0e6fa1011c47d1f265e35714708765adf69bd
sdk-team <sdk-team@alibabacloud.com>
2022-06-17 10:15:38 +0000

    ListOrderConsumeStatisticRecords add AliOrderCode.

--- idaas-doraemon-20210520/src/main/java/com/aliyun/idaas_doraemon20210520/models/ListOrderConsumeStatisticRecordsRequest.java
+++ idaas-doraemon-20210520/src/main/java/com/aliyun/idaas_doraemon20210520/models/ListOrderConsumeStatisticRecordsRequest.java
@@ -4,22 +4,37 @@ package com.aliyun.idaas_doraemon20210520.models;
 import com.aliyun.tea.*;
 
 public class ListOrderConsumeStatisticRecordsRequest extends TeaModel {
+    // 阿里订单编号
+    @NameInMap("AliOrderCode")
+    public String aliOrderCode;
+
+    // 页码
     @NameInMap("PageNumber")
     public Integer pageNumber;
 
+    // 每页记录数
     @NameInMap("PageSize")
     public Integer pageSize;
 
+    // 统计时间最大值
     @NameInMap("StatisticTimeMax")
     public String statisticTimeMax;
 
-    // 结束创建时间
+    // 统计时间最小值
     @NameInMap("StatisticTimeMin")
     public String statisticTimeMin;
 
     public static ListOrderConsumeStatisticRecordsRequest build(java.util.Map<String, ?> map) throws Exception {
         ListOrderConsumeStatisticRecordsRequest self = new ListOrderConsumeStatisticRecordsRequest();
         return TeaModel.build(map, self);
+    }
+
+    public ListOrderConsumeStatisticRecordsRequest setAliOrderCode(String aliOrderCode) {
+        this.aliOrderCode = aliOrderCode;
+        return this;
+    }
+    public String getAliOrderCode() {
+        return this.aliOrderCode;
     }
 
     public ListOrderConsumeStatisticRecordsRequest setPageNumber(Integer pageNumber) {
