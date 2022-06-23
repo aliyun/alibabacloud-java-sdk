34381effd1907848f9cf9cc6bb44617073aff7eb
sdk-team <sdk-team@alibabacloud.com>
2022-06-23 11:43:04 +0000

    Unirest java v12 for user.

--- iacservice-20210722/src/main/java/com/aliyun/iacservice20210722/models/GetResourceRequest.java
+++ iacservice-20210722/src/main/java/com/aliyun/iacservice20210722/models/GetResourceRequest.java
@@ -4,12 +4,23 @@ package com.aliyun.iacservice20210722.models;
 import com.aliyun.tea.*;
 
 public class GetResourceRequest extends TeaModel {
+    @NameInMap("dataType")
+    public String dataType;
+
     @NameInMap("regionId")
     public String regionId;
 
     public static GetResourceRequest build(java.util.Map<String, ?> map) throws Exception {
         GetResourceRequest self = new GetResourceRequest();
         return TeaModel.build(map, self);
+    }
+
+    public GetResourceRequest setDataType(String dataType) {
+        this.dataType = dataType;
+        return this;
+    }
+    public String getDataType() {
+        return this.dataType;
     }
 
     public GetResourceRequest setRegionId(String regionId) {
