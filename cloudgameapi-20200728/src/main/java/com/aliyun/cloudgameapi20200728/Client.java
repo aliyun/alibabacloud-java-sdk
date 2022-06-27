149c786d4abf25bdf5d823033d4a4953d1f97af1
sdk-team <sdk-team@alibabacloud.com>
2022-06-27 06:52:16 +0000

    Add GetQueuingSize.

--- cloudgameapi-20200728/src/main/java/com/aliyun/cloudgameapi20200728/Client.java
+++ cloudgameapi-20200728/src/main/java/com/aliyun/cloudgameapi20200728/Client.java
@@ -1187,6 +1187,39 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.listDeployableInstancesWithOptions(request, runtime);
     }
 
+    public ListGameServerIpResponse listGameServerIpWithOptions(ListGameServerIpRequest request, RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.pageNumber)) {
+            query.put("PageNumber", request.pageNumber);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.pageSize)) {
+            query.put("PageSize", request.pageSize);
+        }
+
+        OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        Params params = Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "ListGameServerIp"),
+            new TeaPair("version", "2020-07-28"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new ListGameServerIpResponse());
+    }
+
+    public ListGameServerIpResponse listGameServerIp(ListGameServerIpRequest request) throws Exception {
+        RuntimeOptions runtime = new RuntimeOptions();
+        return this.listGameServerIpWithOptions(request, runtime);
+    }
+
     public ListGameVersionsResponse listGameVersionsWithOptions(ListGameVersionsRequest request, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
