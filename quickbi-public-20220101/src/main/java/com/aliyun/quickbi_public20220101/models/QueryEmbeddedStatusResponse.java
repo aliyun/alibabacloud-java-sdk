b2c129f0cd2db36344883e90f7920fc1612aae40
sdk-team <sdk-team@alibabacloud.com>
2022-07-04 13:01:11 +0000

    Add API QueryEmbeddedStaus.

---
+++ quickbi-public-20220101/src/main/java/com/aliyun/quickbi_public20220101/models/QueryEmbeddedStatusResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.quickbi_public20220101.models;
+
+import com.aliyun.tea.*;
+
+public class QueryEmbeddedStatusResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public QueryEmbeddedStatusResponseBody body;
+
+    public static QueryEmbeddedStatusResponse build(java.util.Map<String, ?> map) throws Exception {
+        QueryEmbeddedStatusResponse self = new QueryEmbeddedStatusResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public QueryEmbeddedStatusResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public QueryEmbeddedStatusResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public QueryEmbeddedStatusResponse setBody(QueryEmbeddedStatusResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public QueryEmbeddedStatusResponseBody getBody() {
+        return this.body;
+    }
+
+}
