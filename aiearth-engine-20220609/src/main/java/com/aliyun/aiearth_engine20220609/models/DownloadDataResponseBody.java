dbd7e4a494b5a08b0f9adc905dd27ab1c9079601
sdk-team <sdk-team@alibabacloud.com>
2022-07-06 06:35:49 +0000

    提供AIEarth相关gis数据查询，下载功能

---
+++ aiearth-engine-20220609/src/main/java/com/aliyun/aiearth_engine20220609/models/DownloadDataResponseBody.java
@@ -0,0 +1,67 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.aiearth_engine20220609.models;
+
+import com.aliyun.tea.*;
+
+public class DownloadDataResponseBody extends TeaModel {
+    @NameInMap("DataId")
+    public String dataId;
+
+    @NameInMap("DownloadUrl")
+    public String downloadUrl;
+
+    @NameInMap("Finished")
+    public Boolean finished;
+
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("Status")
+    public String status;
+
+    public static DownloadDataResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        DownloadDataResponseBody self = new DownloadDataResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public DownloadDataResponseBody setDataId(String dataId) {
+        this.dataId = dataId;
+        return this;
+    }
+    public String getDataId() {
+        return this.dataId;
+    }
+
+    public DownloadDataResponseBody setDownloadUrl(String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+        return this;
+    }
+    public String getDownloadUrl() {
+        return this.downloadUrl;
+    }
+
+    public DownloadDataResponseBody setFinished(Boolean finished) {
+        this.finished = finished;
+        return this;
+    }
+    public Boolean getFinished() {
+        return this.finished;
+    }
+
+    public DownloadDataResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public DownloadDataResponseBody setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+    public String getStatus() {
+        return this.status;
+    }
+
+}
