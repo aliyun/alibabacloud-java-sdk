5847247a676edee8dec931d2e71f37664d86c763
sdk-team <sdk-team@alibabacloud.com>
2022-07-13 09:23:46 +0000

    Init 2022-04-08.

---
+++ chatbot-20220408/src/main/java/com/aliyun/chatbot20220408/models/GenerateUserAccessTokenResponseBody.java
@@ -0,0 +1,68 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.chatbot20220408.models;
+
+import com.aliyun.tea.*;
+
+public class GenerateUserAccessTokenResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public String code;
+
+    @NameInMap("Data")
+    public String data;
+
+    @NameInMap("Message")
+    public String message;
+
+    // Id of the request
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("Success")
+    public Boolean success;
+
+    public static GenerateUserAccessTokenResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        GenerateUserAccessTokenResponseBody self = new GenerateUserAccessTokenResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public GenerateUserAccessTokenResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public GenerateUserAccessTokenResponseBody setData(String data) {
+        this.data = data;
+        return this;
+    }
+    public String getData() {
+        return this.data;
+    }
+
+    public GenerateUserAccessTokenResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public GenerateUserAccessTokenResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public GenerateUserAccessTokenResponseBody setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+}
