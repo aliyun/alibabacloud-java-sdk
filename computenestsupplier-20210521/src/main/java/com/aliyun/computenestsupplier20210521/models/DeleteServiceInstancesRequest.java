ad8d5788965290cd561a5ac2dee3a9b018c2d665
sdk-team <sdk-team@alibabacloud.com>
2022-07-21 12:08:42 +0000

    Modify tag parameters name.

---
+++ computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/models/DeleteServiceInstancesRequest.java
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.computenestsupplier20210521.models;
+
+import com.aliyun.tea.*;
+
+public class DeleteServiceInstancesRequest extends TeaModel {
+    @NameInMap("ClientToken")
+    public String clientToken;
+
+    @NameInMap("RegionId")
+    public String regionId;
+
+    @NameInMap("ServiceInstanceId")
+    public java.util.List<String> serviceInstanceId;
+
+    public static DeleteServiceInstancesRequest build(java.util.Map<String, ?> map) throws Exception {
+        DeleteServiceInstancesRequest self = new DeleteServiceInstancesRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public DeleteServiceInstancesRequest setClientToken(String clientToken) {
+        this.clientToken = clientToken;
+        return this;
+    }
+    public String getClientToken() {
+        return this.clientToken;
+    }
+
+    public DeleteServiceInstancesRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public DeleteServiceInstancesRequest setServiceInstanceId(java.util.List<String> serviceInstanceId) {
+        this.serviceInstanceId = serviceInstanceId;
+        return this;
+    }
+    public java.util.List<String> getServiceInstanceId() {
+        return this.serviceInstanceId;
+    }
+
+}
