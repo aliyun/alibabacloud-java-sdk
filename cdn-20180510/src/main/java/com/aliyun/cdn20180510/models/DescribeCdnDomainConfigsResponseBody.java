70e541ba6fa91a6c36fe1ca0084b628defb8a078
sdk-team <sdk-team@alibabacloud.com>
2022-07-28 08:27:46 +0000

    Update SetDomainServerCertificate.

--- cdn-20180510/src/main/java/com/aliyun/cdn20180510/models/DescribeCdnDomainConfigsResponseBody.java
+++ cdn-20180510/src/main/java/com/aliyun/cdn20180510/models/DescribeCdnDomainConfigsResponseBody.java
@@ -90,6 +90,9 @@ public class DescribeCdnDomainConfigsResponseBody extends TeaModel {
         @NameInMap("FunctionName")
         public String functionName;
 
+        @NameInMap("ParentId")
+        public String parentId;
+
         @NameInMap("Status")
         public String status;
 
@@ -120,6 +123,14 @@ public class DescribeCdnDomainConfigsResponseBody extends TeaModel {
         }
         public String getFunctionName() {
             return this.functionName;
+        }
+
+        public DescribeCdnDomainConfigsResponseBodyDomainConfigsDomainConfig setParentId(String parentId) {
+            this.parentId = parentId;
+            return this;
+        }
+        public String getParentId() {
+            return this.parentId;
         }
 
         public DescribeCdnDomainConfigsResponseBodyDomainConfigsDomainConfig setStatus(String status) {
