e190d94ef28f722681b233b70bc917878f7dd621
sdk-team <sdk-team@alibabacloud.com>
2022-08-01 03:30:02 +0000

    Add library api.

--- ddi-20200617/src/main/java/com/aliyun/ddi20200617/models/DeleteFlowCategoryResponseBody.java
+++ ddi-20200617/src/main/java/com/aliyun/ddi20200617/models/DeleteFlowCategoryResponseBody.java
@@ -4,37 +4,23 @@ package com.aliyun.ddi20200617.models;
 import com.aliyun.tea.*;
 
 public class DeleteFlowCategoryResponseBody extends TeaModel {
-    @NameInMap("FlowId")
-    public String flowId;
-
-    @NameInMap("JobId")
-    public String jobId;
+    @NameInMap("Data")
+    public Boolean data;
 
     @NameInMap("RequestId")
     public String requestId;
-
-    @NameInMap("Result")
-    public Boolean result;
 
     public static DeleteFlowCategoryResponseBody build(java.util.Map<String, ?> map) throws Exception {
         DeleteFlowCategoryResponseBody self = new DeleteFlowCategoryResponseBody();
         return TeaModel.build(map, self);
     }
 
-    public DeleteFlowCategoryResponseBody setFlowId(String flowId) {
-        this.flowId = flowId;
+    public DeleteFlowCategoryResponseBody setData(Boolean data) {
+        this.data = data;
         return this;
     }
-    public String getFlowId() {
-        return this.flowId;
-    }
-
-    public DeleteFlowCategoryResponseBody setJobId(String jobId) {
-        this.jobId = jobId;
-        return this;
-    }
-    public String getJobId() {
-        return this.jobId;
+    public Boolean getData() {
+        return this.data;
     }
 
     public DeleteFlowCategoryResponseBody setRequestId(String requestId) {
@@ -43,14 +29,6 @@ public class DeleteFlowCategoryResponseBody extends TeaModel {
     }
     public String getRequestId() {
         return this.requestId;
-    }
-
-    public DeleteFlowCategoryResponseBody setResult(Boolean result) {
-        this.result = result;
-        return this;
-    }
-    public Boolean getResult() {
-        return this.result;
     }
 
 }
