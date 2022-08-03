5844d0c3f90eb11c4425e4fc2ac080a4e0e92c53
sdk-team <sdk-team@alibabacloud.com>
2022-08-03 07:35:38 +0000

    Init.

--- rmc-20211104/src/main/java/com/aliyun/rmc20211104/models/SearchResourcesResponse.java
+++ rmc-20211104/src/main/java/com/aliyun/rmc20211104/models/SearchResourcesResponse.java
@@ -8,6 +8,10 @@ public class SearchResourcesResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public SearchResourcesResponseBody body;
@@ -23,6 +27,14 @@ public class SearchResourcesResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public SearchResourcesResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public SearchResourcesResponse setBody(SearchResourcesResponseBody body) {
