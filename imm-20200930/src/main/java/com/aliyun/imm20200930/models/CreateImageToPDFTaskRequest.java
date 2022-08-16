861b3822d14bc0d085dada94758f6c7f779705cd
sdk-team <sdk-team@alibabacloud.com>
2022-08-16 09:50:49 +0000

    Add ImageToPDF.

---
+++ imm-20200930/src/main/java/com/aliyun/imm20200930/models/CreateImageToPDFTaskRequest.java
@@ -0,0 +1,119 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.imm20200930.models;
+
+import com.aliyun.tea.*;
+
+public class CreateImageToPDFTaskRequest extends TeaModel {
+    @NameInMap("CredentialConfig")
+    public CredentialConfig credentialConfig;
+
+    @NameInMap("NotifyTopicName")
+    public String notifyTopicName;
+
+    @NameInMap("ProjectName")
+    public String projectName;
+
+    @NameInMap("Sources")
+    public java.util.List<CreateImageToPDFTaskRequestSources> sources;
+
+    @NameInMap("Tags")
+    public java.util.Map<String, ?> tags;
+
+    @NameInMap("TargetURI")
+    public String targetURI;
+
+    @NameInMap("UserData")
+    public String userData;
+
+    public static CreateImageToPDFTaskRequest build(java.util.Map<String, ?> map) throws Exception {
+        CreateImageToPDFTaskRequest self = new CreateImageToPDFTaskRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateImageToPDFTaskRequest setCredentialConfig(CredentialConfig credentialConfig) {
+        this.credentialConfig = credentialConfig;
+        return this;
+    }
+    public CredentialConfig getCredentialConfig() {
+        return this.credentialConfig;
+    }
+
+    public CreateImageToPDFTaskRequest setNotifyTopicName(String notifyTopicName) {
+        this.notifyTopicName = notifyTopicName;
+        return this;
+    }
+    public String getNotifyTopicName() {
+        return this.notifyTopicName;
+    }
+
+    public CreateImageToPDFTaskRequest setProjectName(String projectName) {
+        this.projectName = projectName;
+        return this;
+    }
+    public String getProjectName() {
+        return this.projectName;
+    }
+
+    public CreateImageToPDFTaskRequest setSources(java.util.List<CreateImageToPDFTaskRequestSources> sources) {
+        this.sources = sources;
+        return this;
+    }
+    public java.util.List<CreateImageToPDFTaskRequestSources> getSources() {
+        return this.sources;
+    }
+
+    public CreateImageToPDFTaskRequest setTags(java.util.Map<String, ?> tags) {
+        this.tags = tags;
+        return this;
+    }
+    public java.util.Map<String, ?> getTags() {
+        return this.tags;
+    }
+
+    public CreateImageToPDFTaskRequest setTargetURI(String targetURI) {
+        this.targetURI = targetURI;
+        return this;
+    }
+    public String getTargetURI() {
+        return this.targetURI;
+    }
+
+    public CreateImageToPDFTaskRequest setUserData(String userData) {
+        this.userData = userData;
+        return this;
+    }
+    public String getUserData() {
+        return this.userData;
+    }
+
+    public static class CreateImageToPDFTaskRequestSources extends TeaModel {
+        @NameInMap("Rotate")
+        public Long rotate;
+
+        @NameInMap("URI")
+        public String URI;
+
+        public static CreateImageToPDFTaskRequestSources build(java.util.Map<String, ?> map) throws Exception {
+            CreateImageToPDFTaskRequestSources self = new CreateImageToPDFTaskRequestSources();
+            return TeaModel.build(map, self);
+        }
+
+        public CreateImageToPDFTaskRequestSources setRotate(Long rotate) {
+            this.rotate = rotate;
+            return this;
+        }
+        public Long getRotate() {
+            return this.rotate;
+        }
+
+        public CreateImageToPDFTaskRequestSources setURI(String URI) {
+            this.URI = URI;
+            return this;
+        }
+        public String getURI() {
+            return this.URI;
+        }
+
+    }
+
+}
---
+++ imm-20200930/src/main/java/com/aliyun/imm20200930/models/CreateImageToPDFTaskShrinkRequest.java
@@ -0,0 +1,89 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.imm20200930.models;
+
+import com.aliyun.tea.*;
+
+public class CreateImageToPDFTaskShrinkRequest extends TeaModel {
+    @NameInMap("CredentialConfig")
+    public String credentialConfigShrink;
+
+    @NameInMap("NotifyTopicName")
+    public String notifyTopicName;
+
+    @NameInMap("ProjectName")
+    public String projectName;
+
+    @NameInMap("Sources")
+    public String sourcesShrink;
+
+    @NameInMap("Tags")
+    public String tagsShrink;
+
+    @NameInMap("TargetURI")
+    public String targetURI;
+
+    @NameInMap("UserData")
+    public String userData;
+
+    public static CreateImageToPDFTaskShrinkRequest build(java.util.Map<String, ?> map) throws Exception {
+        CreateImageToPDFTaskShrinkRequest self = new CreateImageToPDFTaskShrinkRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateImageToPDFTaskShrinkRequest setCredentialConfigShrink(String credentialConfigShrink) {
+        this.credentialConfigShrink = credentialConfigShrink;
+        return this;
+    }
+    public String getCredentialConfigShrink() {
+        return this.credentialConfigShrink;
+    }
+
+    public CreateImageToPDFTaskShrinkRequest setNotifyTopicName(String notifyTopicName) {
+        this.notifyTopicName = notifyTopicName;
+        return this;
+    }
+    public String getNotifyTopicName() {
+        return this.notifyTopicName;
+    }
+
+    public CreateImageToPDFTaskShrinkRequest setProjectName(String projectName) {
+        this.projectName = projectName;
+        return this;
+    }
+    public String getProjectName() {
+        return this.projectName;
+    }
+
+    public CreateImageToPDFTaskShrinkRequest setSourcesShrink(String sourcesShrink) {
+        this.sourcesShrink = sourcesShrink;
+        return this;
+    }
+    public String getSourcesShrink() {
+        return this.sourcesShrink;
+    }
+
+    public CreateImageToPDFTaskShrinkRequest setTagsShrink(String tagsShrink) {
+        this.tagsShrink = tagsShrink;
+        return this;
+    }
+    public String getTagsShrink() {
+        return this.tagsShrink;
+    }
+
+    public CreateImageToPDFTaskShrinkRequest setTargetURI(String targetURI) {
+        this.targetURI = targetURI;
+        return this;
+    }
+    public String getTargetURI() {
+        return this.targetURI;
+    }
+
+    public CreateImageToPDFTaskShrinkRequest setUserData(String userData) {
+        this.userData = userData;
+        return this;
+    }
+    public String getUserData() {
+        return this.userData;
+    }
+
+}
