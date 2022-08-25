2d4be4f30d0df82f1e52187f001caabbe94ffb25
sdk-team <sdk-team@alibabacloud.com>
2022-08-25 06:44:26 +0000

    DTS SDK.

---
+++ dts-20200101/src/main/java/com/aliyun/dts20200101/models/SwitchPhysicalDtsJobToCloudRequest.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.dts20200101.models;
+
+import com.aliyun.tea.*;
+
+public class SwitchPhysicalDtsJobToCloudRequest extends TeaModel {
+    @NameInMap("DtsInstanceId")
+    public String dtsInstanceId;
+
+    @NameInMap("DtsJobId")
+    public String dtsJobId;
+
+    @NameInMap("RegionId")
+    public String regionId;
+
+    @NameInMap("SynchronizationDirection")
+    public String synchronizationDirection;
+
+    public static SwitchPhysicalDtsJobToCloudRequest build(java.util.Map<String, ?> map) throws Exception {
+        SwitchPhysicalDtsJobToCloudRequest self = new SwitchPhysicalDtsJobToCloudRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public SwitchPhysicalDtsJobToCloudRequest setDtsInstanceId(String dtsInstanceId) {
+        this.dtsInstanceId = dtsInstanceId;
+        return this;
+    }
+    public String getDtsInstanceId() {
+        return this.dtsInstanceId;
+    }
+
+    public SwitchPhysicalDtsJobToCloudRequest setDtsJobId(String dtsJobId) {
+        this.dtsJobId = dtsJobId;
+        return this;
+    }
+    public String getDtsJobId() {
+        return this.dtsJobId;
+    }
+
+    public SwitchPhysicalDtsJobToCloudRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public SwitchPhysicalDtsJobToCloudRequest setSynchronizationDirection(String synchronizationDirection) {
+        this.synchronizationDirection = synchronizationDirection;
+        return this;
+    }
+    public String getSynchronizationDirection() {
+        return this.synchronizationDirection;
+    }
+
+}
