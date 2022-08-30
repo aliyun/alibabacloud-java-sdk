ccd57629f343c588e32ef176b7c810ee39a0a8ab
sdk-team <sdk-team@alibabacloud.com>
2022-08-30 10:52:20 +0000

    DSW support query instance metrics .

---
+++ pai-dsw-20220101/src/main/java/com/aliyun/pai_dsw20220101/models/GetInstanceMetricsRequest.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.pai_dsw20220101.models;
+
+import com.aliyun.tea.*;
+
+public class GetInstanceMetricsRequest extends TeaModel {
+    @NameInMap("EndTime")
+    public String endTime;
+
+    @NameInMap("MetricType")
+    public String metricType;
+
+    @NameInMap("StartTime")
+    public String startTime;
+
+    @NameInMap("TimeStep")
+    public String timeStep;
+
+    public static GetInstanceMetricsRequest build(java.util.Map<String, ?> map) throws Exception {
+        GetInstanceMetricsRequest self = new GetInstanceMetricsRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public GetInstanceMetricsRequest setEndTime(String endTime) {
+        this.endTime = endTime;
+        return this;
+    }
+    public String getEndTime() {
+        return this.endTime;
+    }
+
+    public GetInstanceMetricsRequest setMetricType(String metricType) {
+        this.metricType = metricType;
+        return this;
+    }
+    public String getMetricType() {
+        return this.metricType;
+    }
+
+    public GetInstanceMetricsRequest setStartTime(String startTime) {
+        this.startTime = startTime;
+        return this;
+    }
+    public String getStartTime() {
+        return this.startTime;
+    }
+
+    public GetInstanceMetricsRequest setTimeStep(String timeStep) {
+        this.timeStep = timeStep;
+        return this;
+    }
+    public String getTimeStep() {
+        return this.timeStep;
+    }
+
+}
---
+++ pai-dsw-20220101/src/main/java/com/aliyun/pai_dsw20220101/models/GetInstanceMetricsResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.pai_dsw20220101.models;
+
+import com.aliyun.tea.*;
+
+public class GetInstanceMetricsResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public GetInstanceMetricsResponseBody body;
+
+    public static GetInstanceMetricsResponse build(java.util.Map<String, ?> map) throws Exception {
+        GetInstanceMetricsResponse self = new GetInstanceMetricsResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public GetInstanceMetricsResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public GetInstanceMetricsResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public GetInstanceMetricsResponse setBody(GetInstanceMetricsResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public GetInstanceMetricsResponseBody getBody() {
+        return this.body;
+    }
+
+}
