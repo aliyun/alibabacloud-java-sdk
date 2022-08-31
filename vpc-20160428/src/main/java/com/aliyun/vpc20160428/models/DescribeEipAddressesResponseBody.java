d930429b85720bc4d0e7b20870109512cd68980c
sdk-team <sdk-team@alibabacloud.com>
2022-08-31 13:49:30 +0000

    Support DescribeEipAddresses return Tags.

--- vpc-20160428/src/main/java/com/aliyun/vpc20160428/models/DescribeEipAddressesResponseBody.java
+++ vpc-20160428/src/main/java/com/aliyun/vpc20160428/models/DescribeEipAddressesResponseBody.java
@@ -141,9 +141,31 @@ public class DescribeEipAddressesResponseBody extends TeaModel {
     }
 
     public static class DescribeEipAddressesResponseBodyEipAddressesEipAddressTagsTag extends TeaModel {
+        @NameInMap("Key")
+        public String key;
+
+        @NameInMap("Value")
+        public String value;
+
         public static DescribeEipAddressesResponseBodyEipAddressesEipAddressTagsTag build(java.util.Map<String, ?> map) throws Exception {
             DescribeEipAddressesResponseBodyEipAddressesEipAddressTagsTag self = new DescribeEipAddressesResponseBodyEipAddressesEipAddressTagsTag();
             return TeaModel.build(map, self);
+        }
+
+        public DescribeEipAddressesResponseBodyEipAddressesEipAddressTagsTag setKey(String key) {
+            this.key = key;
+            return this;
+        }
+        public String getKey() {
+            return this.key;
+        }
+
+        public DescribeEipAddressesResponseBodyEipAddressesEipAddressTagsTag setValue(String value) {
+            this.value = value;
+            return this;
+        }
+        public String getValue() {
+            return this.value;
         }
 
     }
