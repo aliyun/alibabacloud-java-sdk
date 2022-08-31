3e56b7bb6d29e4dd6aa842a01f7a5e1f177ed032
sdk-team <sdk-team@alibabacloud.com>
2022-08-31 13:49:30 +0000

    Support DescribeEipAddresses return Tags.

--- vpc-20160428/src/main/java/com/aliyun/vpc20160428/models/OpenFlowLogServiceResponseBody.java
+++ vpc-20160428/src/main/java/com/aliyun/vpc20160428/models/OpenFlowLogServiceResponseBody.java
@@ -4,12 +4,34 @@ package com.aliyun.vpc20160428.models;
 import com.aliyun.tea.*;
 
 public class OpenFlowLogServiceResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public String code;
+
+    @NameInMap("Message")
+    public String message;
+
     @NameInMap("RequestId")
     public String requestId;
 
     public static OpenFlowLogServiceResponseBody build(java.util.Map<String, ?> map) throws Exception {
         OpenFlowLogServiceResponseBody self = new OpenFlowLogServiceResponseBody();
         return TeaModel.build(map, self);
+    }
+
+    public OpenFlowLogServiceResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public OpenFlowLogServiceResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
     }
 
     public OpenFlowLogServiceResponseBody setRequestId(String requestId) {
