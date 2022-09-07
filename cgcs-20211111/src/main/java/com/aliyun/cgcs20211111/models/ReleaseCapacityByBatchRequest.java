dfe2618e18382a09d5508fec45f878e42573de8a
sdk-team <sdk-team@alibabacloud.com>
2022-09-07 08:04:21 +0000

    Add proposeCluster.

---
+++ cgcs-20211111/src/main/java/com/aliyun/cgcs20211111/models/ReleaseCapacityByBatchRequest.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cgcs20211111.models;
+
+import com.aliyun.tea.*;
+
+public class ReleaseCapacityByBatchRequest extends TeaModel {
+    @NameInMap("ResBatchId")
+    public String resBatchId;
+
+    public static ReleaseCapacityByBatchRequest build(java.util.Map<String, ?> map) throws Exception {
+        ReleaseCapacityByBatchRequest self = new ReleaseCapacityByBatchRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public ReleaseCapacityByBatchRequest setResBatchId(String resBatchId) {
+        this.resBatchId = resBatchId;
+        return this;
+    }
+    public String getResBatchId() {
+        return this.resBatchId;
+    }
+
+}
---
+++ cgcs-20211111/src/main/java/com/aliyun/cgcs20211111/models/ReleaseCapacityByBatchResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cgcs20211111.models;
+
+import com.aliyun.tea.*;
+
+public class ReleaseCapacityByBatchResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public ReleaseCapacityByBatchResponseBody body;
+
+    public static ReleaseCapacityByBatchResponse build(java.util.Map<String, ?> map) throws Exception {
+        ReleaseCapacityByBatchResponse self = new ReleaseCapacityByBatchResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public ReleaseCapacityByBatchResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public ReleaseCapacityByBatchResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public ReleaseCapacityByBatchResponse setBody(ReleaseCapacityByBatchResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public ReleaseCapacityByBatchResponseBody getBody() {
+        return this.body;
+    }
+
+}
