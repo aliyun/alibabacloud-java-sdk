2758ccc1ae5a5772f7338cfbe36562291acbb72f
sdk-team <sdk-team@alibabacloud.com>
2022-09-14 02:24:41 +0000

    Automatically generate sdk tasks.

--- ecs-20140526/src/main/java/com/aliyun/ecs20140526/Client.java
+++ ecs-20140526/src/main/java/com/aliyun/ecs20140526/Client.java
@@ -13486,6 +13486,10 @@ public class Client extends com.aliyun.teaopenapi.Client {
             query.put("ResourceOwnerId", request.resourceOwnerId);
         }
 
+        if (!com.aliyun.teautil.Common.isUnset(request.zoneId)) {
+            query.put("ZoneId", request.zoneId);
+        }
+
         com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
             new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
         ));
