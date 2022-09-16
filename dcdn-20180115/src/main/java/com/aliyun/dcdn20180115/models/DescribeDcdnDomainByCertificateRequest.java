68d3d7e0245e65a86db6af943249133dc8ee3676
sdk-team <sdk-team@alibabacloud.com>
2022-09-16 07:43:02 +0000

    Update DescribeDcdnReport.

--- dcdn-20180115/src/main/java/com/aliyun/dcdn20180115/models/DescribeDcdnDomainByCertificateRequest.java
+++ dcdn-20180115/src/main/java/com/aliyun/dcdn20180115/models/DescribeDcdnDomainByCertificateRequest.java
@@ -10,6 +10,9 @@ public class DescribeDcdnDomainByCertificateRequest extends TeaModel {
     @NameInMap("SSLPub")
     public String SSLPub;
 
+    @NameInMap("SSLStatus")
+    public Boolean SSLStatus;
+
     public static DescribeDcdnDomainByCertificateRequest build(java.util.Map<String, ?> map) throws Exception {
         DescribeDcdnDomainByCertificateRequest self = new DescribeDcdnDomainByCertificateRequest();
         return TeaModel.build(map, self);
@@ -29,6 +32,14 @@ public class DescribeDcdnDomainByCertificateRequest extends TeaModel {
     }
     public String getSSLPub() {
         return this.SSLPub;
+    }
+
+    public DescribeDcdnDomainByCertificateRequest setSSLStatus(Boolean SSLStatus) {
+        this.SSLStatus = SSLStatus;
+        return this;
+    }
+    public Boolean getSSLStatus() {
+        return this.SSLStatus;
     }
 
 }
