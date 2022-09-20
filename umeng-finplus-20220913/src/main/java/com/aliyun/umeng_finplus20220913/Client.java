614350099223683b813375898234935b73798a75
sdk-team <sdk-team@alibabacloud.com>
2022-09-20 11:12:11 +0000

    V1.

--- umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/Client.java
+++ umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/Client.java
@@ -208,32 +208,6 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return TeaModel.toModel(this.callApi(params, req, runtime), new ListDataSetResponse());
     }
 
-    public RemoveDataSetResponse removeDataSet(RemoveDataSetRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
-        return this.removeDataSetWithOptions(request, headers, runtime);
-    }
-
-    public RemoveDataSetResponse removeDataSetWithOptions(RemoveDataSetRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("headers", headers),
-            new TeaPair("body", request.body)
-        ));
-        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
-            new TeaPair("action", "RemoveDataSet"),
-            new TeaPair("version", "2022-09-13"),
-            new TeaPair("protocol", "HTTPS"),
-            new TeaPair("pathname", "/bc/removeDataSet"),
-            new TeaPair("method", "POST"),
-            new TeaPair("authType", "AK"),
-            new TeaPair("style", "ROA"),
-            new TeaPair("reqBodyType", "json"),
-            new TeaPair("bodyType", "json")
-        ));
-        return TeaModel.toModel(this.callApi(params, req, runtime), new RemoveDataSetResponse());
-    }
-
     public SelectComputeTaskResponse selectComputeTask(SelectComputeTaskRequest request) throws Exception {
         com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
         java.util.Map<String, String> headers = new java.util.HashMap<>();
