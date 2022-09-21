1e9724a4a2d9c64e5f31d4e4d561a2cc29326af1
sdk-team <sdk-team@alibabacloud.com>
2022-09-21 07:17:31 +0000

    Update endpoint.

---
+++ cs-20151215/src/main/java/com/aliyun/cs20151215/models/RemoveNodePoolNodesRequest.java
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cs20151215.models;
+
+import com.aliyun.tea.*;
+
+public class RemoveNodePoolNodesRequest extends TeaModel {
+    @NameInMap("drain_node")
+    public Boolean drainNode;
+
+    @NameInMap("nodes")
+    public java.util.List<String> nodes;
+
+    @NameInMap("release_node")
+    public Boolean releaseNode;
+
+    public static RemoveNodePoolNodesRequest build(java.util.Map<String, ?> map) throws Exception {
+        RemoveNodePoolNodesRequest self = new RemoveNodePoolNodesRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public RemoveNodePoolNodesRequest setDrainNode(Boolean drainNode) {
+        this.drainNode = drainNode;
+        return this;
+    }
+    public Boolean getDrainNode() {
+        return this.drainNode;
+    }
+
+    public RemoveNodePoolNodesRequest setNodes(java.util.List<String> nodes) {
+        this.nodes = nodes;
+        return this;
+    }
+    public java.util.List<String> getNodes() {
+        return this.nodes;
+    }
+
+    public RemoveNodePoolNodesRequest setReleaseNode(Boolean releaseNode) {
+        this.releaseNode = releaseNode;
+        return this;
+    }
+    public Boolean getReleaseNode() {
+        return this.releaseNode;
+    }
+
+}
