42d24681922bce7c426274a4ee8e49005443b787
sdk-team <sdk-team@alibabacloud.com>
2022-09-21 03:15:42 +0000

    Automatically generate sdk tasks.

---
+++ dytnsapi-20200217/src/main/java/com/aliyun/dytnsapi20200217/models/DescribePhoneTwiceTelVerifyResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.dytnsapi20200217.models;
+
+import com.aliyun.tea.*;
+
+public class DescribePhoneTwiceTelVerifyResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public DescribePhoneTwiceTelVerifyResponseBody body;
+
+    public static DescribePhoneTwiceTelVerifyResponse build(java.util.Map<String, ?> map) throws Exception {
+        DescribePhoneTwiceTelVerifyResponse self = new DescribePhoneTwiceTelVerifyResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribePhoneTwiceTelVerifyResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public DescribePhoneTwiceTelVerifyResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public DescribePhoneTwiceTelVerifyResponse setBody(DescribePhoneTwiceTelVerifyResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public DescribePhoneTwiceTelVerifyResponseBody getBody() {
+        return this.body;
+    }
+
+}
