938813e9c5e7bf0507ff94981712422cbd6e8831
sdk-team <sdk-team@alibabacloud.com>
2022-09-26 07:16:12 +0000

    Update api frequency.

--- resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetCouponPageRequest.java
+++ resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetCouponPageRequest.java
@@ -4,50 +4,8 @@ package com.aliyun.resellertrade20191227.models;
 import com.aliyun.tea.*;
 
 public class GetCouponPageRequest extends TeaModel {
-    @NameInMap("ActivitySite")
-    public String activitySite;
-
-    @NameInMap("Bid")
-    public Long bid;
-
-    @NameInMap("BizTime")
-    public String bizTime;
-
-    @NameInMap("Creator")
-    public String creator;
-
-    @NameInMap("Currency")
-    public GetCouponPageRequestCurrency currency;
-
-    @NameInMap("EffectiveEndDate")
-    public String effectiveEndDate;
-
-    @NameInMap("EffectiveStartDate")
-    public String effectiveStartDate;
-
-    @NameInMap("End")
-    public String end;
-
-    @NameInMap("ExpireEndDate")
-    public String expireEndDate;
-
-    @NameInMap("ExpireStartDate")
-    public String expireStartDate;
-
     @NameInMap("FromApp")
     public String fromApp;
-
-    @NameInMap("Id")
-    public Long id;
-
-    @NameInMap("IsQueryExtend")
-    public Boolean isQueryExtend;
-
-    @NameInMap("ItemCodeSet")
-    public java.util.List<String> itemCodeSet;
-
-    @NameInMap("OrderType")
-    public String orderType;
 
     @NameInMap("PageNo")
     public Integer pageNo;
@@ -55,134 +13,21 @@ public class GetCouponPageRequest extends TeaModel {
     @NameInMap("PageSize")
     public Integer pageSize;
 
-    @NameInMap("QueryType")
-    public String queryType;
-
     @NameInMap("RequestId")
     public String requestId;
 
     @NameInMap("SellerId")
     public Long sellerId;
 
-    @NameInMap("Site")
-    public String site;
-
-    @NameInMap("SpId")
-    public Long spId;
-
-    @NameInMap("Start")
-    public String start;
-
-    @NameInMap("Status")
-    public String status;
-
     @NameInMap("TemplateId")
     public Long templateId;
-
-    @NameInMap("Type")
-    public String type;
 
     @NameInMap("Uids")
     public java.util.List<Long> uids;
 
-    @NameInMap("UserId")
-    public Long userId;
-
-    @NameInMap("UserIds")
-    public java.util.List<Long> userIds;
-
-    @NameInMap("YhClientType")
-    public String yhClientType;
-
-    @NameInMap("YhMarket")
-    public String yhMarket;
-
-    @NameInMap("YhMarketType")
-    public Long yhMarketType;
-
     public static GetCouponPageRequest build(java.util.Map<String, ?> map) throws Exception {
         GetCouponPageRequest self = new GetCouponPageRequest();
         return TeaModel.build(map, self);
-    }
-
-    public GetCouponPageRequest setActivitySite(String activitySite) {
-        this.activitySite = activitySite;
-        return this;
-    }
-    public String getActivitySite() {
-        return this.activitySite;
-    }
-
-    public GetCouponPageRequest setBid(Long bid) {
-        this.bid = bid;
-        return this;
-    }
-    public Long getBid() {
-        return this.bid;
-    }
-
-    public GetCouponPageRequest setBizTime(String bizTime) {
-        this.bizTime = bizTime;
-        return this;
-    }
-    public String getBizTime() {
-        return this.bizTime;
-    }
-
-    public GetCouponPageRequest setCreator(String creator) {
-        this.creator = creator;
-        return this;
-    }
-    public String getCreator() {
-        return this.creator;
-    }
-
-    public GetCouponPageRequest setCurrency(GetCouponPageRequestCurrency currency) {
-        this.currency = currency;
-        return this;
-    }
-    public GetCouponPageRequestCurrency getCurrency() {
-        return this.currency;
-    }
-
-    public GetCouponPageRequest setEffectiveEndDate(String effectiveEndDate) {
-        this.effectiveEndDate = effectiveEndDate;
-        return this;
-    }
-    public String getEffectiveEndDate() {
-        return this.effectiveEndDate;
-    }
-
-    public GetCouponPageRequest setEffectiveStartDate(String effectiveStartDate) {
-        this.effectiveStartDate = effectiveStartDate;
-        return this;
-    }
-    public String getEffectiveStartDate() {
-        return this.effectiveStartDate;
-    }
-
-    public GetCouponPageRequest setEnd(String end) {
-        this.end = end;
-        return this;
-    }
-    public String getEnd() {
-        return this.end;
-    }
-
-    public GetCouponPageRequest setExpireEndDate(String expireEndDate) {
-        this.expireEndDate = expireEndDate;
-        return this;
-    }
-    public String getExpireEndDate() {
-        return this.expireEndDate;
-    }
-
-    public GetCouponPageRequest setExpireStartDate(String expireStartDate) {
-        this.expireStartDate = expireStartDate;
-        return this;
-    }
-    public String getExpireStartDate() {
-        return this.expireStartDate;
     }
 
     public GetCouponPageRequest setFromApp(String fromApp) {
@@ -191,38 +36,6 @@ public class GetCouponPageRequest extends TeaModel {
     }
     public String getFromApp() {
         return this.fromApp;
-    }
-
-    public GetCouponPageRequest setId(Long id) {
-        this.id = id;
-        return this;
-    }
-    public Long getId() {
-        return this.id;
-    }
-
-    public GetCouponPageRequest setIsQueryExtend(Boolean isQueryExtend) {
-        this.isQueryExtend = isQueryExtend;
-        return this;
-    }
-    public Boolean getIsQueryExtend() {
-        return this.isQueryExtend;
-    }
-
-    public GetCouponPageRequest setItemCodeSet(java.util.List<String> itemCodeSet) {
-        this.itemCodeSet = itemCodeSet;
-        return this;
-    }
-    public java.util.List<String> getItemCodeSet() {
-        return this.itemCodeSet;
-    }
-
-    public GetCouponPageRequest setOrderType(String orderType) {
-        this.orderType = orderType;
-        return this;
-    }
-    public String getOrderType() {
-        return this.orderType;
     }
 
     public GetCouponPageRequest setPageNo(Integer pageNo) {
@@ -241,14 +54,6 @@ public class GetCouponPageRequest extends TeaModel {
         return this.pageSize;
     }
 
-    public GetCouponPageRequest setQueryType(String queryType) {
-        this.queryType = queryType;
-        return this;
-    }
-    public String getQueryType() {
-        return this.queryType;
-    }
-
     public GetCouponPageRequest setRequestId(String requestId) {
         this.requestId = requestId;
         return this;
@@ -265,38 +70,6 @@ public class GetCouponPageRequest extends TeaModel {
         return this.sellerId;
     }
 
-    public GetCouponPageRequest setSite(String site) {
-        this.site = site;
-        return this;
-    }
-    public String getSite() {
-        return this.site;
-    }
-
-    public GetCouponPageRequest setSpId(Long spId) {
-        this.spId = spId;
-        return this;
-    }
-    public Long getSpId() {
-        return this.spId;
-    }
-
-    public GetCouponPageRequest setStart(String start) {
-        this.start = start;
-        return this;
-    }
-    public String getStart() {
-        return this.start;
-    }
-
-    public GetCouponPageRequest setStatus(String status) {
-        this.status = status;
-        return this;
-    }
-    public String getStatus() {
-        return this.status;
-    }
-
     public GetCouponPageRequest setTemplateId(Long templateId) {
         this.templateId = templateId;
         return this;
@@ -305,101 +78,12 @@ public class GetCouponPageRequest extends TeaModel {
         return this.templateId;
     }
 
-    public GetCouponPageRequest setType(String type) {
-        this.type = type;
-        return this;
-    }
-    public String getType() {
-        return this.type;
-    }
-
     public GetCouponPageRequest setUids(java.util.List<Long> uids) {
         this.uids = uids;
         return this;
     }
     public java.util.List<Long> getUids() {
         return this.uids;
-    }
-
-    public GetCouponPageRequest setUserId(Long userId) {
-        this.userId = userId;
-        return this;
-    }
-    public Long getUserId() {
-        return this.userId;
-    }
-
-    public GetCouponPageRequest setUserIds(java.util.List<Long> userIds) {
-        this.userIds = userIds;
-        return this;
-    }
-    public java.util.List<Long> getUserIds() {
-        return this.userIds;
-    }
-
-    public GetCouponPageRequest setYhClientType(String yhClientType) {
-        this.yhClientType = yhClientType;
-        return this;
-    }
-    public String getYhClientType() {
-        return this.yhClientType;
-    }
-
-    public GetCouponPageRequest setYhMarket(String yhMarket) {
-        this.yhMarket = yhMarket;
-        return this;
-    }
-    public String getYhMarket() {
-        return this.yhMarket;
-    }
-
-    public GetCouponPageRequest setYhMarketType(Long yhMarketType) {
-        this.yhMarketType = yhMarketType;
-        return this;
-    }
-    public Long getYhMarketType() {
-        return this.yhMarketType;
-    }
-
-    public static class GetCouponPageRequestCurrency extends TeaModel {
-        @NameInMap("CurrencyCode")
-        public String currencyCode;
-
-        @NameInMap("DefaultFractionDigits")
-        public Integer defaultFractionDigits;
-
-        @NameInMap("NumericCode")
-        public Integer numericCode;
-
-        public static GetCouponPageRequestCurrency build(java.util.Map<String, ?> map) throws Exception {
-            GetCouponPageRequestCurrency self = new GetCouponPageRequestCurrency();
-            return TeaModel.build(map, self);
-        }
-
-        public GetCouponPageRequestCurrency setCurrencyCode(String currencyCode) {
-            this.currencyCode = currencyCode;
-            return this;
-        }
-        public String getCurrencyCode() {
-            return this.currencyCode;
-        }
-
-        public GetCouponPageRequestCurrency setDefaultFractionDigits(Integer defaultFractionDigits) {
-            this.defaultFractionDigits = defaultFractionDigits;
-            return this;
-        }
-        public Integer getDefaultFractionDigits() {
-            return this.defaultFractionDigits;
-        }
-
-        public GetCouponPageRequestCurrency setNumericCode(Integer numericCode) {
-            this.numericCode = numericCode;
-            return this;
-        }
-        public Integer getNumericCode() {
-            return this.numericCode;
-        }
-
     }
 
 }
--- resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetCouponPageResponseBody.java
+++ resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetCouponPageResponseBody.java
@@ -101,12 +101,6 @@ public class GetCouponPageResponseBody extends TeaModel {
         @NameInMap("CurrencyCode")
         public String currencyCode;
 
-        @NameInMap("DefaultFractionDigits")
-        public Integer defaultFractionDigits;
-
-        @NameInMap("NumericCode")
-        public Integer numericCode;
-
         public static GetCouponPageResponseBodyDataCurrency build(java.util.Map<String, ?> map) throws Exception {
             GetCouponPageResponseBodyDataCurrency self = new GetCouponPageResponseBodyDataCurrency();
             return TeaModel.build(map, self);
@@ -120,60 +114,20 @@ public class GetCouponPageResponseBody extends TeaModel {
             return this.currencyCode;
         }
 
-        public GetCouponPageResponseBodyDataCurrency setDefaultFractionDigits(Integer defaultFractionDigits) {
-            this.defaultFractionDigits = defaultFractionDigits;
-            return this;
-        }
-        public Integer getDefaultFractionDigits() {
-            return this.defaultFractionDigits;
-        }
-
-        public GetCouponPageResponseBodyDataCurrency setNumericCode(Integer numericCode) {
-            this.numericCode = numericCode;
-            return this;
-        }
-        public Integer getNumericCode() {
-            return this.numericCode;
-        }
-
     }
 
     public static class GetCouponPageResponseBodyData extends TeaModel {
-        @NameInMap("ActivationId")
-        public Long activationId;
-
-        @NameInMap("ActivityId")
-        public Long activityId;
-
         @NameInMap("Amount")
         public Double amount;
-
-        @NameInMap("Bid")
-        public Long bid;
 
         @NameInMap("CertainMoney")
         public Double certainMoney;
 
-        @NameInMap("ClientType")
-        public String clientType;
-
-        @NameInMap("CommodityType")
-        public String commodityType;
-
         @NameInMap("CouponId")
         public Long couponId;
 
-        @NameInMap("CouponTileHint")
-        public String couponTileHint;
-
-        @NameInMap("CouponTitle")
-        public String couponTitle;
-
         @NameInMap("CouponType")
         public String couponType;
-
-        @NameInMap("Creator")
-        public String creator;
 
         @NameInMap("Currency")
         public GetCouponPageResponseBodyDataCurrency currency;
@@ -187,53 +141,17 @@ public class GetCouponPageResponseBody extends TeaModel {
         @NameInMap("EndTime")
         public String endTime;
 
-        @NameInMap("ExtendsMap")
-        public java.util.Map<String, String> extendsMap;
-
         @NameInMap("FrozenAmount")
         public Double frozenAmount;
 
         @NameInMap("FrozenCount")
         public Integer frozenCount;
 
-        @NameInMap("GmtCreate")
-        public String gmtCreate;
-
-        @NameInMap("GmtModified")
-        public String gmtModified;
-
-        @NameInMap("ItemCodeSet")
-        public java.util.List<String> itemCodeSet;
-
-        @NameInMap("Market")
-        public String market;
-
-        @NameInMap("MarketType")
-        public Long marketType;
-
-        @NameInMap("Modifier")
-        public String modifier;
-
-        @NameInMap("OrderTypeSet")
-        public java.util.List<String> orderTypeSet;
-
         @NameInMap("PromotionId")
         public Long promotionId;
 
-        @NameInMap("SelectionIds")
-        public String selectionIds;
-
         @NameInMap("SellerId")
         public Long sellerId;
-
-        @NameInMap("Site")
-        public String site;
-
-        @NameInMap("SnapId")
-        public Long snapId;
-
-        @NameInMap("SpId")
-        public Long spId;
 
         @NameInMap("StartTime")
         public String startTime;
@@ -267,36 +185,12 @@ public class GetCouponPageResponseBody extends TeaModel {
             return TeaModel.build(map, self);
         }
 
-        public GetCouponPageResponseBodyData setActivationId(Long activationId) {
-            this.activationId = activationId;
-            return this;
-        }
-        public Long getActivationId() {
-            return this.activationId;
-        }
-
-        public GetCouponPageResponseBodyData setActivityId(Long activityId) {
-            this.activityId = activityId;
-            return this;
-        }
-        public Long getActivityId() {
-            return this.activityId;
-        }
-
         public GetCouponPageResponseBodyData setAmount(Double amount) {
             this.amount = amount;
             return this;
         }
         public Double getAmount() {
             return this.amount;
-        }
-
-        public GetCouponPageResponseBodyData setBid(Long bid) {
-            this.bid = bid;
-            return this;
-        }
-        public Long getBid() {
-            return this.bid;
         }
 
         public GetCouponPageResponseBodyData setCertainMoney(Double certainMoney) {
@@ -307,22 +201,6 @@ public class GetCouponPageResponseBody extends TeaModel {
             return this.certainMoney;
         }
 
-        public GetCouponPageResponseBodyData setClientType(String clientType) {
-            this.clientType = clientType;
-            return this;
-        }
-        public String getClientType() {
-            return this.clientType;
-        }
-
-        public GetCouponPageResponseBodyData setCommodityType(String commodityType) {
-            this.commodityType = commodityType;
-            return this;
-        }
-        public String getCommodityType() {
-            return this.commodityType;
-        }
-
         public GetCouponPageResponseBodyData setCouponId(Long couponId) {
             this.couponId = couponId;
             return this;
@@ -331,36 +209,12 @@ public class GetCouponPageResponseBody extends TeaModel {
             return this.couponId;
         }
 
-        public GetCouponPageResponseBodyData setCouponTileHint(String couponTileHint) {
-            this.couponTileHint = couponTileHint;
-            return this;
-        }
-        public String getCouponTileHint() {
-            return this.couponTileHint;
-        }
-
-        public GetCouponPageResponseBodyData setCouponTitle(String couponTitle) {
-            this.couponTitle = couponTitle;
-            return this;
-        }
-        public String getCouponTitle() {
-            return this.couponTitle;
-        }
-
         public GetCouponPageResponseBodyData setCouponType(String couponType) {
             this.couponType = couponType;
             return this;
         }
         public String getCouponType() {
             return this.couponType;
-        }
-
-        public GetCouponPageResponseBodyData setCreator(String creator) {
-            this.creator = creator;
-            return this;
-        }
-        public String getCreator() {
-            return this.creator;
         }
 
         public GetCouponPageResponseBodyData setCurrency(GetCouponPageResponseBodyDataCurrency currency) {
@@ -395,14 +249,6 @@ public class GetCouponPageResponseBody extends TeaModel {
             return this.endTime;
         }
 
-        public GetCouponPageResponseBodyData setExtendsMap(java.util.Map<String, String> extendsMap) {
-            this.extendsMap = extendsMap;
-            return this;
-        }
-        public java.util.Map<String, String> getExtendsMap() {
-            return this.extendsMap;
-        }
-
         public GetCouponPageResponseBodyData setFrozenAmount(Double frozenAmount) {
             this.frozenAmount = frozenAmount;
             return this;
@@ -419,62 +265,6 @@ public class GetCouponPageResponseBody extends TeaModel {
             return this.frozenCount;
         }
 
-        public GetCouponPageResponseBodyData setGmtCreate(String gmtCreate) {
-            this.gmtCreate = gmtCreate;
-            return this;
-        }
-        public String getGmtCreate() {
-            return this.gmtCreate;
-        }
-
-        public GetCouponPageResponseBodyData setGmtModified(String gmtModified) {
-            this.gmtModified = gmtModified;
-            return this;
-        }
-        public String getGmtModified() {
-            return this.gmtModified;
-        }
-
-        public GetCouponPageResponseBodyData setItemCodeSet(java.util.List<String> itemCodeSet) {
-            this.itemCodeSet = itemCodeSet;
-            return this;
-        }
-        public java.util.List<String> getItemCodeSet() {
-            return this.itemCodeSet;
-        }
-
-        public GetCouponPageResponseBodyData setMarket(String market) {
-            this.market = market;
-            return this;
-        }
-        public String getMarket() {
-            return this.market;
-        }
-
-        public GetCouponPageResponseBodyData setMarketType(Long marketType) {
-            this.marketType = marketType;
-            return this;
-        }
-        public Long getMarketType() {
-            return this.marketType;
-        }
-
-        public GetCouponPageResponseBodyData setModifier(String modifier) {
-            this.modifier = modifier;
-            return this;
-        }
-        public String getModifier() {
-            return this.modifier;
-        }
-
-        public GetCouponPageResponseBodyData setOrderTypeSet(java.util.List<String> orderTypeSet) {
-            this.orderTypeSet = orderTypeSet;
-            return this;
-        }
-        public java.util.List<String> getOrderTypeSet() {
-            return this.orderTypeSet;
-        }
-
         public GetCouponPageResponseBodyData setPromotionId(Long promotionId) {
             this.promotionId = promotionId;
             return this;
@@ -483,44 +273,12 @@ public class GetCouponPageResponseBody extends TeaModel {
             return this.promotionId;
         }
 
-        public GetCouponPageResponseBodyData setSelectionIds(String selectionIds) {
-            this.selectionIds = selectionIds;
-            return this;
-        }
-        public String getSelectionIds() {
-            return this.selectionIds;
-        }
-
         public GetCouponPageResponseBodyData setSellerId(Long sellerId) {
             this.sellerId = sellerId;
             return this;
         }
         public Long getSellerId() {
             return this.sellerId;
-        }
-
-        public GetCouponPageResponseBodyData setSite(String site) {
-            this.site = site;
-            return this;
-        }
-        public String getSite() {
-            return this.site;
-        }
-
-        public GetCouponPageResponseBodyData setSnapId(Long snapId) {
-            this.snapId = snapId;
-            return this;
-        }
-        public Long getSnapId() {
-            return this.snapId;
-        }
-
-        public GetCouponPageResponseBodyData setSpId(Long spId) {
-            this.spId = spId;
-            return this;
-        }
-        public Long getSpId() {
-            return this.spId;
         }
 
         public GetCouponPageResponseBodyData setStartTime(String startTime) {
