e65101bd2e3f7463f0337afb813e630822f4a8ec
sdk-team <sdk-team@alibabacloud.com>
2022-09-29 03:43:01 +0000

    Automatically generate sdk tasks.

--- dm-20151123/src/main/java/com/aliyun/dm20151123/models/AddIpfilterResponseBody.java
+++ dm-20151123/src/main/java/com/aliyun/dm20151123/models/AddIpfilterResponseBody.java
@@ -4,12 +4,23 @@ package com.aliyun.dm20151123.models;
 import com.aliyun.tea.*;
 
 public class AddIpfilterResponseBody extends TeaModel {
+    @NameInMap("IpFilterId")
+    public String ipFilterId;
+
     @NameInMap("RequestId")
     public String requestId;
 
     public static AddIpfilterResponseBody build(java.util.Map<String, ?> map) throws Exception {
         AddIpfilterResponseBody self = new AddIpfilterResponseBody();
         return TeaModel.build(map, self);
+    }
+
+    public AddIpfilterResponseBody setIpFilterId(String ipFilterId) {
+        this.ipFilterId = ipFilterId;
+        return this;
+    }
+    public String getIpFilterId() {
+        return this.ipFilterId;
     }
 
     public AddIpfilterResponseBody setRequestId(String requestId) {
