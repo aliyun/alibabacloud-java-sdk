5fc4024e1888ea0e2d5da8add05613c7727f96ae
sdk-team <sdk-team@alibabacloud.com>
2022-10-17 03:51:00 +0000

    Update RefineMask.

--- imageseg-20191230/src/main/java/com/aliyun/imageseg20191230/models/SegmentHDCommonImageResponseBody.java
+++ imageseg-20191230/src/main/java/com/aliyun/imageseg20191230/models/SegmentHDCommonImageResponseBody.java
@@ -7,6 +7,9 @@ public class SegmentHDCommonImageResponseBody extends TeaModel {
     @NameInMap("Data")
     public SegmentHDCommonImageResponseBodyData data;
 
+    @NameInMap("Message")
+    public String message;
+
     @NameInMap("RequestId")
     public String requestId;
 
@@ -21,6 +24,14 @@ public class SegmentHDCommonImageResponseBody extends TeaModel {
     }
     public SegmentHDCommonImageResponseBodyData getData() {
         return this.data;
+    }
+
+    public SegmentHDCommonImageResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
     }
 
     public SegmentHDCommonImageResponseBody setRequestId(String requestId) {
