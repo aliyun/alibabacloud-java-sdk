f7f3fca86a992c3250876529036b679af1bce1b6
sdk-team <sdk-team@alibabacloud.com>
2022-10-17 03:22:10 +0000

    Release RecognizeVATInvoice.

--- ocr-20191230/src/main/java/com/aliyun/ocr20191230/models/TrimDocumentResponseBody.java
+++ ocr-20191230/src/main/java/com/aliyun/ocr20191230/models/TrimDocumentResponseBody.java
@@ -7,6 +7,9 @@ public class TrimDocumentResponseBody extends TeaModel {
     @NameInMap("Data")
     public TrimDocumentResponseBodyData data;
 
+    @NameInMap("Message")
+    public String message;
+
     @NameInMap("RequestId")
     public String requestId;
 
@@ -21,6 +24,14 @@ public class TrimDocumentResponseBody extends TeaModel {
     }
     public TrimDocumentResponseBodyData getData() {
         return this.data;
+    }
+
+    public TrimDocumentResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
     }
 
     public TrimDocumentResponseBody setRequestId(String requestId) {
