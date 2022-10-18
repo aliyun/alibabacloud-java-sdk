e69d36630e468547b7e01f8d5054b4d23b0f1c11
sdk-team <sdk-team@alibabacloud.com>
2022-10-18 08:27:39 +0000

    Public Sdk.

--- workorder-20210610/src/main/java/com/aliyun/workorder20210610/models/ReopenTicketResponse.java
+++ workorder-20210610/src/main/java/com/aliyun/workorder20210610/models/ReopenTicketResponse.java
@@ -8,6 +8,10 @@ public class ReopenTicketResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public ReopenTicketResponseBody body;
@@ -23,6 +27,14 @@ public class ReopenTicketResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public ReopenTicketResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public ReopenTicketResponse setBody(ReopenTicketResponseBody body) {
