d215674012730028da54f1c695267e0b104573c7
sdk-team <sdk-team@alibabacloud.com>
2022-10-27 10:37:29 +0000

    Iot_20180120 TeaDSL sdk.

--- iot-20180120/src/main/java/com/aliyun/iot20180120/Client.java
+++ iot-20180120/src/main/java/com/aliyun/iot20180120/Client.java
@@ -7490,6 +7490,43 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.gisSearchDeviceTraceWithOptions(request, runtime);
     }
 
+    public ImportDTDataResponse importDTDataWithOptions(ImportDTDataRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.iotInstanceId)) {
+            body.put("IotInstanceId", request.iotInstanceId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.items)) {
+            body.put("Items", request.items);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.productKey)) {
+            body.put("ProductKey", request.productKey);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "ImportDTData"),
+            new TeaPair("version", "2018-01-20"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new ImportDTDataResponse());
+    }
+
+    public ImportDTDataResponse importDTData(ImportDTDataRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.importDTDataWithOptions(request, runtime);
+    }
+
     public ImportDeviceResponse importDeviceWithOptions(ImportDeviceRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
---
+++ iot-20180120/src/main/java/com/aliyun/iot20180120/models/ImportDTDataRequest.java
@@ -0,0 +1,75 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.iot20180120.models;
+
+import com.aliyun.tea.*;
+
+public class ImportDTDataRequest extends TeaModel {
+    @NameInMap("IotInstanceId")
+    public String iotInstanceId;
+
+    @NameInMap("Items")
+    public java.util.List<ImportDTDataRequestItems> items;
+
+    @NameInMap("ProductKey")
+    public String productKey;
+
+    public static ImportDTDataRequest build(java.util.Map<String, ?> map) throws Exception {
+        ImportDTDataRequest self = new ImportDTDataRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public ImportDTDataRequest setIotInstanceId(String iotInstanceId) {
+        this.iotInstanceId = iotInstanceId;
+        return this;
+    }
+    public String getIotInstanceId() {
+        return this.iotInstanceId;
+    }
+
+    public ImportDTDataRequest setItems(java.util.List<ImportDTDataRequestItems> items) {
+        this.items = items;
+        return this;
+    }
+    public java.util.List<ImportDTDataRequestItems> getItems() {
+        return this.items;
+    }
+
+    public ImportDTDataRequest setProductKey(String productKey) {
+        this.productKey = productKey;
+        return this;
+    }
+    public String getProductKey() {
+        return this.productKey;
+    }
+
+    public static class ImportDTDataRequestItems extends TeaModel {
+        @NameInMap("DeviceName")
+        public String deviceName;
+
+        @NameInMap("Params")
+        public String params;
+
+        public static ImportDTDataRequestItems build(java.util.Map<String, ?> map) throws Exception {
+            ImportDTDataRequestItems self = new ImportDTDataRequestItems();
+            return TeaModel.build(map, self);
+        }
+
+        public ImportDTDataRequestItems setDeviceName(String deviceName) {
+            this.deviceName = deviceName;
+            return this;
+        }
+        public String getDeviceName() {
+            return this.deviceName;
+        }
+
+        public ImportDTDataRequestItems setParams(String params) {
+            this.params = params;
+            return this;
+        }
+        public String getParams() {
+            return this.params;
+        }
+
+    }
+
+}
