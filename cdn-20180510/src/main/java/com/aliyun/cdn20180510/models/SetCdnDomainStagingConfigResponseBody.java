22323cb60cce68a27eb06d450bb9c18fec5c5b0f
sdk-team <sdk-team@alibabacloud.com>
2022-10-27 10:25:37 +0000

    Add DescribeWildcardDomainDetailData.

--- cdn-20180510/src/main/java/com/aliyun/cdn20180510/models/SetCdnDomainStagingConfigResponseBody.java
+++ cdn-20180510/src/main/java/com/aliyun/cdn20180510/models/SetCdnDomainStagingConfigResponseBody.java
@@ -4,6 +4,9 @@ package com.aliyun.cdn20180510.models;
 import com.aliyun.tea.*;
 
 public class SetCdnDomainStagingConfigResponseBody extends TeaModel {
+    @NameInMap("DomainConfigList")
+    public java.util.List<SetCdnDomainStagingConfigResponseBodyDomainConfigList> domainConfigList;
+
     @NameInMap("RequestId")
     public String requestId;
 
@@ -12,12 +15,61 @@ public class SetCdnDomainStagingConfigResponseBody extends TeaModel {
         return TeaModel.build(map, self);
     }
 
+    public SetCdnDomainStagingConfigResponseBody setDomainConfigList(java.util.List<SetCdnDomainStagingConfigResponseBodyDomainConfigList> domainConfigList) {
+        this.domainConfigList = domainConfigList;
+        return this;
+    }
+    public java.util.List<SetCdnDomainStagingConfigResponseBodyDomainConfigList> getDomainConfigList() {
+        return this.domainConfigList;
+    }
+
     public SetCdnDomainStagingConfigResponseBody setRequestId(String requestId) {
         this.requestId = requestId;
         return this;
     }
     public String getRequestId() {
         return this.requestId;
+    }
+
+    public static class SetCdnDomainStagingConfigResponseBodyDomainConfigList extends TeaModel {
+        @NameInMap("ConfigId")
+        public Long configId;
+
+        @NameInMap("DomainName")
+        public String domainName;
+
+        @NameInMap("FunctionName")
+        public String functionName;
+
+        public static SetCdnDomainStagingConfigResponseBodyDomainConfigList build(java.util.Map<String, ?> map) throws Exception {
+            SetCdnDomainStagingConfigResponseBodyDomainConfigList self = new SetCdnDomainStagingConfigResponseBodyDomainConfigList();
+            return TeaModel.build(map, self);
+        }
+
+        public SetCdnDomainStagingConfigResponseBodyDomainConfigList setConfigId(Long configId) {
+            this.configId = configId;
+            return this;
+        }
+        public Long getConfigId() {
+            return this.configId;
+        }
+
+        public SetCdnDomainStagingConfigResponseBodyDomainConfigList setDomainName(String domainName) {
+            this.domainName = domainName;
+            return this;
+        }
+        public String getDomainName() {
+            return this.domainName;
+        }
+
+        public SetCdnDomainStagingConfigResponseBodyDomainConfigList setFunctionName(String functionName) {
+            this.functionName = functionName;
+            return this;
+        }
+        public String getFunctionName() {
+            return this.functionName;
+        }
+
     }
 
 }
