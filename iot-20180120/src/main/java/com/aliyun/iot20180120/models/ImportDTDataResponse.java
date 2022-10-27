049774d7b4f2406c7114aaad536fbad99f84e8eb
sdk-team <sdk-team@alibabacloud.com>
2022-10-27 10:37:29 +0000

    Iot_20180120 TeaDSL sdk.

---
+++ iot-20180120/src/main/java/com/aliyun/iot20180120/models/ImportDTDataResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.iot20180120.models;
+
+import com.aliyun.tea.*;
+
+public class ImportDTDataResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public ImportDTDataResponseBody body;
+
+    public static ImportDTDataResponse build(java.util.Map<String, ?> map) throws Exception {
+        ImportDTDataResponse self = new ImportDTDataResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public ImportDTDataResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public ImportDTDataResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public ImportDTDataResponse setBody(ImportDTDataResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public ImportDTDataResponseBody getBody() {
+        return this.body;
+    }
+
+}
