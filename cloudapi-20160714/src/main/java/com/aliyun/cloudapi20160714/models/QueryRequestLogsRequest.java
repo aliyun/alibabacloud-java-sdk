bcf7b090dc9bfea0c27fffd410d526cd5b95270d
sdk-team <sdk-team@alibabacloud.com>
2022-11-03 08:25:10 +0000

    Add QueryRequestLogs.

---
+++ cloudapi-20160714/src/main/java/com/aliyun/cloudapi20160714/models/QueryRequestLogsRequest.java
@@ -0,0 +1,34 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cloudapi20160714.models;
+
+import com.aliyun.tea.*;
+
+public class QueryRequestLogsRequest extends TeaModel {
+    @NameInMap("RequestLogId")
+    public String requestLogId;
+
+    @NameInMap("SecurityToken")
+    public String securityToken;
+
+    public static QueryRequestLogsRequest build(java.util.Map<String, ?> map) throws Exception {
+        QueryRequestLogsRequest self = new QueryRequestLogsRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public QueryRequestLogsRequest setRequestLogId(String requestLogId) {
+        this.requestLogId = requestLogId;
+        return this;
+    }
+    public String getRequestLogId() {
+        return this.requestLogId;
+    }
+
+    public QueryRequestLogsRequest setSecurityToken(String securityToken) {
+        this.securityToken = securityToken;
+        return this;
+    }
+    public String getSecurityToken() {
+        return this.securityToken;
+    }
+
+}
