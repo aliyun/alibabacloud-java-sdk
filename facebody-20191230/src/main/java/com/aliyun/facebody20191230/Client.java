2ecb39322d635484a7591537d20f667a21d0604a
sdk-team <sdk-team@alibabacloud.com>
2022-11-16 08:32:23 +0000

    Publish Swift.

--- facebody-20191230/src/main/java/com/aliyun/facebody20191230/Client.java
+++ facebody-20191230/src/main/java/com/aliyun/facebody20191230/Client.java
@@ -2601,43 +2601,6 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return extractPedestrianFeatureAttrResp;
     }
 
-    public ExtractPedestrianFeatureAttributeResponse extractPedestrianFeatureAttributeWithOptions(ExtractPedestrianFeatureAttributeRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> body = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.imageURL)) {
-            body.put("ImageURL", request.imageURL);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.mode)) {
-            body.put("Mode", request.mode);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.urlList)) {
-            body.put("UrlList", request.urlList);
-        }
-
-        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
-        ));
-        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
-            new TeaPair("action", "ExtractPedestrianFeatureAttribute"),
-            new TeaPair("version", "2019-12-30"),
-            new TeaPair("protocol", "HTTPS"),
-            new TeaPair("pathname", "/"),
-            new TeaPair("method", "POST"),
-            new TeaPair("authType", "AK"),
-            new TeaPair("style", "RPC"),
-            new TeaPair("reqBodyType", "formData"),
-            new TeaPair("bodyType", "json")
-        ));
-        return TeaModel.toModel(this.callApi(params, req, runtime), new ExtractPedestrianFeatureAttributeResponse());
-    }
-
-    public ExtractPedestrianFeatureAttributeResponse extractPedestrianFeatureAttribute(ExtractPedestrianFeatureAttributeRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        return this.extractPedestrianFeatureAttributeWithOptions(request, runtime);
-    }
-
     public FaceBeautyResponse faceBeautyWithOptions(FaceBeautyRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> body = new java.util.HashMap<>();
