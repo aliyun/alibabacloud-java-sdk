05ed2f096d97e651129bca5bb540e241117a79b6
sdk-team <sdk-team@alibabacloud.com>
2022-11-22 06:48:47 +0000

    Add DescribeDomainReport SDK.

---
+++ sasti-20200512/src/main/java/com/aliyun/sasti20200512/Client.java
@@ -0,0 +1,61 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sasti20200512;
+
+import com.aliyun.tea.*;
+import com.aliyun.sasti20200512.models.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(com.aliyun.teaopenapi.models.Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("sasti", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public DescribeDomainReportResponse describeDomainReportWithOptions(DescribeDomainReportRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.domain)) {
+            query.put("Domain", request.domain);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.field)) {
+            query.put("Field", request.field);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "DescribeDomainReport"),
+            new TeaPair("version", "2020-05-12"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new DescribeDomainReportResponse());
+    }
+
+    public DescribeDomainReportResponse describeDomainReport(DescribeDomainReportRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.describeDomainReportWithOptions(request, runtime);
+    }
+}
---
+++ sasti-20200512/src/main/java/com/aliyun/sasti20200512/models/DescribeDomainReportResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.sasti20200512.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeDomainReportResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public DescribeDomainReportResponseBody body;
+
+    public static DescribeDomainReportResponse build(java.util.Map<String, ?> map) throws Exception {
+        DescribeDomainReportResponse self = new DescribeDomainReportResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeDomainReportResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public DescribeDomainReportResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public DescribeDomainReportResponse setBody(DescribeDomainReportResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public DescribeDomainReportResponseBody getBody() {
+        return this.body;
+    }
+
+}
