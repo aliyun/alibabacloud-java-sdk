cec7b2eabe2db6ae9922e4e63a1da87b7cfa310e
sdk-team <sdk-team@alibabacloud.com>
2022-11-28 07:53:04 +0000

    Add UpdateAlertPlan.

---
+++ umeng-apm-20220214/src/main/java/com/aliyun/umeng_apm20220214/models/UpdateAlertPlanRequest.java
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.umeng_apm20220214.models;
+
+import com.aliyun.tea.*;
+
+public class UpdateAlertPlanRequest extends TeaModel {
+    @NameInMap("dataSourceId")
+    public String dataSourceId;
+
+    @NameInMap("planId")
+    public Long planId;
+
+    @NameInMap("versions")
+    public String versions;
+
+    public static UpdateAlertPlanRequest build(java.util.Map<String, ?> map) throws Exception {
+        UpdateAlertPlanRequest self = new UpdateAlertPlanRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public UpdateAlertPlanRequest setDataSourceId(String dataSourceId) {
+        this.dataSourceId = dataSourceId;
+        return this;
+    }
+    public String getDataSourceId() {
+        return this.dataSourceId;
+    }
+
+    public UpdateAlertPlanRequest setPlanId(Long planId) {
+        this.planId = planId;
+        return this;
+    }
+    public Long getPlanId() {
+        return this.planId;
+    }
+
+    public UpdateAlertPlanRequest setVersions(String versions) {
+        this.versions = versions;
+        return this;
+    }
+    public String getVersions() {
+        return this.versions;
+    }
+
+}
