d6cd0f901ab9737b70c9962acff83e473d6a3263
sdk-team <sdk-team@alibabacloud.com>
2022-12-13 05:41:17 +0000

    Support Face Searching Api.

--- imm-20200930/src/main/java/com/aliyun/imm20200930/models/SemanticQueryResponseBody.java
+++ imm-20200930/src/main/java/com/aliyun/imm20200930/models/SemanticQueryResponseBody.java
@@ -4,9 +4,6 @@ package com.aliyun.imm20200930.models;
 import com.aliyun.tea.*;
 
 public class SemanticQueryResponseBody extends TeaModel {
-    @NameInMap("Aggregations")
-    public java.util.List<SemanticQueryResponseBodyAggregations> aggregations;
-
     @NameInMap("Files")
     public java.util.List<File> files;
 
@@ -19,14 +16,6 @@ public class SemanticQueryResponseBody extends TeaModel {
     public static SemanticQueryResponseBody build(java.util.Map<String, ?> map) throws Exception {
         SemanticQueryResponseBody self = new SemanticQueryResponseBody();
         return TeaModel.build(map, self);
-    }
-
-    public SemanticQueryResponseBody setAggregations(java.util.List<SemanticQueryResponseBodyAggregations> aggregations) {
-        this.aggregations = aggregations;
-        return this;
-    }
-    public java.util.List<SemanticQueryResponseBodyAggregations> getAggregations() {
-        return this.aggregations;
     }
 
     public SemanticQueryResponseBody setFiles(java.util.List<File> files) {
@@ -51,88 +40,6 @@ public class SemanticQueryResponseBody extends TeaModel {
     }
     public String getRequestId() {
         return this.requestId;
-    }
-
-    public static class SemanticQueryResponseBodyAggregationsGroups extends TeaModel {
-        @NameInMap("Count")
-        public Long count;
-
-        @NameInMap("Value")
-        public String value;
-
-        public static SemanticQueryResponseBodyAggregationsGroups build(java.util.Map<String, ?> map) throws Exception {
-            SemanticQueryResponseBodyAggregationsGroups self = new SemanticQueryResponseBodyAggregationsGroups();
-            return TeaModel.build(map, self);
-        }
-
-        public SemanticQueryResponseBodyAggregationsGroups setCount(Long count) {
-            this.count = count;
-            return this;
-        }
-        public Long getCount() {
-            return this.count;
-        }
-
-        public SemanticQueryResponseBodyAggregationsGroups setValue(String value) {
-            this.value = value;
-            return this;
-        }
-        public String getValue() {
-            return this.value;
-        }
-
-    }
-
-    public static class SemanticQueryResponseBodyAggregations extends TeaModel {
-        @NameInMap("Field")
-        public String field;
-
-        @NameInMap("Groups")
-        public java.util.List<SemanticQueryResponseBodyAggregationsGroups> groups;
-
-        @NameInMap("Operation")
-        public String operation;
-
-        @NameInMap("Value")
-        public Float value;
-
-        public static SemanticQueryResponseBodyAggregations build(java.util.Map<String, ?> map) throws Exception {
-            SemanticQueryResponseBodyAggregations self = new SemanticQueryResponseBodyAggregations();
-            return TeaModel.build(map, self);
-        }
-
-        public SemanticQueryResponseBodyAggregations setField(String field) {
-            this.field = field;
-            return this;
-        }
-        public String getField() {
-            return this.field;
-        }
-
-        public SemanticQueryResponseBodyAggregations setGroups(java.util.List<SemanticQueryResponseBodyAggregationsGroups> groups) {
-            this.groups = groups;
-            return this;
-        }
-        public java.util.List<SemanticQueryResponseBodyAggregationsGroups> getGroups() {
-            return this.groups;
-        }
-
-        public SemanticQueryResponseBodyAggregations setOperation(String operation) {
-            this.operation = operation;
-            return this;
-        }
-        public String getOperation() {
-            return this.operation;
-        }
-
-        public SemanticQueryResponseBodyAggregations setValue(Float value) {
-            this.value = value;
-            return this;
-        }
-        public Float getValue() {
-            return this.value;
-        }
-
     }
 
 }
