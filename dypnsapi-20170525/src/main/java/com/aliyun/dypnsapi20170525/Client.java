79114445f0b3019d3fd3560060c1c012173e6a2a
sdk-team <sdk-team@alibabacloud.com>
2022-12-27 06:07:12 +0000

    Support Sms Verification for Dypnsapi.

--- dypnsapi-20170525/src/main/java/com/aliyun/dypnsapi20170525/Client.java
+++ dypnsapi-20170525/src/main/java/com/aliyun/dypnsapi20170525/Client.java
@@ -312,47 +312,6 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.getAuthorizationUrlWithOptions(request, runtime);
     }
 
-    public GetCertifyResultResponse getCertifyResultWithOptions(GetCertifyResultRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> query = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.ownerId)) {
-            query.put("OwnerId", request.ownerId);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.resourceOwnerAccount)) {
-            query.put("ResourceOwnerAccount", request.resourceOwnerAccount);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.resourceOwnerId)) {
-            query.put("ResourceOwnerId", request.resourceOwnerId);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.token)) {
-            query.put("Token", request.token);
-        }
-
-        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
-        ));
-        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
-            new TeaPair("action", "GetCertifyResult"),
-            new TeaPair("version", "2017-05-25"),
-            new TeaPair("protocol", "HTTPS"),
-            new TeaPair("pathname", "/"),
-            new TeaPair("method", "POST"),
-            new TeaPair("authType", "AK"),
-            new TeaPair("style", "RPC"),
-            new TeaPair("reqBodyType", "formData"),
-            new TeaPair("bodyType", "json")
-        ));
-        return TeaModel.toModel(this.callApi(params, req, runtime), new GetCertifyResultResponse());
-    }
-
-    public GetCertifyResultResponse getCertifyResult(GetCertifyResultRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        return this.getCertifyResultWithOptions(request, runtime);
-    }
-
     public GetMobileResponse getMobileWithOptions(GetMobileRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
