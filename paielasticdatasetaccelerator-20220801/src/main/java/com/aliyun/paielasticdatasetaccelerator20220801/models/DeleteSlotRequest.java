f0ccf3432db3407e164be421cb3aef07c8bb2297
sdk-team <sdk-team@alibabacloud.com>
2023-01-03 13:34:57 +0000

    Support DLC integration.

---
+++ paielasticdatasetaccelerator-20220801/src/main/java/com/aliyun/paielasticdatasetaccelerator20220801/models/DeleteSlotRequest.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.paielasticdatasetaccelerator20220801.models;
+
+import com.aliyun.tea.*;
+
+public class DeleteSlotRequest extends TeaModel {
+    @NameInMap("Force")
+    public Boolean force;
+
+    public static DeleteSlotRequest build(java.util.Map<String, ?> map) throws Exception {
+        DeleteSlotRequest self = new DeleteSlotRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public DeleteSlotRequest setForce(Boolean force) {
+        this.force = force;
+        return this;
+    }
+    public Boolean getForce() {
+        return this.force;
+    }
+
+}
