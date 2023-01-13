5bf600f0817224859c0d0e67aa7c2a8a12d3c7f6
sdk-team <sdk-team@alibabacloud.com>
2023-01-13 03:02:06 +0000

    Support Service of empty number.

---
+++ dytnsapi-20200217/src/main/java/com/aliyun/dytnsapi20200217/models/DescribeEmptyNumberRequest.java
@@ -0,0 +1,78 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.dytnsapi20200217.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeEmptyNumberRequest extends TeaModel {
+    @NameInMap("AuthCode")
+    public String authCode;
+
+    @NameInMap("InputNumber")
+    public String inputNumber;
+
+    @NameInMap("Mask")
+    public String mask;
+
+    @NameInMap("OwnerId")
+    public Long ownerId;
+
+    @NameInMap("ResourceOwnerAccount")
+    public String resourceOwnerAccount;
+
+    @NameInMap("ResourceOwnerId")
+    public Long resourceOwnerId;
+
+    public static DescribeEmptyNumberRequest build(java.util.Map<String, ?> map) throws Exception {
+        DescribeEmptyNumberRequest self = new DescribeEmptyNumberRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeEmptyNumberRequest setAuthCode(String authCode) {
+        this.authCode = authCode;
+        return this;
+    }
+    public String getAuthCode() {
+        return this.authCode;
+    }
+
+    public DescribeEmptyNumberRequest setInputNumber(String inputNumber) {
+        this.inputNumber = inputNumber;
+        return this;
+    }
+    public String getInputNumber() {
+        return this.inputNumber;
+    }
+
+    public DescribeEmptyNumberRequest setMask(String mask) {
+        this.mask = mask;
+        return this;
+    }
+    public String getMask() {
+        return this.mask;
+    }
+
+    public DescribeEmptyNumberRequest setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+        return this;
+    }
+    public Long getOwnerId() {
+        return this.ownerId;
+    }
+
+    public DescribeEmptyNumberRequest setResourceOwnerAccount(String resourceOwnerAccount) {
+        this.resourceOwnerAccount = resourceOwnerAccount;
+        return this;
+    }
+    public String getResourceOwnerAccount() {
+        return this.resourceOwnerAccount;
+    }
+
+    public DescribeEmptyNumberRequest setResourceOwnerId(Long resourceOwnerId) {
+        this.resourceOwnerId = resourceOwnerId;
+        return this;
+    }
+    public Long getResourceOwnerId() {
+        return this.resourceOwnerId;
+    }
+
+}
---
+++ dytnsapi-20200217/src/main/java/com/aliyun/dytnsapi20200217/models/DescribeEmptyNumberResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.dytnsapi20200217.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeEmptyNumberResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public DescribeEmptyNumberResponseBody body;
+
+    public static DescribeEmptyNumberResponse build(java.util.Map<String, ?> map) throws Exception {
+        DescribeEmptyNumberResponse self = new DescribeEmptyNumberResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeEmptyNumberResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public DescribeEmptyNumberResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public DescribeEmptyNumberResponse setBody(DescribeEmptyNumberResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public DescribeEmptyNumberResponseBody getBody() {
+        return this.body;
+    }
+
+}
