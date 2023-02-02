a4299730c23e0d812335d0173a57953a00107f3b
sdk-team <sdk-team@alibabacloud.com>
2023-02-02 02:29:32 +0000

    Generated java 2015-03-03 for Ft.

--- ft-20150303/src/main/java/com/aliyun/ft20150303/models/UpdateInsRequest.java
+++ ft-20150303/src/main/java/com/aliyun/ft20150303/models/UpdateInsRequest.java
@@ -10,6 +10,9 @@ public class UpdateInsRequest extends TeaModel {
     @NameInMap("Succeed")
     public String succeed;
 
+    @NameInMap("new-param-1")
+    public String newParam1;
+
     @NameInMap("new-param-72444")
     public String newParam72444;
 
@@ -32,6 +35,14 @@ public class UpdateInsRequest extends TeaModel {
     }
     public String getSucceed() {
         return this.succeed;
+    }
+
+    public UpdateInsRequest setNewParam1(String newParam1) {
+        this.newParam1 = newParam1;
+        return this;
+    }
+    public String getNewParam1() {
+        return this.newParam1;
     }
 
     public UpdateInsRequest setNewParam72444(String newParam72444) {
