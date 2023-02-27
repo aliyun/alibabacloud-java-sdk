d13dcba8525a54287a6ad5cd3feb12784be82639
sdk-team <sdk-team@alibabacloud.com>
2023-02-27 08:37:58 +0000

    Support modify user displayName.

---
+++ ccc-20200701/src/main/java/com/aliyun/ccc20200701/models/GetDataChannelCredentialsResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ccc20200701.models;
+
+import com.aliyun.tea.*;
+
+public class GetDataChannelCredentialsResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public GetDataChannelCredentialsResponseBody body;
+
+    public static GetDataChannelCredentialsResponse build(java.util.Map<String, ?> map) throws Exception {
+        GetDataChannelCredentialsResponse self = new GetDataChannelCredentialsResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public GetDataChannelCredentialsResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public GetDataChannelCredentialsResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public GetDataChannelCredentialsResponse setBody(GetDataChannelCredentialsResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public GetDataChannelCredentialsResponseBody getBody() {
+        return this.body;
+    }
+
+}
