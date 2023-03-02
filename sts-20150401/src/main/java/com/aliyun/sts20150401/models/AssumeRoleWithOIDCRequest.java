25b71481a8eb7d28a25ffa08bf17ffe2605f3cab
sdk-team <sdk-team@alibabacloud.com>
2023-03-02 08:58:23 +0000

    Publish AssumeRole API With ExternalId Parameter.

--- sts-20150401/src/main/java/com/aliyun/sts20150401/models/AssumeRoleWithOIDCRequest.java
+++ sts-20150401/src/main/java/com/aliyun/sts20150401/models/AssumeRoleWithOIDCRequest.java
@@ -4,21 +4,68 @@ package com.aliyun.sts20150401.models;
 import com.aliyun.tea.*;
 
 public class AssumeRoleWithOIDCRequest extends TeaModel {
+    /**
+     * <p>The validity period of the STS token. Unit: seconds.</p>
+     * <br>
+     * <p>Default value: 3600. Minimum value: 900. Maximum value: the value of the `MaxSessionDuration` parameter.</p>
+     * <br>
+     * <p>For more information about how to specify `MaxSessionDuration`, see [CreateRole](~~28710~~) or [UpdateRole](~~28712~~).</p>
+     */
     @NameInMap("DurationSeconds")
     public Long durationSeconds;
 
+    /**
+     * <p>The Alibaba Cloud Resource Name (ARN) of the OIDC IdP.</p>
+     * <br>
+     * <p>You can view the ARN in the RAM console or by calling operations.</p>
+     * <br>
+     * <p>- For more information about how to view the ARN in the RAM console, see [View the information about an OIDC IdP](~~327123~~).</p>
+     * <p>- For more information about how to view the ARN by calling operations, see [GetOIDCProvider](~~327126~~) or [ListOIDCProviders](~~327127~~).</p>
+     */
     @NameInMap("OIDCProviderArn")
     public String OIDCProviderArn;
 
+    /**
+     * <p>The OIDC token that is issued by the external IdP.</p>
+     * <br>
+     * <p>The OIDC token must be 4 to 20,000 characters in length.</p>
+     * <br>
+     * <p>> You must enter the original OIDC token. You do not need to enter the Base64-encoded OIDC token.</p>
+     */
     @NameInMap("OIDCToken")
     public String OIDCToken;
 
+    /**
+     * <p>The policy that specifies the permissions of the returned STS token. You can use this parameter to grant the STS token fewer permissions than the permissions granted to the RAM role.</p>
+     * <br>
+     * <p>- If you specify this parameter, the permissions of the returned STS token are the permissions that are included in the value of this parameter and owned by the RAM role.</p>
+     * <p>- If you do not specify this parameter, the returned STS token has all the permissions of the RAM role.</p>
+     * <br>
+     * <p>The value must be 1 to 2,048 characters in length.</p>
+     */
     @NameInMap("Policy")
     public String policy;
 
+    /**
+     * <p>The ARN of the RAM role.</p>
+     * <br>
+     * <p>You can view the ARN in the RAM console or by calling operations.</p>
+     * <br>
+     * <p>- For more information about how to view the ARN in the RAM console, see [How do I view the ARN of the RAM role?](~~39744~~)</p>
+     * <p>- For more information about how to view the ARN by calling operations, see [ListRoles](~~28713~~) or [GetRole](~~28711~~).</p>
+     */
     @NameInMap("RoleArn")
     public String roleArn;
 
+    /**
+     * <p>The custom name of the role session.</p>
+     * <br>
+     * <p>You can specify the value of this parameter based on your business requirements. In most cases, you can set this parameter to the identity of the user who calls the operation. For example, specify a username. In ActionTrail logs, you can distinguish the users who assume the same RAM role to perform operations based on the value of the RoleSessionName parameter. This way, you can perform user-specific auditing.</p>
+     * <br>
+     * <p>The value can contain letters, digits, periods (.), at signs (@), hyphens (-), and underscores (\_).</p>
+     * <br>
+     * <p>The value must be 2 to 64 characters in length.</p>
+     */
     @NameInMap("RoleSessionName")
     public String roleSessionName;
 
