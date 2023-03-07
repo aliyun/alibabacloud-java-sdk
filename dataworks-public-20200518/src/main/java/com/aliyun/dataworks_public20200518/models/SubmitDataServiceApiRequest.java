4f466174b0821689caa811a81d34c556d212fd02
sdk-team <sdk-team@alibabacloud.com>
2023-03-07 06:14:02 +0000

    Generated java 2020-05-18 for dataworks-public.

---
+++ dataworks-public-20200518/src/main/java/com/aliyun/dataworks_public20200518/models/SubmitDataServiceApiRequest.java
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.dataworks_public20200518.models;
+
+import com.aliyun.tea.*;
+
+public class SubmitDataServiceApiRequest extends TeaModel {
+    @NameInMap("ApiId")
+    public Long apiId;
+
+    @NameInMap("ProjectId")
+    public Long projectId;
+
+    @NameInMap("TenantId")
+    public Long tenantId;
+
+    public static SubmitDataServiceApiRequest build(java.util.Map<String, ?> map) throws Exception {
+        SubmitDataServiceApiRequest self = new SubmitDataServiceApiRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public SubmitDataServiceApiRequest setApiId(Long apiId) {
+        this.apiId = apiId;
+        return this;
+    }
+    public Long getApiId() {
+        return this.apiId;
+    }
+
+    public SubmitDataServiceApiRequest setProjectId(Long projectId) {
+        this.projectId = projectId;
+        return this;
+    }
+    public Long getProjectId() {
+        return this.projectId;
+    }
+
+    public SubmitDataServiceApiRequest setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+        return this;
+    }
+    public Long getTenantId() {
+        return this.tenantId;
+    }
+
+}
