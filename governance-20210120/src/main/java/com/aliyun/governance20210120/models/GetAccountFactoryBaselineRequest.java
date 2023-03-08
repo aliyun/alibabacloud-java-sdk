295c390c7eebf09adee9c5ce25b988eae41562e8
sdk-team <sdk-team@alibabacloud.com>
2023-03-08 03:31:33 +0000

    Add api ListAccountFactoryBaselines.

---
+++ governance-20210120/src/main/java/com/aliyun/governance20210120/models/GetAccountFactoryBaselineRequest.java
@@ -0,0 +1,37 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.governance20210120.models;
+
+import com.aliyun.tea.*;
+
+public class GetAccountFactoryBaselineRequest extends TeaModel {
+    @NameInMap("BaselineId")
+    public String baselineId;
+
+    /**
+     * <p>RegionId</p>
+     */
+    @NameInMap("RegionId")
+    public String regionId;
+
+    public static GetAccountFactoryBaselineRequest build(java.util.Map<String, ?> map) throws Exception {
+        GetAccountFactoryBaselineRequest self = new GetAccountFactoryBaselineRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public GetAccountFactoryBaselineRequest setBaselineId(String baselineId) {
+        this.baselineId = baselineId;
+        return this;
+    }
+    public String getBaselineId() {
+        return this.baselineId;
+    }
+
+    public GetAccountFactoryBaselineRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+}
