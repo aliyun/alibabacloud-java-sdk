a609892e072d82952b6bdd81b20bf4ba70fa5184
sdk-team <sdk-team@alibabacloud.com>
2023-03-09 08:17:31 +0000

    V4. support encrypted invoking

--- umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/Client.java
+++ umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/Client.java
@@ -328,6 +328,53 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.createDataSet2WithOptions(request, headers, runtime);
     }
 
+    public EncryptInvokeResponse encryptInvokeWithOptions(EncryptInvokeRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.clientId)) {
+            body.put("clientId", request.clientId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.data)) {
+            body.put("data", request.data);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.encryptKey)) {
+            body.put("encryptKey", request.encryptKey);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.methodName)) {
+            body.put("methodName", request.methodName);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.sign)) {
+            body.put("sign", request.sign);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "EncryptInvoke"),
+            new TeaPair("version", "2022-09-13"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/bc/encryptInvoke"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new EncryptInvokeResponse());
+    }
+
+    public EncryptInvokeResponse encryptInvoke(EncryptInvokeRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.encryptInvokeWithOptions(request, headers, runtime);
+    }
+
     public ListComputeTaskResponse listComputeTaskWithOptions(java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
             new TeaPair("headers", headers)
