2aca3977b48b52bd9825d52adbaba80a75e1a063
sdk-team <sdk-team@alibabacloud.com>
2023-03-10 02:55:19 +0000

    Add Unbind Open API.

--- appstream-center-20210901/src/main/java/com/aliyun/appstream_center20210901/Client.java
+++ appstream-center-20210901/src/main/java/com/aliyun/appstream_center20210901/Client.java
@@ -988,6 +988,47 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.renewAppInstanceGroupWithOptions(request, runtime);
     }
 
+    public UnbindResponse unbindWithOptions(UnbindRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.appInstanceGroupId)) {
+            body.put("AppInstanceGroupId", request.appInstanceGroupId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.appInstanceId)) {
+            body.put("AppInstanceId", request.appInstanceId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.endUserId)) {
+            body.put("EndUserId", request.endUserId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.productType)) {
+            body.put("ProductType", request.productType);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "Unbind"),
+            new TeaPair("version", "2021-09-01"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new UnbindResponse());
+    }
+
+    public UnbindResponse unbind(UnbindRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.unbindWithOptions(request, runtime);
+    }
+
     public UpdateAppInstanceGroupImageResponse updateAppInstanceGroupImageWithOptions(UpdateAppInstanceGroupImageRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
---
+++ appstream-center-20210901/src/main/java/com/aliyun/appstream_center20210901/models/UnbindRequest.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.appstream_center20210901.models;
+
+import com.aliyun.tea.*;
+
+public class UnbindRequest extends TeaModel {
+    @NameInMap("AppInstanceGroupId")
+    public String appInstanceGroupId;
+
+    @NameInMap("AppInstanceId")
+    public String appInstanceId;
+
+    @NameInMap("EndUserId")
+    public String endUserId;
+
+    @NameInMap("ProductType")
+    public String productType;
+
+    public static UnbindRequest build(java.util.Map<String, ?> map) throws Exception {
+        UnbindRequest self = new UnbindRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public UnbindRequest setAppInstanceGroupId(String appInstanceGroupId) {
+        this.appInstanceGroupId = appInstanceGroupId;
+        return this;
+    }
+    public String getAppInstanceGroupId() {
+        return this.appInstanceGroupId;
+    }
+
+    public UnbindRequest setAppInstanceId(String appInstanceId) {
+        this.appInstanceId = appInstanceId;
+        return this;
+    }
+    public String getAppInstanceId() {
+        return this.appInstanceId;
+    }
+
+    public UnbindRequest setEndUserId(String endUserId) {
+        this.endUserId = endUserId;
+        return this;
+    }
+    public String getEndUserId() {
+        return this.endUserId;
+    }
+
+    public UnbindRequest setProductType(String productType) {
+        this.productType = productType;
+        return this;
+    }
+    public String getProductType() {
+        return this.productType;
+    }
+
+}
