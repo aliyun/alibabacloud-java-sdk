c0e7c3f1dd97a40537f1896b052937d548a84598
sdk-team <sdk-team@alibabacloud.com>
2023-03-22 07:49:45 +0000

    Add ListAppInstances Open API.

--- appstream-center-20210901/src/main/java/com/aliyun/appstream_center20210901/Client.java
+++ appstream-center-20210901/src/main/java/com/aliyun/appstream_center20210901/Client.java
@@ -610,6 +610,53 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.listAppInstanceGroupWithOptions(request, runtime);
     }
 
+    public ListAppInstancesResponse listAppInstancesWithOptions(ListAppInstancesRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.appInstanceGroupId)) {
+            query.put("AppInstanceGroupId", request.appInstanceGroupId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.appInstanceId)) {
+            query.put("AppInstanceId", request.appInstanceId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.pageNumber)) {
+            query.put("PageNumber", request.pageNumber);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.pageSize)) {
+            query.put("PageSize", request.pageSize);
+        }
+
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.status)) {
+            body.put("Status", request.status);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query)),
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "ListAppInstances"),
+            new TeaPair("version", "2021-09-01"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new ListAppInstancesResponse());
+    }
+
+    public ListAppInstancesResponse listAppInstances(ListAppInstancesRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.listAppInstancesWithOptions(request, runtime);
+    }
+
     public ListNodeInstanceTypeResponse listNodeInstanceTypeWithOptions(ListNodeInstanceTypeRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
---
+++ appstream-center-20210901/src/main/java/com/aliyun/appstream_center20210901/models/ListAppInstancesResponseBody.java
@@ -0,0 +1,152 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.appstream_center20210901.models;
+
+import com.aliyun.tea.*;
+
+public class ListAppInstancesResponseBody extends TeaModel {
+    @NameInMap("AppInstanceModels")
+    public java.util.List<ListAppInstancesResponseBodyAppInstanceModels> appInstanceModels;
+
+    @NameInMap("PageNumber")
+    public Integer pageNumber;
+
+    @NameInMap("PageSize")
+    public Integer pageSize;
+
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("TotalCount")
+    public Integer totalCount;
+
+    public static ListAppInstancesResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        ListAppInstancesResponseBody self = new ListAppInstancesResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public ListAppInstancesResponseBody setAppInstanceModels(java.util.List<ListAppInstancesResponseBodyAppInstanceModels> appInstanceModels) {
+        this.appInstanceModels = appInstanceModels;
+        return this;
+    }
+    public java.util.List<ListAppInstancesResponseBodyAppInstanceModels> getAppInstanceModels() {
+        return this.appInstanceModels;
+    }
+
+    public ListAppInstancesResponseBody setPageNumber(Integer pageNumber) {
+        this.pageNumber = pageNumber;
+        return this;
+    }
+    public Integer getPageNumber() {
+        return this.pageNumber;
+    }
+
+    public ListAppInstancesResponseBody setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+        return this;
+    }
+    public Integer getPageSize() {
+        return this.pageSize;
+    }
+
+    public ListAppInstancesResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public ListAppInstancesResponseBody setTotalCount(Integer totalCount) {
+        this.totalCount = totalCount;
+        return this;
+    }
+    public Integer getTotalCount() {
+        return this.totalCount;
+    }
+
+    public static class ListAppInstancesResponseBodyAppInstanceModels extends TeaModel {
+        @NameInMap("AppInstanceGroupId")
+        public String appInstanceGroupId;
+
+        @NameInMap("AppInstanceId")
+        public String appInstanceId;
+
+        @NameInMap("GmtCreate")
+        public String gmtCreate;
+
+        @NameInMap("GmtModified")
+        public String gmtModified;
+
+        @NameInMap("MainEthPublicIp")
+        public String mainEthPublicIp;
+
+        @NameInMap("SessionStatus")
+        public String sessionStatus;
+
+        @NameInMap("Status")
+        public String status;
+
+        public static ListAppInstancesResponseBodyAppInstanceModels build(java.util.Map<String, ?> map) throws Exception {
+            ListAppInstancesResponseBodyAppInstanceModels self = new ListAppInstancesResponseBodyAppInstanceModels();
+            return TeaModel.build(map, self);
+        }
+
+        public ListAppInstancesResponseBodyAppInstanceModels setAppInstanceGroupId(String appInstanceGroupId) {
+            this.appInstanceGroupId = appInstanceGroupId;
+            return this;
+        }
+        public String getAppInstanceGroupId() {
+            return this.appInstanceGroupId;
+        }
+
+        public ListAppInstancesResponseBodyAppInstanceModels setAppInstanceId(String appInstanceId) {
+            this.appInstanceId = appInstanceId;
+            return this;
+        }
+        public String getAppInstanceId() {
+            return this.appInstanceId;
+        }
+
+        public ListAppInstancesResponseBodyAppInstanceModels setGmtCreate(String gmtCreate) {
+            this.gmtCreate = gmtCreate;
+            return this;
+        }
+        public String getGmtCreate() {
+            return this.gmtCreate;
+        }
+
+        public ListAppInstancesResponseBodyAppInstanceModels setGmtModified(String gmtModified) {
+            this.gmtModified = gmtModified;
+            return this;
+        }
+        public String getGmtModified() {
+            return this.gmtModified;
+        }
+
+        public ListAppInstancesResponseBodyAppInstanceModels setMainEthPublicIp(String mainEthPublicIp) {
+            this.mainEthPublicIp = mainEthPublicIp;
+            return this;
+        }
+        public String getMainEthPublicIp() {
+            return this.mainEthPublicIp;
+        }
+
+        public ListAppInstancesResponseBodyAppInstanceModels setSessionStatus(String sessionStatus) {
+            this.sessionStatus = sessionStatus;
+            return this;
+        }
+        public String getSessionStatus() {
+            return this.sessionStatus;
+        }
+
+        public ListAppInstancesResponseBodyAppInstanceModels setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+        public String getStatus() {
+            return this.status;
+        }
+
+    }
+
+}
