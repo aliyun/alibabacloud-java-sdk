fb0d94d34b184e0b7baee7c59be0524aac3c1ccd
sdk-team <sdk-team@alibabacloud.com>
2023-04-06 07:07:34 +0000

    Add risk score and risk level.

---
+++ antirisk-20221128/src/main/java/com/aliyun/antirisk20221128/models/GetZidTagScoreByAtokenRequest.java
@@ -0,0 +1,37 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.antirisk20221128.models;
+
+import com.aliyun.tea.*;
+
+public class GetZidTagScoreByAtokenRequest extends TeaModel {
+    /**
+     * <p>atoken</p>
+     */
+    @NameInMap("atoken")
+    public String atoken;
+
+    @NameInMap("dataSourceId")
+    public String dataSourceId;
+
+    public static GetZidTagScoreByAtokenRequest build(java.util.Map<String, ?> map) throws Exception {
+        GetZidTagScoreByAtokenRequest self = new GetZidTagScoreByAtokenRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public GetZidTagScoreByAtokenRequest setAtoken(String atoken) {
+        this.atoken = atoken;
+        return this;
+    }
+    public String getAtoken() {
+        return this.atoken;
+    }
+
+    public GetZidTagScoreByAtokenRequest setDataSourceId(String dataSourceId) {
+        this.dataSourceId = dataSourceId;
+        return this;
+    }
+    public String getDataSourceId() {
+        return this.dataSourceId;
+    }
+
+}
---
+++ antirisk-20221128/src/main/java/com/aliyun/antirisk20221128/models/GetZidTagScoreByAtokenResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.antirisk20221128.models;
+
+import com.aliyun.tea.*;
+
+public class GetZidTagScoreByAtokenResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public GetZidTagScoreByAtokenResponseBody body;
+
+    public static GetZidTagScoreByAtokenResponse build(java.util.Map<String, ?> map) throws Exception {
+        GetZidTagScoreByAtokenResponse self = new GetZidTagScoreByAtokenResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public GetZidTagScoreByAtokenResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public GetZidTagScoreByAtokenResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public GetZidTagScoreByAtokenResponse setBody(GetZidTagScoreByAtokenResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public GetZidTagScoreByAtokenResponseBody getBody() {
+        return this.body;
+    }
+
+}
