810cd673bbde43a8ccbc4db327a818065f1e6021
sdk-team <sdk-team@alibabacloud.com>
2023-05-05 10:22:10 +0000

    Update.

---
+++ btripopen-20220520/src/main/java/com/aliyun/btripopen20220520/models/TBAccountUnbindResponseBody.java
@@ -0,0 +1,78 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.btripopen20220520.models;
+
+import com.aliyun.tea.*;
+
+public class TBAccountUnbindResponseBody extends TeaModel {
+    @NameInMap("code")
+    public String code;
+
+    @NameInMap("message")
+    public String message;
+
+    @NameInMap("module")
+    public Boolean module;
+
+    @NameInMap("requestId")
+    public String requestId;
+
+    @NameInMap("success")
+    public Boolean success;
+
+    @NameInMap("traceId")
+    public String traceId;
+
+    public static TBAccountUnbindResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        TBAccountUnbindResponseBody self = new TBAccountUnbindResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public TBAccountUnbindResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public TBAccountUnbindResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public TBAccountUnbindResponseBody setModule(Boolean module) {
+        this.module = module;
+        return this;
+    }
+    public Boolean getModule() {
+        return this.module;
+    }
+
+    public TBAccountUnbindResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public TBAccountUnbindResponseBody setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+    public TBAccountUnbindResponseBody setTraceId(String traceId) {
+        this.traceId = traceId;
+        return this;
+    }
+    public String getTraceId() {
+        return this.traceId;
+    }
+
+}
