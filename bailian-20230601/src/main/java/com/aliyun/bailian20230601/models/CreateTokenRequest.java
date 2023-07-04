f7911262f386986c8484d9fa0a64cc70590fb9ae
sdk-team <sdk-team@alibabacloud.com>
2023-07-04 12:33:27 +0000

    改造api符合pop接口规范

---
+++ bailian-20230601/src/main/java/com/aliyun/bailian20230601/models/CreateTokenRequest.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.bailian20230601.models;
+
+import com.aliyun.tea.*;
+
+public class CreateTokenRequest extends TeaModel {
+    @NameInMap("AgentKey")
+    public String agentKey;
+
+    public static CreateTokenRequest build(java.util.Map<String, ?> map) throws Exception {
+        CreateTokenRequest self = new CreateTokenRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateTokenRequest setAgentKey(String agentKey) {
+        this.agentKey = agentKey;
+        return this;
+    }
+    public String getAgentKey() {
+        return this.agentKey;
+    }
+
+}
---
+++ bailian-20230601/src/main/java/com/aliyun/bailian20230601/models/CreateTokenResponseBody.java
@@ -0,0 +1,108 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.bailian20230601.models;
+
+import com.aliyun.tea.*;
+
+public class CreateTokenResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public String code;
+
+    @NameInMap("Data")
+    public CreateTokenResponseBodyData data;
+
+    @NameInMap("HttpStatusCode")
+    public String httpStatusCode;
+
+    @NameInMap("Message")
+    public String message;
+
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("Success")
+    public Boolean success;
+
+    public static CreateTokenResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        CreateTokenResponseBody self = new CreateTokenResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateTokenResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public CreateTokenResponseBody setData(CreateTokenResponseBodyData data) {
+        this.data = data;
+        return this;
+    }
+    public CreateTokenResponseBodyData getData() {
+        return this.data;
+    }
+
+    public CreateTokenResponseBody setHttpStatusCode(String httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+        return this;
+    }
+    public String getHttpStatusCode() {
+        return this.httpStatusCode;
+    }
+
+    public CreateTokenResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public CreateTokenResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public CreateTokenResponseBody setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+    public static class CreateTokenResponseBodyData extends TeaModel {
+        @NameInMap("ExpiredTime")
+        public Long expiredTime;
+
+        @NameInMap("Token")
+        public String token;
+
+        public static CreateTokenResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
+            CreateTokenResponseBodyData self = new CreateTokenResponseBodyData();
+            return TeaModel.build(map, self);
+        }
+
+        public CreateTokenResponseBodyData setExpiredTime(Long expiredTime) {
+            this.expiredTime = expiredTime;
+            return this;
+        }
+        public Long getExpiredTime() {
+            return this.expiredTime;
+        }
+
+        public CreateTokenResponseBodyData setToken(String token) {
+            this.token = token;
+            return this;
+        }
+        public String getToken() {
+            return this.token;
+        }
+
+    }
+
+}
