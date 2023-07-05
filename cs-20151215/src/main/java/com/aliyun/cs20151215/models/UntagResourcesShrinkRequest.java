5633b0c4b4ef343968b8cb3a479f61d6d2b4142c
sdk-team <sdk-team@alibabacloud.com>
2023-07-05 10:14:43 +0000

    Update API.

---
+++ cs-20151215/src/main/java/com/aliyun/cs20151215/models/UntagResourcesShrinkRequest.java
@@ -0,0 +1,67 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cs20151215.models;
+
+import com.aliyun.tea.*;
+
+public class UntagResourcesShrinkRequest extends TeaModel {
+    @NameInMap("all")
+    public Boolean all;
+
+    @NameInMap("region_id")
+    public String regionId;
+
+    @NameInMap("resource_ids")
+    public String resourceIdsShrink;
+
+    @NameInMap("resource_type")
+    public String resourceType;
+
+    @NameInMap("tag_keys")
+    public String tagKeysShrink;
+
+    public static UntagResourcesShrinkRequest build(java.util.Map<String, ?> map) throws Exception {
+        UntagResourcesShrinkRequest self = new UntagResourcesShrinkRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public UntagResourcesShrinkRequest setAll(Boolean all) {
+        this.all = all;
+        return this;
+    }
+    public Boolean getAll() {
+        return this.all;
+    }
+
+    public UntagResourcesShrinkRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public UntagResourcesShrinkRequest setResourceIdsShrink(String resourceIdsShrink) {
+        this.resourceIdsShrink = resourceIdsShrink;
+        return this;
+    }
+    public String getResourceIdsShrink() {
+        return this.resourceIdsShrink;
+    }
+
+    public UntagResourcesShrinkRequest setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+        return this;
+    }
+    public String getResourceType() {
+        return this.resourceType;
+    }
+
+    public UntagResourcesShrinkRequest setTagKeysShrink(String tagKeysShrink) {
+        this.tagKeysShrink = tagKeysShrink;
+        return this;
+    }
+    public String getTagKeysShrink() {
+        return this.tagKeysShrink;
+    }
+
+}
