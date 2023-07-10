d1d5b32ba9a11ed6dbdad04922ee78aa7ee55a6b
sdk-team <sdk-team@alibabacloud.com>
2023-07-10 07:49:14 +0000

    Add DescribeClassDetails Api.

---
+++ rds-20140815/src/main/java/com/aliyun/rds20140815/models/DescribeClassDetailsResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.rds20140815.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeClassDetailsResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public DescribeClassDetailsResponseBody body;
+
+    public static DescribeClassDetailsResponse build(java.util.Map<String, ?> map) throws Exception {
+        DescribeClassDetailsResponse self = new DescribeClassDetailsResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeClassDetailsResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public DescribeClassDetailsResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public DescribeClassDetailsResponse setBody(DescribeClassDetailsResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public DescribeClassDetailsResponseBody getBody() {
+        return this.body;
+    }
+
+}
