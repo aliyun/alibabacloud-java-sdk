64f4491b443454ce4a8679cf7ca13491a3b1c113
sdk-team <sdk-team@alibabacloud.com>
2023-07-17 11:37:50 +0000

    Method FindRisk add return params-aliyunIdkp, codeType.

--- risk-20160713/src/main/java/com/aliyun/risk20160713/models/UpdateNameListResponse.java
+++ risk-20160713/src/main/java/com/aliyun/risk20160713/models/UpdateNameListResponse.java
@@ -8,6 +8,10 @@ public class UpdateNameListResponse extends TeaModel {
     @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
     @NameInMap("body")
     @Validation(required = true)
     public UpdateNameListResponseBody body;
@@ -23,6 +27,14 @@ public class UpdateNameListResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public UpdateNameListResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public UpdateNameListResponse setBody(UpdateNameListResponseBody body) {
