c8a1920bc431a4aec74f6d0e0403839d8111c68b
sdk-team <sdk-team@alibabacloud.com>
2023-07-17 06:46:44 +0000

    Add SetDcdnDomainSSLCertificate.

---
+++ dcdn-20180115/src/main/java/com/aliyun/dcdn20180115/models/SetDcdnDomainSSLCertificateRequest.java
@@ -0,0 +1,122 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.dcdn20180115.models;
+
+import com.aliyun.tea.*;
+
+public class SetDcdnDomainSSLCertificateRequest extends TeaModel {
+    @NameInMap("CertId")
+    public Long certId;
+
+    @NameInMap("CertName")
+    public String certName;
+
+    @NameInMap("CertRegion")
+    public String certRegion;
+
+    @NameInMap("CertType")
+    public String certType;
+
+    @NameInMap("DomainName")
+    public String domainName;
+
+    @NameInMap("OwnerId")
+    public Long ownerId;
+
+    @NameInMap("SSLPri")
+    public String SSLPri;
+
+    @NameInMap("SSLProtocol")
+    public String SSLProtocol;
+
+    @NameInMap("SSLPub")
+    public String SSLPub;
+
+    @NameInMap("SecurityToken")
+    public String securityToken;
+
+    public static SetDcdnDomainSSLCertificateRequest build(java.util.Map<String, ?> map) throws Exception {
+        SetDcdnDomainSSLCertificateRequest self = new SetDcdnDomainSSLCertificateRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setCertId(Long certId) {
+        this.certId = certId;
+        return this;
+    }
+    public Long getCertId() {
+        return this.certId;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setCertName(String certName) {
+        this.certName = certName;
+        return this;
+    }
+    public String getCertName() {
+        return this.certName;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setCertRegion(String certRegion) {
+        this.certRegion = certRegion;
+        return this;
+    }
+    public String getCertRegion() {
+        return this.certRegion;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setCertType(String certType) {
+        this.certType = certType;
+        return this;
+    }
+    public String getCertType() {
+        return this.certType;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setDomainName(String domainName) {
+        this.domainName = domainName;
+        return this;
+    }
+    public String getDomainName() {
+        return this.domainName;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+        return this;
+    }
+    public Long getOwnerId() {
+        return this.ownerId;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setSSLPri(String SSLPri) {
+        this.SSLPri = SSLPri;
+        return this;
+    }
+    public String getSSLPri() {
+        return this.SSLPri;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setSSLProtocol(String SSLProtocol) {
+        this.SSLProtocol = SSLProtocol;
+        return this;
+    }
+    public String getSSLProtocol() {
+        return this.SSLProtocol;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setSSLPub(String SSLPub) {
+        this.SSLPub = SSLPub;
+        return this;
+    }
+    public String getSSLPub() {
+        return this.SSLPub;
+    }
+
+    public SetDcdnDomainSSLCertificateRequest setSecurityToken(String securityToken) {
+        this.securityToken = securityToken;
+        return this;
+    }
+    public String getSecurityToken() {
+        return this.securityToken;
+    }
+
+}
