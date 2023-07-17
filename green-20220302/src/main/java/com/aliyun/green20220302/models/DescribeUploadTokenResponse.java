ad8f8a6aca0e1630b4228a9a8568c244e1910708
sdk-team <sdk-team@alibabacloud.com>
2023-07-17 06:51:52 +0000

    Add Query Upload Token API.

---
+++ green-20220302/src/main/java/com/aliyun/green20220302/models/DescribeUploadTokenResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.green20220302.models;
+
+import com.aliyun.tea.*;
+
+public class DescribeUploadTokenResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public DescribeUploadTokenResponseBody body;
+
+    public static DescribeUploadTokenResponse build(java.util.Map<String, ?> map) throws Exception {
+        DescribeUploadTokenResponse self = new DescribeUploadTokenResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public DescribeUploadTokenResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public DescribeUploadTokenResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public DescribeUploadTokenResponse setBody(DescribeUploadTokenResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public DescribeUploadTokenResponseBody getBody() {
+        return this.body;
+    }
+
+}
