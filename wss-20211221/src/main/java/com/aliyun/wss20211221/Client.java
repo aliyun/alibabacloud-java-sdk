f4bbf7c544ab6b054f01db85065858cc544f64ec
sdk-team <sdk-team@alibabacloud.com>
2023-07-31 09:45:00 +0000

    OpenAPI DescribeDeliveryAddress.

--- wss-20211221/src/main/java/com/aliyun/wss20211221/Client.java
+++ wss-20211221/src/main/java/com/aliyun/wss20211221/Client.java
@@ -104,6 +104,27 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.describeBillingsWithOptions(request, runtime);
     }
 
+    public DescribeDeliveryAddressResponse describeDeliveryAddressWithOptions(com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teaopenapi.models.OpenApiRequest req = new com.aliyun.teaopenapi.models.OpenApiRequest();
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "DescribeDeliveryAddress"),
+            new TeaPair("version", "2021-12-21"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new DescribeDeliveryAddressResponse());
+    }
+
+    public DescribeDeliveryAddressResponse describeDeliveryAddress() throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.describeDeliveryAddressWithOptions(runtime);
+    }
+
     public DescribePriceResponse describePriceWithOptions(DescribePriceRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
