d10f8847d474c0fe0de285f589d5e4ad36c26c14
sdk-team <sdk-team@alibabacloud.com>
2023-08-01 12:14:44 +0000

    Add video moderation api.

---
+++ green-20220302/src/main/java/com/aliyun/green20220302/models/VideoModerationResponseBody.java
@@ -0,0 +1,78 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.green20220302.models;
+
+import com.aliyun.tea.*;
+
+public class VideoModerationResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public Integer code;
+
+    @NameInMap("Data")
+    public VideoModerationResponseBodyData data;
+
+    @NameInMap("Message")
+    public String message;
+
+    /**
+     * <p>Id of the request</p>
+     */
+    @NameInMap("RequestId")
+    public String requestId;
+
+    public static VideoModerationResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        VideoModerationResponseBody self = new VideoModerationResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public VideoModerationResponseBody setCode(Integer code) {
+        this.code = code;
+        return this;
+    }
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public VideoModerationResponseBody setData(VideoModerationResponseBodyData data) {
+        this.data = data;
+        return this;
+    }
+    public VideoModerationResponseBodyData getData() {
+        return this.data;
+    }
+
+    public VideoModerationResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
+    }
+
+    public VideoModerationResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public static class VideoModerationResponseBodyData extends TeaModel {
+        @NameInMap("TaskId")
+        public String taskId;
+
+        public static VideoModerationResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
+            VideoModerationResponseBodyData self = new VideoModerationResponseBodyData();
+            return TeaModel.build(map, self);
+        }
+
+        public VideoModerationResponseBodyData setTaskId(String taskId) {
+            this.taskId = taskId;
+            return this;
+        }
+        public String getTaskId() {
+            return this.taskId;
+        }
+
+    }
+
+}
---
+++ green-20220302/src/main/java/com/aliyun/green20220302/models/VideoModerationResultResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.green20220302.models;
+
+import com.aliyun.tea.*;
+
+public class VideoModerationResultResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public VideoModerationResultResponseBody body;
+
+    public static VideoModerationResultResponse build(java.util.Map<String, ?> map) throws Exception {
+        VideoModerationResultResponse self = new VideoModerationResultResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public VideoModerationResultResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public VideoModerationResultResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public VideoModerationResultResponse setBody(VideoModerationResultResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public VideoModerationResultResponseBody getBody() {
+        return this.body;
+    }
+
+}
