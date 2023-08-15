9b7a66a3395867b841652ea2f14f3b1c4f9a1122
sdk-team <sdk-team@alibabacloud.com>
2023-08-15 12:29:42 +0000

    Generated java 2015-12-15 for CS.

--- cs-20151215/src/main/java/com/aliyun/cs20151215/models/DescribeExternalAgentRequest.java
+++ cs-20151215/src/main/java/com/aliyun/cs20151215/models/DescribeExternalAgentRequest.java
@@ -4,12 +4,23 @@ package com.aliyun.cs20151215.models;
 import com.aliyun.tea.*;
 
 public class DescribeExternalAgentRequest extends TeaModel {
+    @NameInMap("AgentMode")
+    public String agentMode;
+
     @NameInMap("PrivateIpAddress")
     public String privateIpAddress;
 
     public static DescribeExternalAgentRequest build(java.util.Map<String, ?> map) throws Exception {
         DescribeExternalAgentRequest self = new DescribeExternalAgentRequest();
         return TeaModel.build(map, self);
+    }
+
+    public DescribeExternalAgentRequest setAgentMode(String agentMode) {
+        this.agentMode = agentMode;
+        return this;
+    }
+    public String getAgentMode() {
+        return this.agentMode;
     }
 
     public DescribeExternalAgentRequest setPrivateIpAddress(String privateIpAddress) {
