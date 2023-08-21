f732332c52ea8f41a5677194d809bee5d4a2f502
sdk-team <sdk-team@alibabacloud.com>
2023-08-21 08:24:24 +0000

    Generated java 2020-05-12 for Sasti.

--- sasti-20200512/src/main/java/com/aliyun/sasti20200512/Client.java
+++ sasti-20200512/src/main/java/com/aliyun/sasti20200512/Client.java
@@ -136,37 +136,4 @@ public class Client extends com.aliyun.teaopenapi.Client {
         com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
         return this.describeIpReportWithOptions(request, runtime);
     }
-
-    public GetGraphQueryTemplatesResponse getGraphQueryTemplatesWithOptions(GetGraphQueryTemplatesRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> body = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.env)) {
-            body.put("Env", request.env);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.serviceUnit)) {
-            body.put("ServiceUnit", request.serviceUnit);
-        }
-
-        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
-        ));
-        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
-            new TeaPair("action", "GetGraphQueryTemplates"),
-            new TeaPair("version", "2020-05-12"),
-            new TeaPair("protocol", "HTTPS"),
-            new TeaPair("pathname", "/"),
-            new TeaPair("method", "POST"),
-            new TeaPair("authType", "AK"),
-            new TeaPair("style", "RPC"),
-            new TeaPair("reqBodyType", "formData"),
-            new TeaPair("bodyType", "json")
-        ));
-        return TeaModel.toModel(this.callApi(params, req, runtime), new GetGraphQueryTemplatesResponse());
-    }
-
-    public GetGraphQueryTemplatesResponse getGraphQueryTemplates(GetGraphQueryTemplatesRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        return this.getGraphQueryTemplatesWithOptions(request, runtime);
-    }
 }
