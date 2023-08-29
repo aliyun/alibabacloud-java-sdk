1708898a96a1ddd736a4e224573dbc06484844fe
sdk-team <sdk-team@alibabacloud.com>
2023-08-29 17:23:04 +0000

    Generated java 2017-12-07 for Cloudfw.

--- cloudfw-20171207/src/main/java/com/aliyun/cloudfw20171207/models/PutEnableFwSwitchResponseBody.java
+++ cloudfw-20171207/src/main/java/com/aliyun/cloudfw20171207/models/PutEnableFwSwitchResponseBody.java
@@ -4,6 +4,9 @@ package com.aliyun.cloudfw20171207.models;
 import com.aliyun.tea.*;
 
 public class PutEnableFwSwitchResponseBody extends TeaModel {
+    @NameInMap("AbnormalResourceStatusList")
+    public java.util.List<PutEnableFwSwitchResponseBodyAbnormalResourceStatusList> abnormalResourceStatusList;
+
     /**
      * <p>The ID of the request.</p>
      */
@@ -15,12 +18,61 @@ public class PutEnableFwSwitchResponseBody extends TeaModel {
         return TeaModel.build(map, self);
     }
 
+    public PutEnableFwSwitchResponseBody setAbnormalResourceStatusList(java.util.List<PutEnableFwSwitchResponseBodyAbnormalResourceStatusList> abnormalResourceStatusList) {
+        this.abnormalResourceStatusList = abnormalResourceStatusList;
+        return this;
+    }
+    public java.util.List<PutEnableFwSwitchResponseBodyAbnormalResourceStatusList> getAbnormalResourceStatusList() {
+        return this.abnormalResourceStatusList;
+    }
+
     public PutEnableFwSwitchResponseBody setRequestId(String requestId) {
         this.requestId = requestId;
         return this;
     }
     public String getRequestId() {
         return this.requestId;
+    }
+
+    public static class PutEnableFwSwitchResponseBodyAbnormalResourceStatusList extends TeaModel {
+        @NameInMap("Msg")
+        public String msg;
+
+        @NameInMap("Resource")
+        public String resource;
+
+        @NameInMap("Status")
+        public String status;
+
+        public static PutEnableFwSwitchResponseBodyAbnormalResourceStatusList build(java.util.Map<String, ?> map) throws Exception {
+            PutEnableFwSwitchResponseBodyAbnormalResourceStatusList self = new PutEnableFwSwitchResponseBodyAbnormalResourceStatusList();
+            return TeaModel.build(map, self);
+        }
+
+        public PutEnableFwSwitchResponseBodyAbnormalResourceStatusList setMsg(String msg) {
+            this.msg = msg;
+            return this;
+        }
+        public String getMsg() {
+            return this.msg;
+        }
+
+        public PutEnableFwSwitchResponseBodyAbnormalResourceStatusList setResource(String resource) {
+            this.resource = resource;
+            return this;
+        }
+        public String getResource() {
+            return this.resource;
+        }
+
+        public PutEnableFwSwitchResponseBodyAbnormalResourceStatusList setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+        public String getStatus() {
+            return this.status;
+        }
+
     }
 
 }
