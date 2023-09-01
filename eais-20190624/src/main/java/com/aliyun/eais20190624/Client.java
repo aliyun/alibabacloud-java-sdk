f18d8516737d074636741c94b91f98fccbee75c3
sdk-team <sdk-team@alibabacloud.com>
2023-09-01 17:16:48 +0000

    Generated java 2019-06-24 for eais.

--- eais-20190624/src/main/java/com/aliyun/eais20190624/Client.java
+++ eais-20190624/src/main/java/com/aliyun/eais20190624/Client.java
@@ -743,6 +743,10 @@ public class Client extends com.aliyun.teaopenapi.Client {
             query.put("Status", request.status);
         }
 
+        if (!com.aliyun.teautil.Common.isUnset(request.tag)) {
+            query.put("Tag", request.tag);
+        }
+
         com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
             new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
         ));
