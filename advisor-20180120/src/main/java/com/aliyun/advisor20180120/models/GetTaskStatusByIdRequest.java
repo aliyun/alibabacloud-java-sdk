a8a1dbedddcdfca87208a0274830010ef4c45e05
sdk-team <sdk-team@alibabacloud.com>
2023-09-06 02:59:09 +0000

    Generated java 2018-01-20 for Advisor.

---
+++ advisor-20180120/src/main/java/com/aliyun/advisor20180120/models/GetTaskStatusByIdRequest.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.advisor20180120.models;
+
+import com.aliyun.tea.*;
+
+public class GetTaskStatusByIdRequest extends TeaModel {
+    @NameInMap("TaskId")
+    public String taskId;
+
+    public static GetTaskStatusByIdRequest build(java.util.Map<String, ?> map) throws Exception {
+        GetTaskStatusByIdRequest self = new GetTaskStatusByIdRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public GetTaskStatusByIdRequest setTaskId(String taskId) {
+        this.taskId = taskId;
+        return this;
+    }
+    public String getTaskId() {
+        return this.taskId;
+    }
+
+}
