ec923dee501824afa4e0e4193fd0df034316139f
sdk-team <sdk-team@alibabacloud.com>
2023-09-07 05:38:28 +0000

    Generated java 2021-07-07 for ocr-api.

---
+++ ocr-api-20210707/src/main/java/com/aliyun/ocr_api20210707/models/DataSubImagesKvInfoKvDetailsValue.java
@@ -0,0 +1,171 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ocr_api20210707.models;
+
+import com.aliyun.tea.*;
+
+public class DataSubImagesKvInfoKvDetailsValue extends TeaModel {
+    @NameInMap("KeyName")
+    public String keyName;
+
+    @NameInMap("KeyConfidence")
+    public Integer keyConfidence;
+
+    @NameInMap("Value")
+    public String value;
+
+    @NameInMap("ValueConfidence")
+    public Integer valueConfidence;
+
+    @NameInMap("ValuePoints")
+    public java.util.List<DataSubImagesKvInfoKvDetailsValueValuePoints> valuePoints;
+
+    @NameInMap("ValueRect")
+    public DataSubImagesKvInfoKvDetailsValueValueRect valueRect;
+
+    @NameInMap("ValueAngle")
+    public Integer valueAngle;
+
+    public static DataSubImagesKvInfoKvDetailsValue build(java.util.Map<String, ?> map) throws Exception {
+        DataSubImagesKvInfoKvDetailsValue self = new DataSubImagesKvInfoKvDetailsValue();
+        return TeaModel.build(map, self);
+    }
+
+    public DataSubImagesKvInfoKvDetailsValue setKeyName(String keyName) {
+        this.keyName = keyName;
+        return this;
+    }
+    public String getKeyName() {
+        return this.keyName;
+    }
+
+    public DataSubImagesKvInfoKvDetailsValue setKeyConfidence(Integer keyConfidence) {
+        this.keyConfidence = keyConfidence;
+        return this;
+    }
+    public Integer getKeyConfidence() {
+        return this.keyConfidence;
+    }
+
+    public DataSubImagesKvInfoKvDetailsValue setValue(String value) {
+        this.value = value;
+        return this;
+    }
+    public String getValue() {
+        return this.value;
+    }
+
+    public DataSubImagesKvInfoKvDetailsValue setValueConfidence(Integer valueConfidence) {
+        this.valueConfidence = valueConfidence;
+        return this;
+    }
+    public Integer getValueConfidence() {
+        return this.valueConfidence;
+    }
+
+    public DataSubImagesKvInfoKvDetailsValue setValuePoints(java.util.List<DataSubImagesKvInfoKvDetailsValueValuePoints> valuePoints) {
+        this.valuePoints = valuePoints;
+        return this;
+    }
+    public java.util.List<DataSubImagesKvInfoKvDetailsValueValuePoints> getValuePoints() {
+        return this.valuePoints;
+    }
+
+    public DataSubImagesKvInfoKvDetailsValue setValueRect(DataSubImagesKvInfoKvDetailsValueValueRect valueRect) {
+        this.valueRect = valueRect;
+        return this;
+    }
+    public DataSubImagesKvInfoKvDetailsValueValueRect getValueRect() {
+        return this.valueRect;
+    }
+
+    public DataSubImagesKvInfoKvDetailsValue setValueAngle(Integer valueAngle) {
+        this.valueAngle = valueAngle;
+        return this;
+    }
+    public Integer getValueAngle() {
+        return this.valueAngle;
+    }
+
+    public static class DataSubImagesKvInfoKvDetailsValueValuePoints extends TeaModel {
+        @NameInMap("X")
+        public Integer x;
+
+        @NameInMap("Y")
+        public Integer y;
+
+        public static DataSubImagesKvInfoKvDetailsValueValuePoints build(java.util.Map<String, ?> map) throws Exception {
+            DataSubImagesKvInfoKvDetailsValueValuePoints self = new DataSubImagesKvInfoKvDetailsValueValuePoints();
+            return TeaModel.build(map, self);
+        }
+
+        public DataSubImagesKvInfoKvDetailsValueValuePoints setX(Integer x) {
+            this.x = x;
+            return this;
+        }
+        public Integer getX() {
+            return this.x;
+        }
+
+        public DataSubImagesKvInfoKvDetailsValueValuePoints setY(Integer y) {
+            this.y = y;
+            return this;
+        }
+        public Integer getY() {
+            return this.y;
+        }
+
+    }
+
+    public static class DataSubImagesKvInfoKvDetailsValueValueRect extends TeaModel {
+        @NameInMap("CenterX")
+        public Integer centerX;
+
+        @NameInMap("CenterY")
+        public Integer centerY;
+
+        @NameInMap("Width")
+        public Integer width;
+
+        @NameInMap("Height")
+        public Integer height;
+
+        public static DataSubImagesKvInfoKvDetailsValueValueRect build(java.util.Map<String, ?> map) throws Exception {
+            DataSubImagesKvInfoKvDetailsValueValueRect self = new DataSubImagesKvInfoKvDetailsValueValueRect();
+            return TeaModel.build(map, self);
+        }
+
+        public DataSubImagesKvInfoKvDetailsValueValueRect setCenterX(Integer centerX) {
+            this.centerX = centerX;
+            return this;
+        }
+        public Integer getCenterX() {
+            return this.centerX;
+        }
+
+        public DataSubImagesKvInfoKvDetailsValueValueRect setCenterY(Integer centerY) {
+            this.centerY = centerY;
+            return this;
+        }
+        public Integer getCenterY() {
+            return this.centerY;
+        }
+
+        public DataSubImagesKvInfoKvDetailsValueValueRect setWidth(Integer width) {
+            this.width = width;
+            return this;
+        }
+        public Integer getWidth() {
+            return this.width;
+        }
+
+        public DataSubImagesKvInfoKvDetailsValueValueRect setHeight(Integer height) {
+            this.height = height;
+            return this;
+        }
+        public Integer getHeight() {
+            return this.height;
+        }
+
+    }
+
+}
---
+++ ocr-api-20210707/src/main/java/com/aliyun/ocr_api20210707/models/RecognizeAllTextResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ocr_api20210707.models;
+
+import com.aliyun.tea.*;
+
+public class RecognizeAllTextResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public RecognizeAllTextResponseBody body;
+
+    public static RecognizeAllTextResponse build(java.util.Map<String, ?> map) throws Exception {
+        RecognizeAllTextResponse self = new RecognizeAllTextResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public RecognizeAllTextResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public RecognizeAllTextResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public RecognizeAllTextResponse setBody(RecognizeAllTextResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public RecognizeAllTextResponseBody getBody() {
+        return this.body;
+    }
+
+}
