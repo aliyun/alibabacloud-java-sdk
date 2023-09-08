bba33709e974f7d3781997a73968104751e87685
sdk-team <sdk-team@alibabacloud.com>
2023-09-08 12:04:13 +0000

    Generated java 2023-06-21 for PaiFeatureStore.

--- paifeaturestore-20230621/src/main/java/com/aliyun/paifeaturestore20230621/models/FeatureViewConfigValue.java
+++ paifeaturestore-20230621/src/main/java/com/aliyun/paifeaturestore20230621/models/FeatureViewConfigValue.java
@@ -7,6 +7,12 @@ public class FeatureViewConfigValue extends TeaModel {
     @NameInMap("Partitions")
     public java.util.Map<String, java.util.Map<String, ?>> partitions;
 
+    @NameInMap("EventTime")
+    public String eventTime;
+
+    @NameInMap("Equal")
+    public Boolean equal;
+
     public static FeatureViewConfigValue build(java.util.Map<String, ?> map) throws Exception {
         FeatureViewConfigValue self = new FeatureViewConfigValue();
         return TeaModel.build(map, self);
@@ -18,6 +24,22 @@ public class FeatureViewConfigValue extends TeaModel {
     }
     public java.util.Map<String, java.util.Map<String, ?>> getPartitions() {
         return this.partitions;
+    }
+
+    public FeatureViewConfigValue setEventTime(String eventTime) {
+        this.eventTime = eventTime;
+        return this;
+    }
+    public String getEventTime() {
+        return this.eventTime;
+    }
+
+    public FeatureViewConfigValue setEqual(Boolean equal) {
+        this.equal = equal;
+        return this;
+    }
+    public Boolean getEqual() {
+        return this.equal;
     }
 
 }
