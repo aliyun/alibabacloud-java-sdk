4bb4a644dd67d39f51e2f9487aa84cac6f4ef2b7
sdk-team <sdk-team@alibabacloud.com>
2023-09-12 08:43:34 +0000

    Generated java 2022-07-04 for imarketing.

--- imarketing-20220704/src/main/java/com/aliyun/imarketing20220704/Client.java
+++ imarketing-20220704/src/main/java/com/aliyun/imarketing20220704/Client.java
@@ -737,6 +737,35 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.queryAuditResultWithOptions(request, runtime);
     }
 
+    public QueryBenefitGrantResultResponse queryBenefitGrantResultWithOptions(QueryBenefitGrantResultRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.cloducodeFlowNo)) {
+            body.put("CloducodeFlowNo", request.cloducodeFlowNo);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "QueryBenefitGrantResult"),
+            new TeaPair("version", "2022-07-04"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new QueryBenefitGrantResultResponse());
+    }
+
+    public QueryBenefitGrantResultResponse queryBenefitGrantResult(QueryBenefitGrantResultRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.queryBenefitGrantResultWithOptions(request, runtime);
+    }
+
     public QueryOrderResponse queryOrderWithOptions(QueryOrderRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> body = new java.util.HashMap<>();
