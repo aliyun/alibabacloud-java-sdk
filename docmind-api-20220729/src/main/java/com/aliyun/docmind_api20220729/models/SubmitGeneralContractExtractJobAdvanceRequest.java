dbdc9abed70a91083faf83c60a646c1210279926
sdk-team <sdk-team@alibabacloud.com>
2023-09-12 11:20:25 +0000

    Generated java 2022-07-29 for docmind-api.

---
+++ docmind-api-20220729/src/main/java/com/aliyun/docmind_api20220729/models/SubmitGeneralContractExtractJobAdvanceRequest.java
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.docmind_api20220729.models;
+
+import com.aliyun.tea.*;
+
+public class SubmitGeneralContractExtractJobAdvanceRequest extends TeaModel {
+    @NameInMap("FileName")
+    public String fileName;
+
+    @NameInMap("FileNameExtension")
+    public String fileNameExtension;
+
+    @NameInMap("FileUrl")
+    public java.io.InputStream fileUrlObject;
+
+    public static SubmitGeneralContractExtractJobAdvanceRequest build(java.util.Map<String, ?> map) throws Exception {
+        SubmitGeneralContractExtractJobAdvanceRequest self = new SubmitGeneralContractExtractJobAdvanceRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public SubmitGeneralContractExtractJobAdvanceRequest setFileName(String fileName) {
+        this.fileName = fileName;
+        return this;
+    }
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public SubmitGeneralContractExtractJobAdvanceRequest setFileNameExtension(String fileNameExtension) {
+        this.fileNameExtension = fileNameExtension;
+        return this;
+    }
+    public String getFileNameExtension() {
+        return this.fileNameExtension;
+    }
+
+    public SubmitGeneralContractExtractJobAdvanceRequest setFileUrlObject(java.io.InputStream fileUrlObject) {
+        this.fileUrlObject = fileUrlObject;
+        return this;
+    }
+    public java.io.InputStream getFileUrlObject() {
+        return this.fileUrlObject;
+    }
+
+}
---
+++ docmind-api-20220729/src/main/java/com/aliyun/docmind_api20220729/models/SubmitGeneralContractExtractJobRequest.java
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.docmind_api20220729.models;
+
+import com.aliyun.tea.*;
+
+public class SubmitGeneralContractExtractJobRequest extends TeaModel {
+    @NameInMap("FileName")
+    public String fileName;
+
+    @NameInMap("FileNameExtension")
+    public String fileNameExtension;
+
+    @NameInMap("FileUrl")
+    public String fileUrl;
+
+    public static SubmitGeneralContractExtractJobRequest build(java.util.Map<String, ?> map) throws Exception {
+        SubmitGeneralContractExtractJobRequest self = new SubmitGeneralContractExtractJobRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public SubmitGeneralContractExtractJobRequest setFileName(String fileName) {
+        this.fileName = fileName;
+        return this;
+    }
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public SubmitGeneralContractExtractJobRequest setFileNameExtension(String fileNameExtension) {
+        this.fileNameExtension = fileNameExtension;
+        return this;
+    }
+    public String getFileNameExtension() {
+        return this.fileNameExtension;
+    }
+
+    public SubmitGeneralContractExtractJobRequest setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+        return this;
+    }
+    public String getFileUrl() {
+        return this.fileUrl;
+    }
+
+}
