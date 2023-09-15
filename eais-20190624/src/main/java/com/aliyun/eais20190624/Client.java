f6b1997915c2017d79d85096d40418bab6a41f11
sdk-team <sdk-team@alibabacloud.com>
2023-09-15 08:49:23 +0000

    Generated java 2019-06-24 for eais.

--- eais-20190624/src/main/java/com/aliyun/eais20190624/Client.java
+++ eais-20190624/src/main/java/com/aliyun/eais20190624/Client.java
@@ -711,6 +711,10 @@ public class Client extends com.aliyun.teaopenapi.Client {
     public DescribeEaisResponse describeEaisWithOptions(DescribeEaisRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.clientInstanceId)) {
+            query.put("ClientInstanceId", request.clientInstanceId);
+        }
+
         if (!com.aliyun.teautil.Common.isUnset(request.elasticAcceleratedInstanceIds)) {
             query.put("ElasticAcceleratedInstanceIds", request.elasticAcceleratedInstanceIds);
         }
