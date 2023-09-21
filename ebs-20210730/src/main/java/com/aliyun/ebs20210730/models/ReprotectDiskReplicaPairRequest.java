a6e08ea711cad7ea1841dd99cc3a42a5d137c82a
sdk-team <sdk-team@alibabacloud.com>
2023-09-21 17:16:37 +0000

    Generated java 2021-07-30 for ebs.

--- ebs-20210730/src/main/java/com/aliyun/ebs20210730/models/ReprotectDiskReplicaPairRequest.java
+++ ebs-20210730/src/main/java/com/aliyun/ebs20210730/models/ReprotectDiskReplicaPairRequest.java
@@ -13,6 +13,12 @@ public class ReprotectDiskReplicaPairRequest extends TeaModel {
     @NameInMap("ReplicaPairId")
     public String replicaPairId;
 
+    /**
+     * <p>反向复制开关：false代表恢复原方向，true代表反向复制。默认值是true。</p>
+     */
+    @NameInMap("ReverseReplicate")
+    public Boolean reverseReplicate;
+
     public static ReprotectDiskReplicaPairRequest build(java.util.Map<String, ?> map) throws Exception {
         ReprotectDiskReplicaPairRequest self = new ReprotectDiskReplicaPairRequest();
         return TeaModel.build(map, self);
@@ -40,6 +46,14 @@ public class ReprotectDiskReplicaPairRequest extends TeaModel {
     }
     public String getReplicaPairId() {
         return this.replicaPairId;
+    }
+
+    public ReprotectDiskReplicaPairRequest setReverseReplicate(Boolean reverseReplicate) {
+        this.reverseReplicate = reverseReplicate;
+        return this;
+    }
+    public Boolean getReverseReplicate() {
+        return this.reverseReplicate;
     }
 
 }
