cf5d6924485ef475d15a87beb1494d09dc3e7e87
sdk-team <sdk-team@alibabacloud.com>
2023-09-26 17:14:57 +0000

    Generated java 2021-02-04 for AIWorkSpace.

--- aiworkspace-20210204/src/main/java/com/aliyun/aiworkspace20210204/models/GetMemberRequest.java
+++ aiworkspace-20210204/src/main/java/com/aliyun/aiworkspace20210204/models/GetMemberRequest.java
@@ -4,12 +4,23 @@ package com.aliyun.aiworkspace20210204.models;
 import com.aliyun.tea.*;
 
 public class GetMemberRequest extends TeaModel {
+    @NameInMap("MemberId")
+    public String memberId;
+
     @NameInMap("UserId")
     public String userId;
 
     public static GetMemberRequest build(java.util.Map<String, ?> map) throws Exception {
         GetMemberRequest self = new GetMemberRequest();
         return TeaModel.build(map, self);
+    }
+
+    public GetMemberRequest setMemberId(String memberId) {
+        this.memberId = memberId;
+        return this;
+    }
+    public String getMemberId() {
+        return this.memberId;
     }
 
     public GetMemberRequest setUserId(String userId) {
