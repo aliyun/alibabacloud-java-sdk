1d2f74538f480745f715023b2bba4b52c10a2926
sdk-team <sdk-team@alibabacloud.com>
2023-10-07 09:55:14 +0000

    Generated java 2021-09-24 for serverless.

--- serverless-20210924/src/main/java/com/aliyun/serverless20210924/models/PipelineStatus.java
+++ serverless-20210924/src/main/java/com/aliyun/serverless20210924/models/PipelineStatus.java
@@ -4,12 +4,23 @@ package com.aliyun.serverless20210924.models;
 import com.aliyun.tea.*;
 
 public class PipelineStatus extends TeaModel {
+    @NameInMap("latestExecError")
+    public TaskExecError latestExecError;
+
     @NameInMap("phase")
     public String phase;
 
     public static PipelineStatus build(java.util.Map<String, ?> map) throws Exception {
         PipelineStatus self = new PipelineStatus();
         return TeaModel.build(map, self);
+    }
+
+    public PipelineStatus setLatestExecError(TaskExecError latestExecError) {
+        this.latestExecError = latestExecError;
+        return this;
+    }
+    public TaskExecError getLatestExecError() {
+        return this.latestExecError;
     }
 
     public PipelineStatus setPhase(String phase) {
