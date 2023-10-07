4ca8e2c0cf5986548255540ff07ab5c3cc2c693e
sdk-team <sdk-team@alibabacloud.com>
2023-10-07 17:12:18 +0000

    Generated java 2021-02-04 for AIWorkSpace.

--- aiworkspace-20210204/src/main/java/com/aliyun/aiworkspace20210204/models/DeleteMembersResponseBody.java
+++ aiworkspace-20210204/src/main/java/com/aliyun/aiworkspace20210204/models/DeleteMembersResponseBody.java
@@ -4,12 +4,34 @@ package com.aliyun.aiworkspace20210204.models;
 import com.aliyun.tea.*;
 
 public class DeleteMembersResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public String code;
+
+    @NameInMap("Message")
+    public String message;
+
     @NameInMap("RequestId")
     public String requestId;
 
     public static DeleteMembersResponseBody build(java.util.Map<String, ?> map) throws Exception {
         DeleteMembersResponseBody self = new DeleteMembersResponseBody();
         return TeaModel.build(map, self);
+    }
+
+    public DeleteMembersResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public DeleteMembersResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
     }
 
     public DeleteMembersResponseBody setRequestId(String requestId) {
