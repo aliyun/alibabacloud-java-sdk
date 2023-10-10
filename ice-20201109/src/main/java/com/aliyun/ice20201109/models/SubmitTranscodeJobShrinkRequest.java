8e626b65e3b83ada20795b6592c938b3f42eee70
sdk-team <sdk-team@alibabacloud.com>
2023-10-10 09:21:10 +0000

    Generated java 2020-11-09 for ICE.

--- ice-20201109/src/main/java/com/aliyun/ice20201109/models/SubmitTranscodeJobShrinkRequest.java
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/models/SubmitTranscodeJobShrinkRequest.java
@@ -4,6 +4,9 @@ package com.aliyun.ice20201109.models;
 import com.aliyun.tea.*;
 
 public class SubmitTranscodeJobShrinkRequest extends TeaModel {
+    @NameInMap("ClientToken")
+    public String clientToken;
+
     @NameInMap("InputGroup")
     public String inputGroupShrink;
 
@@ -22,6 +25,14 @@ public class SubmitTranscodeJobShrinkRequest extends TeaModel {
     public static SubmitTranscodeJobShrinkRequest build(java.util.Map<String, ?> map) throws Exception {
         SubmitTranscodeJobShrinkRequest self = new SubmitTranscodeJobShrinkRequest();
         return TeaModel.build(map, self);
+    }
+
+    public SubmitTranscodeJobShrinkRequest setClientToken(String clientToken) {
+        this.clientToken = clientToken;
+        return this;
+    }
+    public String getClientToken() {
+        return this.clientToken;
     }
 
     public SubmitTranscodeJobShrinkRequest setInputGroupShrink(String inputGroupShrink) {
