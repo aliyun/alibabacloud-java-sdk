078b226988b43c9a6737b37935a2f153d5aa3127
sdk-team <sdk-team@alibabacloud.com>
2023-10-19 17:20:29 +0000

    Generated java 2015-12-01 for Dds.

--- dds-20151201/src/main/java/com/aliyun/dds20151201/models/DescribeDBInstanceTDEInfoResponseBody.java
+++ dds-20151201/src/main/java/com/aliyun/dds20151201/models/DescribeDBInstanceTDEInfoResponseBody.java
@@ -4,8 +4,17 @@ package com.aliyun.dds20151201.models;
 import com.aliyun.tea.*;
 
 public class DescribeDBInstanceTDEInfoResponseBody extends TeaModel {
+    @NameInMap("EncryptionKey")
+    public String encryptionKey;
+
+    @NameInMap("EncryptorName")
+    public String encryptorName;
+
     @NameInMap("RequestId")
     public String requestId;
+
+    @NameInMap("RoleARN")
+    public String roleARN;
 
     @NameInMap("TDEStatus")
     public String TDEStatus;
@@ -15,12 +24,36 @@ public class DescribeDBInstanceTDEInfoResponseBody extends TeaModel {
         return TeaModel.build(map, self);
     }
 
+    public DescribeDBInstanceTDEInfoResponseBody setEncryptionKey(String encryptionKey) {
+        this.encryptionKey = encryptionKey;
+        return this;
+    }
+    public String getEncryptionKey() {
+        return this.encryptionKey;
+    }
+
+    public DescribeDBInstanceTDEInfoResponseBody setEncryptorName(String encryptorName) {
+        this.encryptorName = encryptorName;
+        return this;
+    }
+    public String getEncryptorName() {
+        return this.encryptorName;
+    }
+
     public DescribeDBInstanceTDEInfoResponseBody setRequestId(String requestId) {
         this.requestId = requestId;
         return this;
     }
     public String getRequestId() {
         return this.requestId;
+    }
+
+    public DescribeDBInstanceTDEInfoResponseBody setRoleARN(String roleARN) {
+        this.roleARN = roleARN;
+        return this;
+    }
+    public String getRoleARN() {
+        return this.roleARN;
     }
 
     public DescribeDBInstanceTDEInfoResponseBody setTDEStatus(String TDEStatus) {
