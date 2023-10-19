a9b45629a400952f0930c3aedcde51ac44af7741
sdk-team <sdk-team@alibabacloud.com>
2023-10-19 08:30:38 +0000

    Generated java 2018-02-08 for Domain.

--- domain-20180208/src/main/java/com/aliyun/domain20180208/Client.java
+++ domain-20180208/src/main/java/com/aliyun/domain20180208/Client.java
@@ -883,6 +883,35 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.reserveIntlDomainWithOptions(request, runtime);
     }
 
+    public SelectedDomainListResponse selectedDomainListWithOptions(SelectedDomainListRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.listDate)) {
+            query.put("ListDate", request.listDate);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "SelectedDomainList"),
+            new TeaPair("version", "2018-02-08"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new SelectedDomainListResponse());
+    }
+
+    public SelectedDomainListResponse selectedDomainList(SelectedDomainListRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.selectedDomainListWithOptions(request, runtime);
+    }
+
     public UpdatePartnerReservePriceResponse updatePartnerReservePriceWithOptions(UpdatePartnerReservePriceRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> body = new java.util.HashMap<>();
