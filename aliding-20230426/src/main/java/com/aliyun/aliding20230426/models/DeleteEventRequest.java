c5b11ac41fa5b0f654d7303d5dc5ad547ce3f052
sdk-team <sdk-team@alibabacloud.com>
2023-10-23 11:17:40 +0000

    Generated java 2023-04-26 for aliding.

--- aliding-20230426/src/main/java/com/aliyun/aliding20230426/models/DeleteEventRequest.java
+++ aliding-20230426/src/main/java/com/aliyun/aliding20230426/models/DeleteEventRequest.java
@@ -10,6 +10,9 @@ public class DeleteEventRequest extends TeaModel {
     @NameInMap("EventId")
     public String eventId;
 
+    @NameInMap("pushNotification")
+    public Boolean pushNotification;
+
     public static DeleteEventRequest build(java.util.Map<String, ?> map) throws Exception {
         DeleteEventRequest self = new DeleteEventRequest();
         return TeaModel.build(map, self);
@@ -29,6 +32,14 @@ public class DeleteEventRequest extends TeaModel {
     }
     public String getEventId() {
         return this.eventId;
+    }
+
+    public DeleteEventRequest setPushNotification(Boolean pushNotification) {
+        this.pushNotification = pushNotification;
+        return this;
+    }
+    public Boolean getPushNotification() {
+        return this.pushNotification;
     }
 
 }
