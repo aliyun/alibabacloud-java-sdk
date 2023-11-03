7a882ffe3065138d3922b6f778a56180f34c4757
sdk-team <sdk-team@alibabacloud.com>
2023-11-03 06:01:14 +0000

    Generated java 2022-02-22 for Ess.

--- ess-20220222/src/main/java/com/aliyun/ess20220222/Client.java
+++ ess-20220222/src/main/java/com/aliyun/ess20220222/Client.java
@@ -181,6 +181,10 @@ public class Client extends com.aliyun.teaopenapi.Client {
     public AttachInstancesResponse attachInstancesWithOptions(AttachInstancesRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.clientToken)) {
+            query.put("ClientToken", request.clientToken);
+        }
+
         if (!com.aliyun.teautil.Common.isUnset(request.entrusted)) {
             query.put("Entrusted", request.entrusted);
         }
