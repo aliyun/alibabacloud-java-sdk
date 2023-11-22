5bccf23970b7cf27abcee0422ced2c89296901ae
sdk-team <sdk-team@alibabacloud.com>
2023-11-22 07:42:08 +0000

    Generated java 2023-03-05 for captcha.

--- captcha-20230305/src/main/java/com/aliyun/captcha20230305/models/VerifyIntelligentCaptchaRequest.java
+++ captcha-20230305/src/main/java/com/aliyun/captcha20230305/models/VerifyIntelligentCaptchaRequest.java
@@ -7,6 +7,9 @@ public class VerifyIntelligentCaptchaRequest extends TeaModel {
     @NameInMap("CaptchaVerifyParam")
     public String captchaVerifyParam;
 
+    @NameInMap("SceneId")
+    public String sceneId;
+
     public static VerifyIntelligentCaptchaRequest build(java.util.Map<String, ?> map) throws Exception {
         VerifyIntelligentCaptchaRequest self = new VerifyIntelligentCaptchaRequest();
         return TeaModel.build(map, self);
@@ -18,6 +21,14 @@ public class VerifyIntelligentCaptchaRequest extends TeaModel {
     }
     public String getCaptchaVerifyParam() {
         return this.captchaVerifyParam;
+    }
+
+    public VerifyIntelligentCaptchaRequest setSceneId(String sceneId) {
+        this.sceneId = sceneId;
+        return this;
+    }
+    public String getSceneId() {
+        return this.sceneId;
     }
 
 }
