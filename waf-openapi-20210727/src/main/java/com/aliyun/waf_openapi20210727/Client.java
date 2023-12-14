5e2c4fe5bab2d9eccf5c77a0b389c5e278df71c5
sdk-team <sdk-team@alibabacloud.com>
2023-12-14 12:21:29 +0000

    Generated java 2021-07-27 for waf-openapi.

---
+++ waf-openapi-20210727/src/main/java/com/aliyun/waf_openapi20210727/Client.java
@@ -0,0 +1,53 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.waf_openapi20210727;
+
+import com.aliyun.tea.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(com.aliyun.teaopenapi.models.Config config) throws Exception {
+        super(config);
+        this._endpointRule = "regional";
+        this._endpointMap = TeaConverter.buildMap(
+            new TeaPair("cn-qingdao", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-beijing", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-chengdu", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-zhangjiakou", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-huhehaote", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-hangzhou", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-shanghai", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-shenzhen", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-heyuan", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-wulanchabu", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-hongkong", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-1", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-2", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-3", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-5", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("eu-west-1", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("us-west-1", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("us-east-1", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("eu-central-1", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("me-east-1", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-south-1", "wafopenapi.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("cn-shanghai-finance-1", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-shenzhen-finance-1", "wafopenapi.cn-hangzhou.aliyuncs.com"),
+            new TeaPair("cn-north-2-gov-1", "wafopenapi.cn-hangzhou.aliyuncs.com")
+        );
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("waf-openapi", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+}
