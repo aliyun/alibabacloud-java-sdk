19ecb7aa4689b36514cd739ced7a6afb301aa570
sdk-team <sdk-team@alibabacloud.com>
2023-12-14 03:32:46 +0000

    Generated java 2019-09-01 for OceanBasePro.

---
+++ oceanbasepro-20190901/src/main/java/com/aliyun/oceanbasepro20190901/models/ModifyInstanceTemporaryCapacityResponseBody.java
@@ -0,0 +1,23 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.oceanbasepro20190901.models;
+
+import com.aliyun.tea.*;
+
+public class ModifyInstanceTemporaryCapacityResponseBody extends TeaModel {
+    @NameInMap("RequestId")
+    public String requestId;
+
+    public static ModifyInstanceTemporaryCapacityResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        ModifyInstanceTemporaryCapacityResponseBody self = new ModifyInstanceTemporaryCapacityResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public ModifyInstanceTemporaryCapacityResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+}
