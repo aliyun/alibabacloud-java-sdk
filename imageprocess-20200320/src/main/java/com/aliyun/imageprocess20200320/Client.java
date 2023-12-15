81f148c5ed7083048025bfbd5900388158683563
sdk-team <sdk-team@alibabacloud.com>
2023-12-15 03:31:46 +0000

    Generated java 2020-03-20 for imageprocess.

--- imageprocess-20200320/src/main/java/com/aliyun/imageprocess20200320/Client.java
+++ imageprocess-20200320/src/main/java/com/aliyun/imageprocess20200320/Client.java
@@ -2631,6 +2631,88 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.screenECWithOptions(request, runtime);
     }
 
+    public ScreenECResponse screenECAdvance(ScreenECAdvanceRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        // Step 0: init client
+        String accessKeyId = _credential.getAccessKeyId();
+        String accessKeySecret = _credential.getAccessKeySecret();
+        String securityToken = _credential.getSecurityToken();
+        String credentialType = _credential.getType();
+        String openPlatformEndpoint = _openPlatformEndpoint;
+        if (com.aliyun.teautil.Common.isUnset(openPlatformEndpoint)) {
+            openPlatformEndpoint = "openplatform.aliyuncs.com";
+        }
+
+        if (com.aliyun.teautil.Common.isUnset(credentialType)) {
+            credentialType = "access_key";
+        }
+
+        com.aliyun.teaopenapi.models.Config authConfig = com.aliyun.teaopenapi.models.Config.build(TeaConverter.buildMap(
+            new TeaPair("accessKeyId", accessKeyId),
+            new TeaPair("accessKeySecret", accessKeySecret),
+            new TeaPair("securityToken", securityToken),
+            new TeaPair("type", credentialType),
+            new TeaPair("endpoint", openPlatformEndpoint),
+            new TeaPair("protocol", _protocol),
+            new TeaPair("regionId", _regionId)
+        ));
+        com.aliyun.openplatform20191219.Client authClient = new com.aliyun.openplatform20191219.Client(authConfig);
+        com.aliyun.openplatform20191219.models.AuthorizeFileUploadRequest authRequest = com.aliyun.openplatform20191219.models.AuthorizeFileUploadRequest.build(TeaConverter.buildMap(
+            new TeaPair("product", "imageprocess"),
+            new TeaPair("regionId", _regionId)
+        ));
+        com.aliyun.openplatform20191219.models.AuthorizeFileUploadResponse authResponse = new com.aliyun.openplatform20191219.models.AuthorizeFileUploadResponse();
+        com.aliyun.oss.models.Config ossConfig = com.aliyun.oss.models.Config.build(TeaConverter.buildMap(
+            new TeaPair("accessKeySecret", accessKeySecret),
+            new TeaPair("type", "access_key"),
+            new TeaPair("protocol", _protocol),
+            new TeaPair("regionId", _regionId)
+        ));
+        com.aliyun.oss.Client ossClient = null;
+        com.aliyun.fileform.models.FileField fileObj = new com.aliyun.fileform.models.FileField();
+        com.aliyun.oss.models.PostObjectRequest.PostObjectRequestHeader ossHeader = new com.aliyun.oss.models.PostObjectRequest.PostObjectRequestHeader();
+        com.aliyun.oss.models.PostObjectRequest uploadRequest = new com.aliyun.oss.models.PostObjectRequest();
+        com.aliyun.ossutil.models.RuntimeOptions ossRuntime = new com.aliyun.ossutil.models.RuntimeOptions();
+        com.aliyun.openapiutil.Client.convert(runtime, ossRuntime);
+        ScreenECRequest screenECReq = new ScreenECRequest();
+        com.aliyun.openapiutil.Client.convert(request, screenECReq);
+        if (!com.aliyun.teautil.Common.isUnset(request.URLList)) {
+            Integer i0 = 0;
+            for (ScreenECAdvanceRequest.ScreenECAdvanceRequestURLList item0 : request.URLList) {
+                if (!com.aliyun.teautil.Common.isUnset(item0.URLObject)) {
+                    authResponse = authClient.authorizeFileUploadWithOptions(authRequest, runtime);
+                    ossConfig.accessKeyId = authResponse.body.accessKeyId;
+                    ossConfig.endpoint = com.aliyun.openapiutil.Client.getEndpoint(authResponse.body.endpoint, authResponse.body.useAccelerate, _endpointType);
+                    ossClient = new com.aliyun.oss.Client(ossConfig);
+                    fileObj = com.aliyun.fileform.models.FileField.build(TeaConverter.buildMap(
+                        new TeaPair("filename", authResponse.body.objectKey),
+                        new TeaPair("content", item0.URLObject),
+                        new TeaPair("contentType", "")
+                    ));
+                    ossHeader = com.aliyun.oss.models.PostObjectRequest.PostObjectRequestHeader.build(TeaConverter.buildMap(
+                        new TeaPair("accessKeyId", authResponse.body.accessKeyId),
+                        new TeaPair("policy", authResponse.body.encodedPolicy),
+                        new TeaPair("signature", authResponse.body.signature),
+                        new TeaPair("key", authResponse.body.objectKey),
+                        new TeaPair("file", fileObj),
+                        new TeaPair("successActionStatus", "201")
+                    ));
+                    uploadRequest = com.aliyun.oss.models.PostObjectRequest.build(TeaConverter.buildMap(
+                        new TeaPair("bucketName", authResponse.body.bucket),
+                        new TeaPair("header", ossHeader)
+                    ));
+                    ossClient.postObject(uploadRequest, ossRuntime);
+                    ScreenECRequest.ScreenECRequestURLList tmp = screenECReq.URLList.get(i0);
+                    tmp.URL = "http://" + authResponse.body.bucket + "." + authResponse.body.endpoint + "/" + authResponse.body.objectKey + "";
+                    i0 = com.aliyun.darabonbanumber.Client.ltoi(com.aliyun.darabonbanumber.Client.add(com.aliyun.darabonbanumber.Client.itol(i0), com.aliyun.darabonbanumber.Client.itol(1)));
+                }
+
+            }
+        }
+
+        ScreenECResponse screenECResp = this.screenECWithOptions(screenECReq, runtime);
+        return screenECResp;
+    }
+
     public ScreenGCResponse screenGCWithOptions(ScreenGCRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> body = new java.util.HashMap<>();
