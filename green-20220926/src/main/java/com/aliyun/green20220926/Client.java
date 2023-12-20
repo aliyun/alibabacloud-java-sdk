0944bee73f99f170340fabea1d4a90fa84170d03
sdk-team <sdk-team@alibabacloud.com>
2023-12-20 03:44:36 +0000

    Generated java 2022-09-26 for Green.

---
+++ green-20220926/src/main/java/com/aliyun/green20220926/Client.java
@@ -0,0 +1,47 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.green20220926;
+
+import com.aliyun.tea.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(com.aliyun.teaopenapi.models.Config config) throws Exception {
+        super(config);
+        this._endpointRule = "regional";
+        this._endpointMap = TeaConverter.buildMap(
+            new TeaPair("ap-northeast-1", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-south-1", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-2", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-3", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-5", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("cn-chengdu", "green.aliyuncs.com"),
+            new TeaPair("cn-hongkong", "green.aliyuncs.com"),
+            new TeaPair("cn-huhehaote", "green.aliyuncs.com"),
+            new TeaPair("cn-qingdao", "green.aliyuncs.com"),
+            new TeaPair("cn-zhangjiakou", "green.aliyuncs.com"),
+            new TeaPair("eu-central-1", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("eu-west-1", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("me-east-1", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("us-east-1", "green.ap-southeast-1.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-finance", "green.aliyuncs.com"),
+            new TeaPair("cn-shenzhen-finance-1", "green.aliyuncs.com"),
+            new TeaPair("cn-shanghai-finance-1", "green.aliyuncs.com"),
+            new TeaPair("cn-north-2-gov-1", "green.aliyuncs.com")
+        );
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("green", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+}
