6ec6dc19eb79663b1500bbbd8b27bee4ff81b81d
sdk-team <sdk-team@alibabacloud.com>
2023-12-28 09:33:04 +0000

    Generated java 2021-01-12 for safconsole.

--- safconsole-20210112/src/main/java/com/aliyun/safconsole20210112/models/UploadSampleApiResponseBody.java
+++ safconsole-20210112/src/main/java/com/aliyun/safconsole20210112/models/UploadSampleApiResponseBody.java
@@ -4,15 +4,40 @@ package com.aliyun.safconsole20210112.models;
 import com.aliyun.tea.*;
 
 public class UploadSampleApiResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public String code;
+
+    @NameInMap("Message")
+    public String message;
+
     /**
      * <p>Id of the request</p>
      */
     @NameInMap("RequestId")
     public String requestId;
 
+    @NameInMap("Success")
+    public String success;
+
     public static UploadSampleApiResponseBody build(java.util.Map<String, ?> map) throws Exception {
         UploadSampleApiResponseBody self = new UploadSampleApiResponseBody();
         return TeaModel.build(map, self);
+    }
+
+    public UploadSampleApiResponseBody setCode(String code) {
+        this.code = code;
+        return this;
+    }
+    public String getCode() {
+        return this.code;
+    }
+
+    public UploadSampleApiResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
     }
 
     public UploadSampleApiResponseBody setRequestId(String requestId) {
@@ -21,6 +46,14 @@ public class UploadSampleApiResponseBody extends TeaModel {
     }
     public String getRequestId() {
         return this.requestId;
+    }
+
+    public UploadSampleApiResponseBody setSuccess(String success) {
+        this.success = success;
+        return this;
+    }
+    public String getSuccess() {
+        return this.success;
     }
 
 }
