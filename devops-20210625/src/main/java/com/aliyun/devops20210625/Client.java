c857ea44b613945e02f93ac662605a1b1a64f9c3
sdk-team <sdk-team@alibabacloud.com>
2024-01-15 17:20:14 +0000

    Generated java 2021-06-25 for devops.

--- devops-20210625/src/main/java/com/aliyun/devops20210625/Client.java
+++ devops-20210625/src/main/java/com/aliyun/devops20210625/Client.java
@@ -4088,6 +4088,30 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.getWorkitemCommentListWithOptions(organizationId, workitemId, headers, runtime);
     }
 
+    public GetWorkitemFileResponse getWorkitemFileWithOptions(String organizationId, String workitemIdentifier, String fileIdentifier, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers)
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "GetWorkitemFile"),
+            new TeaPair("version", "2021-06-25"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/organization/" + com.aliyun.openapiutil.Client.getEncodeParam(organizationId) + "/workitem/" + com.aliyun.openapiutil.Client.getEncodeParam(workitemIdentifier) + "/files/" + com.aliyun.openapiutil.Client.getEncodeParam(fileIdentifier) + ""),
+            new TeaPair("method", "GET"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new GetWorkitemFileResponse());
+    }
+
+    public GetWorkitemFileResponse getWorkitemFile(String organizationId, String workitemIdentifier, String fileIdentifier) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.getWorkitemFileWithOptions(organizationId, workitemIdentifier, fileIdentifier, headers, runtime);
+    }
+
     public GetWorkitemRelationsResponse getWorkitemRelationsWithOptions(String organizationId, String workitemId, GetWorkitemRelationsRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
---
+++ devops-20210625/src/main/java/com/aliyun/devops20210625/models/GetWorkitemFileResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.devops20210625.models;
+
+import com.aliyun.tea.*;
+
+public class GetWorkitemFileResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public GetWorkitemFileResponseBody body;
+
+    public static GetWorkitemFileResponse build(java.util.Map<String, ?> map) throws Exception {
+        GetWorkitemFileResponse self = new GetWorkitemFileResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public GetWorkitemFileResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public GetWorkitemFileResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public GetWorkitemFileResponse setBody(GetWorkitemFileResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public GetWorkitemFileResponseBody getBody() {
+        return this.body;
+    }
+
+}
