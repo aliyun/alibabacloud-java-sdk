9b783a7c0e07639cfb4afc3033736cdfc0ba232b
sdk-team <sdk-team@alibabacloud.com>
2024-01-16 10:35:01 +0000

    Generated java 2023-08-01 for AiMiaoBi.

---
+++ aimiaobi-20230801/src/main/java/com/aliyun/aimiaobi20230801/models/CreateTokenResponse.java
@@ -0,0 +1,48 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.aimiaobi20230801.models;
+
+import com.aliyun.tea.*;
+
+public class CreateTokenResponse extends TeaModel {
+    @NameInMap("headers")
+    @Validation(required = true)
+    public java.util.Map<String, String> headers;
+
+    @NameInMap("statusCode")
+    @Validation(required = true)
+    public Integer statusCode;
+
+    @NameInMap("body")
+    @Validation(required = true)
+    public CreateTokenResponseBody body;
+
+    public static CreateTokenResponse build(java.util.Map<String, ?> map) throws Exception {
+        CreateTokenResponse self = new CreateTokenResponse();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateTokenResponse setHeaders(java.util.Map<String, String> headers) {
+        this.headers = headers;
+        return this;
+    }
+    public java.util.Map<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public CreateTokenResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
+    }
+
+    public CreateTokenResponse setBody(CreateTokenResponseBody body) {
+        this.body = body;
+        return this;
+    }
+    public CreateTokenResponseBody getBody() {
+        return this.body;
+    }
+
+}
