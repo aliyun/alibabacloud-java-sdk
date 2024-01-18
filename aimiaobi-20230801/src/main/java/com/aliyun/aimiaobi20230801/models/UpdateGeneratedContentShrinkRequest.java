4f62265bffc63638ae5bdcd08b225bcc268801ff
sdk-team <sdk-team@alibabacloud.com>
2024-01-18 03:36:28 +0000

    Generated java 2023-08-01 for AiMiaoBi.

---
+++ aimiaobi-20230801/src/main/java/com/aliyun/aimiaobi20230801/models/UpdateGeneratedContentShrinkRequest.java
@@ -0,0 +1,89 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.aimiaobi20230801.models;
+
+import com.aliyun.tea.*;
+
+public class UpdateGeneratedContentShrinkRequest extends TeaModel {
+    @NameInMap("AgentKey")
+    public String agentKey;
+
+    @NameInMap("Content")
+    public String content;
+
+    @NameInMap("ContentText")
+    public String contentText;
+
+    @NameInMap("Id")
+    public Long id;
+
+    @NameInMap("Keywords")
+    public String keywordsShrink;
+
+    @NameInMap("Prompt")
+    public String prompt;
+
+    @NameInMap("Title")
+    public String title;
+
+    public static UpdateGeneratedContentShrinkRequest build(java.util.Map<String, ?> map) throws Exception {
+        UpdateGeneratedContentShrinkRequest self = new UpdateGeneratedContentShrinkRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public UpdateGeneratedContentShrinkRequest setAgentKey(String agentKey) {
+        this.agentKey = agentKey;
+        return this;
+    }
+    public String getAgentKey() {
+        return this.agentKey;
+    }
+
+    public UpdateGeneratedContentShrinkRequest setContent(String content) {
+        this.content = content;
+        return this;
+    }
+    public String getContent() {
+        return this.content;
+    }
+
+    public UpdateGeneratedContentShrinkRequest setContentText(String contentText) {
+        this.contentText = contentText;
+        return this;
+    }
+    public String getContentText() {
+        return this.contentText;
+    }
+
+    public UpdateGeneratedContentShrinkRequest setId(Long id) {
+        this.id = id;
+        return this;
+    }
+    public Long getId() {
+        return this.id;
+    }
+
+    public UpdateGeneratedContentShrinkRequest setKeywordsShrink(String keywordsShrink) {
+        this.keywordsShrink = keywordsShrink;
+        return this;
+    }
+    public String getKeywordsShrink() {
+        return this.keywordsShrink;
+    }
+
+    public UpdateGeneratedContentShrinkRequest setPrompt(String prompt) {
+        this.prompt = prompt;
+        return this;
+    }
+    public String getPrompt() {
+        return this.prompt;
+    }
+
+    public UpdateGeneratedContentShrinkRequest setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+    public String getTitle() {
+        return this.title;
+    }
+
+}
