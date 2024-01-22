803907e5b886b1dd1a887bff0c5ae21625abaa6b
sdk-team <sdk-team@alibabacloud.com>
2024-01-22 02:51:21 +0000

    Generated java 2018-01-16 for linkedmall.

--- linkedmall-20180116/src/main/java/com/aliyun/linkedmall20180116/Client.java
+++ linkedmall-20180116/src/main/java/com/aliyun/linkedmall20180116/Client.java
@@ -4961,6 +4961,51 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.queryItemInventoryV2WithOptions(request, runtime);
     }
 
+    public QueryItemNextCycleRestrictionResponse queryItemNextCycleRestrictionWithOptions(QueryItemNextCycleRestrictionRequest tmpReq, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(tmpReq);
+        QueryItemNextCycleRestrictionShrinkRequest request = new QueryItemNextCycleRestrictionShrinkRequest();
+        com.aliyun.openapiutil.Client.convert(tmpReq, request);
+        if (!com.aliyun.teautil.Common.isUnset(tmpReq.lmItemId)) {
+            request.lmItemIdShrink = com.aliyun.openapiutil.Client.arrayToStringWithSpecifiedStyle(tmpReq.lmItemId, "LmItemId", "json");
+        }
+
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.bizId)) {
+            query.put("BizId", request.bizId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.subBizId)) {
+            query.put("SubBizId", request.subBizId);
+        }
+
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.lmItemIdShrink)) {
+            body.put("LmItemId", request.lmItemIdShrink);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query)),
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "QueryItemNextCycleRestriction"),
+            new TeaPair("version", "2018-01-16"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new QueryItemNextCycleRestrictionResponse());
+    }
+
+    public QueryItemNextCycleRestrictionResponse queryItemNextCycleRestriction(QueryItemNextCycleRestrictionRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.queryItemNextCycleRestrictionWithOptions(request, runtime);
+    }
+
     public QueryLogisticsResponse queryLogisticsWithOptions(QueryLogisticsRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
