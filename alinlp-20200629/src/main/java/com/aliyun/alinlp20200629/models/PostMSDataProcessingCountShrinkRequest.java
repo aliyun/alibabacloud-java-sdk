a13df169ccb151b2500fe848b3720de480291378
sdk-team <sdk-team@alibabacloud.com>
2024-01-23 17:18:49 +0000

    Generated java 2020-06-29 for alinlp.

---
+++ alinlp-20200629/src/main/java/com/aliyun/alinlp20200629/models/PostMSDataProcessingCountShrinkRequest.java
@@ -0,0 +1,45 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.alinlp20200629.models;
+
+import com.aliyun.tea.*;
+
+public class PostMSDataProcessingCountShrinkRequest extends TeaModel {
+    @NameInMap("DataIds")
+    public String dataIdsShrink;
+
+    @NameInMap("DataImportId")
+    public Long dataImportId;
+
+    @NameInMap("ServiceId")
+    public Long serviceId;
+
+    public static PostMSDataProcessingCountShrinkRequest build(java.util.Map<String, ?> map) throws Exception {
+        PostMSDataProcessingCountShrinkRequest self = new PostMSDataProcessingCountShrinkRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public PostMSDataProcessingCountShrinkRequest setDataIdsShrink(String dataIdsShrink) {
+        this.dataIdsShrink = dataIdsShrink;
+        return this;
+    }
+    public String getDataIdsShrink() {
+        return this.dataIdsShrink;
+    }
+
+    public PostMSDataProcessingCountShrinkRequest setDataImportId(Long dataImportId) {
+        this.dataImportId = dataImportId;
+        return this;
+    }
+    public Long getDataImportId() {
+        return this.dataImportId;
+    }
+
+    public PostMSDataProcessingCountShrinkRequest setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+        return this;
+    }
+    public Long getServiceId() {
+        return this.serviceId;
+    }
+
+}
