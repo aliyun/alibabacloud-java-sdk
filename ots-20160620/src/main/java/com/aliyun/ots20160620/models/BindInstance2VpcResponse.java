2cd2e00ed8fe8a14897427d325088ee6ad0fdadc
sdk-team <sdk-team@alibabacloud.com>
2024-01-25 15:09:15 +0000

    Update Tea.

--- ots-20160620/src/main/java/com/aliyun/ots20160620/models/BindInstance2VpcResponse.java
+++ ots-20160620/src/main/java/com/aliyun/ots20160620/models/BindInstance2VpcResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class BindInstance2VpcResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public BindInstance2VpcResponseBody body;
 
     public static BindInstance2VpcResponse build(java.util.Map<String, ?> map) throws Exception {
