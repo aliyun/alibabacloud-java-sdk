6a2a5e389748573fae3b0dbed86fec4fa3e4248a
sdk-team <sdk-team@alibabacloud.com>
2024-01-25 14:47:20 +0000

    Update Tea.

--- imagesearch-20190325/ChangeLog.txt
+++ imagesearch-20190325/ChangeLog.txt
@@ -1,3 +1,6 @@
+2024-01-25 Version: 2.0.2
+- Update Tea.
+
 2021-11-17 Version: 2.0.1
 - Support multi region.
 - Fix Bug for V2.
--- imagesearch-20190325/src/main/java/com/aliyun/imagesearch20190325/models/AddImageResponse.java
+++ imagesearch-20190325/src/main/java/com/aliyun/imagesearch20190325/models/AddImageResponse.java
@@ -5,11 +5,12 @@ import com.aliyun.tea.*;
 
 public class AddImageResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    public Integer statusCode;
+
     @NameInMap("body")
-    @Validation(required = true)
     public AddImageResponseBody body;
 
     public static AddImageResponse build(java.util.Map<String, ?> map) throws Exception {
@@ -23,6 +24,14 @@ public class AddImageResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public AddImageResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public AddImageResponse setBody(AddImageResponseBody body) {
