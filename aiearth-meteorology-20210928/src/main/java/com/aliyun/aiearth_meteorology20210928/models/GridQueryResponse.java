b55837a8801612deee80ac41eb292539fd31b7dc
sdk-team <sdk-team@alibabacloud.com>
2024-01-25 14:23:41 +0000

    Update Tea.

--- aiearth-meteorology-20210928/src/main/java/com/aliyun/aiearth_meteorology20210928/models/GridQueryResponse.java
+++ aiearth-meteorology-20210928/src/main/java/com/aliyun/aiearth_meteorology20210928/models/GridQueryResponse.java
@@ -5,11 +5,12 @@ import com.aliyun.tea.*;
 
 public class GridQueryResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    public Integer statusCode;
+
     @NameInMap("body")
-    @Validation(required = true)
     public GridQueryResponseBody body;
 
     public static GridQueryResponse build(java.util.Map<String, ?> map) throws Exception {
@@ -23,6 +24,14 @@ public class GridQueryResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public GridQueryResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public GridQueryResponse setBody(GridQueryResponseBody body) {
