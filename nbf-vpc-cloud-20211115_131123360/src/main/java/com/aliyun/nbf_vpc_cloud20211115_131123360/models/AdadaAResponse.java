6ece62093342a012b677ca56d5df40b0d0e63262
sdk-team <sdk-team@alibabacloud.com>
2024-01-25 15:03:04 +0000

    Update Tea.

--- nbf-vpc-cloud-20211115_131123360/src/main/java/com/aliyun/nbf_vpc_cloud20211115_131123360/models/AdadaAResponse.java
+++ nbf-vpc-cloud-20211115_131123360/src/main/java/com/aliyun/nbf_vpc_cloud20211115_131123360/models/AdadaAResponse.java
@@ -5,11 +5,12 @@ import com.aliyun.tea.*;
 
 public class AdadaAResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    public Integer statusCode;
+
     @NameInMap("body")
-    @Validation(required = true)
     public AdadaAResponseBody body;
 
     public static AdadaAResponse build(java.util.Map<String, ?> map) throws Exception {
@@ -23,6 +24,14 @@ public class AdadaAResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public AdadaAResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public AdadaAResponse setBody(AdadaAResponseBody body) {
