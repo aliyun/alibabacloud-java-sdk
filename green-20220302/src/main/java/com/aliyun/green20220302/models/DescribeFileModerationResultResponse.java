c81df4f16090b927a03d6112d0377e444a07abf8
sdk-team <sdk-team@alibabacloud.com>
2024-01-31 17:34:36 +0000

    Support API DescribeUrlModerationResult.

--- green-20220302/src/main/java/com/aliyun/green20220302/models/DescribeFileModerationResultResponse.java
+++ green-20220302/src/main/java/com/aliyun/green20220302/models/DescribeFileModerationResultResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class DescribeFileModerationResultResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public DescribeFileModerationResultResponseBody body;
 
     public static DescribeFileModerationResultResponse build(java.util.Map<String, ?> map) throws Exception {
--- green-20220302/src/main/java/com/aliyun/green20220302/models/DescribeImageResultExtResponse.java
+++ green-20220302/src/main/java/com/aliyun/green20220302/models/DescribeImageResultExtResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class DescribeImageResultExtResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public DescribeImageResultExtResponseBody body;
 
     public static DescribeImageResultExtResponse build(java.util.Map<String, ?> map) throws Exception {
