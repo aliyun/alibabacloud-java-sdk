01448f06fdf70ecb16c7226d5481b9a266675a20
sdk-team <sdk-team@alibabacloud.com>
2024-02-19 12:30:09 +0000

    Update API AddZone: add param DnsGroup.

--- pvtz-20180101/src/main/java/com/aliyun/pvtz20180101/models/DescribeUserVpcAuthorizationsResponse.java
+++ pvtz-20180101/src/main/java/com/aliyun/pvtz20180101/models/DescribeUserVpcAuthorizationsResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class DescribeUserVpcAuthorizationsResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public DescribeUserVpcAuthorizationsResponseBody body;
 
     public static DescribeUserVpcAuthorizationsResponse build(java.util.Map<String, ?> map) throws Exception {
