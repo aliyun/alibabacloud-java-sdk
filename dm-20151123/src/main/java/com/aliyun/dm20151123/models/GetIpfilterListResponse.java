4a663d052073b0fef27efc5270ad811244d678ee
sdk-team <sdk-team@alibabacloud.com>
2024-02-20 17:18:03 +0000

    Update API BatchSendMail: add param UnSubscribeFilterLevel.

--- dm-20151123/src/main/java/com/aliyun/dm20151123/models/GetIpfilterListResponse.java
+++ dm-20151123/src/main/java/com/aliyun/dm20151123/models/GetIpfilterListResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class GetIpfilterListResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public GetIpfilterListResponseBody body;
 
     public static GetIpfilterListResponse build(java.util.Map<String, ?> map) throws Exception {
--- dm-20151123/src/main/java/com/aliyun/dm20151123/models/GetTrackListResponse.java
+++ dm-20151123/src/main/java/com/aliyun/dm20151123/models/GetTrackListResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class GetTrackListResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public GetTrackListResponseBody body;
 
     public static GetTrackListResponse build(java.util.Map<String, ?> map) throws Exception {
