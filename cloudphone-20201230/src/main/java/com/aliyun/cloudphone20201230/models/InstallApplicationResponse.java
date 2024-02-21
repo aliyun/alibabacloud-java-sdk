ac76d48bef0a49959346fa43576c4bb8ee7df227
sdk-team <sdk-team@alibabacloud.com>
2024-02-21 07:08:35 +0000

    Generated java 2020-12-30 for cloudphone.

--- cloudphone-20201230/src/main/java/com/aliyun/cloudphone20201230/models/InstallApplicationResponse.java
+++ cloudphone-20201230/src/main/java/com/aliyun/cloudphone20201230/models/InstallApplicationResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class InstallApplicationResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public InstallApplicationResponseBody body;
 
     public static InstallApplicationResponse build(java.util.Map<String, ?> map) throws Exception {
