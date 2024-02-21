08b49e13f0395b535fc2b3782db8ddaabcecc58d
sdk-team <sdk-team@alibabacloud.com>
2024-02-21 07:08:35 +0000

    Generated java 2020-12-30 for cloudphone.

--- cloudphone-20201230/src/main/java/com/aliyun/cloudphone20201230/models/FetchFileResponse.java
+++ cloudphone-20201230/src/main/java/com/aliyun/cloudphone20201230/models/FetchFileResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class FetchFileResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public FetchFileResponseBody body;
 
     public static FetchFileResponse build(java.util.Map<String, ?> map) throws Exception {
