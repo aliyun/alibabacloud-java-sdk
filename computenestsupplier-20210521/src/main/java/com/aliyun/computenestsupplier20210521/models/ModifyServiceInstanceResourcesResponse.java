af2b2cd31cb84e228d99c9635608418a2404b81c
sdk-team <sdk-team@alibabacloud.com>
2024-02-22 17:27:28 +0000

    Update API GetService: update response param.

--- computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/models/ModifyServiceInstanceResourcesResponse.java
+++ computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/models/ModifyServiceInstanceResourcesResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class ModifyServiceInstanceResourcesResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public ModifyServiceInstanceResourcesResponseBody body;
 
     public static ModifyServiceInstanceResourcesResponse build(java.util.Map<String, ?> map) throws Exception {
--- computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/models/RegisterServiceResponse.java
+++ computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/models/RegisterServiceResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class RegisterServiceResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public RegisterServiceResponseBody body;
 
     public static RegisterServiceResponse build(java.util.Map<String, ?> map) throws Exception {
