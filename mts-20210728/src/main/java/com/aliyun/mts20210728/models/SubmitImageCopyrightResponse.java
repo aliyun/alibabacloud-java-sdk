bc20df09805572267e32522945f5b39cc9d8c6d5
sdk-team <sdk-team@alibabacloud.com>
2024-02-22 17:21:06 +0000

    Update API QueryCopyrightExtract: add param body.

--- mts-20210728/src/main/java/com/aliyun/mts20210728/models/SubmitImageCopyrightResponse.java
+++ mts-20210728/src/main/java/com/aliyun/mts20210728/models/SubmitImageCopyrightResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class SubmitImageCopyrightResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public SubmitImageCopyrightResponseBody body;
 
     public static SubmitImageCopyrightResponse build(java.util.Map<String, ?> map) throws Exception {
--- mts-20210728/src/main/java/com/aliyun/mts20210728/models/SubmitTracemuResponse.java
+++ mts-20210728/src/main/java/com/aliyun/mts20210728/models/SubmitTracemuResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class SubmitTracemuResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public SubmitTracemuResponseBody body;
 
     public static SubmitTracemuResponse build(java.util.Map<String, ?> map) throws Exception {
