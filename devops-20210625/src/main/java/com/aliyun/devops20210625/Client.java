eda1f060def67d4699e020df79d5e64747bcd6b2
sdk-team <sdk-team@alibabacloud.com>
2024-02-27 09:01:33 +0000

    Support API ListUserDrawRecordByPk.

--- devops-20210625/src/main/java/com/aliyun/devops20210625/Client.java
+++ devops-20210625/src/main/java/com/aliyun/devops20210625/Client.java
@@ -6686,6 +6686,45 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.listTestCaseFieldsWithOptions(organizationId, request, headers, runtime);
     }
 
+    public ListUserDrawRecordByPkResponse listUserDrawRecordByPkWithOptions(ListUserDrawRecordByPkRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.aliyunPk)) {
+            query.put("aliyunPk", request.aliyunPk);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.drawGroup)) {
+            query.put("drawGroup", request.drawGroup);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.drawPoolName)) {
+            query.put("drawPoolName", request.drawPoolName);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "ListUserDrawRecordByPk"),
+            new TeaPair("version", "2021-06-25"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/listUserDrawRecords"),
+            new TeaPair("method", "GET"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new ListUserDrawRecordByPkResponse());
+    }
+
+    public ListUserDrawRecordByPkResponse listUserDrawRecordByPk(ListUserDrawRecordByPkRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.listUserDrawRecordByPkWithOptions(request, headers, runtime);
+    }
+
     public ListUserKeysResponse listUserKeysWithOptions(ListUserKeysRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
