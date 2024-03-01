70e269ef8137cbb03dc3312aab4c4bd8e6eea14b
sdk-team <sdk-team@alibabacloud.com>
2024-03-01 02:00:14 +0000

    Update API CreateUser: update param body.

--- openitag-20220616/src/main/java/com/aliyun/openitag20220616/models/GetTaskStatisticsResponse.java
+++ openitag-20220616/src/main/java/com/aliyun/openitag20220616/models/GetTaskStatisticsResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class GetTaskStatisticsResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public GetTaskStatisticsResponseBody body;
 
     public static GetTaskStatisticsResponse build(java.util.Map<String, ?> map) throws Exception {
