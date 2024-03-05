6c5c29c8df23e43d2b967441202e103e63ee326c
sdk-team <sdk-team@alibabacloud.com>
2024-03-05 17:15:30 +0000

    Update API Verify: update response param.

--- carbonfootprint-20230711/src/main/java/com/aliyun/carbonfootprint20230711/models/VerifyResponseBody.java
+++ carbonfootprint-20230711/src/main/java/com/aliyun/carbonfootprint20230711/models/VerifyResponseBody.java
@@ -31,9 +31,54 @@ public class VerifyResponseBody extends TeaModel {
         return this.requestId;
     }
 
+    public static class VerifyResponseBodyDataAllMultiAccountUids extends TeaModel {
+        @NameInMap("accountId")
+        public String accountId;
+
+        @NameInMap("displayName")
+        public String displayName;
+
+        public static VerifyResponseBodyDataAllMultiAccountUids build(java.util.Map<String, ?> map) throws Exception {
+            VerifyResponseBodyDataAllMultiAccountUids self = new VerifyResponseBodyDataAllMultiAccountUids();
+            return TeaModel.build(map, self);
+        }
+
+        public VerifyResponseBodyDataAllMultiAccountUids setAccountId(String accountId) {
+            this.accountId = accountId;
+            return this;
+        }
+        public String getAccountId() {
+            return this.accountId;
+        }
+
+        public VerifyResponseBodyDataAllMultiAccountUids setDisplayName(String displayName) {
+            this.displayName = displayName;
+            return this;
+        }
+        public String getDisplayName() {
+            return this.displayName;
+        }
+
+    }
+
     public static class VerifyResponseBodyData extends TeaModel {
         @NameInMap("AllowedUids")
         public java.util.List<String> allowedUids;
+
+        @NameInMap("accountType")
+        public Integer accountType;
+
+        @NameInMap("allMultiAccountUids")
+        public java.util.List<VerifyResponseBodyDataAllMultiAccountUids> allMultiAccountUids;
+
+        @NameInMap("code")
+        public String code;
+
+        @NameInMap("message")
+        public String message;
+
+        @NameInMap("multiAccountsAllow")
+        public Integer multiAccountsAllow;
 
         public static VerifyResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
             VerifyResponseBodyData self = new VerifyResponseBodyData();
@@ -46,6 +91,46 @@ public class VerifyResponseBody extends TeaModel {
         }
         public java.util.List<String> getAllowedUids() {
             return this.allowedUids;
+        }
+
+        public VerifyResponseBodyData setAccountType(Integer accountType) {
+            this.accountType = accountType;
+            return this;
+        }
+        public Integer getAccountType() {
+            return this.accountType;
+        }
+
+        public VerifyResponseBodyData setAllMultiAccountUids(java.util.List<VerifyResponseBodyDataAllMultiAccountUids> allMultiAccountUids) {
+            this.allMultiAccountUids = allMultiAccountUids;
+            return this;
+        }
+        public java.util.List<VerifyResponseBodyDataAllMultiAccountUids> getAllMultiAccountUids() {
+            return this.allMultiAccountUids;
+        }
+
+        public VerifyResponseBodyData setCode(String code) {
+            this.code = code;
+            return this;
+        }
+        public String getCode() {
+            return this.code;
+        }
+
+        public VerifyResponseBodyData setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+        public String getMessage() {
+            return this.message;
+        }
+
+        public VerifyResponseBodyData setMultiAccountsAllow(Integer multiAccountsAllow) {
+            this.multiAccountsAllow = multiAccountsAllow;
+            return this;
+        }
+        public Integer getMultiAccountsAllow() {
+            return this.multiAccountsAllow;
         }
 
     }
