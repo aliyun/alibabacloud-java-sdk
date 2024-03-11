0bc38b74663eee80787dee75df48263d75d95b74
sdk-team <sdk-team@alibabacloud.com>
2024-03-11 09:21:59 +0000

    Generated java 2021-04-08 for BizWorks.

---
+++ bizworks-20210408/src/main/java/com/aliyun/bizworks20210408/Client.java
@@ -0,0 +1,49 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.bizworks20210408;
+
+import com.aliyun.tea.*;
+import com.aliyun.bizworks20210408.models.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(com.aliyun.teaopenapi.models.Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("bizworks", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public QueryUsageResponse queryUsageWithOptions(com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teaopenapi.models.OpenApiRequest req = new com.aliyun.teaopenapi.models.OpenApiRequest();
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "QueryUsage"),
+            new TeaPair("version", "2021-04-08"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "GET"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new QueryUsageResponse());
+    }
+
+    public QueryUsageResponse queryUsage() throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.queryUsageWithOptions(runtime);
+    }
+}
