e1fafdaf1daead881670fe5fe6a7f16a1c881094
sdk-team <sdk-team@alibabacloud.com>
2024-03-15 17:19:52 +0000

    Update API VerifyVATInvoice: add param InvoiceKind.

--- ocr-api-20210707/src/main/java/com/aliyun/ocr_api20210707/models/RecognizeFoodManageLicenseResponse.java
+++ ocr-api-20210707/src/main/java/com/aliyun/ocr_api20210707/models/RecognizeFoodManageLicenseResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class RecognizeFoodManageLicenseResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public RecognizeFoodManageLicenseResponseBody body;
 
     public static RecognizeFoodManageLicenseResponse build(java.util.Map<String, ?> map) throws Exception {
--- ocr-api-20210707/src/main/java/com/aliyun/ocr_api20210707/models/RecognizeVehicleLicenseResponse.java
+++ ocr-api-20210707/src/main/java/com/aliyun/ocr_api20210707/models/RecognizeVehicleLicenseResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class RecognizeVehicleLicenseResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public RecognizeVehicleLicenseResponseBody body;
 
     public static RecognizeVehicleLicenseResponse build(java.util.Map<String, ?> map) throws Exception {
