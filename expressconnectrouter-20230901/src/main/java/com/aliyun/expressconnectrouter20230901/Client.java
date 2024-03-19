dc5895e51253f588bae388363280fe1a027f0b1f
sdk-team <sdk-team@alibabacloud.com>
2024-03-19 06:04:37 +0000

    Delete API GetExpressConnectRouter.

--- expressconnectrouter-20230901/src/main/java/com/aliyun/expressconnectrouter20230901/Client.java
+++ expressconnectrouter-20230901/src/main/java/com/aliyun/expressconnectrouter20230901/Client.java
@@ -981,43 +981,6 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.forceDeleteExpressConnectRouterWithOptions(request, runtime);
     }
 
-    public GetExpressConnectRouterResponse getExpressConnectRouterWithOptions(GetExpressConnectRouterRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
-        com.aliyun.teautil.Common.validateModel(request);
-        java.util.Map<String, Object> body = new java.util.HashMap<>();
-        if (!com.aliyun.teautil.Common.isUnset(request.clientToken)) {
-            body.put("ClientToken", request.clientToken);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.dryRun)) {
-            body.put("DryRun", request.dryRun);
-        }
-
-        if (!com.aliyun.teautil.Common.isUnset(request.ecrId)) {
-            body.put("EcrId", request.ecrId);
-        }
-
-        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
-        ));
-        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
-            new TeaPair("action", "GetExpressConnectRouter"),
-            new TeaPair("version", "2023-09-01"),
-            new TeaPair("protocol", "HTTPS"),
-            new TeaPair("pathname", "/"),
-            new TeaPair("method", "POST"),
-            new TeaPair("authType", "AK"),
-            new TeaPair("style", "RPC"),
-            new TeaPair("reqBodyType", "formData"),
-            new TeaPair("bodyType", "json")
-        ));
-        return TeaModel.toModel(this.callApi(params, req, runtime), new GetExpressConnectRouterResponse());
-    }
-
-    public GetExpressConnectRouterResponse getExpressConnectRouter(GetExpressConnectRouterRequest request) throws Exception {
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        return this.getExpressConnectRouterWithOptions(request, runtime);
-    }
-
     public GrantInstanceToExpressConnectRouterResponse grantInstanceToExpressConnectRouterWithOptions(GrantInstanceToExpressConnectRouterRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> body = new java.util.HashMap<>();
