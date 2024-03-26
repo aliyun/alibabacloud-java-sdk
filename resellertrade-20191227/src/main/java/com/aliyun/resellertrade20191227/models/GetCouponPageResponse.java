a4d25e863bbc16ef7a2bce7231f2cbd890d90a28
sdk-team <sdk-team@alibabacloud.com>
2024-03-26 17:28:02 +0000

    Generated java 2019-12-27 for ResellerTrade.

--- resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetCouponPageResponse.java
+++ resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetCouponPageResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class GetCouponPageResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public GetCouponPageResponseBody body;
 
     public static GetCouponPageResponse build(java.util.Map<String, ?> map) throws Exception {
