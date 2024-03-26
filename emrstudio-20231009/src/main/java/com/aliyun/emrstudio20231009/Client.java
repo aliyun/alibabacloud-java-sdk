e192b3898c909ad8abae3b5b5336978bd45e3bc9
sdk-team <sdk-team@alibabacloud.com>
2024-03-26 10:19:52 +0000

    Generated java 2023-10-09 for EmrStudio.

---
+++ emrstudio-20231009/src/main/java/com/aliyun/emrstudio20231009/Client.java
@@ -0,0 +1,71 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.emrstudio20231009;
+
+import com.aliyun.tea.*;
+import com.aliyun.emrstudio20231009.models.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(com.aliyun.teaopenapi.models.Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("emrstudio", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    public ListProjectsResponse listProjectsWithOptions(ListProjectsRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.maxResults)) {
+            query.put("maxResults", request.maxResults);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.nextToken)) {
+            query.put("nextToken", request.nextToken);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.searchVal)) {
+            query.put("searchVal", request.searchVal);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.workspaceId)) {
+            query.put("workspaceId", request.workspaceId);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "ListProjects"),
+            new TeaPair("version", "2023-10-09"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/dolphinscheduler/v3/projects"),
+            new TeaPair("method", "GET"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new ListProjectsResponse());
+    }
+
+    public ListProjectsResponse listProjects(ListProjectsRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.listProjectsWithOptions(request, headers, runtime);
+    }
+}
---
+++ emrstudio-20231009/src/main/java/com/aliyun/emrstudio20231009/models/ListProjectsRequest.java
@@ -0,0 +1,56 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.emrstudio20231009.models;
+
+import com.aliyun.tea.*;
+
+public class ListProjectsRequest extends TeaModel {
+    @NameInMap("maxResults")
+    public Integer maxResults;
+
+    @NameInMap("nextToken")
+    public String nextToken;
+
+    @NameInMap("searchVal")
+    public String searchVal;
+
+    @NameInMap("workspaceId")
+    public Long workspaceId;
+
+    public static ListProjectsRequest build(java.util.Map<String, ?> map) throws Exception {
+        ListProjectsRequest self = new ListProjectsRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public ListProjectsRequest setMaxResults(Integer maxResults) {
+        this.maxResults = maxResults;
+        return this;
+    }
+    public Integer getMaxResults() {
+        return this.maxResults;
+    }
+
+    public ListProjectsRequest setNextToken(String nextToken) {
+        this.nextToken = nextToken;
+        return this;
+    }
+    public String getNextToken() {
+        return this.nextToken;
+    }
+
+    public ListProjectsRequest setSearchVal(String searchVal) {
+        this.searchVal = searchVal;
+        return this;
+    }
+    public String getSearchVal() {
+        return this.searchVal;
+    }
+
+    public ListProjectsRequest setWorkspaceId(Long workspaceId) {
+        this.workspaceId = workspaceId;
+        return this;
+    }
+    public Long getWorkspaceId() {
+        return this.workspaceId;
+    }
+
+}
