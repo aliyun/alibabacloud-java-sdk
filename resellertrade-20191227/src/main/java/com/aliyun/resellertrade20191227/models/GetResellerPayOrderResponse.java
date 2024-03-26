c5ff4f1a6209c21e6821d8a4b38cd6d3dfcfce0e
sdk-team <sdk-team@alibabacloud.com>
2024-03-26 17:28:02 +0000

    Generated java 2019-12-27 for ResellerTrade.

--- resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetResellerPayOrderResponse.java
+++ resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/GetResellerPayOrderResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class GetResellerPayOrderResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public GetResellerPayOrderResponseBody body;
 
     public static GetResellerPayOrderResponse build(java.util.Map<String, ?> map) throws Exception {
--- resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/PayResultCallbackResponse.java
+++ resellertrade-20191227/src/main/java/com/aliyun/resellertrade20191227/models/PayResultCallbackResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class PayResultCallbackResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public PayResultCallbackResponseBody body;
 
     public static PayResultCallbackResponse build(java.util.Map<String, ?> map) throws Exception {
