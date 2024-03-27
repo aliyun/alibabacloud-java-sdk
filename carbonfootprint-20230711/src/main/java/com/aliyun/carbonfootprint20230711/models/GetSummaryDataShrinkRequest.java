7b860cee73e1bff1926e9df393b08060351edc48
sdk-team <sdk-team@alibabacloud.com>
2024-03-27 06:04:36 +0000

    Update API GetSummaryData: add param Uids.

--- carbonfootprint-20230711/src/main/java/com/aliyun/carbonfootprint20230711/models/GetSummaryDataShrinkRequest.java
+++ carbonfootprint-20230711/src/main/java/com/aliyun/carbonfootprint20230711/models/GetSummaryDataShrinkRequest.java
@@ -4,15 +4,27 @@ package com.aliyun.carbonfootprint20230711.models;
 import com.aliyun.tea.*;
 
 public class GetSummaryDataShrinkRequest extends TeaModel {
+    /**
+     * <p>The end of the time range to query. Specify the time in the yyyy-MM-dd HH:mm:ss format. Example: 2023-02-01 23:59:59.</p>
+     */
     @NameInMap("EndTime")
     public String endTime;
 
+    /**
+     * <p>The statistical dimension. A value of productCode specifies that statistics are collected based on cloud service. A value of region specifies that statistics are collected based on region. A value of subUid specifies that statistics are collected based on Resource Access Management (RAM) user. If you do not specify this parameter, statistics are collected based on Alibaba Cloud account.</p>
+     */
     @NameInMap("Group")
     public String group;
 
+    /**
+     * <p>The beginning of the time range to query. Specify the time in the yyyy-MM-dd HH:mm:ss format. Example: 2023-01-01 00:00:00.</p>
+     */
     @NameInMap("StartTime")
     public String startTime;
 
+    /**
+     * <p>The list of Alibaba Cloud account IDs whose data needs to be queried.(used after enabling multi-account management).</p>
+     */
     @NameInMap("Uids")
     public String uidsShrink;
 
