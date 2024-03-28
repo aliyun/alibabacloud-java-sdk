9d5c7177d954b412674c175b29a7a43169720be8
sdk-team <sdk-team@alibabacloud.com>
2024-03-28 17:21:52 +0000

    Generated java 2020-03-06 for companyreg.

--- companyreg-20200306/src/main/java/com/aliyun/companyreg20200306/models/QueryUserNeedAuthResponse.java
+++ companyreg-20200306/src/main/java/com/aliyun/companyreg20200306/models/QueryUserNeedAuthResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class QueryUserNeedAuthResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public QueryUserNeedAuthResponseBody body;
 
     public static QueryUserNeedAuthResponse build(java.util.Map<String, ?> map) throws Exception {
--- companyreg-20200306/src/main/java/com/aliyun/companyreg20200306/models/StartBackToBackCallResponse.java
+++ companyreg-20200306/src/main/java/com/aliyun/companyreg20200306/models/StartBackToBackCallResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class StartBackToBackCallResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public StartBackToBackCallResponseBody body;
 
     public static StartBackToBackCallResponse build(java.util.Map<String, ?> map) throws Exception {
