1cbe85ae0ed0ed5ac212a1a59d79b36cb42d1011
sdk-team <sdk-team@alibabacloud.com>
2024-03-29 03:30:30 +0000

    Generated java 2021-05-20 for idaas-doraemon.

--- idaas-doraemon-20210520/src/main/java/com/aliyun/idaas_doraemon20210520/models/ListOrderConsumeStatisticRecordsResponse.java
+++ idaas-doraemon-20210520/src/main/java/com/aliyun/idaas_doraemon20210520/models/ListOrderConsumeStatisticRecordsResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class ListOrderConsumeStatisticRecordsResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public ListOrderConsumeStatisticRecordsResponseBody body;
 
     public static ListOrderConsumeStatisticRecordsResponse build(java.util.Map<String, ?> map) throws Exception {
--- idaas-doraemon-20210520/src/main/java/com/aliyun/idaas_doraemon20210520/models/QuerySmsUpsResponse.java
+++ idaas-doraemon-20210520/src/main/java/com/aliyun/idaas_doraemon20210520/models/QuerySmsUpsResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class QuerySmsUpsResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public QuerySmsUpsResponseBody body;
 
     public static QuerySmsUpsResponse build(java.util.Map<String, ?> map) throws Exception {
