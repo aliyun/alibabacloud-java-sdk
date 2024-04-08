a541e8427b839c237ef1545aac97b902c852d454
sdk-team <sdk-team@alibabacloud.com>
2024-04-08 04:26:11 +0000

    Update API GetSelectionProduct: update param divisionCode.

--- linkedmall-20230930/src/main/java/com/aliyun/linkedmall20230930/models/ListSelectionProductSaleInfosResponse.java
+++ linkedmall-20230930/src/main/java/com/aliyun/linkedmall20230930/models/ListSelectionProductSaleInfosResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class ListSelectionProductSaleInfosResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public ProductSaleInfoListResult body;
 
     public static ListSelectionProductSaleInfosResponse build(java.util.Map<String, ?> map) throws Exception {
