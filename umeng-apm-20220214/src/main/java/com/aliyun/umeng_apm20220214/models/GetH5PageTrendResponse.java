f1f2dd960d2aa788c22c9b7faf88f7aa4578be18
sdk-team <sdk-team@alibabacloud.com>
2024-04-09 02:20:54 +0000

    Update API GetStatTrend: update response param.

--- umeng-apm-20220214/src/main/java/com/aliyun/umeng_apm20220214/models/GetH5PageTrendResponse.java
+++ umeng-apm-20220214/src/main/java/com/aliyun/umeng_apm20220214/models/GetH5PageTrendResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class GetH5PageTrendResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public GetH5PageTrendResponseBody body;
 
     public static GetH5PageTrendResponse build(java.util.Map<String, ?> map) throws Exception {
