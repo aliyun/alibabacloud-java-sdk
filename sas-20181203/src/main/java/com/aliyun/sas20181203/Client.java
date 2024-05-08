1d25838839d3f98145d0552a5ebe745eb6677984
sdk-team <sdk-team@alibabacloud.com>
2024-05-08 17:18:09 +0000

    Update API DescribeAssetDetailByUuid: update response param.

--- sas-20181203/src/main/java/com/aliyun/sas20181203/Client.java
+++ sas-20181203/src/main/java/com/aliyun/sas20181203/Client.java
@@ -11,38 +11,49 @@ public class Client extends com.aliyun.teaopenapi.Client {
         this._endpointRule = "regional";
         this._endpointMap = TeaConverter.buildMap(
             new TeaPair("cn-hangzhou", "tds.aliyuncs.com"),
-            new TeaPair("cn-qingdao", "tds.aliyuncs.com"),
-            new TeaPair("cn-beijing", "tds.aliyuncs.com"),
-            new TeaPair("cn-zhangjiakou", "tds.aliyuncs.com"),
-            new TeaPair("cn-huhehaote", "tds.aliyuncs.com"),
-            new TeaPair("cn-wulanchabu", "tds.aliyuncs.com"),
-            new TeaPair("cn-shanghai", "tds.aliyuncs.com"),
-            new TeaPair("cn-nanjing", "tds.aliyuncs.com"),
-            new TeaPair("cn-fuzhou", "tds.aliyuncs.com"),
-            new TeaPair("cn-shenzhen", "tds.aliyuncs.com"),
-            new TeaPair("cn-heyuan", "tds.aliyuncs.com"),
-            new TeaPair("cn-guangzhou", "tds.aliyuncs.com"),
-            new TeaPair("cn-chengdu", "tds.aliyuncs.com"),
-            new TeaPair("cn-hongkong", "tds.aliyuncs.com"),
+            new TeaPair("cn-shanghai", "tds.cn-shanghai.aliyuncs.com"),
             new TeaPair("ap-southeast-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-northeast-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-northeast-2", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-southeast-2", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-southeast-3", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-southeast-5", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-southeast-6", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-southeast-7", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("us-east-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("us-west-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("eu-west-1", "tds.ap-southeast-1.aliyuncs.com"),
             new TeaPair("eu-central-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("ap-south-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("me-east-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("me-central-1", "tds.ap-southeast-1.aliyuncs.com"),
-            new TeaPair("cn-hangzhou-finance", "tds.aliyuncs.com"),
-            new TeaPair("cn-shanghai-finance-1", "tds.aliyuncs.com"),
-            new TeaPair("cn-shenzhen-finance-1", "tds.aliyuncs.com"),
-            new TeaPair("cn-beijing-finance-1", "tds.aliyuncs.com")
+            new TeaPair("ap-northeast-2-pop", "sas.aliyuncs.com"),
+            new TeaPair("ap-south-1", "tds-vpc.ap-south-1.aliyuncs.com"),
+            new TeaPair("ap-southeast-3", "tds.ap-southeast-3.aliyuncs.com"),
+            new TeaPair("cn-beijing-finance-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-beijing-finance-pop", "sas.aliyuncs.com"),
+            new TeaPair("cn-beijing-gov-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-beijing-nu16-b01", "sas.aliyuncs.com"),
+            new TeaPair("cn-edge-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-fujian", "sas.aliyuncs.com"),
+            new TeaPair("cn-haidian-cm12-c01", "sas.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-bj-b01", "sas.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-finance", "sas.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-internal-prod-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-internal-test-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-internal-test-2", "sas.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-internal-test-3", "sas.aliyuncs.com"),
+            new TeaPair("cn-hangzhou-test-306", "sas.aliyuncs.com"),
+            new TeaPair("cn-hongkong-finance-pop", "sas.aliyuncs.com"),
+            new TeaPair("cn-huhehaote-nebula-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-north-2-gov-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-qingdao-nebula", "sas.aliyuncs.com"),
+            new TeaPair("cn-shanghai-et15-b01", "sas.aliyuncs.com"),
+            new TeaPair("cn-shanghai-et2-b01", "sas.aliyuncs.com"),
+            new TeaPair("cn-shanghai-inner", "sas.aliyuncs.com"),
+            new TeaPair("cn-shanghai-internal-test-1", "sas.aliyuncs.com"),
+            new TeaPair("cn-shenzhen-inner", "sas.aliyuncs.com"),
+            new TeaPair("cn-shenzhen-st4-d01", "sas.aliyuncs.com"),
+            new TeaPair("cn-shenzhen-su18-b01", "sas.aliyuncs.com"),
+            new TeaPair("cn-wuhan", "sas.aliyuncs.com"),
+            new TeaPair("cn-wulanchabu", "sas.aliyuncs.com"),
+            new TeaPair("cn-yushanfang", "sas.aliyuncs.com"),
+            new TeaPair("cn-zhangbei", "sas.aliyuncs.com"),
+            new TeaPair("cn-zhangbei-na61-b01", "sas.aliyuncs.com"),
+            new TeaPair("cn-zhangjiakou-na62-a01", "sas.aliyuncs.com"),
+            new TeaPair("cn-zhengzhou-nebula-1", "sas.aliyuncs.com"),
+            new TeaPair("eu-west-1-oxs", "sas.aliyuncs.com"),
+            new TeaPair("me-east-1", "sas.aliyuncs.com"),
+            new TeaPair("rus-west-1-pop", "sas.aliyuncs.com"),
+            new TeaPair("us-east-1", "tds-vpc.us-east-1.aliyuncs.com"),
+            new TeaPair("us-west-1", "tds.us-west-1.aliyuncs.com")
         );
         this.checkConfig(config);
         this._endpoint = this.getEndpoint("sas", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
@@ -6256,6 +6267,14 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return this.describeCloudCenterInstancesWithOptions(request, runtime);
     }
 
+    /**
+      * @deprecated : DescribeCloudProductFieldStatistics is deprecated, please use Sas::2018-12-03::GetCloudAssetSummary instead.
+      *
+      * @param request DescribeCloudProductFieldStatisticsRequest
+      * @param runtime runtime options for this request RuntimeOptions
+      * @return DescribeCloudProductFieldStatisticsResponse
+     */
+    // Deprecated
     public DescribeCloudProductFieldStatisticsResponse describeCloudProductFieldStatisticsWithOptions(com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teaopenapi.models.OpenApiRequest req = new com.aliyun.teaopenapi.models.OpenApiRequest();
         com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
@@ -6272,6 +6291,12 @@ public class Client extends com.aliyun.teaopenapi.Client {
         return TeaModel.toModel(this.callApi(params, req, runtime), new DescribeCloudProductFieldStatisticsResponse());
     }
 
+    /**
+      * @deprecated : DescribeCloudProductFieldStatistics is deprecated, please use Sas::2018-12-03::GetCloudAssetSummary instead.
+      *
+      * @return DescribeCloudProductFieldStatisticsResponse
+     */
+    // Deprecated
     public DescribeCloudProductFieldStatisticsResponse describeCloudProductFieldStatistics() throws Exception {
         com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
         return this.describeCloudProductFieldStatisticsWithOptions(runtime);
