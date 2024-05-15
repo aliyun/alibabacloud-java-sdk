7af21175ba52bdfb33848d4480c8d306c7271ee9
sdk-team <sdk-team@alibabacloud.com>
2024-05-15 02:10:37 +0000

    Update API CreateJob: update param Tasks.

--- ehpcinstant-20230701/src/main/java/com/aliyun/ehpcinstant20230701/models/GetImageRequest.java
+++ ehpcinstant-20230701/src/main/java/com/aliyun/ehpcinstant20230701/models/GetImageRequest.java
@@ -4,15 +4,29 @@ package com.aliyun.ehpcinstant20230701.models;
 import com.aliyun.tea.*;
 
 public class GetImageRequest extends TeaModel {
+    @NameInMap("ImageCategory")
+    public String imageCategory;
+
     /**
      * <p>This parameter is required.</p>
      */
     @NameInMap("ImageId")
     public String imageId;
 
+    @NameInMap("ImageType")
+    public String imageType;
+
     public static GetImageRequest build(java.util.Map<String, ?> map) throws Exception {
         GetImageRequest self = new GetImageRequest();
         return TeaModel.build(map, self);
+    }
+
+    public GetImageRequest setImageCategory(String imageCategory) {
+        this.imageCategory = imageCategory;
+        return this;
+    }
+    public String getImageCategory() {
+        return this.imageCategory;
     }
 
     public GetImageRequest setImageId(String imageId) {
@@ -21,6 +35,14 @@ public class GetImageRequest extends TeaModel {
     }
     public String getImageId() {
         return this.imageId;
+    }
+
+    public GetImageRequest setImageType(String imageType) {
+        this.imageType = imageType;
+        return this;
+    }
+    public String getImageType() {
+        return this.imageType;
     }
 
 }
