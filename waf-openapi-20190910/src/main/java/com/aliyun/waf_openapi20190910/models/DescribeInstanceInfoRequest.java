caad6c87343873f6759267f936c36ae7115e1c85
sdk-team <sdk-team@alibabacloud.com>
2024-05-27 17:21:30 +0000

    Update API DescribeRules: update param RuleGroupId.

--- waf-openapi-20190910/src/main/java/com/aliyun/waf_openapi20190910/models/DescribeInstanceInfoRequest.java
+++ waf-openapi-20190910/src/main/java/com/aliyun/waf_openapi20190910/models/DescribeInstanceInfoRequest.java
@@ -4,12 +4,26 @@ package com.aliyun.waf_openapi20190910.models;
 import com.aliyun.tea.*;
 
 public class DescribeInstanceInfoRequest extends TeaModel {
+    /**
+     * <p>The ID of the WAF instance.</p>
+     * <br>
+     * <p>If you do not configure this parameter, all WAF instances in the Chinese mainland or all WAF instances outside the Chinese mainland are queried.</p>
+     */
     @NameInMap("InstanceId")
     public String instanceId;
 
+    /**
+     * <p>The region in which the WAF instance is deployed. Valid values:</p>
+     * <br>
+     * <p>*   **cn-hangzhou**: Chinese mainland.</p>
+     * <p>*   **ap-southeast-1**: outside the Chinese mainland.</p>
+     */
     @NameInMap("RegionId")
     public String regionId;
 
+    /**
+     * <p>The ID of the resource group to which the WAF instance belongs in Resource Management. If you do not configure this parameter, the WAF instance belongs to the default resource group.</p>
+     */
     @NameInMap("ResourceGroupId")
     public String resourceGroupId;
 
--- waf-openapi-20190910/src/main/java/com/aliyun/waf_openapi20190910/models/DescribeInstanceInfoResponseBody.java
+++ waf-openapi-20190910/src/main/java/com/aliyun/waf_openapi20190910/models/DescribeInstanceInfoResponseBody.java
@@ -4,9 +4,15 @@ package com.aliyun.waf_openapi20190910.models;
 import com.aliyun.tea.*;
 
 public class DescribeInstanceInfoResponseBody extends TeaModel {
+    /**
+     * <p>The information about the WAF instance.</p>
+     */
     @NameInMap("InstanceInfo")
     public DescribeInstanceInfoResponseBodyInstanceInfo instanceInfo;
 
+    /**
+     * <p>The ID of the request.</p>
+     */
     @NameInMap("RequestId")
     public String requestId;
 
@@ -32,33 +38,109 @@ public class DescribeInstanceInfoResponseBody extends TeaModel {
     }
 
     public static class DescribeInstanceInfoResponseBodyInstanceInfo extends TeaModel {
+        /**
+         * <p>The expiration time of the WAF instance. This value is a UNIX timestamp. Unit: seconds.</p>
+         * <br>
+         * <p>>  If the value of **PayType** is **0**, this parameter is not returned. The value 0 indicates that no WAF instances are purchased.</p>
+         */
         @NameInMap("EndDate")
         public Long endDate;
 
+        /**
+         * <p>Indicates whether the WAF instance has overdue payments. Valid values:</p>
+         * <br>
+         * <p>*   **0**: The instance has overdue payments.</p>
+         * <p>*   **1**: The instance does not have overdue payments.</p>
+         * <br>
+         * <p>>  If the value of **PayType** is **0**, this parameter is not returned. The value 0 indicates that no WAF instances are purchased.</p>
+         */
         @NameInMap("InDebt")
         public Integer inDebt;
 
+        /**
+         * <p>The ID of the WAF instance.</p>
+         * <br>
+         * <p>>  If the value of **PayType** is **0**, this parameter is not returned. The value 0 indicates that no WAF instances are purchased.</p>
+         */
         @NameInMap("InstanceId")
         public String instanceId;
 
+        /**
+         * <p>The activation status of WAF. Valid values:</p>
+         * <br>
+         * <p>*   **0**: No WAF instances are purchased within the Alibaba Cloud account.</p>
+         * <p>*   **1**: A subscription WAF instance is purchased within the Alibaba Cloud account.</p>
+         */
         @NameInMap("PayType")
         public Integer payType;
 
+        /**
+         * <p>The region in which the WAF instance resides. Valid values:</p>
+         * <br>
+         * <p>*   **cn**: a region in the Chinese mainland</p>
+         * <p>*   **cn-hongkong**: a region outside the Chinese mainland</p>
+         * <br>
+         * <p>>  If the value of **PayType** is **0**, this parameter is not returned. The value 0 indicates that no WAF instances are purchased.</p>
+         */
         @NameInMap("Region")
         public String region;
 
+        /**
+         * <p>The number of remaining days before the trial period of the WAF instance ends.</p>
+         * <br>
+         * <p>>  This parameter is returned only if the value of **Trial** is **1**. The value 1 indicates that the free trial of a WAF instance is activated.</p>
+         */
         @NameInMap("RemainDay")
         public Integer remainDay;
 
+        /**
+         * <p>Indicates whether the WAF instance expires. Valid values:</p>
+         * <br>
+         * <p>*   **0**: The instance expires.</p>
+         * <p>*   **1**: The instance does not expire.</p>
+         * <br>
+         * <p>>  If the value of **PayType** is **0**, this parameter is not returned. The value 0 indicates that no WAF instances are purchased.</p>
+         */
         @NameInMap("Status")
         public Integer status;
 
+        /**
+         * <p>The billing method of the WAF instance: The value is fixed as **Subscription**.</p>
+         * <br>
+         * <p>>  If the value of **PayType** is **0**, this parameter is not returned. The value 0 indicates that no WAF instances are purchased.</p>
+         */
         @NameInMap("SubscriptionType")
         public String subscriptionType;
 
+        /**
+         * <p>Indicates whether a WAF instance of the free trial edition is activated within the Alibaba Cloud account. Valid values:</p>
+         * <br>
+         * <p>*   **0**: no</p>
+         * <p>*   **1**: yes</p>
+         * <br>
+         * <p>>  This parameter is returned only if a WAF instance of the free trial edition is activated within the Alibaba Cloud account.</p>
+         */
         @NameInMap("Trial")
         public Integer trial;
 
+        /**
+         * <p>The edition of the WAF instance. Valid values:</p>
+         * <br>
+         * <p>*   **version_pro_china**: a WAF Pro instance in the Chinese mainland</p>
+         * <p>*   **version_business_china**: a WAF Business instance in the Chinese mainland</p>
+         * <p>*   **version_enterprise_china**: a WAF Enterprise instance in the Chinese mainland</p>
+         * <p>*   **version_exclusive_china**: a WAF Exclusive instance in the Chinese mainland</p>
+         * <p>*   **version_hybrid_cloud_standard_china**: a Hybrid Cloud WAF instance in the Chinese mainland</p>
+         * <p>*   **version_pro_china**: a WAF Pro instance outside the Chinese mainland</p>
+         * <p>*   **version_business**: a WAF Business instance outside the Chinese mainland</p>
+         * <p>*   **version_enterprise**: a WAF Enterprise instance outside the Chinese mainland</p>
+         * <p>*   **version_exclusive**: a WAF Exclusive instance outside the Chinese mainland</p>
+         * <p>*   **version_hybrid_cloud_standard**: a Hybrid Cloud WAF instance outside the Chinese mainland</p>
+         * <br>
+         * <p>The preceding list contains all the editions of WAF instances within accounts that are created at the International site. If the returned version is not in the list, check whether your account is created at the International site.</p>
+         * <br>
+         * <p>>  If the value of **PayType** is **0**, this parameter is not returned. The value 0 indicates that no WAF instances are purchased.</p>
+         */
         @NameInMap("Version")
         public String version;
 
