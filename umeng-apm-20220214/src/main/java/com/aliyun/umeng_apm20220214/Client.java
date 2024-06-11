d3a2c2f04de03d6ae537e225a881ae2e58c82b83
sdk-team <sdk-team@alibabacloud.com>
2024-06-11 02:15:20 +0000

    Support API DeleteSymRecords.

--- umeng-apm-20220214/src/main/java/com/aliyun/umeng_apm20220214/Client.java
+++ umeng-apm-20220214/src/main/java/com/aliyun/umeng_apm20220214/Client.java
@@ -27,6 +27,65 @@ public class Client extends com.aliyun.teaopenapi.Client {
     }
 
     /**
+     * @summary 删除符号表记录
+     *
+     * @param tmpReq DeleteSymRecordsRequest
+     * @param headers map
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return DeleteSymRecordsResponse
+     */
+    public DeleteSymRecordsResponse deleteSymRecordsWithOptions(DeleteSymRecordsRequest tmpReq, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(tmpReq);
+        DeleteSymRecordsShrinkRequest request = new DeleteSymRecordsShrinkRequest();
+        com.aliyun.openapiutil.Client.convert(tmpReq, request);
+        if (!com.aliyun.teautil.Common.isUnset(tmpReq.appVersions)) {
+            request.appVersionsShrink = com.aliyun.openapiutil.Client.arrayToStringWithSpecifiedStyle(tmpReq.appVersions, "appVersions", "simple");
+        }
+
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.appVersionsShrink)) {
+            body.put("appVersions", request.appVersionsShrink);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.dataSourceId)) {
+            body.put("dataSourceId", request.dataSourceId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.fileType)) {
+            body.put("fileType", request.fileType);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "DeleteSymRecords"),
+            new TeaPair("version", "2022-02-14"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/deleteSymRecords"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new DeleteSymRecordsResponse());
+    }
+
+    /**
+     * @summary 删除符号表记录
+     *
+     * @param request DeleteSymRecordsRequest
+     * @return DeleteSymRecordsResponse
+     */
+    public DeleteSymRecordsResponse deleteSymRecords(DeleteSymRecordsRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.deleteSymRecordsWithOptions(request, headers, runtime);
+    }
+
+    /**
      * @summary 获取H5页面性能统计数据
      *
      * @param request GetH5PageTrendRequest
