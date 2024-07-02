95238efb364115a09dec15f1aac7e2392c23beff
sdk-team <sdk-team@alibabacloud.com>
2024-07-02 05:12:43 +0000

    Update API CreateSampleApi: update response param.

--- xtee-20210910/src/main/java/com/aliyun/xtee20210910/models/CreateSampleApiResponseBody.java
+++ xtee-20210910/src/main/java/com/aliyun/xtee20210910/models/CreateSampleApiResponseBody.java
@@ -4,6 +4,12 @@ package com.aliyun.xtee20210910.models;
 import com.aliyun.tea.*;
 
 public class CreateSampleApiResponseBody extends TeaModel {
+    @NameInMap("Code")
+    public Integer code;
+
+    @NameInMap("Message")
+    public String message;
+
     /**
      * <p>Id of the request</p>
      * 
@@ -16,6 +22,22 @@ public class CreateSampleApiResponseBody extends TeaModel {
     public static CreateSampleApiResponseBody build(java.util.Map<String, ?> map) throws Exception {
         CreateSampleApiResponseBody self = new CreateSampleApiResponseBody();
         return TeaModel.build(map, self);
+    }
+
+    public CreateSampleApiResponseBody setCode(Integer code) {
+        this.code = code;
+        return this;
+    }
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public CreateSampleApiResponseBody setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+    public String getMessage() {
+        return this.message;
     }
 
     public CreateSampleApiResponseBody setRequestId(String requestId) {
