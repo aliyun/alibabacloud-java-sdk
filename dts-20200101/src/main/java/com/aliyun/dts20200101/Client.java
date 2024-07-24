1277cfdfc6242ee42dd4bba4d4679e4e292eb75e
sdk-team <sdk-team@alibabacloud.com>
2024-07-24 08:23:21 +0000

    Support API DescribeDtsJobConfig.

--- dts-20200101/src/main/java/com/aliyun/dts20200101/Client.java
+++ dts-20200101/src/main/java/com/aliyun/dts20200101/Client.java
@@ -3496,6 +3496,70 @@ public class Client extends com.aliyun.teaopenapi.Client {
 
     /**
      * <b>summary</b> : 
+     * <p>查询DTS任务配置</p>
+     * 
+     * @param request DescribeDtsJobConfigRequest
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return DescribeDtsJobConfigResponse
+     */
+    public DescribeDtsJobConfigResponse describeDtsJobConfigWithOptions(DescribeDtsJobConfigRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.dtsJobId)) {
+            query.put("DtsJobId", request.dtsJobId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.forAcceleration)) {
+            query.put("ForAcceleration", request.forAcceleration);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.module)) {
+            query.put("Module", request.module);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.ownerId)) {
+            query.put("OwnerId", request.ownerId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.regionId)) {
+            query.put("RegionId", request.regionId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.resourceGroupId)) {
+            query.put("ResourceGroupId", request.resourceGroupId);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "DescribeDtsJobConfig"),
+            new TeaPair("version", "2020-01-01"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new DescribeDtsJobConfigResponse());
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>查询DTS任务配置</p>
+     * 
+     * @param request DescribeDtsJobConfigRequest
+     * @return DescribeDtsJobConfigResponse
+     */
+    public DescribeDtsJobConfigResponse describeDtsJobConfig(DescribeDtsJobConfigRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.describeDtsJobConfigWithOptions(request, runtime);
+    }
+
+    /**
+     * <b>summary</b> : 
      * <p>The latency of incremental data migration or synchronization.</p>
      * <blockquote>
      * <p> If you query data migration tasks, the unit of this parameter is milliseconds. If you query data synchronization tasks, the unit of this parameter is seconds.</p>
