e7d7f2e999adc4841c4c94805acf0ba95d0a4ddd
sdk-team <sdk-team@alibabacloud.com>
2024-07-25 10:13:14 +0000

    Support API SubmitMediaAiAnalysisJob.

--- ice-20201109/src/main/java/com/aliyun/ice20201109/Client.java
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/Client.java
@@ -9238,6 +9238,54 @@ public class Client extends com.aliyun.teaopenapi.Client {
     }
 
     /**
+     * <b>summary</b> : 
+     * <p>提交媒资结构化分析任务</p>
+     * 
+     * @param request SubmitMediaAiAnalysisJobRequest
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return SubmitMediaAiAnalysisJobResponse
+     */
+    public SubmitMediaAiAnalysisJobResponse submitMediaAiAnalysisJobWithOptions(SubmitMediaAiAnalysisJobRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.analysisParams)) {
+            query.put("AnalysisParams", request.analysisParams);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.input)) {
+            query.put("Input", request.input);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "SubmitMediaAiAnalysisJob"),
+            new TeaPair("version", "2020-11-09"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new SubmitMediaAiAnalysisJobResponse());
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>提交媒资结构化分析任务</p>
+     * 
+     * @param request SubmitMediaAiAnalysisJobRequest
+     * @return SubmitMediaAiAnalysisJobResponse
+     */
+    public SubmitMediaAiAnalysisJobResponse submitMediaAiAnalysisJob(SubmitMediaAiAnalysisJobRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.submitMediaAiAnalysisJobWithOptions(request, runtime);
+    }
+
+    /**
      * @param tmpReq SubmitMediaCensorJobRequest
      * @param runtime runtime options for this request RuntimeOptions
      * @return SubmitMediaCensorJobResponse
---
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/models/SubmitMediaAiAnalysisJobRequest.java
@@ -0,0 +1,42 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ice20201109.models;
+
+import com.aliyun.tea.*;
+
+public class SubmitMediaAiAnalysisJobRequest extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p>{&quot;nlpParams&quot;:{&quot;sourceLanguage&quot;:&quot;cn&quot;,&quot;diarizationEnabled&quot;:true,&quot;speakerCount&quot;:0,&quot;summarizationEnabled&quot;:false,&quot;translationEnabled&quot;:false}}</p>
+     */
+    @NameInMap("AnalysisParams")
+    public String analysisParams;
+
+    /**
+     * <strong>example:</strong>
+     * <p>{&quot;MediaType&quot;:&quot;video&quot;,&quot;Media&quot;:&quot;<a href="https://xxx.com/your_movie.mp4%22%7D">https://xxx.com/your_movie.mp4&quot;}</a></p>
+     */
+    @NameInMap("Input")
+    public String input;
+
+    public static SubmitMediaAiAnalysisJobRequest build(java.util.Map<String, ?> map) throws Exception {
+        SubmitMediaAiAnalysisJobRequest self = new SubmitMediaAiAnalysisJobRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public SubmitMediaAiAnalysisJobRequest setAnalysisParams(String analysisParams) {
+        this.analysisParams = analysisParams;
+        return this;
+    }
+    public String getAnalysisParams() {
+        return this.analysisParams;
+    }
+
+    public SubmitMediaAiAnalysisJobRequest setInput(String input) {
+        this.input = input;
+        return this;
+    }
+    public String getInput() {
+        return this.input;
+    }
+
+}
