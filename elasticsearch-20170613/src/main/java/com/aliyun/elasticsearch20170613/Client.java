811ac8dc19cd274034bce65e24aa853cdf9e8913
sdk-team <sdk-team@alibabacloud.com>
2024-07-31 17:39:32 +0000

    Update API UpgradeEngineVersion: update param body.

--- elasticsearch-20170613/src/main/java/com/aliyun/elasticsearch20170613/Client.java
+++ elasticsearch-20170613/src/main/java/com/aliyun/elasticsearch20170613/Client.java
@@ -10130,6 +10130,10 @@ public class Client extends com.aliyun.teaopenapi.Client {
         }
 
         java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.plugins)) {
+            body.put("plugins", request.plugins);
+        }
+
         if (!com.aliyun.teautil.Common.isUnset(request.type)) {
             body.put("type", request.type);
         }
