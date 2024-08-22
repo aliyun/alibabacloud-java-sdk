926f628073379eea10b22de7dbcafa01615045e7
sdk-team <sdk-team@alibabacloud.com>
2024-08-22 12:37:06 +0000

    Generated java 2024-08-15 for TrafficFxOpen.

---
+++ trafficfxopen-20240815/src/main/java/com/aliyun/trafficfxopen20240815/Client.java
@@ -0,0 +1,79 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.trafficfxopen20240815;
+
+import com.aliyun.tea.*;
+import com.aliyun.trafficfxopen20240815.models.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(com.aliyun.teaopenapi.models.Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("trafficfxopen", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>创建token</p>
+     * 
+     * @param request GetTokenRequest
+     * @param headers map
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return GetTokenResponse
+     */
+    public GetTokenResponse getTokenWithOptions(GetTokenRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.appKey)) {
+            query.put("appKey", request.appKey);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.appSecret)) {
+            query.put("appSecret", request.appSecret);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "GetToken"),
+            new TeaPair("version", "2024-08-15"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/v1/distribution/trade/getToken"),
+            new TeaPair("method", "GET"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new GetTokenResponse());
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>创建token</p>
+     * 
+     * @param request GetTokenRequest
+     * @return GetTokenResponse
+     */
+    public GetTokenResponse getToken(GetTokenRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.getTokenWithOptions(request, headers, runtime);
+    }
+}
