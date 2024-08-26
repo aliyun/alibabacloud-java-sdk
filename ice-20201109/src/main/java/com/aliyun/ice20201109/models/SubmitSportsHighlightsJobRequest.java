a22b3a7558a8e44b3a3b30985537bb9ec29c3a09
sdk-team <sdk-team@alibabacloud.com>
2024-08-26 03:22:44 +0000

    Support API SubmitSportsHighlightsJob.

---
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/models/SubmitSportsHighlightsJobRequest.java
@@ -0,0 +1,69 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ice20201109.models;
+
+import com.aliyun.tea.*;
+
+public class SubmitSportsHighlightsJobRequest extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p><strong><strong>12e8864746a0a398</strong></strong></p>
+     */
+    @NameInMap("ClientToken")
+    public String clientToken;
+
+    @NameInMap("InputConfig")
+    public String inputConfig;
+
+    /**
+     * <strong>example:</strong>
+     * <p>{
+     *   &quot;MediaURL&quot;: &quot;<a href="http://xxx.oss-cn-shanghai.aliyuncs.com/xxx_%7Bindex%7D.mp4">http://xxx.oss-cn-shanghai.aliyuncs.com/xxx_{index}.mp4</a>&quot;,
+     *   &quot;Count&quot;: 1,
+     *   &quot;Width&quot;: 1080,
+     *   &quot;Height&quot;: 1920
+     * }</p>
+     */
+    @NameInMap("OutputConfig")
+    public String outputConfig;
+
+    @NameInMap("UserData")
+    public String userData;
+
+    public static SubmitSportsHighlightsJobRequest build(java.util.Map<String, ?> map) throws Exception {
+        SubmitSportsHighlightsJobRequest self = new SubmitSportsHighlightsJobRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public SubmitSportsHighlightsJobRequest setClientToken(String clientToken) {
+        this.clientToken = clientToken;
+        return this;
+    }
+    public String getClientToken() {
+        return this.clientToken;
+    }
+
+    public SubmitSportsHighlightsJobRequest setInputConfig(String inputConfig) {
+        this.inputConfig = inputConfig;
+        return this;
+    }
+    public String getInputConfig() {
+        return this.inputConfig;
+    }
+
+    public SubmitSportsHighlightsJobRequest setOutputConfig(String outputConfig) {
+        this.outputConfig = outputConfig;
+        return this;
+    }
+    public String getOutputConfig() {
+        return this.outputConfig;
+    }
+
+    public SubmitSportsHighlightsJobRequest setUserData(String userData) {
+        this.userData = userData;
+        return this;
+    }
+    public String getUserData() {
+        return this.userData;
+    }
+
+}
---
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/models/SubmitSportsHighlightsJobResponseBody.java
@@ -0,0 +1,42 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ice20201109.models;
+
+import com.aliyun.tea.*;
+
+public class SubmitSportsHighlightsJobResponseBody extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p><strong><strong>d80e4e4044975745c14b</strong></strong></p>
+     */
+    @NameInMap("JobId")
+    public String jobId;
+
+    /**
+     * <strong>example:</strong>
+     * <p><strong><strong>36-3C1E-4417-BDB2-1E034F</strong></strong></p>
+     */
+    @NameInMap("RequestId")
+    public String requestId;
+
+    public static SubmitSportsHighlightsJobResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        SubmitSportsHighlightsJobResponseBody self = new SubmitSportsHighlightsJobResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public SubmitSportsHighlightsJobResponseBody setJobId(String jobId) {
+        this.jobId = jobId;
+        return this;
+    }
+    public String getJobId() {
+        return this.jobId;
+    }
+
+    public SubmitSportsHighlightsJobResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+}
