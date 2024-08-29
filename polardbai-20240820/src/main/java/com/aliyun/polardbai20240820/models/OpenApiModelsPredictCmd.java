d8ce048519f5158b7b0140a3bae111b9dd16f811
sdk-team <sdk-team@alibabacloud.com>
2024-08-29 12:24:39 +0000

    Generated java 2024-08-20 for PolardbAI.

---
+++ polardbai-20240820/src/main/java/com/aliyun/polardbai20240820/models/OpenApiModelsPredictCmd.java
@@ -0,0 +1,12 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.polardbai20240820.models;
+
+import com.aliyun.tea.*;
+
+public class OpenApiModelsPredictCmd extends TeaModel {
+    public static OpenApiModelsPredictCmd build(java.util.Map<String, ?> map) throws Exception {
+        OpenApiModelsPredictCmd self = new OpenApiModelsPredictCmd();
+        return TeaModel.build(map, self);
+    }
+
+}
