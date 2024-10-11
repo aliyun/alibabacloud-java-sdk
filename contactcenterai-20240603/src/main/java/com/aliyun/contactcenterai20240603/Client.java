58fed4774499658b87a8fa546842a7f21e696f03
sdk-team <sdk-team@alibabacloud.com>
2024-10-11 01:51:27 +0000

    Support API CreateTask.

--- contactcenterai-20240603/src/main/java/com/aliyun/contactcenterai20240603/Client.java
+++ contactcenterai-20240603/src/main/java/com/aliyun/contactcenterai20240603/Client.java
@@ -206,6 +206,85 @@ public class Client extends com.aliyun.teaopenapi.Client {
 
     /**
      * <b>summary</b> : 
+     * <p>创建语音文件调用llm任务</p>
+     * 
+     * @param request CreateTaskRequest
+     * @param headers map
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return CreateTaskResponse
+     */
+    public CreateTaskResponse createTaskWithOptions(String workspaceId, String appId, CreateTaskRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> body = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.dialogue)) {
+            body.put("dialogue", request.dialogue);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.examples)) {
+            body.put("examples", request.examples);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.fields)) {
+            body.put("fields", request.fields);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.modelCode)) {
+            body.put("modelCode", request.modelCode);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.resultTypes)) {
+            body.put("resultTypes", request.resultTypes);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.serviceInspection)) {
+            body.put("serviceInspection", request.serviceInspection);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.taskType)) {
+            body.put("taskType", request.taskType);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.templateIds)) {
+            body.put("templateIds", request.templateIds);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.transcription)) {
+            body.put("transcription", request.transcription);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "CreateTask"),
+            new TeaPair("version", "2024-06-03"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/" + com.aliyun.openapiutil.Client.getEncodeParam(workspaceId) + "/ccai/app/" + com.aliyun.openapiutil.Client.getEncodeParam(appId) + "/createTask"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new CreateTaskResponse());
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>创建语音文件调用llm任务</p>
+     * 
+     * @param request CreateTaskRequest
+     * @return CreateTaskResponse
+     */
+    public CreateTaskResponse createTask(String workspaceId, String appId, CreateTaskRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.createTaskWithOptions(workspaceId, appId, request, headers, runtime);
+    }
+
+    /**
+     * <b>summary</b> : 
      * <p>语音文件调用大模型获取结果</p>
      * 
      * @param request GetTaskResultRequest
