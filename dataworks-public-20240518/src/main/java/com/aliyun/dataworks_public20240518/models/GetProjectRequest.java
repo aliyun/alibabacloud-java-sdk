95a03ea0b9942de878dbb9e84b83b5c6d3923e93
sdk-team <sdk-team@alibabacloud.com>
2024-10-30 03:24:47 +0000

    Generated java 2024-05-18 for dataworks-public.

--- dataworks-public-20240518/src/main/java/com/aliyun/dataworks_public20240518/models/GetProjectRequest.java
+++ dataworks-public-20240518/src/main/java/com/aliyun/dataworks_public20240518/models/GetProjectRequest.java
@@ -5,6 +5,8 @@ import com.aliyun.tea.*;
 
 public class GetProjectRequest extends TeaModel {
     /**
+     * <p>The DataWorks workspace ID. You can log on to the <a href="https://dataworks.console.aliyun.com/workspace/list">DataWorks console</a> and go to the Workspace page to query the ID.</p>
+     * <p>You must configure this parameter to specify the DataWorks workspace to which the API operation is applied.</p>
      * <p>This parameter is required.</p>
      * 
      * <strong>example:</strong>
