1127d4d6024839196462deb285b80eb9ca6ff79a
sdk-team <sdk-team@alibabacloud.com>
2024-10-31 09:16:53 +0000

    Generated java 2024-09-30 for LinkedmallRetrieval.

---
+++ linkedmallretrieval-20240930/src/main/java/com/aliyun/linkedmallretrieval20240930/models/SearchInformation.java
@@ -0,0 +1,34 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.linkedmallretrieval20240930.models;
+
+import com.aliyun.tea.*;
+
+public class SearchInformation extends TeaModel {
+    @NameInMap("searchTime")
+    public Long searchTime;
+
+    @NameInMap("total")
+    public Long total;
+
+    public static SearchInformation build(java.util.Map<String, ?> map) throws Exception {
+        SearchInformation self = new SearchInformation();
+        return TeaModel.build(map, self);
+    }
+
+    public SearchInformation setSearchTime(Long searchTime) {
+        this.searchTime = searchTime;
+        return this;
+    }
+    public Long getSearchTime() {
+        return this.searchTime;
+    }
+
+    public SearchInformation setTotal(Long total) {
+        this.total = total;
+        return this;
+    }
+    public Long getTotal() {
+        return this.total;
+    }
+
+}
---
+++ linkedmallretrieval-20240930/src/main/java/com/aliyun/linkedmallretrieval20240930/models/WeiboItem.java
@@ -0,0 +1,121 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.linkedmallretrieval20240930.models;
+
+import com.aliyun.tea.*;
+
+public class WeiboItem extends TeaModel {
+    /**
+     * <p>This parameter is required.</p>
+     * 
+     * <strong>example:</strong>
+     * <p>weibo_strong</p>
+     */
+    @NameInMap("cardType")
+    public String cardType;
+
+    /**
+     * <strong>example:</strong>
+     * <p><a href="https://m.weibo.cn/u/7761793874?luicode=20000061&lfid=5024099350350075">https://m.weibo.cn/u/7761793874?luicode=20000061&amp;lfid=5024099350350075</a></p>
+     */
+    @NameInMap("homepageLink")
+    public String homepageLink;
+
+    /**
+     * <p>This parameter is required.</p>
+     * 
+     * <strong>example:</strong>
+     * <p>【小调查：你会买<em>小米SU7</em>吗？】#小米SU7路测覆盖300多城市#4月17日，@小米汽车 发文称SU7道路测试覆盖全国300多个城市，涵盖极寒，极热天气，总里程数高达540万公里，目前仍在进行中。  网页链接</p>
+     */
+    @NameInMap("htmlSnippet")
+    public String htmlSnippet;
+
+    @NameInMap("images")
+    public java.util.List<String> images;
+
+    /**
+     * <p>This parameter is required.</p>
+     * 
+     * <strong>example:</strong>
+     * <p><a href="https://m.weibo.cn/detail/5024099350350075?wm=90194_90009">https://m.weibo.cn/detail/5024099350350075?wm=90194_90009</a></p>
+     */
+    @NameInMap("link")
+    public String link;
+
+    /**
+     * <p>This parameter is required.</p>
+     * 
+     * <strong>example:</strong>
+     * <p>1小时前</p>
+     */
+    @NameInMap("publishDisplayTime")
+    public String publishDisplayTime;
+
+    /**
+     * <strong>example:</strong>
+     * <p>白鹿科技</p>
+     */
+    @NameInMap("username")
+    public String username;
+
+    public static WeiboItem build(java.util.Map<String, ?> map) throws Exception {
+        WeiboItem self = new WeiboItem();
+        return TeaModel.build(map, self);
+    }
+
+    public WeiboItem setCardType(String cardType) {
+        this.cardType = cardType;
+        return this;
+    }
+    public String getCardType() {
+        return this.cardType;
+    }
+
+    public WeiboItem setHomepageLink(String homepageLink) {
+        this.homepageLink = homepageLink;
+        return this;
+    }
+    public String getHomepageLink() {
+        return this.homepageLink;
+    }
+
+    public WeiboItem setHtmlSnippet(String htmlSnippet) {
+        this.htmlSnippet = htmlSnippet;
+        return this;
+    }
+    public String getHtmlSnippet() {
+        return this.htmlSnippet;
+    }
+
+    public WeiboItem setImages(java.util.List<String> images) {
+        this.images = images;
+        return this;
+    }
+    public java.util.List<String> getImages() {
+        return this.images;
+    }
+
+    public WeiboItem setLink(String link) {
+        this.link = link;
+        return this;
+    }
+    public String getLink() {
+        return this.link;
+    }
+
+    public WeiboItem setPublishDisplayTime(String publishDisplayTime) {
+        this.publishDisplayTime = publishDisplayTime;
+        return this;
+    }
+    public String getPublishDisplayTime() {
+        return this.publishDisplayTime;
+    }
+
+    public WeiboItem setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+
+}
