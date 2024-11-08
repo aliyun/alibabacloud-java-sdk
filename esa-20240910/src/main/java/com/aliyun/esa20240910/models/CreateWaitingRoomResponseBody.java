e553c87abd83f28ae823fcb01bac8bf715ff522a
sdk-team <sdk-team@alibabacloud.com>
2024-11-08 06:55:11 +0000

    Generated java 2024-09-10 for ESA.

--- esa-20240910/src/main/java/com/aliyun/esa20240910/models/CreateWaitingRoomResponseBody.java
+++ esa-20240910/src/main/java/com/aliyun/esa20240910/models/CreateWaitingRoomResponseBody.java
@@ -4,10 +4,6 @@ package com.aliyun.esa20240910.models;
 import com.aliyun.tea.*;
 
 public class CreateWaitingRoomResponseBody extends TeaModel {
-    /**
-     * <strong>example:</strong>
-     * <p>85H66C7B-671A-4297-9187-2C4477247A74</p>
-     */
     @NameInMap("RequestId")
     public String requestId;
 
