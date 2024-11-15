2d6ce5e9f8b71e3b6df0ded40802e1d8320b564c
sdk-team <sdk-team@alibabacloud.com>
2024-11-15 02:11:59 +0000

    Support API GetSessionCluster.

--- emr-serverless-spark-20230808/src/main/java/com/aliyun/emr_serverless_spark20230808/Client.java
+++ emr-serverless-spark-20230808/src/main/java/com/aliyun/emr_serverless_spark20230808/Client.java
@@ -248,6 +248,53 @@ public class Client extends com.aliyun.teaopenapi.Client {
 
     /**
      * <b>summary</b> : 
+     * <p>查询SessionCluster集群</p>
+     * 
+     * @param request GetSessionClusterRequest
+     * @param headers map
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return GetSessionClusterResponse
+     */
+    public GetSessionClusterResponse getSessionClusterWithOptions(String workspaceId, String sessionClusterId, GetSessionClusterRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.regionId)) {
+            query.put("regionId", request.regionId);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "GetSessionCluster"),
+            new TeaPair("version", "2023-08-08"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/api/v1/workspaces/" + com.aliyun.openapiutil.Client.getEncodeParam(workspaceId) + "/sessionClusters/" + com.aliyun.openapiutil.Client.getEncodeParam(sessionClusterId) + ""),
+            new TeaPair("method", "GET"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new GetSessionClusterResponse());
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>查询SessionCluster集群</p>
+     * 
+     * @param request GetSessionClusterRequest
+     * @return GetSessionClusterResponse
+     */
+    public GetSessionClusterResponse getSessionCluster(String workspaceId, String sessionClusterId, GetSessionClusterRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.getSessionClusterWithOptions(workspaceId, sessionClusterId, request, headers, runtime);
+    }
+
+    /**
+     * <b>summary</b> : 
      * <p>Queries the status of an SQL query task.</p>
      * 
      * @param request GetSqlStatementRequest
