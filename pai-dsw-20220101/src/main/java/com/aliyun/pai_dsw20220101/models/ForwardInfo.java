dde4e69f12b1321ac9016a8e169d6ee58c1aa8e7
sdk-team <sdk-team@alibabacloud.com>
2024-11-15 17:28:50 +0000

    Update API CreateInstance: update param body.

--- pai-dsw-20220101/src/main/java/com/aliyun/pai_dsw20220101/models/ForwardInfo.java
+++ pai-dsw-20220101/src/main/java/com/aliyun/pai_dsw20220101/models/ForwardInfo.java
@@ -4,6 +4,9 @@ package com.aliyun.pai_dsw20220101.models;
 import com.aliyun.tea.*;
 
 public class ForwardInfo extends TeaModel {
+    @NameInMap("AccessType")
+    public java.util.List<String> accessType;
+
     /**
      * <strong>example:</strong>
      * <p>dsw-notebook</p>
@@ -27,17 +30,31 @@ public class ForwardInfo extends TeaModel {
 
     /**
      * <strong>example:</strong>
-     * <p>ngw-bp1uewa15k4iy5770****</p>
+     * <p>10086</p>
      */
-    @NameInMap("NatGatewayId")
-    public String natGatewayId;
+    @NameInMap("ExternalPort")
+    public String externalPort;
 
     /**
      * <strong>example:</strong>
      * <p>22</p>
      */
-    @NameInMap("Port")
-    public String port;
+    @NameInMap("ForwardPort")
+    public String forwardPort;
+
+    /**
+     * <strong>example:</strong>
+     * <p>ssh</p>
+     */
+    @NameInMap("Name")
+    public String name;
+
+    /**
+     * <strong>example:</strong>
+     * <p>ngw-bp1uewa15k4iy5770****</p>
+     */
+    @NameInMap("NatGatewayId")
+    public String natGatewayId;
 
     @NameInMap("SSHPublicKey")
     public String SSHPublicKey;
@@ -45,6 +62,14 @@ public class ForwardInfo extends TeaModel {
     public static ForwardInfo build(java.util.Map<String, ?> map) throws Exception {
         ForwardInfo self = new ForwardInfo();
         return TeaModel.build(map, self);
+    }
+
+    public ForwardInfo setAccessType(java.util.List<String> accessType) {
+        this.accessType = accessType;
+        return this;
+    }
+    public java.util.List<String> getAccessType() {
+        return this.accessType;
     }
 
     public ForwardInfo setContainerName(String containerName) {
@@ -71,20 +96,36 @@ public class ForwardInfo extends TeaModel {
         return this.enable;
     }
 
+    public ForwardInfo setExternalPort(String externalPort) {
+        this.externalPort = externalPort;
+        return this;
+    }
+    public String getExternalPort() {
+        return this.externalPort;
+    }
+
+    public ForwardInfo setForwardPort(String forwardPort) {
+        this.forwardPort = forwardPort;
+        return this;
+    }
+    public String getForwardPort() {
+        return this.forwardPort;
+    }
+
+    public ForwardInfo setName(String name) {
+        this.name = name;
+        return this;
+    }
+    public String getName() {
+        return this.name;
+    }
+
     public ForwardInfo setNatGatewayId(String natGatewayId) {
         this.natGatewayId = natGatewayId;
         return this;
     }
     public String getNatGatewayId() {
         return this.natGatewayId;
-    }
-
-    public ForwardInfo setPort(String port) {
-        this.port = port;
-        return this;
-    }
-    public String getPort() {
-        return this.port;
     }
 
     public ForwardInfo setSSHPublicKey(String SSHPublicKey) {
