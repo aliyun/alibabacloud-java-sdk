bfa9738591af62d90c31a693b7c65332be91bcce
sdk-team <sdk-team@alibabacloud.com>
2024-11-22 09:11:29 +0000

    Update API GetUserCertificateDetail: update response param.

--- cas-20200407/src/main/java/com/aliyun/cas20200407/models/ListCloudResourcesShrinkRequest.java
+++ cas-20200407/src/main/java/com/aliyun/cas20200407/models/ListCloudResourcesShrinkRequest.java
@@ -4,6 +4,9 @@ package com.aliyun.cas20200407.models;
 import com.aliyun.tea.*;
 
 public class ListCloudResourcesShrinkRequest extends TeaModel {
+    /**
+     * <p>The certificate IDs.</p>
+     */
     @NameInMap("CertIds")
     public String certIdsShrink;
 
@@ -11,8 +14,10 @@ public class ListCloudResourcesShrinkRequest extends TeaModel {
      * <p>The cloud service provider.</p>
      * <p>Valid values:</p>
      * <ul>
-     * <li>Tencent: Tencent Cloud</li>
-     * <li>aliyun: Alibaba Cloud</li>
+     * <li>Tencent</li>
+     * <li>Huawei</li>
+     * <li>Aws</li>
+     * <li>aliyun</li>
      * </ul>
      * 
      * <strong>example:</strong>
@@ -22,7 +27,44 @@ public class ListCloudResourcesShrinkRequest extends TeaModel {
     public String cloudName;
 
     /**
-     * <p>The cloud service. Only Content Delivery Network (CDN) is supported for Tencent Cloud.</p>
+     * <p>The cloud service.</p>
+     * <p>Valid values when CloudName is set to aliyun:</p>
+     * <ul>
+     * <li>SLB: Classic Load Balancer (CLB). This value is available only on the China site (aliyun.com).</li>
+     * <li>LIVE: ApsaraVideo Live. This value is available only on the China site (aliyun.com).</li>
+     * <li>webHosting: Cloud Web Hosting. This value is available only on the China site (aliyun.com).</li>
+     * <li>VOD: ApsaraVideo VOD. This value is available only on the China site (aliyun.com).</li>
+     * <li>CR: Container Registry. This value is available only on the China site (aliyun.com).</li>
+     * <li>DCDN: Dynamic Content Delivery Network (DCDN).</li>
+     * <li>DDOS: Anti-DDoS.</li>
+     * <li>CDN: Alibaba Cloud CDN (CDN).</li>
+     * <li>ALB: Application Load Balancer (ALB).</li>
+     * <li>APIGateway: API Gateway.</li>
+     * <li>FC: Function Compute.</li>
+     * <li>GA: Global Accelerator (GA).</li>
+     * <li>MSE: Microservices Engine (MSE).</li>
+     * <li>NLB: Network Load Balancer (NLB).</li>
+     * <li>OSS: Object Storage Service (OSS).</li>
+     * <li>SAE: Serverless App Engine (SAE).</li>
+     * <li>WAF: Web Application Firewall (WAF).</li>
+     * </ul>
+     * <p>Valid values when CloudName is set to Tencent:</p>
+     * <ul>
+     * <li>TencentCDN: Content Delivery Network (CDN).</li>
+     * <li>TencentCLB: CLB.</li>
+     * <li>TencentWAF: WAF.</li>
+     * </ul>
+     * <p>Valid value when CloudName is set to Huawei:</p>
+     * <ul>
+     * <li>HuaweiCDN: CDN.</li>
+     * </ul>
+     * <p>Valid values when CloudName is set to Aws:</p>
+     * <ul>
+     * <li>AwsCloudFront: Amazon CloudFront.</li>
+     * <li>AwsCLB: CLB.</li>
+     * <li>AwsALB: ALB.</li>
+     * <li>AwsNLB: NLB.</li>
+     * </ul>
      * 
      * <strong>example:</strong>
      * <p>SLB</p>
@@ -40,7 +82,7 @@ public class ListCloudResourcesShrinkRequest extends TeaModel {
     public Integer currentPage;
 
     /**
-     * <p>The domain name bound to the cloud resource.</p>
+     * <p>The keyword of the domain name or instance ID bound to the cloud resource.</p>
      * 
      * <strong>example:</strong>
      * <p>cert-instanceId</p>
@@ -49,7 +91,7 @@ public class ListCloudResourcesShrinkRequest extends TeaModel {
     public String keyword;
 
     /**
-     * <p>The AccessKey ID used to access cloud resources.</p>
+     * <p>The AccessKey ID that is used to access cloud resources.</p>
      * 
      * <strong>example:</strong>
      * <p>21</p>
@@ -58,7 +100,7 @@ public class ListCloudResourcesShrinkRequest extends TeaModel {
     public String secretId;
 
     /**
-     * <p>The number of revoked certificates per page. Default value: <strong>20</strong>.</p>
+     * <p>The number of entries per page. Default value: <strong>20</strong>.</p>
      * 
      * <strong>example:</strong>
      * <p>20</p>
