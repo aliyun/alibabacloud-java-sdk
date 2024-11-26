d1c8146b3484675c271d9303ddfd10accd3998ab
sdk-team <sdk-team@alibabacloud.com>
2024-11-26 06:40:29 +0000

    Support API RollbackServiceInstance.

---
+++ computenestsupplier-20210521/src/main/java/com/aliyun/computenestsupplier20210521/models/RollbackServiceInstanceRequest.java
@@ -0,0 +1,57 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.computenestsupplier20210521.models;
+
+import com.aliyun.tea.*;
+
+public class RollbackServiceInstanceRequest extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p>10CM943JP0EN9D51H</p>
+     */
+    @NameInMap("ClientToken")
+    public String clientToken;
+
+    /**
+     * <strong>example:</strong>
+     * <p>cn-hangzhou</p>
+     */
+    @NameInMap("RegionId")
+    public String regionId;
+
+    /**
+     * <strong>example:</strong>
+     * <p>si-3a8f9a75da074f52b969</p>
+     */
+    @NameInMap("ServiceInstanceId")
+    public String serviceInstanceId;
+
+    public static RollbackServiceInstanceRequest build(java.util.Map<String, ?> map) throws Exception {
+        RollbackServiceInstanceRequest self = new RollbackServiceInstanceRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public RollbackServiceInstanceRequest setClientToken(String clientToken) {
+        this.clientToken = clientToken;
+        return this;
+    }
+    public String getClientToken() {
+        return this.clientToken;
+    }
+
+    public RollbackServiceInstanceRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public RollbackServiceInstanceRequest setServiceInstanceId(String serviceInstanceId) {
+        this.serviceInstanceId = serviceInstanceId;
+        return this;
+    }
+    public String getServiceInstanceId() {
+        return this.serviceInstanceId;
+    }
+
+}
