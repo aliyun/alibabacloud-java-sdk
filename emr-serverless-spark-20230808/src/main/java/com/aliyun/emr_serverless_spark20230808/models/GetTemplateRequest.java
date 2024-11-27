541738b6c2e84b8e194140bc7c0112e2f9f9384a
sdk-team <sdk-team@alibabacloud.com>
2024-11-27 06:32:23 +0000

    Support API GetTemplate.

---
+++ emr-serverless-spark-20230808/src/main/java/com/aliyun/emr_serverless_spark20230808/models/GetTemplateRequest.java
@@ -0,0 +1,42 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.emr_serverless_spark20230808.models;
+
+import com.aliyun.tea.*;
+
+public class GetTemplateRequest extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p>cn-hangzhou</p>
+     */
+    @NameInMap("regionId")
+    public String regionId;
+
+    /**
+     * <strong>example:</strong>
+     * <p>TASK</p>
+     */
+    @NameInMap("templateType")
+    public String templateType;
+
+    public static GetTemplateRequest build(java.util.Map<String, ?> map) throws Exception {
+        GetTemplateRequest self = new GetTemplateRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public GetTemplateRequest setRegionId(String regionId) {
+        this.regionId = regionId;
+        return this;
+    }
+    public String getRegionId() {
+        return this.regionId;
+    }
+
+    public GetTemplateRequest setTemplateType(String templateType) {
+        this.templateType = templateType;
+        return this;
+    }
+    public String getTemplateType() {
+        return this.templateType;
+    }
+
+}
---
+++ emr-serverless-spark-20230808/src/main/java/com/aliyun/emr_serverless_spark20230808/models/GetTemplateResponseBody.java
@@ -0,0 +1,100 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.emr_serverless_spark20230808.models;
+
+import com.aliyun.tea.*;
+
+public class GetTemplateResponseBody extends TeaModel {
+    @NameInMap("data")
+    public Template data;
+
+    /**
+     * <strong>example:</strong>
+     * <p>040003</p>
+     */
+    @NameInMap("errorCode")
+    public String errorCode;
+
+    /**
+     * <strong>example:</strong>
+     * <p>InvalidUser.NotFound</p>
+     */
+    @NameInMap("errorMessage")
+    public String errorMessage;
+
+    /**
+     * <p>Id of the request</p>
+     * 
+     * <strong>example:</strong>
+     * <p>200</p>
+     */
+    @NameInMap("httpStatusCode")
+    public String httpStatusCode;
+
+    /**
+     * <strong>example:</strong>
+     * <p>484D9DDA-300D-525E-AF7A-0CCCA5C64A7A</p>
+     */
+    @NameInMap("requestId")
+    public String requestId;
+
+    /**
+     * <strong>example:</strong>
+     * <p>True</p>
+     */
+    @NameInMap("success")
+    public Boolean success;
+
+    public static GetTemplateResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        GetTemplateResponseBody self = new GetTemplateResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public GetTemplateResponseBody setData(Template data) {
+        this.data = data;
+        return this;
+    }
+    public Template getData() {
+        return this.data;
+    }
+
+    public GetTemplateResponseBody setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+        return this;
+    }
+    public String getErrorCode() {
+        return this.errorCode;
+    }
+
+    public GetTemplateResponseBody setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+        return this;
+    }
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+    public GetTemplateResponseBody setHttpStatusCode(String httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+        return this;
+    }
+    public String getHttpStatusCode() {
+        return this.httpStatusCode;
+    }
+
+    public GetTemplateResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public GetTemplateResponseBody setSuccess(Boolean success) {
+        this.success = success;
+        return this;
+    }
+    public Boolean getSuccess() {
+        return this.success;
+    }
+
+}
