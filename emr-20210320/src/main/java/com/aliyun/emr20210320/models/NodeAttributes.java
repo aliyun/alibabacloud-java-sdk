8ac6ff9357c0fee01d0ef906ebfec39d3280cb80
sdk-team <sdk-team@alibabacloud.com>
2024-12-02 04:04:30 +0000

    Update API CreateCluster: add param DeletionProtection.

--- emr-20210320/src/main/java/com/aliyun/emr20210320/models/NodeAttributes.java
+++ emr-20210320/src/main/java/com/aliyun/emr20210320/models/NodeAttributes.java
@@ -5,6 +5,29 @@ import com.aliyun.tea.*;
 
 public class NodeAttributes extends TeaModel {
     /**
+     * <p>是否启用云盘加密。取值范围：</p>
+     * <ul>
+     * <li>true：启用加密。</li>
+     * <li>false：不加密。</li>
+     * </ul>
+     * <p>默认值：false，不加密</p>
+     * 
+     * <strong>example:</strong>
+     * <p>false</p>
+     */
+    @NameInMap("DataDiskEncrypted")
+    public Boolean dataDiskEncrypted;
+
+    /**
+     * <p>KMS加密秘钥ID。</p>
+     * 
+     * <strong>example:</strong>
+     * <p>0e478b7a-4262-4802-b8cb-00d3fb40****</p>
+     */
+    @NameInMap("DataDiskKMSKeyId")
+    public String dataDiskKMSKeyId;
+
+    /**
      * <p>ECS ssh登录秘钥。</p>
      * 
      * <strong>example:</strong>
@@ -42,6 +65,29 @@ public class NodeAttributes extends TeaModel {
     public String securityGroupId;
 
     /**
+     * <p>是否启用云盘加密。取值范围：</p>
+     * <ul>
+     * <li>true：启用加密。</li>
+     * <li>false：不加密。</li>
+     * </ul>
+     * <p>默认值：false，不加密</p>
+     * 
+     * <strong>example:</strong>
+     * <p>false</p>
+     */
+    @NameInMap("SystemDiskEncrypted")
+    public Boolean systemDiskEncrypted;
+
+    /**
+     * <p>KMS加密秘钥ID。</p>
+     * 
+     * <strong>example:</strong>
+     * <p>0e478b7a-4262-4802-b8cb-00d3fb40****</p>
+     */
+    @NameInMap("SystemDiskKMSKeyId")
+    public String systemDiskKMSKeyId;
+
+    /**
      * <p>专有网络ID。</p>
      * <p>This parameter is required.</p>
      * 
@@ -64,6 +110,22 @@ public class NodeAttributes extends TeaModel {
     public static NodeAttributes build(java.util.Map<String, ?> map) throws Exception {
         NodeAttributes self = new NodeAttributes();
         return TeaModel.build(map, self);
+    }
+
+    public NodeAttributes setDataDiskEncrypted(Boolean dataDiskEncrypted) {
+        this.dataDiskEncrypted = dataDiskEncrypted;
+        return this;
+    }
+    public Boolean getDataDiskEncrypted() {
+        return this.dataDiskEncrypted;
+    }
+
+    public NodeAttributes setDataDiskKMSKeyId(String dataDiskKMSKeyId) {
+        this.dataDiskKMSKeyId = dataDiskKMSKeyId;
+        return this;
+    }
+    public String getDataDiskKMSKeyId() {
+        return this.dataDiskKMSKeyId;
     }
 
     public NodeAttributes setKeyPairName(String keyPairName) {
@@ -96,6 +158,22 @@ public class NodeAttributes extends TeaModel {
     }
     public String getSecurityGroupId() {
         return this.securityGroupId;
+    }
+
+    public NodeAttributes setSystemDiskEncrypted(Boolean systemDiskEncrypted) {
+        this.systemDiskEncrypted = systemDiskEncrypted;
+        return this;
+    }
+    public Boolean getSystemDiskEncrypted() {
+        return this.systemDiskEncrypted;
+    }
+
+    public NodeAttributes setSystemDiskKMSKeyId(String systemDiskKMSKeyId) {
+        this.systemDiskKMSKeyId = systemDiskKMSKeyId;
+        return this;
+    }
+    public String getSystemDiskKMSKeyId() {
+        return this.systemDiskKMSKeyId;
     }
 
     public NodeAttributes setVpcId(String vpcId) {
