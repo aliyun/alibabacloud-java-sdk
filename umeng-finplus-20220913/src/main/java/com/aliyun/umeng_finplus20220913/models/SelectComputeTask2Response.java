51ddbe9cc402f9796909c4bdd38593f249aea791
sdk-team <sdk-team@alibabacloud.com>
2024-12-04 07:07:42 +0000

    Update API SelectDataSet2: update response param.

--- umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/models/SelectComputeTask2Response.java
+++ umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/models/SelectComputeTask2Response.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class SelectComputeTask2Response extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public SelectComputeTask2ResponseBody body;
 
     public static SelectComputeTask2Response build(java.util.Map<String, ?> map) throws Exception {
--- umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/models/SelectComputeTaskResponse.java
+++ umeng-finplus-20220913/src/main/java/com/aliyun/umeng_finplus20220913/models/SelectComputeTaskResponse.java
@@ -5,15 +5,12 @@ import com.aliyun.tea.*;
 
 public class SelectComputeTaskResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
     @NameInMap("statusCode")
-    @Validation(required = true)
     public Integer statusCode;
 
     @NameInMap("body")
-    @Validation(required = true)
     public SelectComputeTaskResponseBody body;
 
     public static SelectComputeTaskResponse build(java.util.Map<String, ?> map) throws Exception {
