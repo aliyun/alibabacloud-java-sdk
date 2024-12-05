c234c00783e1b9a7f6ca0d4f347735b26d7f6e7a
sdk-team <sdk-team@alibabacloud.com>
2024-12-05 08:33:26 +0000

    Support API GetWebshellToken.

--- sae-20190506/src/main/java/com/aliyun/sae20190506/Client.java
+++ sae-20190506/src/main/java/com/aliyun/sae20190506/Client.java
@@ -4595,6 +4595,51 @@ public class Client extends com.aliyun.teaopenapi.Client {
     }
 
     /**
+     * @param request GetWebshellTokenRequest
+     * @param headers map
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return GetWebshellTokenResponse
+     */
+    public GetWebshellTokenResponse getWebshellTokenWithOptions(GetWebshellTokenRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.appId)) {
+            query.put("AppId", request.appId);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.podName)) {
+            query.put("PodName", request.podName);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "GetWebshellToken"),
+            new TeaPair("version", "2019-05-06"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/pop/v1/sam/instance/webshellToken"),
+            new TeaPair("method", "GET"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new GetWebshellTokenResponse());
+    }
+
+    /**
+     * @param request GetWebshellTokenRequest
+     * @return GetWebshellTokenResponse
+     */
+    public GetWebshellTokenResponse getWebshellToken(GetWebshellTokenRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.getWebshellTokenWithOptions(request, headers, runtime);
+    }
+
+    /**
      * <b>summary</b> : 
      * <p>Queries the events that occurred in an application.</p>
      * 
