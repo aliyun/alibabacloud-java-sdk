0055ab4c30cf4f7aa00c8c67789806947e84b549
sdk-team <sdk-team@alibabacloud.com>
2024-12-09 17:30:07 +0000

    Support API DescribeActiveOperationTasks.

---
+++ adb-20190315/src/main/java/com/aliyun/adb20190315/models/ModifyActiveOperationTasksResponseBody.java
@@ -0,0 +1,42 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.adb20190315.models;
+
+import com.aliyun.tea.*;
+
+public class ModifyActiveOperationTasksResponseBody extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p>1</p>
+     */
+    @NameInMap("Ids")
+    public String ids;
+
+    /**
+     * <strong>example:</strong>
+     * <p>8B709A33-5E8D-511F-A225-AFC0B6AAAD76</p>
+     */
+    @NameInMap("RequestId")
+    public String requestId;
+
+    public static ModifyActiveOperationTasksResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        ModifyActiveOperationTasksResponseBody self = new ModifyActiveOperationTasksResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public ModifyActiveOperationTasksResponseBody setIds(String ids) {
+        this.ids = ids;
+        return this;
+    }
+    public String getIds() {
+        return this.ids;
+    }
+
+    public ModifyActiveOperationTasksResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+}
