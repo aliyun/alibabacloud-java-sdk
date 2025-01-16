c5fcc351626daad31bc200165169950c6f597864
sdk-team <sdk-team@alibabacloud.com>
2025-01-16 03:48:19 +0000

    Support API UpdatePayerForAccount.

---
+++ resourcedirectorymaster-20220419/src/main/java/com/aliyun/resourcedirectorymaster20220419/models/UpdatePayerForAccountRequest.java
@@ -0,0 +1,46 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.resourcedirectorymaster20220419.models;
+
+import com.aliyun.tea.*;
+
+public class UpdatePayerForAccountRequest extends TeaModel {
+    /**
+     * <p>This parameter is required.</p>
+     * 
+     * <strong>example:</strong>
+     * <p>138660628348****</p>
+     */
+    @NameInMap("AccountId")
+    public String accountId;
+
+    /**
+     * <p>This parameter is required.</p>
+     * 
+     * <strong>example:</strong>
+     * <p>12323344****</p>
+     */
+    @NameInMap("PayerAccountId")
+    public String payerAccountId;
+
+    public static UpdatePayerForAccountRequest build(java.util.Map<String, ?> map) throws Exception {
+        UpdatePayerForAccountRequest self = new UpdatePayerForAccountRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public UpdatePayerForAccountRequest setAccountId(String accountId) {
+        this.accountId = accountId;
+        return this;
+    }
+    public String getAccountId() {
+        return this.accountId;
+    }
+
+    public UpdatePayerForAccountRequest setPayerAccountId(String payerAccountId) {
+        this.payerAccountId = payerAccountId;
+        return this;
+    }
+    public String getPayerAccountId() {
+        return this.payerAccountId;
+    }
+
+}
