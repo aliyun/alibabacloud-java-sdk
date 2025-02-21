4cddc2a42df7adb45fa021f1625dccd17ec5549f
sdk-team <sdk-team@alibabacloud.com>
2025-02-21 07:16:25 +0000

    Support API AnalyzeVlRealtime.

--- energyexpertexternal-20220923/src/main/java/com/aliyun/energyexpertexternal20220923/Client.java
+++ energyexpertexternal-20220923/src/main/java/com/aliyun/energyexpertexternal20220923/Client.java
@@ -27,6 +27,147 @@ public class Client extends com.aliyun.teaopenapi.Client {
     }
 
     /**
+     * <b>description</b> :
+     * <p>Users obtain real-time VL results by uploading a document URL.</p>
+     * 
+     * <b>summary</b> : 
+     * <p>Get Document Results</p>
+     * 
+     * @param request AnalyzeVlRealtimeRequest
+     * @param headers map
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return AnalyzeVlRealtimeResponse
+     */
+    public AnalyzeVlRealtimeResponse analyzeVlRealtimeWithOptions(AnalyzeVlRealtimeRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.fileUrl)) {
+            query.put("fileUrl", request.fileUrl);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.language)) {
+            query.put("language", request.language);
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(request.templateId)) {
+            query.put("templateId", request.templateId);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers),
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "AnalyzeVlRealtime"),
+            new TeaPair("version", "2022-09-23"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/api/v1/aidoc/document/analyzeVlRealtime"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        if (com.aliyun.teautil.Common.isUnset(_signatureVersion) || !com.aliyun.teautil.Common.equalString(_signatureVersion, "v4")) {
+            return TeaModel.toModel(this.callApi(params, req, runtime), new AnalyzeVlRealtimeResponse());
+        } else {
+            return TeaModel.toModel(this.execute(params, req, runtime), new AnalyzeVlRealtimeResponse());
+        }
+
+    }
+
+    /**
+     * <b>description</b> :
+     * <p>Users obtain real-time VL results by uploading a document URL.</p>
+     * 
+     * <b>summary</b> : 
+     * <p>Get Document Results</p>
+     * 
+     * @param request AnalyzeVlRealtimeRequest
+     * @return AnalyzeVlRealtimeResponse
+     */
+    public AnalyzeVlRealtimeResponse analyzeVlRealtime(AnalyzeVlRealtimeRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.analyzeVlRealtimeWithOptions(request, headers, runtime);
+    }
+
+    public AnalyzeVlRealtimeResponse analyzeVlRealtimeAdvance(AnalyzeVlRealtimeAdvanceRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        // Step 0: init client
+        String accessKeyId = _credential.getAccessKeyId();
+        String accessKeySecret = _credential.getAccessKeySecret();
+        String securityToken = _credential.getSecurityToken();
+        String credentialType = _credential.getType();
+        String openPlatformEndpoint = _openPlatformEndpoint;
+        if (com.aliyun.teautil.Common.empty(openPlatformEndpoint)) {
+            openPlatformEndpoint = "openplatform.aliyuncs.com";
+        }
+
+        if (com.aliyun.teautil.Common.isUnset(credentialType)) {
+            credentialType = "access_key";
+        }
+
+        com.aliyun.teaopenapi.models.Config authConfig = com.aliyun.teaopenapi.models.Config.build(TeaConverter.buildMap(
+            new TeaPair("accessKeyId", accessKeyId),
+            new TeaPair("accessKeySecret", accessKeySecret),
+            new TeaPair("securityToken", securityToken),
+            new TeaPair("type", credentialType),
+            new TeaPair("endpoint", openPlatformEndpoint),
+            new TeaPair("protocol", _protocol),
+            new TeaPair("regionId", _regionId)
+        ));
+        com.aliyun.openplatform20191219.Client authClient = new com.aliyun.openplatform20191219.Client(authConfig);
+        com.aliyun.openplatform20191219.models.AuthorizeFileUploadRequest authRequest = com.aliyun.openplatform20191219.models.AuthorizeFileUploadRequest.build(TeaConverter.buildMap(
+            new TeaPair("product", "energyExpertExternal"),
+            new TeaPair("regionId", _regionId)
+        ));
+        com.aliyun.openplatform20191219.models.AuthorizeFileUploadResponse authResponse = new com.aliyun.openplatform20191219.models.AuthorizeFileUploadResponse();
+        com.aliyun.oss.models.Config ossConfig = com.aliyun.oss.models.Config.build(TeaConverter.buildMap(
+            new TeaPair("accessKeyId", accessKeyId),
+            new TeaPair("accessKeySecret", accessKeySecret),
+            new TeaPair("type", "access_key"),
+            new TeaPair("protocol", _protocol),
+            new TeaPair("regionId", _regionId)
+        ));
+        com.aliyun.oss.Client ossClient = new com.aliyun.oss.Client(ossConfig);
+        com.aliyun.fileform.models.FileField fileObj = new com.aliyun.fileform.models.FileField();
+        com.aliyun.oss.models.PostObjectRequest.PostObjectRequestHeader ossHeader = new com.aliyun.oss.models.PostObjectRequest.PostObjectRequestHeader();
+        com.aliyun.oss.models.PostObjectRequest uploadRequest = new com.aliyun.oss.models.PostObjectRequest();
+        com.aliyun.ossutil.models.RuntimeOptions ossRuntime = new com.aliyun.ossutil.models.RuntimeOptions();
+        com.aliyun.openapiutil.Client.convert(runtime, ossRuntime);
+        AnalyzeVlRealtimeRequest analyzeVlRealtimeReq = new AnalyzeVlRealtimeRequest();
+        com.aliyun.openapiutil.Client.convert(request, analyzeVlRealtimeReq);
+        if (!com.aliyun.teautil.Common.isUnset(request.fileUrlObject)) {
+            authResponse = authClient.authorizeFileUploadWithOptions(authRequest, runtime);
+            ossConfig.accessKeyId = authResponse.body.accessKeyId;
+            ossConfig.endpoint = com.aliyun.openapiutil.Client.getEndpoint(authResponse.body.endpoint, authResponse.body.useAccelerate, _endpointType);
+            ossClient = new com.aliyun.oss.Client(ossConfig);
+            fileObj = com.aliyun.fileform.models.FileField.build(TeaConverter.buildMap(
+                new TeaPair("filename", authResponse.body.objectKey),
+                new TeaPair("content", request.fileUrlObject),
+                new TeaPair("contentType", "")
+            ));
+            ossHeader = com.aliyun.oss.models.PostObjectRequest.PostObjectRequestHeader.build(TeaConverter.buildMap(
+                new TeaPair("accessKeyId", authResponse.body.accessKeyId),
+                new TeaPair("policy", authResponse.body.encodedPolicy),
+                new TeaPair("signature", authResponse.body.signature),
+                new TeaPair("key", authResponse.body.objectKey),
+                new TeaPair("file", fileObj),
+                new TeaPair("successActionStatus", "201")
+            ));
+            uploadRequest = com.aliyun.oss.models.PostObjectRequest.build(TeaConverter.buildMap(
+                new TeaPair("bucketName", authResponse.body.bucket),
+                new TeaPair("header", ossHeader)
+            ));
+            ossClient.postObject(uploadRequest, ossRuntime);
+            analyzeVlRealtimeReq.fileUrl = "http://" + authResponse.body.bucket + "." + authResponse.body.endpoint + "/" + authResponse.body.objectKey + "";
+        }
+
+        AnalyzeVlRealtimeResponse analyzeVlRealtimeResp = this.analyzeVlRealtimeWithOptions(analyzeVlRealtimeReq, headers, runtime);
+        return analyzeVlRealtimeResp;
+    }
+
+    /**
      * <b>summary</b> : 
      * <p>策略执行状态反馈</p>
      * 
---
+++ energyexpertexternal-20220923/src/main/java/com/aliyun/energyexpertexternal20220923/models/AnalyzeVlRealtimeResponseBody.java
@@ -0,0 +1,213 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.energyexpertexternal20220923.models;
+
+import com.aliyun.tea.*;
+
+public class AnalyzeVlRealtimeResponseBody extends TeaModel {
+    /**
+     * <p>Return result.</p>
+     */
+    @NameInMap("data")
+    public AnalyzeVlRealtimeResponseBodyData data;
+
+    /**
+     * <p>Request ID.</p>
+     * 
+     * <strong>example:</strong>
+     * <p>83A5A7DD-8974-5769-952E-590A97BEA34E</p>
+     */
+    @NameInMap("requestId")
+    public String requestId;
+
+    public static AnalyzeVlRealtimeResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        AnalyzeVlRealtimeResponseBody self = new AnalyzeVlRealtimeResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public AnalyzeVlRealtimeResponseBody setData(AnalyzeVlRealtimeResponseBodyData data) {
+        this.data = data;
+        return this;
+    }
+    public AnalyzeVlRealtimeResponseBodyData getData() {
+        return this.data;
+    }
+
+    public AnalyzeVlRealtimeResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public static class AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence extends TeaModel {
+        /**
+         * <p>Key confidence</p>
+         * 
+         * <strong>example:</strong>
+         * <p>0.9994202852249146</p>
+         */
+        @NameInMap("keyConfidence")
+        public Double keyConfidence;
+
+        /**
+         * <p>Value confidence</p>
+         * 
+         * <strong>example:</strong>
+         * <p>0.9794202852249146</p>
+         */
+        @NameInMap("valueConfidence")
+        public Double valueConfidence;
+
+        public static AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence build(java.util.Map<String, ?> map) throws Exception {
+            AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence self = new AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence();
+            return TeaModel.build(map, self);
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence setKeyConfidence(Double keyConfidence) {
+            this.keyConfidence = keyConfidence;
+            return this;
+        }
+        public Double getKeyConfidence() {
+            return this.keyConfidence;
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence setValueConfidence(Double valueConfidence) {
+            this.valueConfidence = valueConfidence;
+            return this;
+        }
+        public Double getValueConfidence() {
+            return this.valueConfidence;
+        }
+
+    }
+
+    public static class AnalyzeVlRealtimeResponseBodyDataKvListInfoContext extends TeaModel {
+        /**
+         * <p>Confidence</p>
+         */
+        @NameInMap("confidence")
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence confidence;
+
+        /**
+         * <p>Key recall information details</p>
+         */
+        @NameInMap("key")
+        public java.util.List<ContentItem> key;
+
+        /**
+         * <p>Value recall information details</p>
+         */
+        @NameInMap("value")
+        public java.util.List<ContentItem> value;
+
+        public static AnalyzeVlRealtimeResponseBodyDataKvListInfoContext build(java.util.Map<String, ?> map) throws Exception {
+            AnalyzeVlRealtimeResponseBodyDataKvListInfoContext self = new AnalyzeVlRealtimeResponseBodyDataKvListInfoContext();
+            return TeaModel.build(map, self);
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContext setConfidence(AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence confidence) {
+            this.confidence = confidence;
+            return this;
+        }
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContextConfidence getConfidence() {
+            return this.confidence;
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContext setKey(java.util.List<ContentItem> key) {
+            this.key = key;
+            return this;
+        }
+        public java.util.List<ContentItem> getKey() {
+            return this.key;
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContext setValue(java.util.List<ContentItem> value) {
+            this.value = value;
+            return this;
+        }
+        public java.util.List<ContentItem> getValue() {
+            return this.value;
+        }
+
+    }
+
+    public static class AnalyzeVlRealtimeResponseBodyDataKvListInfo extends TeaModel {
+        /**
+         * <p>Recall content</p>
+         */
+        @NameInMap("context")
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContext context;
+
+        /**
+         * <p>Field Key name</p>
+         * 
+         * <strong>example:</strong>
+         * <p>username</p>
+         */
+        @NameInMap("keyName")
+        public String keyName;
+
+        /**
+         * <p>Field key value</p>
+         * 
+         * <strong>example:</strong>
+         * <p>bob</p>
+         */
+        @NameInMap("keyValue")
+        public String keyValue;
+
+        public static AnalyzeVlRealtimeResponseBodyDataKvListInfo build(java.util.Map<String, ?> map) throws Exception {
+            AnalyzeVlRealtimeResponseBodyDataKvListInfo self = new AnalyzeVlRealtimeResponseBodyDataKvListInfo();
+            return TeaModel.build(map, self);
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfo setContext(AnalyzeVlRealtimeResponseBodyDataKvListInfoContext context) {
+            this.context = context;
+            return this;
+        }
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfoContext getContext() {
+            return this.context;
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfo setKeyName(String keyName) {
+            this.keyName = keyName;
+            return this;
+        }
+        public String getKeyName() {
+            return this.keyName;
+        }
+
+        public AnalyzeVlRealtimeResponseBodyDataKvListInfo setKeyValue(String keyValue) {
+            this.keyValue = keyValue;
+            return this;
+        }
+        public String getKeyValue() {
+            return this.keyValue;
+        }
+
+    }
+
+    public static class AnalyzeVlRealtimeResponseBodyData extends TeaModel {
+        /**
+         * <p>Document parsing result details</p>
+         */
+        @NameInMap("kvListInfo")
+        public java.util.List<AnalyzeVlRealtimeResponseBodyDataKvListInfo> kvListInfo;
+
+        public static AnalyzeVlRealtimeResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
+            AnalyzeVlRealtimeResponseBodyData self = new AnalyzeVlRealtimeResponseBodyData();
+            return TeaModel.build(map, self);
+        }
+
+        public AnalyzeVlRealtimeResponseBodyData setKvListInfo(java.util.List<AnalyzeVlRealtimeResponseBodyDataKvListInfo> kvListInfo) {
+            this.kvListInfo = kvListInfo;
+            return this;
+        }
+        public java.util.List<AnalyzeVlRealtimeResponseBodyDataKvListInfo> getKvListInfo() {
+            return this.kvListInfo;
+        }
+
+    }
+
+}
