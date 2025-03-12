c92d860d2beecc1a0fb36732442ab01176659358
sdk-team <sdk-team@alibabacloud.com>
2025-03-12 02:40:00 +0000

    Update API DeleteTranscriptionPhrases: update response param.

--- tingwu-20230930/src/main/java/com/aliyun/tingwu20230930/models/DeleteTranscriptionPhrasesResponseBody.java
+++ tingwu-20230930/src/main/java/com/aliyun/tingwu20230930/models/DeleteTranscriptionPhrasesResponseBody.java
@@ -4,6 +4,9 @@ package com.aliyun.tingwu20230930.models;
 import com.aliyun.tea.*;
 
 public class DeleteTranscriptionPhrasesResponseBody extends TeaModel {
+    @NameInMap("Data")
+    public DeleteTranscriptionPhrasesResponseBodyData data;
+
     /**
      * <strong>example:</strong>
      * <p>0</p>
@@ -30,6 +33,14 @@ public class DeleteTranscriptionPhrasesResponseBody extends TeaModel {
         return TeaModel.build(map, self);
     }
 
+    public DeleteTranscriptionPhrasesResponseBody setData(DeleteTranscriptionPhrasesResponseBodyData data) {
+        this.data = data;
+        return this;
+    }
+    public DeleteTranscriptionPhrasesResponseBodyData getData() {
+        return this.data;
+    }
+
     public DeleteTranscriptionPhrasesResponseBody setErrorCode(String errorCode) {
         this.errorCode = errorCode;
         return this;
@@ -52,6 +63,47 @@ public class DeleteTranscriptionPhrasesResponseBody extends TeaModel {
     }
     public String getStatus() {
         return this.status;
+    }
+
+    public static class DeleteTranscriptionPhrasesResponseBodyData extends TeaModel {
+        @NameInMap("ErrorCode")
+        public String errorCode;
+
+        @NameInMap("ErrorMessage")
+        public String errorMessage;
+
+        @NameInMap("Status")
+        public String status;
+
+        public static DeleteTranscriptionPhrasesResponseBodyData build(java.util.Map<String, ?> map) throws Exception {
+            DeleteTranscriptionPhrasesResponseBodyData self = new DeleteTranscriptionPhrasesResponseBodyData();
+            return TeaModel.build(map, self);
+        }
+
+        public DeleteTranscriptionPhrasesResponseBodyData setErrorCode(String errorCode) {
+            this.errorCode = errorCode;
+            return this;
+        }
+        public String getErrorCode() {
+            return this.errorCode;
+        }
+
+        public DeleteTranscriptionPhrasesResponseBodyData setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+            return this;
+        }
+        public String getErrorMessage() {
+            return this.errorMessage;
+        }
+
+        public DeleteTranscriptionPhrasesResponseBodyData setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+        public String getStatus() {
+            return this.status;
+        }
+
     }
 
 }
