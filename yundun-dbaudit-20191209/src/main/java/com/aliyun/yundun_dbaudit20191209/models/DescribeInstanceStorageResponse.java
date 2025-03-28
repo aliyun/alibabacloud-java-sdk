f8cb7eca1ddfaec3af2688d1b682aa61f050d02a
sdk-team <sdk-team@alibabacloud.com>
2025-03-28 17:16:08 +0000

    Generated java 2019-12-09 for Yundun-dbaudit.

--- yundun-dbaudit-20191209/src/main/java/com/aliyun/yundun_dbaudit20191209/models/DescribeInstanceStorageResponse.java
+++ yundun-dbaudit-20191209/src/main/java/com/aliyun/yundun_dbaudit20191209/models/DescribeInstanceStorageResponse.java
@@ -5,11 +5,12 @@ import com.aliyun.tea.*;
 
 public class DescribeInstanceStorageResponse extends TeaModel {
     @NameInMap("headers")
-    @Validation(required = true)
     public java.util.Map<String, String> headers;
 
+    @NameInMap("statusCode")
+    public Integer statusCode;
+
     @NameInMap("body")
-    @Validation(required = true)
     public DescribeInstanceStorageResponseBody body;
 
     public static DescribeInstanceStorageResponse build(java.util.Map<String, ?> map) throws Exception {
@@ -23,6 +24,14 @@ public class DescribeInstanceStorageResponse extends TeaModel {
     }
     public java.util.Map<String, String> getHeaders() {
         return this.headers;
+    }
+
+    public DescribeInstanceStorageResponse setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+        return this;
+    }
+    public Integer getStatusCode() {
+        return this.statusCode;
     }
 
     public DescribeInstanceStorageResponse setBody(DescribeInstanceStorageResponseBody body) {
--- yundun-dbaudit-20191209/src/main/java/com/aliyun/yundun_dbaudit20191209/models/DescribeInstancesResponseBody.java
+++ yundun-dbaudit-20191209/src/main/java/com/aliyun/yundun_dbaudit20191209/models/DescribeInstancesResponseBody.java
@@ -4,34 +4,26 @@ package com.aliyun.yundun_dbaudit20191209.models;
 import com.aliyun.tea.*;
 
 public class DescribeInstancesResponseBody extends TeaModel {
-    @NameInMap("TotalCount")
-    public Long totalCount;
+    @NameInMap("Instances")
+    public java.util.List<DescribeInstancesResponseBodyInstances> instances;
 
+    /**
+     * <strong>example:</strong>
+     * <p>12AA0353-F01D-43E2-A85C-9040F7A35D93</p>
+     */
     @NameInMap("RequestId")
     public String requestId;
 
-    @NameInMap("Instances")
-    public java.util.List<DescribeInstancesResponseBodyInstances> instances;
+    /**
+     * <strong>example:</strong>
+     * <p>18</p>
+     */
+    @NameInMap("TotalCount")
+    public Long totalCount;
 
     public static DescribeInstancesResponseBody build(java.util.Map<String, ?> map) throws Exception {
         DescribeInstancesResponseBody self = new DescribeInstancesResponseBody();
         return TeaModel.build(map, self);
-    }
-
-    public DescribeInstancesResponseBody setTotalCount(Long totalCount) {
-        this.totalCount = totalCount;
-        return this;
-    }
-    public Long getTotalCount() {
-        return this.totalCount;
-    }
-
-    public DescribeInstancesResponseBody setRequestId(String requestId) {
-        this.requestId = requestId;
-        return this;
-    }
-    public String getRequestId() {
-        return this.requestId;
     }
 
     public DescribeInstancesResponseBody setInstances(java.util.List<DescribeInstancesResponseBodyInstances> instances) {
@@ -42,65 +34,121 @@ public class DescribeInstancesResponseBody extends TeaModel {
         return this.instances;
     }
 
+    public DescribeInstancesResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public DescribeInstancesResponseBody setTotalCount(Long totalCount) {
+        this.totalCount = totalCount;
+        return this;
+    }
+    public Long getTotalCount() {
+        return this.totalCount;
+    }
+
     public static class DescribeInstancesResponseBodyInstances extends TeaModel {
-        @NameInMap("VpcId")
-        public String vpcId;
-
-        @NameInMap("VswitchId")
-        public String vswitchId;
-
-        @NameInMap("ExpireTime")
-        public Long expireTime;
-
-        @NameInMap("InstanceId")
-        public String instanceId;
-
-        @NameInMap("InternetEndpoint")
-        public String internetEndpoint;
-
-        @NameInMap("RegionId")
-        public String regionId;
-
-        @NameInMap("IntranetEndpoint")
-        public String intranetEndpoint;
-
-        @NameInMap("StartTime")
-        public Long startTime;
-
-        @NameInMap("SeriesCode")
-        public String seriesCode;
-
         @NameInMap("Description")
         public String description;
 
+        /**
+         * <strong>example:</strong>
+         * <p>1578067200000</p>
+         */
+        @NameInMap("ExpireTime")
+        public Long expireTime;
+
+        /**
+         * <strong>example:</strong>
+         * <p>dbaudit-cn-78v1gc****</p>
+         */
+        @NameInMap("InstanceId")
+        public String instanceId;
+
+        /**
+         * <strong>example:</strong>
+         * <p>RUNNING</p>
+         */
         @NameInMap("InstanceStatus")
         public String instanceStatus;
 
+        /**
+         * <strong>example:</strong>
+         * <p>tsiqvqjjlq-public.dbaudit.aliyuncs.com</p>
+         */
+        @NameInMap("InternetEndpoint")
+        public String internetEndpoint;
+
+        /**
+         * <strong>example:</strong>
+         * <p>tsiqvqjjlq.dbaudit.aliyuncs.com</p>
+         */
+        @NameInMap("IntranetEndpoint")
+        public String intranetEndpoint;
+
+        /**
+         * <strong>example:</strong>
+         * <p>alpha.professional</p>
+         */
         @NameInMap("LicenseCode")
         public String licenseCode;
 
+        /**
+         * <strong>example:</strong>
+         * <p>true</p>
+         */
         @NameInMap("PublicNetworkAccess")
         public Boolean publicNetworkAccess;
+
+        /**
+         * <strong>example:</strong>
+         * <p>cn-hangzhou</p>
+         */
+        @NameInMap("RegionId")
+        public String regionId;
+
+        /**
+         * <strong>example:</strong>
+         * <p>alpha</p>
+         */
+        @NameInMap("SeriesCode")
+        public String seriesCode;
+
+        /**
+         * <strong>example:</strong>
+         * <p>1577437765000</p>
+         */
+        @NameInMap("StartTime")
+        public Long startTime;
+
+        /**
+         * <strong>example:</strong>
+         * <p>vpc-bp1c85tzgqu1bf5b****</p>
+         */
+        @NameInMap("VpcId")
+        public String vpcId;
+
+        /**
+         * <strong>example:</strong>
+         * <p>vsw-bp1kep1f0k5fnyfs****</p>
+         */
+        @NameInMap("VswitchId")
+        public String vswitchId;
 
         public static DescribeInstancesResponseBodyInstances build(java.util.Map<String, ?> map) throws Exception {
             DescribeInstancesResponseBodyInstances self = new DescribeInstancesResponseBodyInstances();
             return TeaModel.build(map, self);
         }
 
-        public DescribeInstancesResponseBodyInstances setVpcId(String vpcId) {
-            this.vpcId = vpcId;
+        public DescribeInstancesResponseBodyInstances setDescription(String description) {
+            this.description = description;
             return this;
         }
-        public String getVpcId() {
-            return this.vpcId;
-        }
-
-        public DescribeInstancesResponseBodyInstances setVswitchId(String vswitchId) {
-            this.vswitchId = vswitchId;
-            return this;
-        }
-        public String getVswitchId() {
-            return this.vswitchId;
+        public String getDescription() {
+            return this.description;
         }
 
         public DescribeInstancesResponseBodyInstances setExpireTime(Long expireTime) {
@@ -119,6 +167,14 @@ public class DescribeInstancesResponseBody extends TeaModel {
             return this.instanceId;
         }
 
+        public DescribeInstancesResponseBodyInstances setInstanceStatus(String instanceStatus) {
+            this.instanceStatus = instanceStatus;
+            return this;
+        }
+        public String getInstanceStatus() {
+            return this.instanceStatus;
+        }
+
         public DescribeInstancesResponseBodyInstances setInternetEndpoint(String internetEndpoint) {
             this.internetEndpoint = internetEndpoint;
             return this;
@@ -127,52 +183,12 @@ public class DescribeInstancesResponseBody extends TeaModel {
             return this.internetEndpoint;
         }
 
-        public DescribeInstancesResponseBodyInstances setRegionId(String regionId) {
-            this.regionId = regionId;
-            return this;
-        }
-        public String getRegionId() {
-            return this.regionId;
-        }
-
         public DescribeInstancesResponseBodyInstances setIntranetEndpoint(String intranetEndpoint) {
             this.intranetEndpoint = intranetEndpoint;
             return this;
         }
         public String getIntranetEndpoint() {
             return this.intranetEndpoint;
-        }
-
-        public DescribeInstancesResponseBodyInstances setStartTime(Long startTime) {
-            this.startTime = startTime;
-            return this;
-        }
-        public Long getStartTime() {
-            return this.startTime;
-        }
-
-        public DescribeInstancesResponseBodyInstances setSeriesCode(String seriesCode) {
-            this.seriesCode = seriesCode;
-            return this;
-        }
-        public String getSeriesCode() {
-            return this.seriesCode;
-        }
-
-        public DescribeInstancesResponseBodyInstances setDescription(String description) {
-            this.description = description;
-            return this;
-        }
-        public String getDescription() {
-            return this.description;
-        }
-
-        public DescribeInstancesResponseBodyInstances setInstanceStatus(String instanceStatus) {
-            this.instanceStatus = instanceStatus;
-            return this;
-        }
-        public String getInstanceStatus() {
-            return this.instanceStatus;
         }
 
         public DescribeInstancesResponseBodyInstances setLicenseCode(String licenseCode) {
@@ -189,6 +205,46 @@ public class DescribeInstancesResponseBody extends TeaModel {
         }
         public Boolean getPublicNetworkAccess() {
             return this.publicNetworkAccess;
+        }
+
+        public DescribeInstancesResponseBodyInstances setRegionId(String regionId) {
+            this.regionId = regionId;
+            return this;
+        }
+        public String getRegionId() {
+            return this.regionId;
+        }
+
+        public DescribeInstancesResponseBodyInstances setSeriesCode(String seriesCode) {
+            this.seriesCode = seriesCode;
+            return this;
+        }
+        public String getSeriesCode() {
+            return this.seriesCode;
+        }
+
+        public DescribeInstancesResponseBodyInstances setStartTime(Long startTime) {
+            this.startTime = startTime;
+            return this;
+        }
+        public Long getStartTime() {
+            return this.startTime;
+        }
+
+        public DescribeInstancesResponseBodyInstances setVpcId(String vpcId) {
+            this.vpcId = vpcId;
+            return this;
+        }
+        public String getVpcId() {
+            return this.vpcId;
+        }
+
+        public DescribeInstancesResponseBodyInstances setVswitchId(String vswitchId) {
+            this.vswitchId = vswitchId;
+            return this;
+        }
+        public String getVswitchId() {
+            return this.vswitchId;
         }
 
     }
