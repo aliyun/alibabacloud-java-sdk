5d9586fad563d26429efd226fcab93b782ca7ca5
sdk-team <sdk-team@alibabacloud.com>
2025-04-07 08:10:33 +0000

    Generated java 2024-07-12 for IQS.

--- iqs-20240712/src/main/java/com/aliyun/iqs20240712/models/CommonAgentQuery.java
+++ iqs-20240712/src/main/java/com/aliyun/iqs20240712/models/CommonAgentQuery.java
@@ -4,15 +4,29 @@ package com.aliyun.iqs20240712.models;
 import com.aliyun.tea.*;
 
 public class CommonAgentQuery extends TeaModel {
+    @NameInMap("limit")
+    public Integer limit;
+
     @NameInMap("query")
     public String query;
 
     @NameInMap("querySceneEnumCode")
     public String querySceneEnumCode;
 
+    @NameInMap("searchModel")
+    public String searchModel;
+
     public static CommonAgentQuery build(java.util.Map<String, ?> map) throws Exception {
         CommonAgentQuery self = new CommonAgentQuery();
         return TeaModel.build(map, self);
+    }
+
+    public CommonAgentQuery setLimit(Integer limit) {
+        this.limit = limit;
+        return this;
+    }
+    public Integer getLimit() {
+        return this.limit;
     }
 
     public CommonAgentQuery setQuery(String query) {
@@ -29,6 +43,14 @@ public class CommonAgentQuery extends TeaModel {
     }
     public String getQuerySceneEnumCode() {
         return this.querySceneEnumCode;
+    }
+
+    public CommonAgentQuery setSearchModel(String searchModel) {
+        this.searchModel = searchModel;
+        return this;
+    }
+    public String getSearchModel() {
+        return this.searchModel;
     }
 
 }
