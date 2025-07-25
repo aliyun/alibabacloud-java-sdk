59d095ccf8e6f2beb550bfcba3be6e0d78a602c0
sdk-team <sdk-team@alibabacloud.com>
2025-07-25 06:39:03 +0000

    Support API CreateTicket.

---
+++ cms-20240330/src/main/java/com/aliyun/cms20240330/models/CreateTicketRequest.java
@@ -0,0 +1,42 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.cms20240330.models;
+
+import com.aliyun.tea.*;
+
+public class CreateTicketRequest extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p>600</p>
+     */
+    @NameInMap("accessTokenExpirationTime")
+    public Long accessTokenExpirationTime;
+
+    /**
+     * <strong>example:</strong>
+     * <p>86400</p>
+     */
+    @NameInMap("expirationTime")
+    public Long expirationTime;
+
+    public static CreateTicketRequest build(java.util.Map<String, ?> map) throws Exception {
+        CreateTicketRequest self = new CreateTicketRequest();
+        return TeaModel.build(map, self);
+    }
+
+    public CreateTicketRequest setAccessTokenExpirationTime(Long accessTokenExpirationTime) {
+        this.accessTokenExpirationTime = accessTokenExpirationTime;
+        return this;
+    }
+    public Long getAccessTokenExpirationTime() {
+        return this.accessTokenExpirationTime;
+    }
+
+    public CreateTicketRequest setExpirationTime(Long expirationTime) {
+        this.expirationTime = expirationTime;
+        return this;
+    }
+    public Long getExpirationTime() {
+        return this.expirationTime;
+    }
+
+}
