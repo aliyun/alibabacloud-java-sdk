eec9cabba0bb29032b18253827ecf7259b5957db
sdk-team <sdk-team@alibabacloud.com>
2025-12-05 17:32:34 +0000

    Update API ListAgentlessRegion: add response parameters Body.VendorRegionList.

--- sas-20181203/src/main/java/com/aliyun/sas20181203/models/ListAgentlessRegionResponseBody.java
+++ sas-20181203/src/main/java/com/aliyun/sas20181203/models/ListAgentlessRegionResponseBody.java
@@ -19,6 +19,9 @@ public class ListAgentlessRegionResponseBody extends TeaModel {
     @NameInMap("RequestId")
     public String requestId;
 
+    @NameInMap("VendorRegionList")
+    public java.util.List<ListAgentlessRegionResponseBodyVendorRegionList> vendorRegionList;
+
     public static ListAgentlessRegionResponseBody build(java.util.Map<String, ?> map) throws Exception {
         ListAgentlessRegionResponseBody self = new ListAgentlessRegionResponseBody();
         return TeaModel.build(map, self);
@@ -38,6 +41,44 @@ public class ListAgentlessRegionResponseBody extends TeaModel {
     }
     public String getRequestId() {
         return this.requestId;
+    }
+
+    public ListAgentlessRegionResponseBody setVendorRegionList(java.util.List<ListAgentlessRegionResponseBodyVendorRegionList> vendorRegionList) {
+        this.vendorRegionList = vendorRegionList;
+        return this;
+    }
+    public java.util.List<ListAgentlessRegionResponseBodyVendorRegionList> getVendorRegionList() {
+        return this.vendorRegionList;
+    }
+
+    public static class ListAgentlessRegionResponseBodyVendorRegionList extends TeaModel {
+        @NameInMap("RegionId")
+        public String regionId;
+
+        @NameInMap("Vendor")
+        public Integer vendor;
+
+        public static ListAgentlessRegionResponseBodyVendorRegionList build(java.util.Map<String, ?> map) throws Exception {
+            ListAgentlessRegionResponseBodyVendorRegionList self = new ListAgentlessRegionResponseBodyVendorRegionList();
+            return TeaModel.build(map, self);
+        }
+
+        public ListAgentlessRegionResponseBodyVendorRegionList setRegionId(String regionId) {
+            this.regionId = regionId;
+            return this;
+        }
+        public String getRegionId() {
+            return this.regionId;
+        }
+
+        public ListAgentlessRegionResponseBodyVendorRegionList setVendor(Integer vendor) {
+            this.vendor = vendor;
+            return this;
+        }
+        public Integer getVendor() {
+            return this.vendor;
+        }
+
     }
 
 }
