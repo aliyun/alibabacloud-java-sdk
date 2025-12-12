a32ca99e1a0a5edeb7ac43c8ca02b644e1a12905
sdk-team <sdk-team@alibabacloud.com>
2025-12-12 08:43:58 +0000

    Generated java 2019-01-15 for Qualitycheck.

--- qualitycheck-20190115/src/main/java/com/aliyun/qualitycheck20190115/models/JudgeNodeMetaDesc.java
+++ qualitycheck-20190115/src/main/java/com/aliyun/qualitycheck20190115/models/JudgeNodeMetaDesc.java
@@ -10,6 +10,9 @@ public class JudgeNodeMetaDesc extends TeaModel {
     @NameInMap("DataType")
     public Integer dataType;
 
+    @NameInMap("ExpressionMetaDesc")
+    public JudgeNodeMetaDescExpressionMetaDesc expressionMetaDesc;
+
     @NameInMap("Field")
     public String field;
 
@@ -43,6 +46,14 @@ public class JudgeNodeMetaDesc extends TeaModel {
         return this.dataType;
     }
 
+    public JudgeNodeMetaDesc setExpressionMetaDesc(JudgeNodeMetaDescExpressionMetaDesc expressionMetaDesc) {
+        this.expressionMetaDesc = expressionMetaDesc;
+        return this;
+    }
+    public JudgeNodeMetaDescExpressionMetaDesc getExpressionMetaDesc() {
+        return this.expressionMetaDesc;
+    }
+
     public JudgeNodeMetaDesc setField(String field) {
         this.field = field;
         return this;
@@ -73,6 +84,80 @@ public class JudgeNodeMetaDesc extends TeaModel {
     }
     public String getValue() {
         return this.value;
+    }
+
+    public static class JudgeNodeMetaDescExpressionMetaDesc extends TeaModel {
+        @NameInMap("LeftFieldType")
+        public Integer leftFieldType;
+
+        @NameInMap("LeftOperand")
+        public String leftOperand;
+
+        @NameInMap("Operator")
+        public String operator;
+
+        @NameInMap("RightFieldType")
+        public Integer rightFieldType;
+
+        @NameInMap("RightOperand")
+        public String rightOperand;
+
+        @NameInMap("RoundingMode")
+        public String roundingMode;
+
+        public static JudgeNodeMetaDescExpressionMetaDesc build(java.util.Map<String, ?> map) throws Exception {
+            JudgeNodeMetaDescExpressionMetaDesc self = new JudgeNodeMetaDescExpressionMetaDesc();
+            return TeaModel.build(map, self);
+        }
+
+        public JudgeNodeMetaDescExpressionMetaDesc setLeftFieldType(Integer leftFieldType) {
+            this.leftFieldType = leftFieldType;
+            return this;
+        }
+        public Integer getLeftFieldType() {
+            return this.leftFieldType;
+        }
+
+        public JudgeNodeMetaDescExpressionMetaDesc setLeftOperand(String leftOperand) {
+            this.leftOperand = leftOperand;
+            return this;
+        }
+        public String getLeftOperand() {
+            return this.leftOperand;
+        }
+
+        public JudgeNodeMetaDescExpressionMetaDesc setOperator(String operator) {
+            this.operator = operator;
+            return this;
+        }
+        public String getOperator() {
+            return this.operator;
+        }
+
+        public JudgeNodeMetaDescExpressionMetaDesc setRightFieldType(Integer rightFieldType) {
+            this.rightFieldType = rightFieldType;
+            return this;
+        }
+        public Integer getRightFieldType() {
+            return this.rightFieldType;
+        }
+
+        public JudgeNodeMetaDescExpressionMetaDesc setRightOperand(String rightOperand) {
+            this.rightOperand = rightOperand;
+            return this;
+        }
+        public String getRightOperand() {
+            return this.rightOperand;
+        }
+
+        public JudgeNodeMetaDescExpressionMetaDesc setRoundingMode(String roundingMode) {
+            this.roundingMode = roundingMode;
+            return this;
+        }
+        public String getRoundingMode() {
+            return this.roundingMode;
+        }
+
     }
 
 }
