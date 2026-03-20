94c16eaf58526fb44f86afcfd3c590ba2963de58
sdk-team <sdk-team@alibabacloud.com>
2026-03-20 17:28:34 +0000

    Generated java 2018-05-18 for nls-cloud-meta.

---
+++ nls-cloud-meta-20180518/src/main/java/com/aliyun/nls_cloud_meta20180518/Client.java
@@ -0,0 +1,65 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.nls_cloud_meta20180518;
+
+import com.aliyun.tea.*;
+import com.aliyun.nls_cloud_meta20180518.models.*;
+
+public class Client extends com.aliyun.teaopenapi.Client {
+
+    public Client(com.aliyun.teaopenapi.models.Config config) throws Exception {
+        super(config);
+        this._endpointRule = "";
+        this.checkConfig(config);
+        this._endpoint = this.getEndpoint("nls-cloud-meta", _regionId, _endpointRule, _network, _suffix, _endpointMap, _endpoint);
+    }
+
+
+    public String getEndpoint(String productId, String regionId, String endpointRule, String network, String suffix, java.util.Map<String, String> endpointMap, String endpoint) throws Exception {
+        if (!com.aliyun.teautil.Common.empty(endpoint)) {
+            return endpoint;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(endpointMap) && !com.aliyun.teautil.Common.empty(endpointMap.get(regionId))) {
+            return endpointMap.get(regionId);
+        }
+
+        return com.aliyun.endpointutil.Client.getEndpointRules(productId, regionId, endpointRule, network, suffix);
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>创建Token</p>
+     * 
+     * @param headers map
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return CreateTokenResponse
+     */
+    public CreateTokenResponse createTokenWithOptions(java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("headers", headers)
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "CreateToken"),
+            new TeaPair("version", "2018-05-18"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/pop/2018-05-18/tokens"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "ROA"),
+            new TeaPair("reqBodyType", "json"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new CreateTokenResponse());
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>创建Token</p>
+     * @return CreateTokenResponse
+     */
+    public CreateTokenResponse createToken() throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        return this.createTokenWithOptions(headers, runtime);
+    }
+}
