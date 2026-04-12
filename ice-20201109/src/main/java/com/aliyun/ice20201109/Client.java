942cb6687e636e69199c2488ef0159f11d0783e2
sdk-team <sdk-team@alibabacloud.com>
2026-04-12 08:24:45 +0000

    Support API GetYikeUser.

--- ice-20201109/src/main/java/com/aliyun/ice20201109/Client.java
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/Client.java
@@ -10841,6 +10841,50 @@ public class Client extends com.aliyun.teaopenapi.Client {
 
     /**
      * <b>summary</b> : 
+     * <p>获取一刻子用户信息</p>
+     * 
+     * @param request GetYikeUserRequest
+     * @param runtime runtime options for this request RuntimeOptions
+     * @return GetYikeUserResponse
+     */
+    public GetYikeUserResponse getYikeUserWithOptions(GetYikeUserRequest request, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
+        com.aliyun.teautil.Common.validateModel(request);
+        java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.userName)) {
+            query.put("UserName", request.userName);
+        }
+
+        com.aliyun.teaopenapi.models.OpenApiRequest req = com.aliyun.teaopenapi.models.OpenApiRequest.build(TeaConverter.buildMap(
+            new TeaPair("query", com.aliyun.openapiutil.Client.query(query))
+        ));
+        com.aliyun.teaopenapi.models.Params params = com.aliyun.teaopenapi.models.Params.build(TeaConverter.buildMap(
+            new TeaPair("action", "GetYikeUser"),
+            new TeaPair("version", "2020-11-09"),
+            new TeaPair("protocol", "HTTPS"),
+            new TeaPair("pathname", "/"),
+            new TeaPair("method", "POST"),
+            new TeaPair("authType", "AK"),
+            new TeaPair("style", "RPC"),
+            new TeaPair("reqBodyType", "formData"),
+            new TeaPair("bodyType", "json")
+        ));
+        return TeaModel.toModel(this.callApi(params, req, runtime), new GetYikeUserResponse());
+    }
+
+    /**
+     * <b>summary</b> : 
+     * <p>获取一刻子用户信息</p>
+     * 
+     * @param request GetYikeUserRequest
+     * @return GetYikeUserResponse
+     */
+    public GetYikeUserResponse getYikeUser(GetYikeUserRequest request) throws Exception {
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        return this.getYikeUserWithOptions(request, runtime);
+    }
+
+    /**
+     * <b>summary</b> : 
      * <p>查询一刻用户积分</p>
      * 
      * @param request GetYikeUserCreditRequest
---
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/models/GetYikeUserResponseBody.java
@@ -0,0 +1,106 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ice20201109.models;
+
+import com.aliyun.tea.*;
+
+public class GetYikeUserResponseBody extends TeaModel {
+    /**
+     * <strong>example:</strong>
+     * <p><strong><strong>63E8B7C7-4812-46AD-0FA56029AC86</strong></strong></p>
+     */
+    @NameInMap("RequestId")
+    public String requestId;
+
+    @NameInMap("UserInfo")
+    public GetYikeUserResponseBodyUserInfo userInfo;
+
+    public static GetYikeUserResponseBody build(java.util.Map<String, ?> map) throws Exception {
+        GetYikeUserResponseBody self = new GetYikeUserResponseBody();
+        return TeaModel.build(map, self);
+    }
+
+    public GetYikeUserResponseBody setRequestId(String requestId) {
+        this.requestId = requestId;
+        return this;
+    }
+    public String getRequestId() {
+        return this.requestId;
+    }
+
+    public GetYikeUserResponseBody setUserInfo(GetYikeUserResponseBodyUserInfo userInfo) {
+        this.userInfo = userInfo;
+        return this;
+    }
+    public GetYikeUserResponseBodyUserInfo getUserInfo() {
+        return this.userInfo;
+    }
+
+    public static class GetYikeUserResponseBodyUserInfo extends TeaModel {
+        /**
+         * <strong>example:</strong>
+         * <p>nick</p>
+         */
+        @NameInMap("Nickname")
+        public String nickname;
+
+        /**
+         * <strong>example:</strong>
+         * <p>name</p>
+         */
+        @NameInMap("UserName")
+        public String userName;
+
+        /**
+         * <strong>example:</strong>
+         * <p>spaceId</p>
+         */
+        @NameInMap("WorkspaceId")
+        public String workspaceId;
+
+        /**
+         * <strong>example:</strong>
+         * <p>id</p>
+         */
+        @NameInMap("YikeUserId")
+        public String yikeUserId;
+
+        public static GetYikeUserResponseBodyUserInfo build(java.util.Map<String, ?> map) throws Exception {
+            GetYikeUserResponseBodyUserInfo self = new GetYikeUserResponseBodyUserInfo();
+            return TeaModel.build(map, self);
+        }
+
+        public GetYikeUserResponseBodyUserInfo setNickname(String nickname) {
+            this.nickname = nickname;
+            return this;
+        }
+        public String getNickname() {
+            return this.nickname;
+        }
+
+        public GetYikeUserResponseBodyUserInfo setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+        public String getUserName() {
+            return this.userName;
+        }
+
+        public GetYikeUserResponseBodyUserInfo setWorkspaceId(String workspaceId) {
+            this.workspaceId = workspaceId;
+            return this;
+        }
+        public String getWorkspaceId() {
+            return this.workspaceId;
+        }
+
+        public GetYikeUserResponseBodyUserInfo setYikeUserId(String yikeUserId) {
+            this.yikeUserId = yikeUserId;
+            return this;
+        }
+        public String getYikeUserId() {
+            return this.yikeUserId;
+        }
+
+    }
+
+}
