2b30c8a86cd76aba926974a93f9deb0b76c9b304
sdk-team <sdk-team@alibabacloud.com>
2026-04-13 17:51:28 +0000

    Update API QueryVideoCognitionJob: add response parameters Body.Input.

--- ice-20201109/src/main/java/com/aliyun/ice20201109/models/QueryVideoCognitionJobResponseBody.java
+++ ice-20201109/src/main/java/com/aliyun/ice20201109/models/QueryVideoCognitionJobResponseBody.java
@@ -4,6 +4,9 @@ package com.aliyun.ice20201109.models;
 import com.aliyun.tea.*;
 
 public class QueryVideoCognitionJobResponseBody extends TeaModel {
+    @NameInMap("Input")
+    public QueryVideoCognitionJobResponseBodyInput input;
+
     /**
      * <p>The status of the task. Valid values:</p>
      * <ul>
@@ -19,6 +22,9 @@ public class QueryVideoCognitionJobResponseBody extends TeaModel {
     @NameInMap("JobStatus")
     public String jobStatus;
 
+    @NameInMap("Params")
+    public String params;
+
     /**
      * <p>The request ID.</p>
      * 
@@ -30,6 +36,9 @@ public class QueryVideoCognitionJobResponseBody extends TeaModel {
 
     @NameInMap("Results")
     public QueryVideoCognitionJobResponseBodyResults results;
+
+    @NameInMap("TemplateId")
+    public String templateId;
 
     /**
      * <p>The user-defined data.</p>
@@ -45,12 +54,28 @@ public class QueryVideoCognitionJobResponseBody extends TeaModel {
         return TeaModel.build(map, self);
     }
 
+    public QueryVideoCognitionJobResponseBody setInput(QueryVideoCognitionJobResponseBodyInput input) {
+        this.input = input;
+        return this;
+    }
+    public QueryVideoCognitionJobResponseBodyInput getInput() {
+        return this.input;
+    }
+
     public QueryVideoCognitionJobResponseBody setJobStatus(String jobStatus) {
         this.jobStatus = jobStatus;
         return this;
     }
     public String getJobStatus() {
         return this.jobStatus;
+    }
+
+    public QueryVideoCognitionJobResponseBody setParams(String params) {
+        this.params = params;
+        return this;
+    }
+    public String getParams() {
+        return this.params;
     }
 
     public QueryVideoCognitionJobResponseBody setRequestId(String requestId) {
@@ -69,12 +94,50 @@ public class QueryVideoCognitionJobResponseBody extends TeaModel {
         return this.results;
     }
 
+    public QueryVideoCognitionJobResponseBody setTemplateId(String templateId) {
+        this.templateId = templateId;
+        return this;
+    }
+    public String getTemplateId() {
+        return this.templateId;
+    }
+
     public QueryVideoCognitionJobResponseBody setUserData(String userData) {
         this.userData = userData;
         return this;
     }
     public String getUserData() {
         return this.userData;
+    }
+
+    public static class QueryVideoCognitionJobResponseBodyInput extends TeaModel {
+        @NameInMap("Media")
+        public String media;
+
+        @NameInMap("Type")
+        public String type;
+
+        public static QueryVideoCognitionJobResponseBodyInput build(java.util.Map<String, ?> map) throws Exception {
+            QueryVideoCognitionJobResponseBodyInput self = new QueryVideoCognitionJobResponseBodyInput();
+            return TeaModel.build(map, self);
+        }
+
+        public QueryVideoCognitionJobResponseBodyInput setMedia(String media) {
+            this.media = media;
+            return this;
+        }
+        public String getMedia() {
+            return this.media;
+        }
+
+        public QueryVideoCognitionJobResponseBodyInput setType(String type) {
+            this.type = type;
+            return this;
+        }
+        public String getType() {
+            return this.type;
+        }
+
     }
 
     public static class QueryVideoCognitionJobResponseBodyResultsResult extends TeaModel {
