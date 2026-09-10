1ffb56fd8157996adfda4ab2508c2b000e57722f
sdk-team <sdk-team@alibabacloud.com>
2026-09-10 18:22:37 +0000

    Update API DeleteHttpApi: add request parameters clientToken.

--- apig-20240327/src/main/java/com/aliyun/apig20240327/Client.java
+++ apig-20240327/src/main/java/com/aliyun/apig20240327/Client.java
@@ -2897,6 +2897,10 @@ public class Client extends com.aliyun.teaopenapi.Client {
     public DeleteHttpApiResponse deleteHttpApiWithOptions(String httpApiId, DeleteHttpApiRequest request, java.util.Map<String, String> headers, com.aliyun.teautil.models.RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> query = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(request.clientToken)) {
+            query.put("clientToken", request.clientToken);
+        }
+
         if (!com.aliyun.teautil.Common.isUnset(request.dryRun)) {
             query.put("dryRun", request.dryRun);
         }
--- apig-20240327/src/main/java/com/aliyun/apig20240327/models/DeleteHttpApiRequest.java
+++ apig-20240327/src/main/java/com/aliyun/apig20240327/models/DeleteHttpApiRequest.java
@@ -5,7 +5,16 @@ import com.aliyun.tea.*;
 
 public class DeleteHttpApiRequest extends TeaModel {
     /**
-     * <p>Specifies whether to perform only a dry run. If set to true, all synchronous validations identical to an actual deletion are executed (including admission checks such as whether a published API cannot be deleted), but the API is not deleted, no associated configurations are cleaned up, and no side effects are produced. If this parameter is not specified or is set to false, the behavior is the same as the existing version.</p>
+     * <p>The idempotence token. If you call this operation multiple times for the same HTTP API by using the same clientToken value and the same delete request parameters, the result of the first successful call is returned.</p>
+     * 
+     * <strong>example:</strong>
+     * <p>delete-http-api-client-token-001</p>
+     */
+    @NameInMap("clientToken")
+    public String clientToken;
+
+    /**
+     * <p>Specifies whether to perform only a dry run. If set to true, all synchronous validations identical to an actual deletion are executed (including admission checks such as whether a published API can be deleted), but the API is not deleted, no associated configurations are cleaned up, and no side effects are produced. If this parameter is not specified or is set to false, the behavior is the same as the existing version.</p>
      */
     @NameInMap("dryRun")
     public Boolean dryRun;
@@ -13,6 +22,14 @@ public class DeleteHttpApiRequest extends TeaModel {
     public static DeleteHttpApiRequest build(java.util.Map<String, ?> map) throws Exception {
         DeleteHttpApiRequest self = new DeleteHttpApiRequest();
         return TeaModel.build(map, self);
+    }
+
+    public DeleteHttpApiRequest setClientToken(String clientToken) {
+        this.clientToken = clientToken;
+        return this;
+    }
+    public String getClientToken() {
+        return this.clientToken;
     }
 
     public DeleteHttpApiRequest setDryRun(Boolean dryRun) {
